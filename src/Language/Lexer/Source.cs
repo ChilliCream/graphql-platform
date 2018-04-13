ec5e67d5644b9e369ab9d28b25a8a8b0a7a22985
Michael Staib <michael.staib@hotmail.com>
2018-04-13 23:02:51 +0200

    Added utility functions to the source class

--- src/Language/Lexer/Source.cs
+++ src/Language/Lexer/Source.cs
@@ -10,11 +10,18 @@ namespace HotChocolate.Language
 		: ISource
 		, IEquatable<Source>
 	{
-		public Source(string body)
+		/// <summary>
+		/// Initializes a new instance of the 
+		/// <see cref="T:HotChocolate.Language.Source"/> class.
+		/// </summary>
+		/// <param name="text">
+		/// The GraphQL source text.
+		/// </param>
+		public Source(string text)
 		{
-			// the normalization might be problematic. 
+			// review: the normalization might be problematic. 
 			// the line count should still work
-			Text = body ?? string.Empty;
+			Text = text ?? string.Empty;
 			Text = Text.Replace("\r\n", "\n")
 				.Replace("\n\r", "\n");
 		}
@@ -107,6 +114,80 @@ namespace HotChocolate.Language
 		public override string ToString()
 		{
 			return Text;
+		}
+
+        /// <summary>
+        /// Reads a GraphQL source from a file.
+        /// </summary>
+		/// <param name="filePath">
+		/// The file path.
+		/// </param>
+        /// <returns>
+		/// Returns a <see cref="Source"/> consisting of the file content.
+		/// </returns>
+		public static Source FromFile(string filePath)
+		{
+			if (filePath == null)
+			{
+				throw new ArgumentNullException(nameof(filePath));
+			}
+
+			if (File.Exists(filePath))
+			{
+				throw new FileNotFoundException(
+					"Could not find the specified GraphQL source file.",
+					filePath);
+			}
+
+			return new Source(File.ReadAllText(filePath));
+		}
+
+		/// <summary>
+        /// Reads a GraphQL source from a file.
+        /// </summary>
+		/// <param name="file">
+		/// The GraphQL source file.
+		/// </param>
+		/// <returns>
+        /// Returns a <see cref="Source"/> consisting of the file content.
+        /// </returns>
+		public static Source FromFile(FileInfo file)
+		{
+			if (file == null)
+			{
+				throw new ArgumentNullException(nameof(file));
+			}
+
+			file.Refresh();
+			if (!file.Exists)
+			{
+				throw new FileNotFoundException(
+					"Could not find the specified GraphQL source file.",
+					file.FullName);
+			}
+
+			return new Source(File.ReadAllText(file.FullName));
+		}
+
+		/// <summary>
+        /// Reads a GraphQL source from a read stream.
+        /// </summary>
+		/// <param name="stream">
+		/// A read stream that provides access to a GraphQL source text.
+		/// </param>
+		/// <returns>
+        /// Returns a <see cref="Source"/> consisting of the streams content.
+        /// </returns>
+		public static Source FromStream(Stream stream)
+		{
+			using (MemoryStream memoryStream = new MemoryStream())
+			{
+				stream.CopyTo(memoryStream);
+				memoryStream.Position = 0;
+
+				StreamReader reader = new StreamReader(stream);
+				return new Source(reader.ReadToEnd());
+			}
 		}
 	}
 }
