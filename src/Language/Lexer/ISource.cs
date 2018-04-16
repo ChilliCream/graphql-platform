79834b6ce1480fae3d3c0fca24b6905f31bd3230
Michael Staib <michael.staib@hotmail.com>
2018-04-16 10:38:43 +0200

    Added core project

--- src/Language/Lexer/ISource.cs
+++ src/Language/Lexer/ISource.cs
@@ -12,5 +12,9 @@ namespace HotChocolate.Language
         /// Returns the GraphQL source text.
         /// </returns>
         string Text { get; }
+
+        // TODO : add ColumnOffset and LineOffset 
+        // to support scenarious where we want to offset
+        // the token location.
     }
 }
