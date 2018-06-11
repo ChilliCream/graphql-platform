df662e88261113259cd5d4366d59edbbcaa75bcc
Michael Staib <michael.staib@hotmail.com>
2018-06-11 15:35:49 +0200

    Added sync execute to schema extensions

--- src/Core/SchemaExtensions.cs
+++ src/Core/SchemaExtensions.cs
@@ -16,14 +16,39 @@ namespace HotChocolate
                 CancellationToken.None);
         }
 
-         public static Task<QueryResult> ExecuteAsync(
-             this Schema schema, string query,
-             string operationName = null,
-             CancellationToken cancellationToken = default)
+        public static Task<QueryResult> ExecuteAsync(
+            this Schema schema, string query,
+            string operationName = null,
+            CancellationToken cancellationToken = default)
         {
             return _operationExecuter.ExecuteRequestAsync(schema,
                 Parser.Default.Parse(query), operationName, null, null,
                 CancellationToken.None);
+        }
+
+        public static QueryResult Execute(this Schema schema, string query)
+        {
+            return Task.Factory.StartNew(
+                () => _operationExecuter.ExecuteRequestAsync(schema,
+                    Parser.Default.Parse(query), null, null, null,
+                    CancellationToken.None))
+                .Unwrap()
+                .GetAwaiter()
+                .GetResult();
+        }
+
+        public static QueryResult Execute(
+            this Schema schema, string query,
+            string operationName = null,
+            CancellationToken cancellationToken = default)
+        {
+            return Task.Factory.StartNew(
+                () => _operationExecuter.ExecuteRequestAsync(schema,
+                    Parser.Default.Parse(query), operationName, null, null,
+                    CancellationToken.None))
+                .Unwrap()
+                .GetAwaiter()
+                .GetResult();
         }
     }
 
