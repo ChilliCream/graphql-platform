4ba48b2d7e5073c937fd259d52ec70ee6d3f1b81
Michael Staib <michael.staib@hotmail.com>
2018-06-14 21:08:18 +0200

    Refactoring: fixed better code hub issue

--- src/Language/Lexer/CharExtensions.cs
+++ src/Language/Lexer/CharExtensions.cs
@@ -5,7 +5,7 @@ namespace HotChocolate.Language
     /// that are used to tokenize a GraphQL source text.
     /// These utilities are used by the lexer dfault implementation.
     /// </summary>
-    internal static class CharExtensions
+    internal static partial class CharExtensions
     {
         private static readonly bool[] _isLetterOrUnderscore = new bool[char.MaxValue + 1];
         private static readonly bool[] _isControlCharacter = new bool[char.MaxValue + 1];
@@ -13,78 +13,6 @@ namespace HotChocolate.Language
         private static readonly bool[] _isWhitespace = new bool[char.MaxValue + 1];
         private static readonly bool[] _isPunctuator = new bool[char.MaxValue + 1];
         private static readonly bool[] _isDigitOrMinus = new bool[char.MaxValue + 1];
-
-        #region Initialize Arrays
-
-        static CharExtensions()
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                _isControlCharacter[i] = true;
-            }
-            for (int i = 10; i <= 31; i++)
-            {
-                _isControlCharacter[i] = true;
-            }
-            _isControlCharacter[127] = true;
-
-            _isEscapeCharacter['"'] = true;
-            _isEscapeCharacter['/'] = true;
-            _isEscapeCharacter['\\'] = true;
-            _isEscapeCharacter['\b'] = true;
-            _isEscapeCharacter['\f'] = true;
-            _isEscapeCharacter['\n'] = true;
-            _isEscapeCharacter['\r'] = true;
-            _isEscapeCharacter['\t'] = true;
-
-            _isWhitespace['\t'] = true;
-            _isWhitespace['\r'] = true;
-            _isWhitespace['\n'] = true;
-            _isWhitespace[' '] = true;
-            _isWhitespace[','] = true;
-            _isWhitespace[0xfeff] = true;
-
-            _isPunctuator['!'] = true;
-            _isPunctuator['$'] = true;
-            _isPunctuator['&'] = true;
-            _isPunctuator['('] = true;
-            _isPunctuator[')'] = true;
-            _isPunctuator[':'] = true;
-            _isPunctuator['='] = true;
-            _isPunctuator['@'] = true;
-            _isPunctuator['['] = true;
-            _isPunctuator[']'] = true;
-            _isPunctuator['{'] = true;
-            _isPunctuator['|'] = true;
-            _isPunctuator['}'] = true;
-            _isPunctuator['.'] = true;
-
-            for (char c = 'a'; c <= 'z'; c++)
-            {
-                _isLetterOrUnderscore[c] = true;
-            }
-
-            for (char c = 'A'; c <= 'Z'; c++)
-            {
-                _isLetterOrUnderscore[c] = true;
-            }
-
-            _isLetterOrUnderscore['_'] = true;
-
-            _isDigitOrMinus['-'] = true;
-            _isDigitOrMinus['0'] = true;
-            _isDigitOrMinus['1'] = true;
-            _isDigitOrMinus['2'] = true;
-            _isDigitOrMinus['3'] = true;
-            _isDigitOrMinus['4'] = true;
-            _isDigitOrMinus['5'] = true;
-            _isDigitOrMinus['6'] = true;
-            _isDigitOrMinus['7'] = true;
-            _isDigitOrMinus['8'] = true;
-            _isDigitOrMinus['9'] = true;
-        }
-
-        #endregion
 
         public static bool IsLetterOrDigitOrUnderscore(in this char c)
         {
