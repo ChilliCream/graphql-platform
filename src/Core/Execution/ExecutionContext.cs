0e2a7f6b199ecb0ca5d860d93c800e77dc7ab263
Michael Staib <michael.staib@hotmail.com>
2018-06-15 07:18:16 +0200

    Reworked Execution Context

--- src/Core/Execution/ExecutionContext.cs
+++ src/Core/Execution/ExecutionContext.cs
@@ -15,60 +15,42 @@ namespace HotChocolate.Execution
             OperationDefinitionNode operation, VariableCollection variables,
             IServiceProvider services, object rootValue, object userContext)
         {
-            if (schema == null)
-            {
-                throw new ArgumentNullException(nameof(schema));
-            }
-
-            if (queryDocument == null)
-            {
-                throw new ArgumentNullException(nameof(queryDocument));
-            }
-
-            if (operation == null)
-            {
-                throw new ArgumentNullException(nameof(operation));
-            }
-
-            if (variables == null)
-            {
-                throw new ArgumentNullException(nameof(variables));
-            }
-
-            if (services == null)
-            {
-                throw new ArgumentNullException(nameof(services));
-            }
-
-            FragmentCollection fragments =
-                new FragmentCollection(schema, queryDocument);
-
-            _services = services;
-            _fieldCollector = new FieldCollector(schema, variables, fragments);
-
-            Schema = schema;
-            QueryDocument = queryDocument;
-            Operation = operation;
-            Variables = variables;
+            Schema = schema
+                ?? throw new ArgumentNullException(nameof(schema));
+            QueryDocument = queryDocument
+                ?? throw new ArgumentNullException(nameof(queryDocument));
+            Operation = operation
+                ?? throw new ArgumentNullException(nameof(operation));
+            Variables = variables
+                ?? throw new ArgumentNullException(nameof(variables));
+            _services = services
+                ?? throw new ArgumentNullException(nameof(services));
             RootValue = rootValue;
             UserContext = userContext;
 
-            Data = new OrderedDictionary();
-            Fragments = fragments;
-            Errors = new List<IQueryError>();
-            NextBatch = new List<FieldResolverTask>();
+            Fragments = new FragmentCollection(schema, queryDocument);
+            _fieldCollector = new FieldCollector(schema, variables, Fragments);
         }
 
         public Schema Schema { get; }
+
         public FragmentCollection Fragments { get; }
+
         public object RootValue { get; }
+
         public object UserContext { get; }
+
         public DocumentNode QueryDocument { get; }
+
         public OperationDefinitionNode Operation { get; }
+
         public VariableCollection Variables { get; }
-        public OrderedDictionary Data { get; }
-        public List<IQueryError> Errors { get; }
-        public List<FieldResolverTask> NextBatch { get; }
+
+        public OrderedDictionary Data { get; } = new OrderedDictionary();
+
+        public List<IQueryError> Errors { get; } = new List<IQueryError>();
+
+        public List<FieldResolverTask> NextBatch { get; } = new List<FieldResolverTask>();
 
         public IReadOnlyCollection<FieldSelection> CollectFields(
             ObjectType objectType, SelectionSetNode selectionSet)
