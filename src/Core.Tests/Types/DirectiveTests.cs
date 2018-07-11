7f1ef6affebae86426f5642788c9e09bf8f161c4
Michael Staib <michael.staib@hotmail.com>
2018-07-11 22:26:07 +0200

    Added more directive tests

---
+++ src/Core.Tests/Types/DirectiveTests.cs
@@ -0,0 +1,71 @@
+using System.Linq;
+using HotChocolate.Configuration;
+using Xunit;
+
+namespace HotChocolate.Types
+{
+    public class DirectiveTests
+    {
+        [Fact]
+        public void CreateSkipDirective()
+        {
+            // arrange
+            SchemaContext schemaContext = SchemaContextFactory.Create();
+            SchemaConfiguration schemaConfiguration =
+                new SchemaConfiguration(sp => { }, schemaContext.Types);
+            TypeFinalizer typeFinalizer = new TypeFinalizer(schemaConfiguration);
+            typeFinalizer.FinalizeTypes(schemaContext, null);
+
+            // assert
+            Directive directive = schemaContext.Directives
+                .GetDirectives().FirstOrDefault(t => t.Name == "skip");
+
+            // assert
+            Assert.NotNull(directive);
+            Assert.IsType<SkipDirective>(directive);
+            Assert.Equal("skip", directive.Name);
+            Assert.Collection(directive.Arguments,
+                t =>
+                {
+                    Assert.Equal("if", t.Name);
+                    Assert.IsType<NonNullType>(t.Type);
+                    Assert.IsType<BooleanType>(((NonNullType)t.Type).Type);
+                });
+            Assert.Collection(directive.Locations,
+                t => Assert.Equal(DirectiveLocation.Field, t),
+                t => Assert.Equal(DirectiveLocation.FragmentSpread, t),
+                t => Assert.Equal(DirectiveLocation.InlineFragment, t));
+        }
+
+        [Fact]
+        public void CreateIncludeDirective()
+        {
+            // arrange
+            SchemaContext schemaContext = SchemaContextFactory.Create();
+            SchemaConfiguration schemaConfiguration =
+                new SchemaConfiguration(sp => { }, schemaContext.Types);
+            TypeFinalizer typeFinalizer = new TypeFinalizer(schemaConfiguration);
+            typeFinalizer.FinalizeTypes(schemaContext, null);
+
+            // assert
+            Directive directive = schemaContext.Directives
+                .GetDirectives().FirstOrDefault(t => t.Name == "include");
+
+            // assert
+            Assert.NotNull(directive);
+            Assert.IsType<IncludeDirective>(directive);
+            Assert.Equal("include", directive.Name);
+            Assert.Collection(directive.Arguments,
+                t =>
+                {
+                    Assert.Equal("if", t.Name);
+                    Assert.IsType<NonNullType>(t.Type);
+                    Assert.IsType<BooleanType>(((NonNullType)t.Type).Type);
+                });
+            Assert.Collection(directive.Locations,
+                t => Assert.Equal(DirectiveLocation.Field, t),
+                t => Assert.Equal(DirectiveLocation.FragmentSpread, t),
+                t => Assert.Equal(DirectiveLocation.InlineFragment, t));
+        }
+    }
+}
