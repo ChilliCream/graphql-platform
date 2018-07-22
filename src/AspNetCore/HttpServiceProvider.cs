c15ab519feb824f242b99cbbe523ab0c1ce6cd91
Michael Staib <michael.staib@hotmail.com>
2018-07-22 23:02:22 +0200

    Added tests for scoped dependency injection and access to HttpContext

---
+++ src/AspNetCore/HttpServiceProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HotChocolate.AspNetCore
+{
+    public class RequestServiceProvider
+        : IServiceProvider
+    {
+        private readonly IServiceProvider _root;
+        private readonly Dictionary<Type, object> _services;
+
+        public RequestServiceProvider(
+            IServiceProvider root,
+            IEnumerable<KeyValuePair<Type, object>> services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            _services = services.ToDictionary(t => t.Key, t => t.Value);
+            _root = root ?? throw new ArgumentNullException(nameof(root));
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException(nameof(serviceType));
+            }
+
+            if (!_services.TryGetValue(serviceType, out object instance))
+            {
+                instance = _root.GetService(serviceType);
+            }
+            return instance;
+        }
+    }
+}
