a6af7537fee4e8d8dc43a773313de7ca3a282ffe
Michael Staib <michael.staib@hotmail.com>
2018-07-24 22:38:40 +0200

    Started work on AllVariableUsagesAreAllowedVisitor

--- src/Core/Validation/AllVariableUsagesAreAllowedVisitor.cs
+++ src/Core/Validation/AllVariableUsagesAreAllowedVisitor.cs
@@ -1,4 +1,58 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using HotChocolate.Language;
+using HotChocolate.Types;
+
 namespace HotChocolate.Validation
 {
+    internal sealed class AllVariableUsagesAreAllowedVisitor
+        : QueryVisitorErrorBase
+    {
+        private Dictionary<string, ITypeNode> _variables
+            = new Dictionary<string, ITypeNode>();
 
+        public AllVariableUsagesAreAllowedVisitor(ISchema schema)
+            : base(schema)
+        {
+        }
+
+        protected override void VisitOperationDefinition(
+            OperationDefinitionNode operation,
+            ImmutableStack<ISyntaxNode> path)
+        {
+
+            foreach (VariableDefinitionNode variable in
+                operation.VariableDefinitions)
+            {
+                _variables[variable.Variable.Name.Value] = variable.Type;
+            }
+
+            base.VisitOperationDefinition(operation, path);
+        }
+
+        protected override void VisitField(
+           FieldNode field,
+           IType type,
+           ImmutableStack<ISyntaxNode> path)
+        {
+            ValidateArguments(field, field.Arguments);
+            base.VisitField(field, type, path);
+        }
+
+        protected override void VisitDirective(
+            DirectiveNode directive,
+            ImmutableStack<ISyntaxNode> path)
+        {
+            ValidateArguments(directive, directive.Arguments);
+            base.VisitDirective(directive, path);
+        }
+
+        private void ValidateArguments(ISyntaxNode node, IEnumerable<ArgumentNode> arguments)
+        {
+            foreach (ArgumentNode argument in arguments)
+            {
+
+            }
+        }
+    }
 }
