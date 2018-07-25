b5f1d95510e73651af293afc3ff012d3ceb6ab2b
Michael Staib <michael.staib@hotmail.com>
2018-07-25 09:32:19 +0200

    Started implementing the AllVariableUsagesAreAllowedVisitor algorithm

--- src/Core/Validation/AllVariableUsagesAreAllowedVisitor.cs
+++ src/Core/Validation/AllVariableUsagesAreAllowedVisitor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using HotChocolate.Language;
@@ -8,8 +9,10 @@ namespace HotChocolate.Validation
     internal sealed class AllVariableUsagesAreAllowedVisitor
         : QueryVisitorErrorBase
     {
-        private Dictionary<string, ITypeNode> _variables
-            = new Dictionary<string, ITypeNode>();
+        private readonly Dictionary<string, VariableDefinitionNode> _variableDefinitions
+            = new Dictionary<string, VariableDefinitionNode>();
+        private readonly List<VariableNode> _variablesUsages =
+            new List<VariableNode>();
 
         public AllVariableUsagesAreAllowedVisitor(ISchema schema)
             : base(schema)
@@ -23,10 +26,14 @@ namespace HotChocolate.Validation
             foreach (VariableDefinitionNode variable in
                 operation.VariableDefinitions)
             {
-                _variables[variable.Variable.Name.Value] = variable.Type;
+                _variableDefinitions[variable.Variable.Name.Value] = variable;
             }
 
             base.VisitOperationDefinition(operation, path);
+
+
+            _variableDefinitions.Clear();
+            _variablesUsages.Clear();
         }
 
         protected override void VisitField(
@@ -52,7 +59,71 @@ namespace HotChocolate.Validation
         {
             foreach (ArgumentNode argument in arguments)
             {
+
             }
+        }
+
+        private void FindVariableUsageErrors()
+        {
+            foreach (VariableNode variableUsage in _variablesUsages)
+            {
+                if (_variableDefinitions.TryGetValue(
+                    variableUsage.Name.Value,
+                    out VariableDefinitionNode expectedVariableType))
+                {
+
+                }
+            }
+        }
+
+        private bool AreTypesCompatible(
+            ITypeNode variableType,
+            ITypeNode locationType)
+        {
+            if (locationType.IsNonNullType())
+            {
+                if (variableType.IsNonNullType())
+                {
+                    return AreTypesCompatible(
+                        variableType.InnerType(),
+                        locationType.InnerType());
+                }
+                return false;
+            }
+
+            if (variableType.IsNonNullType())
+            {
+                return AreTypesCompatible(
+                    variableType.InnerType(),
+                    locationType);
+            }
+
+            if (locationType.IsListType())
+            {
+                if (variableType.IsListType())
+                {
+                    return AreTypesCompatible(
+                        variableType.InnerType(),
+                        locationType.InnerType());
+                }
+                return false;
+            }
+
+            if (variableType.IsListType())
+            {
+                return false;
+            }
+
+            if (variableType is NamedTypeNode vn
+                && locationType is NamedTypeNode ln)
+            {
+                return string.Equals(
+                    vn.Name.Value,
+                    ln.Name.Value,
+                    StringComparison.Ordinal);
+            }
+
+            return false;
         }
     }
 }
