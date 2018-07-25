714828ac917072cd80deae4e5b8de2c63ef4f640
Michael Staib <michael.staib@hotmail.com>
2018-07-25 10:08:15 +0200

    Added integration tests for object types that are also used as input types

---
+++ src/Core.Tests/Integration/InputOutputObjectAreTheSame/Query.cs
@@ -0,0 +1,11 @@
+namespace HotChocolate.Integration.InputOutputObjectAreTheSame
+{
+    public class Query
+    {
+        public Person GetPerson(Person person)
+        {
+            return person;
+        }
+    }
+
+}
