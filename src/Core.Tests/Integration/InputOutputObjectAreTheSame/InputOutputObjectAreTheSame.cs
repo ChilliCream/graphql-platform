ac46950800de6dcd4dc220750317b36bc6c087dd
Michael Staib <michael.staib@hotmail.com>
2018-07-25 10:29:10 +0200

    Added integration tests for object types that are also used as input types (#188)

---
+++ src/Core.Tests/Integration/InputOutputObjectAreTheSame/InputOutputObjectAreTheSame.cs
@@ -0,0 +1,56 @@
+using HotChocolate.Execution;
+using HotChocolate.Types;
+using Xunit;
+
+namespace HotChocolate.Integration.InputOutputObjectAreTheSame
+{
+    public class InputOutputObjectAreTheSame
+    {
+        [Fact]
+        public void CheckIfTypesAreRegisteredCorrectly()
+        {
+            // arrange
+            Schema schema = CreateSchema();
+
+            // act
+            bool containsPersonInputType = schema
+                .TryGetType("PersonInput", out INamedInputType _);
+            bool containsPersonOutputType = schema
+                .TryGetType("Person", out INamedOutputType _);
+
+            // assert
+            Assert.True(containsPersonInputType);
+            Assert.True(containsPersonOutputType);
+        }
+
+        [Fact]
+        public void ExecuteQueryThatReturnsPerson()
+        {
+            // arrange
+            Schema schema = CreateSchema();
+
+            // act
+            IExecutionResult result = schema.Execute(@"{
+                person(person: { firstName:""a"", lastName:""b""  }) {
+                    lastName
+                    firstName
+                }
+             }");
+
+            // assert
+            Assert.Null(result.Errors);
+            Assert.Equal(Snapshot.Current(), Snapshot.New(result));
+        }
+
+        private static Schema CreateSchema()
+        {
+            return Schema.Create(c =>
+            {
+                c.RegisterQueryType<Query>();
+                c.RegisterType<ObjectType<Person>>();
+                c.RegisterType(new InputObjectType<Person>(
+                    d => d.Name("PersonInput")));
+            });
+        }
+    }
+}
