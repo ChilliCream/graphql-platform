912ce2d9f784879a7718b158499f95dea78b80f0
Michael Staib <michael.staib@hotmail.com>
2018-08-16 19:04:40 -0500

    Added some comments

--- src/Types/Resolvers/IDirectiveFieldResolver.cs
+++ src/Types/Resolvers/IDirectiveFieldResolver.cs
@@ -11,5 +11,16 @@ namespace HotChocolate.Resolvers
             CancellationToken cancellationToken);
     }
 
+    public interface IDirectiveFieldResolverHandler // TODO: naming is not quite good
+    {
+        void OnBeforeInvoke(
+            IDirectiveContext directiveContext,
+            IResolverContext context);
 
+        Task<object> OnAfterInvokeAsync(
+            IDirectiveContext directiveContext,
+            IResolverContext context,
+            object resolverResult,
+            CancellationToken cancellationToken);
+    }
 }
