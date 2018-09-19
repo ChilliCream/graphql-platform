08133d27ff99da133a43dac8af5e11fd3ca4fa30
Michael Staib <michael.staib@hotmail.com>
2018-09-19 15:27:12 +0200

    Added boolean value node tests

---
+++ src/Language.Tests/AST/BooleanValueNodeTests.cs
@@ -0,0 +1,146 @@
+using Xunit;
+
+namespace HotChocolate.Language
+{
+    public class BooleanValueNodeTests
+    {
+        [InlineData(false)]
+        [InlineData(true)]
+        [Theory]
+        public void CreateBooleanValue(bool value)
+        {
+            // act
+            var booleanValueNode = new BooleanValueNode(value);
+
+            // assert
+            Assert.Equal(value, booleanValueNode.Value);
+            Assert.Equal(NodeKind.BooleanValue, booleanValueNode.Kind);
+            Assert.Null(booleanValueNode.Location);
+        }
+
+        [InlineData(false)]
+        [InlineData(true)]
+        [Theory]
+        public void CreateBooleanValueWithLocation(bool value)
+        {
+            // arrange
+            var token = new SyntaxToken(
+                TokenKind.StartOfFile, 0, 0, 0, 0, null);
+            Location location = new Location(new Source("{}"), token, token);
+
+            // act
+            var booleanValueNode = new BooleanValueNode(location, value);
+
+            // assert
+            Assert.Equal(value, booleanValueNode.Value);
+            Assert.Equal(NodeKind.BooleanValue, booleanValueNode.Kind);
+            Assert.Equal(location, booleanValueNode.Location);
+        }
+
+        [Fact]
+        public void EqualsBooleanValueNode()
+        {
+            // arrange
+            var a = new BooleanValueNode(false);
+            var b = new BooleanValueNode(false);
+            var c = new BooleanValueNode(true);
+
+            // act
+            bool ab_result = a.Equals(b);
+            bool aa_result = a.Equals(a);
+            bool ac_result = a.Equals(c);
+            bool anull_result = a.Equals(default(BooleanValueNode));
+
+            // assert
+            Assert.True(ab_result);
+            Assert.True(aa_result);
+            Assert.False(ac_result);
+            Assert.False(anull_result);
+        }
+
+        [Fact]
+        public void EqualsIValueNode()
+        {
+            // arrange
+            var a = new BooleanValueNode(false);
+            var b = new BooleanValueNode(false);
+            var c = new BooleanValueNode(true);
+            var d = new StringValueNode("foo");
+
+            // act
+            bool ab_result = a.Equals(b);
+            bool aa_result = a.Equals(a);
+            bool ac_result = a.Equals(c);
+            bool ad_result = a.Equals(d);
+            bool anull_result = a.Equals(default(BooleanValueNode));
+
+            // assert
+            Assert.True(ab_result);
+            Assert.True(aa_result);
+            Assert.False(ac_result);
+            Assert.False(ad_result);
+            Assert.False(anull_result);
+        }
+
+        [Fact]
+        public void EqualsObject()
+        {
+            // arrange
+            var a = new BooleanValueNode(false);
+            var b = new BooleanValueNode(false);
+            var c = new BooleanValueNode(true);
+            var d = "foo";
+            var e = 1;
+
+            // act
+            bool ab_result = a.Equals(b);
+            bool aa_result = a.Equals(a);
+            bool ac_result = a.Equals(c);
+            bool ad_result = a.Equals(d);
+            bool ae_result = a.Equals(e);
+            bool anull_result = a.Equals(default(BooleanValueNode));
+
+            // assert
+            Assert.True(ab_result);
+            Assert.True(aa_result);
+            Assert.False(ac_result);
+            Assert.False(ad_result);
+            Assert.False(ae_result);
+            Assert.False(anull_result);
+        }
+
+        [Fact]
+        public void CompareGetHashCode()
+        {
+            // arrange
+            var a = new BooleanValueNode(false);
+            var b = new BooleanValueNode(false);
+            var c = new BooleanValueNode(true);
+
+            // act
+            int ahash = a.GetHashCode();
+            int bhash = b.GetHashCode();
+            int chash = c.GetHashCode();
+
+            // assert
+            Assert.Equal(ahash, bhash);
+            Assert.NotEqual(ahash, chash);
+        }
+
+        [Fact]
+        public void StringRepresentation()
+        {
+            // arrange
+            var a = new BooleanValueNode(false);
+            var b = new BooleanValueNode(true);
+
+            // act
+            string astring = a.ToString();
+            string bstring = b.ToString();
+
+            // assert
+            Assert.Equal("False", astring);
+            Assert.Equal("True", bstring);
+        }
+    }
+}
--- src/Language/AST/NamedTypeNode.cs
+++ src/Language/AST/NamedTypeNode.cs
@@ -19,7 +19,9 @@ namespace HotChocolate.Language
         }
 
         public NodeKind Kind { get; } = NodeKind.NamedType;
+
         public Location Location { get; }
+
         public NameNode Name { get; }
 
         public override string ToString()
