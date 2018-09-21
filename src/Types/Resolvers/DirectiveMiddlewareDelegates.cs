aa4240f7747cf08788fa794017db545e28bd5ee4
Michael Staib <michael.staib@hotmail.com>
2018-09-21 14:50:05 +0200

    Removed middlewares

--- src/Types/Resolvers/DirectiveMiddlewareDelegates.cs
+++ src/Types/Resolvers/DirectiveMiddlewareDelegates.cs
@@ -3,35 +3,19 @@ using System.Threading.Tasks;
 
 namespace HotChocolate.Resolvers
 {
-    public delegate void OnBeforeInvokeResolver(
-        IDirectiveContext directiveContext,
-        IResolverContext resolverContext,
-        CancellationToken cancellationToken);
-
-    public delegate object OnAfterInvokeResolver(
-        IDirectiveContext directiveContext,
-        IResolverContext resolverContext,
-        object resolverResult,
-        CancellationToken cancellationToken);
-
     public delegate Task OnBeforeInvokeResolverAsync(
-        IDirectiveContext directiveContext,
         IResolverContext resolverContext,
+        IDirectiveContext directiveContext,
         CancellationToken cancellationToken);
 
     public delegate Task<object> OnAfterInvokeResolverAsync(
-        IDirectiveContext directiveContext,
         IResolverContext resolverContext,
+        IDirectiveContext directiveContext,
         object resolverResult,
         CancellationToken cancellationToken);
 
     public delegate Task<object> AsyncDirectiveResolver(
-        IDirectiveContext directiveContext,
         IResolverContext resolverContext,
-        CancellationToken cancellationToken);
-
-    public delegate object DirectiveResolver(
         IDirectiveContext directiveContext,
-        IResolverContext resolverContext,
         CancellationToken cancellationToken);
 }
