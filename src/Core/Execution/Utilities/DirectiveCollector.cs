bac67696cea7e36465bae7131eb3cf13bb22ed85
Michael Staib <michael.staib@hotmail.com>
2018-09-25 15:56:52 +0200

    Reworking directive collector

--- src/Core/Execution/Utilities/DirectiveCollector.cs
+++ src/Core/Execution/Utilities/DirectiveCollector.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using HotChocolate.Language;
 using HotChocolate.Resolvers;
@@ -122,6 +123,139 @@ namespace HotChocolate.Execution
                     if (processed.Add(directive.Name))
                     {
                         directives.Push(directive);
+                    }
+                }
+            }
+        }
+    }
+
+    internal sealed class DirectiveCollector2
+        : Validation.QueryVisitor
+    {
+        private readonly Dictionary<FieldNode, List<IDirective>> _directives =
+            new Dictionary<FieldNode, List<IDirective>>();
+
+        public DirectiveCollector2(ISchema schema)
+            : base(schema)
+        {
+        }
+
+
+
+        protected override void VisitField(
+            FieldNode fieldSelection,
+            IType type,
+            ImmutableStack<ISyntaxNode> path)
+        {
+            if (type is IComplexOutputType complexType
+                && complexType.Fields.ContainsField(fieldSelection.Name.Value))
+            {
+                IOutputField field =
+                    complexType.Fields[fieldSelection.Name.Value];
+
+                CollectDirectives(complexType, field, fieldSelection);
+
+
+            }
+
+            base.VisitField(fieldSelection, type, path);
+        }
+
+
+        public IReadOnlyCollection<IDirective> CollectDirectives(
+            IOutputType objectType,
+            IOutputField field,
+            FieldNode fieldSelection)
+        {
+            HashSet<string> processed = new HashSet<string>();
+            List<IDirective> directives = new List<IDirective>();
+
+            CollectSelectionDirectives(processed, directives, fieldSelection);
+            CollectFieldDirectives(processed, directives, field);
+            CollectTypeDirectives(processed, directives, objectType);
+
+            return directives;
+        }
+
+        private void CollectSelectionDirectives(
+            HashSet<string> processed,
+            List<IDirective> directives,
+            FieldNode fieldSelection)
+        {
+            foreach (IDirective directive in
+                GetFieldSelectionDirectives(fieldSelection))
+            {
+                if (processed.Add(directive.Name))
+                {
+                    directives.Add(directive);
+                }
+            }
+        }
+
+        private IEnumerable<IDirective> GetFieldSelectionDirectives(
+            FieldNode fieldSelection)
+        {
+            foreach (DirectiveNode directive in fieldSelection.Directives)
+            {
+                if (Schema.TryGetDirectiveType(directive.Name.Value,
+                    out DirectiveType directiveType))
+                {
+                    yield return new Directive(directiveType, directive);
+                }
+            }
+        }
+
+        private void CollectFieldDirectives(
+            HashSet<string> processed,
+            List<IDirective> directives,
+            IEnumerable<IField> fields)
+        {
+            foreach (IField field in fields)
+            {
+                CollectFieldDirectives(processed, directives, field);
+            }
+        }
+
+        private void CollectFieldDirectives(
+            HashSet<string> processed,
+            List<IDirective> directives,
+            IField field)
+        {
+            if (field is Types.IHasDirectives d)
+            {
+                foreach (IDirective directive in d.Directives)
+                {
+                    if (processed.Add(directive.Name))
+                    {
+                        directives.Add(directive);
+                    }
+                }
+            }
+        }
+
+        private void CollectTypeDirectives(
+            HashSet<string> processed,
+            List<IDirective> directives,
+            IEnumerable<IOutputType> types)
+        {
+            foreach (IOutputType type in types)
+            {
+                CollectTypeDirectives(processed, directives, type);
+            }
+        }
+
+        private void CollectTypeDirectives(
+            HashSet<string> processed,
+            List<IDirective> directives,
+            IOutputType type)
+        {
+            if (type is Types.IHasDirectives d)
+            {
+                foreach (IDirective directive in d.Directives)
+                {
+                    if (processed.Add(directive.Name))
+                    {
+                        directives.Add(directive);
                     }
                 }
             }
