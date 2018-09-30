e12046b03ea1c64835d1cfdf6fa3846916f80bf0
Nikolay <dolphinn@bk.ru>
2018-09-30 10:51:12 +0300

    Fixed typos in filePath variables

--- src/Utilities/FileResource.cs
+++ src/Utilities/FileResource.cs
@@ -6,11 +6,11 @@ namespace HotChocolate
     {
         public static string Open(string name)
         {
-            string fielPath = Path.Combine(
+            string filePath = Path.Combine(
                 "__resources__", name);
-            if (File.Exists(fielPath))
+            if (File.Exists(filePath))
             {
-                return File.ReadAllText(fielPath);
+                return File.ReadAllText(filePath);
             }
             return null;
         }
