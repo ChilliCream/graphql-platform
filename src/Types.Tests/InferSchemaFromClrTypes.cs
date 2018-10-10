a51b4919860132dbb84960cbc8f9159f252d4a6c
Michael Staib <michael.staib@hotmail.com>
2018-10-10 13:52:21 +0200

    Fixed type detection

--- src/Types.Tests/InferSchemaFromClrTypes.cs
+++ src/Types.Tests/InferSchemaFromClrTypes.cs
@@ -6,7 +6,7 @@ namespace HotChocolate
     public class InferSchemaFromClrTypes
     {
         [Fact]
-        public void InferObjectTypes()
+        public void RegisterType_ClrClass_BecomesGraphQLObjectType()
         {
             // arrange
             // act
@@ -16,6 +16,25 @@ namespace HotChocolate
             ObjectType type = schema.GetType<ObjectType>("MyQuery");
             Assert.NotNull(type);
             Assert.IsType<ObjectType<Foo>>(type.Fields["foo"].Type);
+        }
+
+        [Fact]
+        public void RegisterType_ClrEnumType_BecomesGraphQLEnumType()
+        {
+            // arrange
+            // act
+            Schema schema = Schema.Create(c =>
+            {
+                c.RegisterType<Baz>();
+                c.Options.StrictValidation = false;
+            });
+
+            // assert
+            EnumType type = schema.GetType<EnumType>("Baz");
+            Assert.NotNull(type);
+            Assert.Collection(type.Values,
+                t => Assert.Equal("FOO", t.Name),
+                t => Assert.Equal("BAR", t.Name));
         }
 
         public class MyQuery
@@ -31,6 +50,12 @@ namespace HotChocolate
         public class Bar
         {
             public string Abc { get; set; } = "CDE";
+        }
+
+        public enum Baz
+        {
+            Foo,
+            Bar
         }
     }
 }
