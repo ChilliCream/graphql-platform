284ac4256158c82b7ee86cc2d7c028c9567d3ccf
Michael Staib <michael.staib@hotmail.com>
2018-10-25 07:54:17 +0200

    Added connection initialize handler

---
+++ src/AspNetCore.Tests/Subscriptions/ConnectionInitializeHandlerTests.cs
@@ -0,0 +1,77 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HotChocolate.AspNetCore.Subscriptions
+{
+    public class ConnectionInitializeHandlerTests
+    {
+        [Fact]
+        public void CanHandle_InitializeMessage_True()
+        {
+            // arrange
+            var handler = new ConnectionInitializeHandler();
+
+            var message = new GenericOperationMessage
+            {
+                Type = MessageTypes.Connection.Initialize
+            };
+
+            // act
+            bool result = handler.CanHandle(message);
+
+            // assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CanHandle_AcceptMessage_False()
+        {
+            // arrange
+            var handler = new ConnectionInitializeHandler();
+
+            var message = new GenericOperationMessage
+            {
+                Type = MessageTypes.Connection.Accept
+            };
+
+            // act
+            bool result = handler.CanHandle(message);
+
+            // assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task Han_AcceptMessage_False()
+        {
+            // arrange
+            var webSocketContext = new InMemoryWebSocketContext();
+
+            var handler = new ConnectionInitializeHandler();
+
+            var message = new GenericOperationMessage
+            {
+                Type = MessageTypes.Connection.Initialize
+            };
+
+            // act
+            await handler.HandleAsync(
+                webSocketContext,
+                message,
+                CancellationToken.None);
+
+            // assert
+            Assert.Collection(webSocketContext.Outgoing,
+                t =>
+                {
+                    Assert.Equal(MessageTypes.Connection.Accept, t.Type);
+                },
+                t =>
+                {
+                    Assert.Equal(MessageTypes.Connection.KeepAlive, t.Type);
+                });
+        }
+    }
+}
