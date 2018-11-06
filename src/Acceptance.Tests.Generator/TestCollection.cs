4f5837d23ede2c8099ad487830626cbb078c830a
Michael Staib <michael.staib@hotmail.com>
2018-11-06 23:06:16 +0100

    moved spec files to generator

---
+++ src/Acceptance.Tests.Generator/TestCollection.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Acceptance.Tests.Generator
+{
+    public class TestCollection
+    {
+        public string Scenario { get; set; }
+
+        public List<Test> Tests { get; set; }
+    }
+
+    public class Test
+    {
+
+    }
+}
