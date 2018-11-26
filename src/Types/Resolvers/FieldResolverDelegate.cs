200e710e0ba443b82b4436d4cc322aa13cf9c9c3
Michael Staib <michael.staib@hotmail.com>
2018-11-26 22:07:05 +0100

    Fixed delegates

--- src/Types/Resolvers/FieldResolverDelegate.cs
+++ src/Types/Resolvers/FieldResolverDelegate.cs
@@ -6,10 +6,6 @@ namespace HotChocolate.Resolvers
     public delegate Task<object> FieldResolverDelegate(
         IResolverContext context);
 
-    public delegate FieldDelegate FieldMiddleware(
-        FieldDelegate next);
-
-    // TODO : InvokeFieldDelegate
-    public delegate Task FieldDelegate(
-        IFieldContext context);
+    public delegate FieldResolverDelegate FieldMiddleware(
+        FieldResolverDelegate next);
 }
