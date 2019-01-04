82f24982081c5b35d58fb79a36d669d63d69fd3c
Michael Staib <michael.staib@hotmail.com>
2019-01-04 23:16:21 +0100

    Introduce pagination amount type

--- src/Core/Types/Types/Scalars/IntType.cs
+++ src/Core/Types/Types/Scalars/IntType.cs
@@ -13,103 +13,11 @@ namespace HotChocolate.Types
     /// </summary>
     [SpecScalar]
     public sealed class IntType
-        : ScalarType
+        : IntTypeBase
     {
         public IntType()
             : base("Int")
         {
-        }
-
-        public override string Description =>
-            TypeResources.IntType_Description();
-
-        public override Type ClrType => typeof(int);
-
-        public override bool IsInstanceOfType(IValueNode literal)
-        {
-            if (literal == null)
-            {
-                throw new ArgumentNullException(nameof(literal));
-            }
-
-            return literal is IntValueNode
-                || literal is NullValueNode;
-        }
-
-        public override object ParseLiteral(IValueNode literal)
-        {
-            if (literal == null)
-            {
-                throw new ArgumentNullException(nameof(literal));
-            }
-
-            if (literal is IntValueNode intLiteral)
-            {
-                return int.Parse(
-                    intLiteral.Value,
-                    NumberStyles.Integer,
-                    CultureInfo.InvariantCulture);
-            }
-
-            if (literal is NullValueNode)
-            {
-                return null;
-            }
-
-            throw new ScalarSerializationException(
-                TypeResources.Scalar_Cannot_ParseLiteral(
-                    Name, literal.GetType()));
-        }
-
-        public override IValueNode ParseValue(object value)
-        {
-            if (value is null)
-            {
-                return NullValueNode.Default;
-            }
-
-            if (value is int i)
-            {
-                return new IntValueNode(i);
-            }
-
-            throw new ScalarSerializationException(
-                TypeResources.Scalar_Cannot_ParseValue(
-                    Name, value.GetType()));
-        }
-
-        public override object Serialize(object value)
-        {
-            if (value == null)
-            {
-                return null;
-            }
-
-            if (value is int i)
-            {
-                return i;
-            }
-
-            throw new ScalarSerializationException(
-                TypeResources.Scalar_Cannot_Serialize(Name));
-        }
-
-        public override bool TryDeserialize(object serialized, out object value)
-        {
-            if (serialized is null)
-            {
-                value = null;
-                return true;
-            }
-
-            if (serialized is int i)
-            {
-                value = i;
-                return true;
-            }
-
-            value = null;
-            return false;
         }
     }
 }
