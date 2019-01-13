7f10e4aaf78411291d9c98c6d48711d1b911fb8b
Michael Staib <michael.staib@hotmail.com>
2019-01-13 08:18:41 +0100

    Added missing complexity middleware tests

---
+++ src/Core/Core.Tests/Execution/Middleware/MaxComplexityMiddlewareTests.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChilliCream.Testing;
+using HotChocolate.Execution.Configuration;
+using HotChocolate.Language;
+using HotChocolate.Runtime;
+using HotChocolate.Utilities;
+using Moq;
+using Xunit;
+
+namespace HotChocolate.Execution
+{
+    public class MaxComplexityMiddlewareTests
+    {
+        [InlineData(5, false)]
+        [InlineData(4, true)]
+        [Theory]
+        public async Task ValidateMaxComplexityWithMiddleware(
+            int count, bool valid)
+        {
+            // arrange
+            var schema = Schema.Create(
+                @"
+                type Query {
+                    foo(i: Int): String
+                        @cost(complexity: 5 multipliers: [""i""])
+                }
+                ",
+                c =>
+                {
+                    c.BindResolver(() => "Hello")
+                        .To("Query", "foo");
+                });
+
+            var options = new Mock<IValidateQueryOptionsAccessor>();
+            options.SetupGet(t => t.MaxOperationComplexity).Returns(20);
+            options.SetupGet(t => t.UseComplexityMultipliers).Returns(true);
+
+            DocumentNode query = Parser.Default.Parse(
+                "{ foo(i:" + count + ") }");
+
+            OperationDefinitionNode operationNode = query.Definitions
+                .OfType<OperationDefinitionNode>()
+                .FirstOrDefault();
+
+            var operation = new Operation(
+                query, operationNode, schema.QueryType,
+                null);
+
+            IReadOnlyQueryRequest request = new QueryRequest("{ a }")
+                .ToReadOnly();
+
+            var services = new DictionaryServiceProvider(
+                new KeyValuePair<Type, object>(
+                    typeof(IErrorHandler),
+                    ErrorHandler.Default));
+
+            var context = new QueryContext(
+                schema, services.CreateRequestServiceScope(), request)
+            {
+                Document = query,
+                Operation = operation,
+                Variables = new VariableValueBuilder(
+                    schema, operation.Definition)
+                    .CreateValues(new Dictionary<string, object>())
+            };
+
+            var middleware = new MaxComplexityMiddleware(
+                c => Task.CompletedTask,
+                options.Object,
+                null);
+
+            // act
+            await middleware.InvokeAsync(context);
+
+            // assert
+            if (valid)
+            {
+                Assert.Null(context.Result);
+            }
+            else
+            {
+                context.Result.Snapshot(
+                    "ValidateMaxComplexityWithMiddleware" +
+                    count);
+            }
+        }
+
+        [InlineData(5, false)]
+        [InlineData(4, true)]
+        [Theory]
+        public async Task ValidateMaxComplexityWithMiddlewareWithVariables(
+            int count, bool valid)
+        {
+            // arrange
+            var schema = Schema.Create(
+                @"
+                type Query {
+                    foo(i: Int): String
+                        @cost(complexity: 5 multipliers: [""i""])
+                }
+                ",
+                c =>
+                {
+                    c.BindResolver(() => "Hello")
+                        .To("Query", "foo");
+                });
+
+            var options = new Mock<IValidateQueryOptionsAccessor>();
+            options.SetupGet(t => t.MaxOperationComplexity).Returns(20);
+            options.SetupGet(t => t.UseComplexityMultipliers).Returns(true);
+
+            DocumentNode query = Parser.Default.Parse(
+                "query f($i: Int) { foo(i: $i) }");
+
+            OperationDefinitionNode operationNode = query.Definitions
+                .OfType<OperationDefinitionNode>()
+                .FirstOrDefault();
+
+            var operation = new Operation(
+                query, operationNode, schema.QueryType,
+                null);
+
+            IReadOnlyQueryRequest request = new QueryRequest("{ a }")
+                .ToReadOnly();
+
+            var services = new DictionaryServiceProvider(
+                new KeyValuePair<Type, object>(
+                    typeof(IErrorHandler),
+                    ErrorHandler.Default));
+
+            var context = new QueryContext(
+                schema, services.CreateRequestServiceScope(), request)
+            {
+                Document = query,
+                Operation = operation,
+                Variables = new VariableValueBuilder(
+                    schema, operation.Definition)
+                    .CreateValues(new Dictionary<string, object>
+                    {
+                        { "i", count }
+                    })
+            };
+
+            var middleware = new MaxComplexityMiddleware(
+                c => Task.CompletedTask,
+                options.Object,
+                null);
+
+            // act
+            await middleware.InvokeAsync(context);
+
+            // assert
+            if (valid)
+            {
+                Assert.Null(context.Result);
+            }
+            else
+            {
+                context.Result.Snapshot(
+                    "ValidateMaxComplexityWithMiddlewareWithVariables" +
+                    count);
+            }
+        }
+    }
+}
