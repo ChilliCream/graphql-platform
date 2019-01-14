3f820902169a917ea30c2565b6cb8c0bc7b7ee9e
Michael Staib <michael.staib@hotmail.com>
2019-01-14 22:38:34 +0100

    Added description to node type

--- src/Core/Types/Types/Relay/NodeType.cs
+++ src/Core/Types/Types/Relay/NodeType.cs
@@ -1,12 +1,14 @@
 ﻿namespace HotChocolate.Types.Relay
 {
-    // TODO : Add description
     public class NodeType
         : InterfaceType
     {
         protected override void Configure(IInterfaceTypeDescriptor descriptor)
         {
             descriptor.Name("Node");
+            descriptor.Description(
+                "The node interface is implemented by entities that have " +
+                "a gloabl unique identifier.");
             descriptor.Field("id").Type<NonNullType<IdType>>();
         }
     }
