2cce8f9425f3b62fa0d92bd80cea180fa7d73b57
Michael Staib <michael.staib@hotmail.com>
2019-01-16 10:42:31 +0100

    Moved DataLoader code into separate lib

--- src/Core/Utilities/InternalsVisibleTo.cs
+++ src/Core/Utilities/InternalsVisibleTo.cs
@@ -3,6 +3,7 @@
 [assembly: InternalsVisibleTo("HotChocolate.Abstractions")]
 [assembly: InternalsVisibleTo("HotChocolate.Language")]
 [assembly: InternalsVisibleTo("HotChocolate.Core")]
+[assembly: InternalsVisibleTo("HotChocolate.DataLoader")]
 [assembly: InternalsVisibleTo("HotChocolate.Types")]
 [assembly: InternalsVisibleTo("HotChocolate.Stitching")]
 [assembly: InternalsVisibleTo("HotChocolate.Utilities.Tests")]
