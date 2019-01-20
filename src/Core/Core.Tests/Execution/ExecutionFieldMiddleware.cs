83c982f31338a60b9df861d20cc53485f3711161
Michael Staib <michael.staib@hotmail.com>
2019-01-20 20:51:38 +0100

    added tests that show the execution bound field middleware works

--- src/Core/Core.Tests/Execution/ExecutionFieldMiddleware.cs
+++ src/Core/Core.Tests/Execution/ExecutionFieldMiddleware.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using ChilliCream.Testing;
+using HotChocolate.Resolvers;
 using Xunit;
 
 namespace HotChocolate.Execution
@@ -35,6 +36,71 @@ namespace HotChocolate.Execution
 
             // assert
             result.Snapshot();
+        }
+
+        [Fact]
+        public async Task ExecuteFieldWithExecutionClassMiddleware()
+        {
+            // arrange
+            var schema = Schema.Create(
+                "type Query { a: String }",
+                c => c.Use(next => context =>
+                {
+                    context.Result = "a";
+                    return next.Invoke(context);
+                }));
+
+            IQueryExecutor executor = schema.MakeExecutable(b =>
+                b.UseDefaultPipeline().UseField<ToUpperMiddleware>());
+
+            // act
+            IExecutionResult result = await executor.ExecuteAsync("{ a }");
+
+            // assert
+            result.Snapshot();
+        }
+
+        [Fact]
+        public async Task ExecuteFieldWithExecutionClassMiddlewareWithFactory()
+        {
+            // arrange
+            var schema = Schema.Create(
+                "type Query { a: String }",
+                c => c.Use(next => context =>
+                {
+                    context.Result = "a";
+                    return next.Invoke(context);
+                }));
+
+            IQueryExecutor executor = schema.MakeExecutable(b =>
+                b.UseDefaultPipeline()
+                    .UseField((sp, next) => new ToUpperMiddleware(next)));
+
+            // act
+            IExecutionResult result = await executor.ExecuteAsync("{ a }");
+
+            // assert
+            result.Snapshot();
+        }
+
+        public class ToUpperMiddleware
+        {
+            private readonly FieldDelegate _next;
+
+            public ToUpperMiddleware(FieldDelegate next)
+            {
+                _next = next;
+            }
+
+            public async Task InvokeAsync(IMiddlewareContext context)
+            {
+                await _next.Invoke(context);
+
+                if (context.Result is string s)
+                {
+                    context.Result = s.ToUpperInvariant();
+                }
+            }
         }
     }
 }
