015829e8cf4ceb79cc5f725d168ff2cbf3af7f70
Scott Rabara <scottrabara@gmail.com>
2019-01-22 20:02:51 -0800

    Fixed issue with GetDigitCount if num was factor of 10

--- src/Core/Core/Execution/Utilities/NumberUtilities.cs
+++ src/Core/Core/Execution/Utilities/NumberUtilities.cs
@@ -6,48 +6,48 @@ namespace HotChocolate.Execution
     {
         internal static int GetDigitCount(int n)
         {
-            if (n > 100000)
+            if (n >= 100000)
             {
-                if (n > 1000000000)
+                if (n >= 1000000000)
                 {
                     return 10;
                 }
-                if (n > 100000000)
+                if (n >= 100000000)
                 {
                     return 9;
                 }
-                if (n > 10000000)
+                if (n >= 10000000)
                 {
                     return 8;
                 }
-                if (n > 1000000)
+                if (n >= 1000000)
                 {
                     return 7;
                 }
-                if (n > 100000)
+                if (n >= 100000)
                 {
                     return 6;
                 }
             }
             else
             {
-                if (n > 10000)
+                if (n >= 10000)
                 {
                     return 5;
                 }
-                if (n > 1000)
+                if (n >= 1000)
                 {
                     return 4;
                 }
-                if (n > 100)
+                if (n >= 100)
                 {
                     return 3;
                 }
-                if (n > 10)
+                if (n >= 10)
                 {
                     return 2;
                 }
-                if (n > 0)
+                if (n >= 0)
                 {
                     return 1;
                 }
@@ -69,44 +69,44 @@ namespace HotChocolate.Execution
             }
 
             // Max length for ulong is 20
-            if (n > 1000000000000000)
+            if (n >= 1000000000000000)
             {
-                if (n > 10000000000000000000)
+                if (n >= 10000000000000000000)
                 {
                     return 20;
                 }
-                if (n > 1000000000000000000)
+                if (n >= 1000000000000000000)
                 {
                     return 19;
                 }
-                if (n > 100000000000000000)
+                if (n >= 100000000000000000)
                 {
                     return 18;
                 }
-                if (n > 10000000000000000)
+                if (n >= 10000000000000000)
                 {
                     return 17;
                 }
-                if (n > 1000000000000000)
+                if (n >= 1000000000000000)
                 {
                     return 16;
                 }
             }
             else
             {
-                if (n > 100000000000000)
+                if (n >= 100000000000000)
                 {
                     return 15;
                 }
-                if (n > 10000000000000)
+                if (n >= 10000000000000)
                 {
                     return 14;
                 }
-                if (n > 1000000000000)
+                if (n >= 1000000000000)
                 {
                     return 13;
                 }
-                if (n > 100000000000)
+                if (n >= 100000000000)
                 {
                     return 12;
                 }
