744b1d812cbc76d60dc83177208a5c44fc7e929a
Michael Staib <michael.staib@hotmail.com>
2019-01-24 14:40:38 +0100

    Added query request tests

--- src/Core/Core/Execution/QueryRequest.cs
+++ src/Core/Core/Execution/QueryRequest.cs
@@ -9,21 +9,16 @@ namespace HotChocolate.Execution
         {
             if (string.IsNullOrEmpty(query))
             {
-                throw new ArgumentException("message", nameof(query));
+                throw new ArgumentException(
+                    "The query cannot be null or empty.",
+                    nameof(query));
             }
             Query = query;
         }
 
         public QueryRequest(string query, string operationName)
+            : this(query)
         {
-            if (string.IsNullOrEmpty(query))
-            {
-                throw new ArgumentException(
-                    "The query cannot be null or empty.",
-                    nameof(query));
-            }
-
-            Query = query;
             OperationName = operationName;
         }
 
