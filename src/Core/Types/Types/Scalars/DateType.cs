bd91bf386b533cf397648ec53b13a9ab894f143f
Michael Staib <michael.staib@hotmail.com>
2019-01-28 16:04:40 +0100

    Fixed DateTime parsing

--- src/Core/Types/Types/Scalars/DateType.cs
+++ src/Core/Types/Types/Scalars/DateType.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using HotChocolate.Language;
 
 namespace HotChocolate.Types
@@ -18,18 +19,22 @@ namespace HotChocolate.Types
 
         protected override string Serialize(DateTime value)
         {
-            return value.ToString("yyyy-MM-dd");
+            return value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
         }
 
         protected override string Serialize(DateTimeOffset value)
         {
-            return value.ToString("yyyy-MM-dd");
+            return value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
         }
 
         protected override bool TryParseLiteral(
             StringValueNode literal, out object obj)
         {
-            if (DateTime.TryParse(literal.Value, out DateTime dateTime))
+            if (DateTime.TryParse(
+                literal.Value,
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out DateTime dateTime))
             {
                 obj = dateTime;
                 return true;
