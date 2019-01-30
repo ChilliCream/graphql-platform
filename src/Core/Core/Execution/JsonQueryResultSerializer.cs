222468386cb573a932f666909c5357afbf74169f
Michael Staib <michael.staib@hotmail.com>
2019-01-30 21:40:04 +0100

    Fixed error serialization

--- src/Core/Core/Execution/JsonQueryResultSerializer.cs
+++ src/Core/Core/Execution/JsonQueryResultSerializer.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -11,6 +12,10 @@ namespace HotChocolate.Execution
         private const string _data = "data";
         private const string _errors = "errors";
         private const string _extensions = "extensions";
+        private const string _message = "message";
+        private const string _locations = "locations";
+        private const string _path = "path";
+
 
         public async Task SerializeAsync(
             IReadOnlyQueryResult result,
@@ -20,7 +25,7 @@ namespace HotChocolate.Execution
 
             if (result.Errors.Count > 0)
             {
-                formatted[_errors] = result.Errors;
+                formatted[_errors] = SerializeErrors(result.Errors);
             }
 
             if (result.Data.Count > 0)
@@ -38,6 +43,37 @@ namespace HotChocolate.Execution
 
             await stream.WriteAsync(buffer, 0, buffer.Length)
                 .ConfigureAwait(false);
+        }
+
+        private ICollection<object> SerializeErrors(
+            IReadOnlyCollection<IError> errors)
+        {
+            var formattedErrors = new List<object>();
+
+            foreach (IError error in errors)
+            {
+                var formattedError = new OrderedDictionary();
+                formattedError[_message] = error.Message;
+
+                if (error.Locations != null && error.Locations.Count > 0)
+                {
+                    formattedError[_locations] = error.Locations;
+                }
+
+                if (error.Path != null && error.Path.Count > 0)
+                {
+                    formattedError[_path] = error.Path;
+                }
+
+                if (error.Extensions != null && error.Extensions.Count > 0)
+                {
+                    formattedError[_extensions] = error.Extensions;
+                }
+
+                formattedErrors.Add(formattedError);
+            }
+
+            return formattedErrors;
         }
     }
 }
