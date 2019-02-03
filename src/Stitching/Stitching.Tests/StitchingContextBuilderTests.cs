9067868a7fbe3d63f07cfeac3cef18f1a4a3139b
Michael Staib <michael.staib@hotmail.com>
2019-02-03 21:10:40 +0100

    Added stitching context builder tests

--- src/Stitching/Stitching.Tests/StitchingContextBuilderTests.cs
+++ src/Stitching/Stitching.Tests/StitchingContextBuilderTests.cs
@@ -1,6 +1,8 @@
 using System;
 using ChilliCream.Testing;
+using HotChocolate.Execution;
 using HotChocolate.Types;
+using Moq;
 using Xunit;
 
 namespace HotChocolate.Stitching
@@ -26,6 +28,74 @@ namespace HotChocolate.Stitching
             Assert.NotNull(context.GetRemoteQueryClient("Contract"));
             Assert.Throws<ArgumentException>(
                 () => context.GetRemoteQueryClient("Foo"));
+        }
+
+        [Fact]
+        public void Build_NoExecutors_InvalidOperationException()
+        {
+            // arrange
+            // act
+            Action action = () => StitchingContextBuilder.New().Build();
+
+            // assert
+            Assert.Throws<InvalidOperationException>(action);
+        }
+
+        [Fact]
+        public void AddExecuter_1_RemoteExecutorBuilderNull_ArgNullException()
+        {
+            // arrange
+            // act
+            Action action = () => StitchingContextBuilder.New()
+                .AddExecutor(default(RemoteExecutorBuilder));
+
+            // assert
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [Fact]
+        public void AddExecuter_2_SchemaNullNull_ArgumentException()
+        {
+            // arrange
+            var executor = new Mock<IQueryExecutor>();
+
+            // act
+            Action action = () => StitchingContextBuilder.New()
+                .AddExecutor(null, executor.Object);
+
+            // assert
+            Assert.Throws<ArgumentException>(action);
+        }
+
+        [Fact]
+        public void AddExecuter_2_ExecutorNull_ArgumentNullException()
+        {
+            // arrange
+            var executor = new Mock<IQueryExecutor>();
+
+            // act
+            Action action = () => StitchingContextBuilder.New()
+                .AddExecutor(null, executor.Object);
+
+            // assert
+            Assert.Throws<ArgumentException>(action);
+        }
+
+        [Fact]
+        public void AddExecuter_3_BuilderNull_ArgumentNullException()
+        {
+            // arrange
+            var executor = new Mock<IQueryExecutor>();
+
+            // act
+            Action action = () => StitchingContextBuilder.New()
+                .AddExecutor
+                (
+                    default(Func<RemoteExecutorBuilder, RemoteExecutorBuilder>)
+                );
+
+            // assert
+            Assert.Throws<ArgumentNullException>(action);
         }
     }
 }
