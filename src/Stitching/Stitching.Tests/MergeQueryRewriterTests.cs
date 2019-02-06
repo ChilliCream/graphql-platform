57b1e88436a45c4975c49f704e8788d374d7e9f3
Michael Staib <michael.staib@hotmail.com>
2019-02-06 14:21:40 +0100

    Added more tests

--- src/Stitching/Stitching.Tests/MergeQueryRewriterTests.cs
+++ src/Stitching/Stitching.Tests/MergeQueryRewriterTests.cs
@@ -25,5 +25,43 @@ namespace HotChocolate.Stitching
             // assert
             QuerySyntaxSerializer.Serialize(document).Snapshot();
         }
+
+        [Fact]
+        public void QueryWithPrivateVariables()
+        {
+            // arrange
+            DocumentNode query_a = Parser.Default.Parse(
+                FileResource.Open("StitchingQueryWithUnion.graphql"));
+            DocumentNode query_b = Parser.Default.Parse(
+                FileResource.Open("StitchingQueryWithVariables.graphql"));
+
+            // act
+            var rewriter = new MergeQueryRewriter(Array.Empty<string>());
+            rewriter.AddQuery(query_a, "_a");
+            rewriter.AddQuery(query_b, "_b");
+            DocumentNode document = rewriter.Merge();
+
+            // assert
+            QuerySyntaxSerializer.Serialize(document).Snapshot();
+        }
+
+        [Fact]
+        public void QueryWithGlobalVariables()
+        {
+            // arrange
+            DocumentNode query_a = Parser.Default.Parse(
+                FileResource.Open("MergeQueryWithVariable.graphql"));
+            DocumentNode query_b = Parser.Default.Parse(
+                FileResource.Open("MergeQueryWithVariable.graphql"));
+
+            // act
+            var rewriter = new MergeQueryRewriter(Array.Empty<string>());
+            rewriter.AddQuery(query_a, "_a");
+            rewriter.AddQuery(query_b, "_b");
+            DocumentNode document = rewriter.Merge();
+
+            // assert
+            QuerySyntaxSerializer.Serialize(document).Snapshot();
+        }
     }
 }
