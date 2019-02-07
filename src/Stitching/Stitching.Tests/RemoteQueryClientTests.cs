50719bb944672d729bf96354ccb7865dc6049020
Michael Staib <michael.staib@hotmail.com>
2019-02-07 17:07:24 +0100

    added more tests

--- src/Stitching/Stitching.Tests/RemoteQueryClientTests.cs
+++ src/Stitching/Stitching.Tests/RemoteQueryClientTests.cs
@@ -142,6 +142,89 @@ namespace HotChocolate.Stitching
         }
 
         [Fact]
+        public async Task DispatchMultipleQueriesWithGlobalError()
+        {
+            // arrange
+            string query = null;
+            int count = 0;
+
+            var result = new QueryResult();
+            result.Data["__0__a"] = "a";
+            result.Data["__1__a"] = "b";
+            result.Data["__1__b"] = "c";
+            result.Errors.Add(ErrorBuilder.New()
+                .SetMessage("foo")
+                .Build());
+
+            var executor = new Mock<IQueryExecutor>();
+            executor.Setup(t => t.ExecuteAsync(
+                It.IsAny<IReadOnlyQueryRequest>(),
+                It.IsAny<CancellationToken>()))
+                .Returns(new Func<IReadOnlyQueryRequest,
+                    CancellationToken, Task<IExecutionResult>>((r, ct) =>
+                {
+                    count++;
+                    query = r.Query;
+                    return Task.FromResult<IExecutionResult>(result);
+                }));
+
+            var request_a = new QueryRequest("query a { a }");
+            var request_b = new QueryRequest("query b { a b }");
+
+            var client = new RemoteQueryClient(
+                new EmptyServiceProvider(),
+                executor.Object);
+
+            // act
+            Task<IExecutionResult> task_a = client.ExecuteAsync(request_a);
+            Task<IExecutionResult> task_b = client.ExecuteAsync(request_b);
+            await client.DispatchAsync(CancellationToken.None);
+
+            // assert
+            Assert.Equal(1, count);
+
+            IExecutionResult result_a = await task_a;
+            result_a.Snapshot("DispatchMultipleQueriesWithGlobalError_A");
+
+            IExecutionResult result_b = await task_b;
+            result_b.Snapshot("DispatchMultipleQueriesWithGlobalError_B");
+        }
+
+        [Fact]
+        public async Task DispatchMultipleQueriesWithGlobalException()
+        {
+            // arrange
+            var executor = new Mock<IQueryExecutor>();
+            executor.Setup(t => t.ExecuteAsync(
+                It.IsAny<IReadOnlyQueryRequest>(),
+                It.IsAny<CancellationToken>()))
+                .Returns(new Func<IReadOnlyQueryRequest,
+                    CancellationToken, Task<IExecutionResult>>((r, ct) =>
+                {
+                    return Task.FromException<IExecutionResult>(
+                        new Exception("foo"));
+                }));
+
+            var request_a = new QueryRequest("query a { a }");
+            var request_b = new QueryRequest("query b { a b }");
+
+            var client = new RemoteQueryClient(
+                new EmptyServiceProvider(),
+                executor.Object);
+
+            // act
+            Task<IExecutionResult> task_a = client.ExecuteAsync(request_a);
+            Task<IExecutionResult> task_b = client.ExecuteAsync(request_b);
+            await client.DispatchAsync(CancellationToken.None);
+
+            // assert
+            Assert.Equal("foo",
+                (await Assert.ThrowsAsync<Exception>(() => task_a)).Message);
+            Assert.Equal("foo",
+                (await Assert.ThrowsAsync<Exception>(() => task_b)).Message);
+        }
+
+        [Fact]
         public async Task DispatchMultipleQueriesWithVariables()
         {
             // arrange
