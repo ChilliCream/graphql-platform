e4d071843041d79619b18431d8e67eb1c7b1cf41
Michael Staib <michael.staib@hotmail.com>
2019-02-10 21:10:17 +0100

    Added more details

--- src/Stitching/Stitching/Utilities/MergeSchema.cs
+++ src/Stitching/Stitching/Utilities/MergeSchema.cs
@@ -1,13 +1,10 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using HotChocolate.Language;
 
 namespace HotChocolate.Stitching
 {
-    internal class SchemaMerger
-    {
-
-    }
-
     public interface ISchemaInfo
     {
         string Name { get; }
@@ -23,7 +20,7 @@ namespace HotChocolate.Stitching
         string SchemaName { get; }
     }
 
-    public interface ITypeMergerContext
+    public interface IMergeSchemaContext
     {
         void AddType(ITypeDefinitionNode type);
     }
@@ -31,7 +28,66 @@ namespace HotChocolate.Stitching
     public interface ITypeMerger
     {
         void Merge(
-            ITypeMergerContext context,
+            IMergeSchemaContext context,
             IReadOnlyList<ITypeInfo> types);
     }
+
+    delegate MergeTypeDelegate MergeTypeFactory(MergeTypeDelegate next);
+    delegate void MergeTypeDelegate(IMergeSchemaContext context, IReadOnlyList<ITypeInfo> types);
+
+
+    public class MergeEnumType
+        : ITypeMerger
+    {
+        private readonly MergeTypeDelegate _next;
+
+        public MergeEnumType(MergeTypeDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public void Merge(
+            IMergeSchemaContext context,
+            IReadOnlyList<ITypeInfo> types)
+        {
+            if (types.All(t => t.Definition is EnumTypeDefinitionNode))
+            {
+                var first = (EnumTypeDefinitionNode)types[0].Definition;
+                var values = new HashSet<string>(
+                    first.Values.Select(t => t.Name.Value));
+
+                for (int i = 0; i < types.Count; i++)
+                {
+                    if (AreEqual(values,
+                        (EnumTypeDefinitionNode)types[i].Definition))
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                _next.Invoke(context, types);
+            }
+        }
+
+        private bool AreEqual(
+            ISet<string> left,
+            EnumTypeDefinitionNode right)
+        {
+            if (left.Count == right.Values.Count)
+            {
+                for (int i = 0; i < right.Values.Count; i++)
+                {
+                    if (!left.Contains(right.Values[i].Name.Value))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+
 }
