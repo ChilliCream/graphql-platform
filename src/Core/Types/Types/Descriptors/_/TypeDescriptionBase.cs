2dc1ae236adac724b5fa1da4a4d5db0d65a5f8ef
Michael Staib <michael.staib@hotmail.com>
2019-02-21 09:53:58 +0100

    Reworked the type description base

---
+++ src/Core/Types/Types/Descriptors/_/TypeDescriptionBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace HotChocolate.Types
+{
+    public class TypeDescriptionBase
+    {
+        protected TypeDescriptionBase() { }
+
+        /// <summary>
+        /// Gets or sets the name the type shall have.
+        /// </summary>
+        public NameString Name { get; set; }
+
+        // <summary>
+        /// Gets or sets the description the type shall have.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets the list of directives the type shall be annotated with.
+        /// </summary>
+        public IList<DirectiveDescription> Directives { get; } =
+            new List<DirectiveDescription>();
+
+        /// <summary>
+        /// Get access to context data that are copied to the type
+        /// and can be used for customizations.
+        /// </summary>
+        public IDictionary<string, object> ContextData { get; } =
+            new Dictionary<string, object>();
+    }
+}
