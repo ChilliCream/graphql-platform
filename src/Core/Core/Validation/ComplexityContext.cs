e6010e74f9e1e3e70ac2311e9916bd8974acbb8e
Michael Staib <michael.staib@hotmail.com>
2019-03-06 21:09:14 +0100

    Exposed the default complexity calculation

---
+++ src/Core/Core/Validation/ComplexityContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using HotChocolate.Execution;
+using HotChocolate.Language;
+using HotChocolate.Types;
+
+namespace HotChocolate.Validation
+{
+    public readonly struct ComplexityContext
+    {
+        internal ComplexityContext(
+            IOutputField fieldDefinition,
+            FieldNode fieldSelection,
+            ICollection<IOutputField> path,
+            IVariableCollection variables,
+            CostDirective cost)
+        {
+            FieldDefinition = fieldDefinition;
+            FieldSelection = fieldSelection;
+            Path = path;
+            Variables = variables;
+            Cost = cost;
+        }
+
+        public IOutputField FieldDefinition { get; }
+        public FieldNode FieldSelection { get; }
+        public ICollection<IOutputField> Path { get; }
+        public IVariableCollection Variables { get; }
+        public CostDirective Cost { get; }
+    }
+}
