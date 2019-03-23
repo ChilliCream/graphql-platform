fa536e414b0f69fa8a502161d93d3a1f7c7b6f58
Michael Staib <michael.staib@hotmail.com>
2019-03-23 08:34:43 +0100

    Added multiline description test for schema first

---
+++ src/Core/Types.Tests/SchemaFirstTests.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using ChilliCream.Testing;
+using HotChocolate.Execution;
+using HotChocolate.Types;
+using Snapshooter.Xunit;
+using Xunit;
+
+namespace HotChocolate
+{
+    public class SchemaFirstTests
+    {
+         [Fact]
+        public async Task CommentsAreCorrectlyRead()
+        {
+            // arrange
+            string source = FileResource.Open(
+                "schema_with_multiline_descriptions.graphql");
+            string query = FileResource.Open(
+                "IntrospectionQuery.graphql");
+
+            // act
+            ISchema schema = Schema.Create(
+                source,
+                c =>
+                {
+                    c.Use(next => context => next(context));
+                });
+
+            // assert
+            IQueryExecutor executor = schema.MakeExecutable();
+            IExecutionResult result =  await executor.ExecuteAsync(query);
+            result.MatchSnapshot();
+        }
+    }
+}
