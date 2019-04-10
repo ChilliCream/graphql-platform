b84dd5101c6c4185faa4cde39c887f045630824f
Michael Staib <michael.staib@hotmail.com>
2019-04-10 12:39:55 +0200

    Added tests comment that specifies the tests that we still need

--- src/Core/Types.Tests/Types/ObjectTypeExtensionTests.cs
+++ src/Core/Types.Tests/Types/ObjectTypeExtensionTests.cs
@@ -20,6 +20,24 @@ namespace HotChocolate.Types
             Assert.True(type.Fields.ContainsField("test"));
         }
 
+        // TODO : ADD THE FOLLOWING TESTS:
+        // Overwrite reslver
+        // Add Middleware
+        // Depricate Field
+        // Depricate Argument
+        // Add Context Data to Type
+        // Add Context Data to Field
+        // Add Context Data to Argument
+        // Add Directive to Type
+        // Add Directive to Field
+        // Add Directive to Argument
+        // Replace Directive to Type
+        // Replace Directive to Field
+        // Replace Directive to Argument
+        // Add Repeatable Directive to Type
+        // Add Repeatable Directive to Field
+        // Add Repeatable Directive to Argument
+
         public class FooType
             : ObjectType<Foo>
         {
