c4e9f1567277586084c457051a52e93892ce5fa0
Michael Staib <michael.staib@hotmail.com>
2019-04-28 15:06:53 +0200

    Added more schema parser tests

---
+++ src/Core/Language.Tests/Utf8/Utf8DirectiveParserTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace HotChocolate.Language
+{
+    public class Utf8DirectiveParserTests
+    {
+        [Fact]
+        public void ParseUniqueDirective()
+        {
+            // arrange
+            var text = "directive @skip(if: Boolean!) " +
+                "on FIELD | FRAGMENT_SPREAD | INLINE_FRAGMENT";
+            var parser = new Utf8GraphQLParser(Encoding.UTF8.GetBytes(text));
+
+            // assert
+            DocumentNode document = parser.Parse();
+
+            // assert
+            DirectiveDefinitionNode directiveDefinition = document.Definitions
+                .OfType<DirectiveDefinitionNode>().FirstOrDefault();
+            Assert.NotNull(directiveDefinition);
+            Assert.False(directiveDefinition.IsRepeatable);
+        }
+
+        [Fact]
+        public void ParseRepeatableDirective()
+        {
+            // arrange
+            var text = "directive @skip(if: Boolean!) repeatable " +
+                "on FIELD | FRAGMENT_SPREAD | INLINE_FRAGMENT";
+            var parser = new Utf8GraphQLParser(Encoding.UTF8.GetBytes(text));
+
+            // assert
+            DocumentNode document = parser.Parse();
+
+            // assert
+            DirectiveDefinitionNode directiveDefinition = document.Definitions
+                .OfType<DirectiveDefinitionNode>().FirstOrDefault();
+            Assert.NotNull(directiveDefinition);
+            Assert.True(directiveDefinition.IsRepeatable);
+        }
+
+        [Fact]
+        public void DirectiveOrderIsSignificant()
+        {
+            // arrange
+            var text = "type Query { field: String @a @b @c }";
+            var parser = new Utf8GraphQLParser(Encoding.UTF8.GetBytes(text));
+
+            // assert
+            DocumentNode document = parser.Parse();
+
+            // assert
+            ObjectTypeDefinitionNode type = document.Definitions
+                .OfType<ObjectTypeDefinitionNode>().FirstOrDefault();
+            Assert.Collection(type.Fields.Single().Directives,
+                t => Assert.Equal("a", t.Name.Value),
+                t => Assert.Equal("b", t.Name.Value),
+                t => Assert.Equal("c", t.Name.Value));
+        }
+    }
+}
---
+++ src/Core/Language.Tests/Utf8/Utf8SchemaParserTests.cs
@@ -0,0 +1,127 @@
+﻿using System.Text;
+using Snapshooter.Xunit;
+using Xunit;
+
+namespace HotChocolate.Language
+{
+    public class Utf8SchemaParserTests
+    {
+        [Fact]
+        public void ParserSimpleObjectType()
+        {
+            // arrange
+            string sourceText = "type a { b: String c: Int }";
+            var parser = new Utf8GraphQLParser(
+                Encoding.UTF8.GetBytes(sourceText));
+
+            // act
+            DocumentNode document = parser.Parse();
+
+            // assert
+            Assert.Collection(document.Definitions,
+                t =>
+                {
+                    Assert.IsType<ObjectTypeDefinitionNode>(t);
+                    var objectTypeDefinition = (ObjectTypeDefinitionNode)t;
+                    Assert.Equal(NodeKind.ObjectTypeDefinition, objectTypeDefinition.Kind);
+                    Assert.Equal("a", objectTypeDefinition.Name.Value);
+                    Assert.Collection(objectTypeDefinition.Fields,
+                        f =>
+                        {
+                            Assert.Equal("b", f.Name.Value);
+                            Assert.IsType<NamedTypeNode>(f.Type);
+                            Assert.Equal("String", ((NamedTypeNode)f.Type).Name.Value);
+                        },
+                        f =>
+                        {
+                            Assert.Equal("c", f.Name.Value);
+                            Assert.IsType<NamedTypeNode>(f.Type);
+                            Assert.Equal("Int", ((NamedTypeNode)f.Type).Name.Value);
+                        });
+                });
+        }
+
+        [Fact]
+        public void ParserSimpleInterfaceType()
+        {
+            // arrange
+            string sourceText = "interface a { b: String c: Int }";
+            var parser = new Utf8GraphQLParser(
+                Encoding.UTF8.GetBytes(sourceText));
+
+            // act
+            DocumentNode document = parser.Parse();
+
+            // assert
+            Assert.Collection(document.Definitions,
+                t =>
+                {
+                    Assert.IsType<InterfaceTypeDefinitionNode>(t);
+                    var objectTypeDefinition = (InterfaceTypeDefinitionNode)t;
+                    Assert.Equal(NodeKind.InterfaceTypeDefinition, objectTypeDefinition.Kind);
+                    Assert.Equal("a", objectTypeDefinition.Name.Value);
+                    Assert.Collection(objectTypeDefinition.Fields,
+                        f =>
+                        {
+                            Assert.Equal("b", f.Name.Value);
+                            Assert.IsType<NamedTypeNode>(f.Type);
+                            Assert.Equal("String", ((NamedTypeNode)f.Type).Name.Value);
+                        },
+                        f =>
+                        {
+                            Assert.Equal("c", f.Name.Value);
+                            Assert.IsType<NamedTypeNode>(f.Type);
+                            Assert.Equal("Int", ((NamedTypeNode)f.Type).Name.Value);
+                        });
+                });
+        }
+
+        [Fact]
+        public void ParseEnum()
+        {
+            // arrange
+            string sourceText = "enum Foo @foo(a: \"123\") "
+                + "{ BAR @foo(a: 123) , BAZ }";
+            var parser = new Utf8GraphQLParser(
+                Encoding.UTF8.GetBytes(sourceText));
+
+            // act
+            DocumentNode document = parser.Parse();
+
+            // assert
+            SchemaSyntaxSerializer.Serialize(document).MatchSnapshot();
+        }
+
+        [Fact]
+        public void ParseUnion()
+        {
+            // arrange
+            string sourceText = "union Foo @foo(a: \"123\") = "
+                + "BAR | BAZ ";
+            var parser = new Utf8GraphQLParser(
+                Encoding.UTF8.GetBytes(sourceText));
+
+            // act
+            DocumentNode document = parser.Parse();
+
+            // assert
+            SchemaSyntaxSerializer.Serialize(document).MatchSnapshot();
+        }
+
+        [Fact]
+        public void ParseUnion_LeadingPipe()
+        {
+            // arrange
+            string sourceText = "union Foo @foo(a: \"123\") = "
+                + "| BAR | BAZ ";
+            var parser = new Utf8GraphQLParser(
+                Encoding.UTF8.GetBytes(sourceText));
+
+            // act
+            DocumentNode document = parser.Parse();
+
+            // assert
+            SchemaSyntaxSerializer.Serialize(document).MatchSnapshot();
+        }
+    }
+}
