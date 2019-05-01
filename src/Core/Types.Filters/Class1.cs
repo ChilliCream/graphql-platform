205ca7aa0deff6f492f389cb81f91ac24f845838
Michael Staib <michael.staib@hotmail.com>
2019-05-01 15:43:53 +0100

    Added filter descriptor

---
+++ src/Core/Types.Filters/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+using HotChocolate.Types.Descriptors.Definitions;
+
+namespace HotChocolate.Types.Filters
+{
+    public interface IFilterDescriptor<T>
+        : IDescriptor<InputObjectTypeDefinition>
+        , IFluent
+    {
+        IStringFilterFieldsDescriptor Filter(Expression<Func<T, string>> propertyOrMethod);
+    }
+
+    public interface IStringFilterFieldsDescriptor
+    {
+        IStringFilterFieldsDescriptor BindFilters(BindingBehavior bindingBehavior);
+        IStringFilterFieldsDescriptor AllowContains();
+        IStringFilterFieldsDescriptor AllowEquals();
+        IStringFilterFieldsDescriptor AllowIn();
+    }
+}
