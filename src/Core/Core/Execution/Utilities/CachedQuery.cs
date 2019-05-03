476deb0537f27ed0251c217741e366f612cf1610
Michael Staib <michael.staib@hotmail.com>
2019-05-03 13:10:45 +0200

    Introduced ChachedQuery

--- src/Core/Core/Execution/Utilities/CachedQuery.cs
+++ src/Core/Core/Execution/Utilities/CachedQuery.cs
@@ -7,8 +7,29 @@ using HotChocolate.Types;
 
 namespace HotChocolate.Execution
 {
-    internal sealed class CachedQuery
+    internal sealed partial class CachedQuery
     {
+        private ConcurrentDictionary<Key, IReadOnlyList<FieldSelection>> _flds =
+            new ConcurrentDictionary<Key, IReadOnlyList<FieldSelection>>();
+
+        public CachedQuery(string query, DocumentNode document)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                throw new ArgumentException(
+                    "The query mustn't be null or empty.",
+                    nameof(query));
+            }
+
+            if (document == null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            Query = query;
+            Document = document;
+        }
+
         public string Query { get; }
 
         public DocumentNode Document { get; }
@@ -18,15 +39,13 @@ namespace HotChocolate.Execution
             ObjectType type,
             Func<IReadOnlyList<FieldSelection>> collectFields)
         {
-            ConcurrentDictionary<string, string> s;
-            throw new NotImplementedException();
-        }
-
-        public FieldDelegate GetOrCreateMiddleware(
-            FieldSelection fieldSelection,
-            Func<FieldDelegate> createMiddleware)
-        {
-            throw new NotImplementedException();
+            var key = new Key(selectionSet, type);
+            if (!_flds.TryGetValue(key, out IReadOnlyList<FieldSelection> flds))
+            {
+                flds = collectFields();
+                _flds.TryAdd(key, flds);
+            }
+            return flds;
         }
     }
 }
