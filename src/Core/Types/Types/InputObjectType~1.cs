e0e3d57d8ec1f5ac55ebf64cfaebd15486746e7a
Billy Wolfington <willwolfram18@gmail.com>
2019-05-07 00:12:38 -0700

    Input object types use XML docstrings

--- src/Core/Types/Types/InputObjectType~1.cs
+++ src/Core/Types/Types/InputObjectType~1.cs
@@ -29,7 +29,11 @@ namespace HotChocolate.Types
             var descriptor =
                 InputObjectTypeDescriptor.New<T>(
                     DescriptorContext.Create(context.Services));
+
+            descriptor.Description(typeof(T).GetXmlSummary());
+
             _configure(descriptor);
+
             return descriptor.CreateDefinition();
         }
 
