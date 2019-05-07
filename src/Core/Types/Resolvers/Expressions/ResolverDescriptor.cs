a42877ffd9bbe2acce8fdcb5f948321b9de50f5b
Michael Staib <michael@chillicream.com>
2019-05-07 08:36:05 -0700

    Expression Resolver Compiler (#720)

---
+++ src/Core/Types/Resolvers/Expressions/ResolverDescriptor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace HotChocolate.Resolvers.Expressions
+{
+    /// <summary>
+    /// Describes a resolver that is based on a resolver type.
+    /// </summary>
+    internal class ResolverDescriptor
+    {
+        public ResolverDescriptor(
+            Type resolverType,
+            Type sourceType,
+            FieldMember field)
+        {
+            ResolverType = resolverType
+                ?? throw new ArgumentNullException(nameof(resolverType));
+            SourceType = sourceType
+                ?? throw new ArgumentNullException(nameof(sourceType));
+            Field = field
+                ?? throw new ArgumentNullException(nameof(field));
+        }
+
+        public ResolverDescriptor(
+            Type sourceType,
+            FieldMember field)
+        {
+            SourceType = sourceType
+                ?? throw new ArgumentNullException(nameof(sourceType));
+            Field = field
+                ?? throw new ArgumentNullException(nameof(field));
+        }
+
+        public Type ResolverType { get; }
+
+        public Type SourceType { get; }
+
+        public FieldMember Field { get; }
+    }
+}
