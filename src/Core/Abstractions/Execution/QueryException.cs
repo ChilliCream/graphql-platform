b42807cf1e3fa0ae865edf89bc2800523aa7ce52
Michael Staib <michael.staib@hotmail.com>
2019-05-14 11:51:01 +0200

    Updpated Snapshots

--- src/Core/Abstractions/Execution/QueryException.cs
+++ src/Core/Abstractions/Execution/QueryException.cs
@@ -14,6 +14,7 @@ namespace HotChocolate.Execution
         }
 
         public QueryException(IError error)
+            : base(error.Message)
         {
             if (error == null)
             {
