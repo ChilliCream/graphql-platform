4841c7d0c592bb658e066ae8aeffeee6fe0a136e
Michael Staib <michael.staib@hotmail.com>
2019-05-21 19:04:09 +0200

    adjusted error codes

--- src/Core/Types/TypeErrorCodes.cs
+++ src/Core/Types/TypeErrorCodes.cs
@@ -2,11 +2,11 @@ namespace HotChocolate
 {
     internal static class TypeErrorCodes
     {
-        public const string NoEnumValues = "TS_ENUM_NO_VALUES";
-        public const string MissingType = "TS_TYPE_MISSING";
-        public const string NoResolver = "TS_FIELD_NO_RESOLVER";
-        public const string UnresolvedTypes = "TS_CANNOT_RESOLVE_TYPES";
+        public const string NoEnumValues = "TS_NO_ENUM_VALUES";
+        public const string MissingType = "TS_MISSING_TYPE";
+        public const string NoResolver = "TS_NO_FIELD_RESOLVER";
+        public const string UnresolvedTypes = "TS_UNRESOLVED_TYPES";
         public const string NoName = "TS_NO_NAME_DEFINED";
-        public const string NoFieldType = "TS_FIELD_NO_TYPE";
+        public const string NoFieldType = "TS_NO_FIELD_TYPE";
     }
 }
