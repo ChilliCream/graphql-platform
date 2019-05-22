8f1cfbf3a5ccc88313cd5e31b6903d05db079e2d
Michael Staib <michael.staib@hotmail.com>
2019-05-22 21:43:45 +0200

    started removing the httpcontext dependency

--- src/Server/AspNetCore/IOwinContextAccessor.cs
+++ src/Server/AspNetCore/IOwinContextAccessor.cs
@@ -5,21 +5,49 @@ using System.Linq;
 using System.Security.Principal;
 using System.Threading;
 using Microsoft.Owin;
-using HttpContext = Microsoft.Owin.IOwinContext;
 
 namespace AspNetCore
 {
     public interface IOwinContextAccessor
     {
-        Microsoft.Owin.IOwinContext OwinContext { get; }
+        IOwinContext OwinContext { get; }
     }
 
     public class OwinContextAccessor
         : IOwinContextAccessor
     {
-        private static Async
+        private static AsyncLocal<OwinContextHolder> _httpContextCurrent =
+            new AsyncLocal<OwinContextHolder>();
 
-        Microsoft.Owin.IOwinContext OwinContext { get; }
+        public HttpContext HttpContext
+        {
+            get
+            {
+                return  _httpContextCurrent.Value?.Context;
+            }
+            set
+            {
+                var holder = _httpContextCurrent.Value;
+
+                if (holder != null)
+                {
+                    holder.Context = null;
+                }
+
+                if (value != null)
+                {
+                    _httpContextCurrent.Value = new HttpContextHolder
+                    {
+                        Context = value
+                    };
+                }
+            }
+        }
+
+        private class OwinContextHolder
+        {
+            public IOwinContext Context;
+        }
     }
 }
 #endif
