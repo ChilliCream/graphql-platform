223b567f8d9ef34211adaadc407d1aad64302a66
Pascal Senn (GIAPSE) <pascal.senn@gia.ch>
2019-06-14 23:42:51 +0200

    test(Types.Filters): add test for explicit

--- src/Core/Types.Filters.Tests/ComparableFilterInputTypeTests.cs
+++ src/Core/Types.Filters.Tests/ComparableFilterInputTypeTests.cs
@@ -18,6 +18,35 @@ namespace HotChocolate.Types.Filters
             schema.ToString().MatchSnapshot();
         }
 
+        /// <summary>
+        /// This test checks if the binding of all allow methods are correct
+        /// </summary>
+        [Fact]
+        public void Create_Explitcit_Filters()
+        {
+            // arrange
+            // act
+            var schema = CreateSchema(new FilterInputType<Foo>(descriptor =>
+            {
+                descriptor.Filter(x => x.BarShort).BindExplicitly().AllowEquals()
+                .And().AllowNotEquals()
+                .And().AllowIn()
+                .And().AllowNotIn()
+                .And().AllowGreaterThan()
+                .And().AllowNotGreaterThan()
+                .And().AllowGreaterThanOrEquals()
+                .And().AllowNotGreaterThanOrEquals()
+                .And().AllowLowerThan()
+                .And().AllowNotLowerThan()
+                .And().AllowLowerThanOrEquals()
+                .And().AllowNotLowerThanOrEquals();
+
+            }));
+
+            // assert
+            schema.ToString().MatchSnapshot();
+        }
+
 
         public class Foo
         {
@@ -35,7 +64,7 @@ namespace HotChocolate.Types.Filters
             protected override void Configure(
                 IFilterInputTypeDescriptor<Foo> descriptor)
             {
-                descriptor.Filter(x => x.BarShort).BindImplicitly();
+                descriptor.Filter(x => x.BarShort);
                 descriptor.Filter(x => x.BarInt);
                 descriptor.Filter(x => x.BarLong);
                 descriptor.Filter(x => x.BarFloat);
