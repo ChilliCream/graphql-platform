b3c246fca151a61c38efc5e053ed2eb541ff0500
Michael Staib <michael.staib@hotmail.com>
2019-06-18 11:51:05 +0200

    work in progress

--- src/Core/Types.Filters/FilterVisitorBase.cs
+++ src/Core/Types.Filters/FilterVisitorBase.cs
@@ -7,6 +7,15 @@ namespace HotChocolate.Types.Filters
     public class FilterVisitorBase
         : SyntaxNodeVisitor
     {
+        protected FilterVisitorBase(InputObjectType initialType)
+        {
+            if (initialType is null)
+            {
+                throw new ArgumentNullException(nameof(initialType));
+            }
+            Types.Push(initialType);
+        }
+
         protected Stack<IType> Types { get; } =
             new Stack<IType>();
 
@@ -25,6 +34,7 @@ namespace HotChocolate.Types.Filters
                     out IInputField field))
                 {
                     Operations.Push(field);
+                    Types.Push(field.Type);
                     return VisitorAction.Continue;
                 }
 
@@ -38,12 +48,89 @@ namespace HotChocolate.Types.Filters
             }
         }
 
+        public override VisitorAction Leave(
+            ObjectFieldNode node,
+            ISyntaxNode parent,
+            IReadOnlyList<object> path,
+            IReadOnlyList<ISyntaxNode> ancestors)
+        {
+            Operations.Pop();
+            Types.Pop();
+            return VisitorAction.Continue;
+        }
+    }
+
+    public class DummyFilter
+        : FilterVisitorBase
+    {
+        public DummyFilter()
+
+        {
+
+        }
+
+        protected Stack<Stack<string>> Level { get; } =
+            new Stack<Stack<String>>();
+
+        public override VisitorAction Enter(
+            ObjectValueNode node,
+            ISyntaxNode parent,
+            IReadOnlyList<object> path,
+            IReadOnlyList<ISyntaxNode> ancestors)
+        {
+            Level.Push(new Stack<string>());
+            return VisitorAction.Continue;
+        }
+
+        public override VisitorAction Leave(
+            ObjectValueNode node,
+            ISyntaxNode parent,
+            IReadOnlyList<object> path,
+            IReadOnlyList<ISyntaxNode> ancestors)
+        {
+            Stack<string> operations = Level.Pop();
+            Level.Peek().Push(string.Join(" AND ", operations));
+            return VisitorAction.Continue;
+        }
+
         public override VisitorAction Enter(
             ListValueNode node,
             ISyntaxNode parent,
             IReadOnlyList<object> path,
             IReadOnlyList<ISyntaxNode> ancestors)
         {
+            switch (Operations.Peek())
+            {
+                case AndField and:
+                case OrField or:
+                    Level.Push(new Stack<string>());
+                    break;
+            }
+
+            return VisitorAction.Continue;
+        }
+
+        public override VisitorAction Leave(
+            ListValueNode node,
+            ISyntaxNode parent,
+            IReadOnlyList<object> path,
+            IReadOnlyList<ISyntaxNode> ancestors)
+        {
+            Stack<string> operations;
+
+            switch (Operations.Peek())
+            {
+                case AndField and:
+                case FilterOperationField op:
+                    operations = Level.Pop();
+                    Level.Peek().Push(string.Join(" AND ", operations));
+                    break;
+                case OrField or:
+                    operations = Level.Pop();
+                    Level.Peek().Push(string.Join(" OR ", operations));
+                    break;
+            }
+
             return VisitorAction.Continue;
         }
     }
