7c6202da0b29aba0e2bc0a8393f754dd084dfcf8
Billy Wolfington <willwolfram18@gmail.com>
2019-06-19 22:38:48 -0700

    Created read and write interfaces

--- src/Core/PersistedQueries.Abstractions/IReadStoredQueries.cs
+++ src/Core/PersistedQueries.Abstractions/IReadStoredQueries.cs
@@ -1,6 +1,18 @@
+using HotChocolate.Execution;
+using System.Threading.Tasks;
+
 namespace HotChocolate.PersistedQueries.Abstractions
 {
+    /// <summary>
+    /// A tool for reading queries from some persistence medium.
+    /// </summary>
     public interface IReadStoredQueries
     {
+        /// <summary>
+        /// Retrieves a query with some identifier.
+        /// </summary>
+        /// <param name="queryIdentifier">The query identifier.</param>
+        /// <returns>The desired query.</returns>
+        Task<IQuery> ReadQueryAsync(string queryIdentifier);
     }
 }
--- src/Core/PersistedQueries.Abstractions/IWriteStoredQueries.cs
+++ src/Core/PersistedQueries.Abstractions/IWriteStoredQueries.cs
@@ -1,7 +1,19 @@
+using HotChocolate.Execution;
+using System.Threading.Tasks;
+
 namespace HotChocolate.PersistedQueries.Abstractions
 {
-    public interface IWriteStoredQueries
+    /// <summary>
+    /// A tool for storing queries to some persistence medium.
+    /// </summary>
+    public interface IStoreQueries
     {
-        
+        /// <summary>
+        /// Stores a given query using the given identifier.
+        /// </summary>
+        /// <param name="queryIdentifier">The query identifier.</param>
+        /// <param name="query">The query to store.</param>
+        /// <returns>An asynchronous operation.</returns>
+        Task StoreQueryAsync(string queryIdentifier, IQuery query);
     }
 }
