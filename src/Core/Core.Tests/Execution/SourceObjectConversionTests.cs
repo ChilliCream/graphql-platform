18ae639b2b0dc9cafee8ede77c1ca9587d428328
Michael Staib <michael.staib@hotmail.com>
2019-06-24 15:04:23 +0200

    Cleanup

--- src/Core/Core.Tests/Execution/SourceObjectConversionTests.cs
+++ src/Core/Core.Tests/Execution/SourceObjectConversionTests.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Threading.Tasks;
-using HotChocolate;
-using HotChocolate.Execution;
 using HotChocolate.Types;
 using HotChocolate.Utilities;
 using Microsoft.Extensions.DependencyInjection;
