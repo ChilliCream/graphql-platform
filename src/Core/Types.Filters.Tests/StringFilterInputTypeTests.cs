6bfd0174b22a9e1bb666e5b6df9d6a28ffe02119
Michael Staib <michael.staib@hotmail.com>
2019-06-25 22:03:09 +0200

    Added more tests

--- src/Core/Types.Filters.Tests/StringFilterInputTypeTests.cs
+++ src/Core/Types.Filters.Tests/StringFilterInputTypeTests.cs
@@ -1,4 +1,5 @@
 using System;
+using HotChocolate.Language;
 using Snapshooter.Xunit;
 using Xunit;
 
@@ -35,6 +36,31 @@ namespace HotChocolate.Types.Filters
         }
 
         [Fact]
+        public void Create_Explicit_Filters_All_Operations()
+        {
+            // arrange
+            // act
+            var schema = CreateSchema(
+                new FilterInputType<Foo>(d => d
+                    .Filter(f => f.Bar)
+                        .BindExplicitly()
+                        .AllowEquals()
+                        .And().AllowContains()
+                        .And().AllowEndsWith()
+                        .And().AllowEquals()
+                        .And().AllowIn()
+                        .And().AllowNotContains()
+                        .And().AllowNotEndsWith()
+                        .And().AllowNotEquals()
+                        .And().AllowNotIn()
+                        .And().AllowNotStartsWith()
+                        .And().AllowStartsWith()));
+
+            // assert
+            schema.ToString().MatchSnapshot();
+        }
+
+        [Fact]
         public void Rename_Specific_Filter()
         {
             // arrange
@@ -60,10 +86,153 @@ namespace HotChocolate.Types.Filters
             schema.ToString().MatchSnapshot();
         }
 
+        [Fact]
+        public void Declare_Name_Explicitly()
+        {
+            // arrange
+            // act
+            var schema = CreateSchema(new FilterInputType<Foo>(descriptor =>
+            {
+                descriptor.Filter(x => x.Bar)
+                    .BindExplicitly()
+                    .AllowEquals()
+                    .Name("custom_equals");
+            }));
+
+            // assert
+            schema.ToString().MatchSnapshot();
+        }
+
+        [Fact]
+        public void Declare_Description_Explicitly()
+        {
+            // arrange
+            // act
+            var schema = CreateSchema(new FilterInputType<Foo>(descriptor =>
+            {
+                descriptor.Filter(x => x.Bar)
+                    .BindExplicitly()
+                    .AllowEquals()
+                    .Description("custom_equals_description");
+            }));
+
+            // assert
+            schema.ToString().MatchSnapshot();
+        }
+
+        [Fact]
+        public void Declare_Directive_By_Name()
+        {
+            // arrange
+            // act
+            var schema = CreateSchema(builder =>
+                builder.AddType(new FilterInputType<Foo>(d =>
+                {
+                    d.Filter(x => x.Bar)
+                        .BindExplicitly()
+                        .AllowEquals()
+                        .Directive("bar");
+                }))
+                .AddDirectiveType(new DirectiveType(d => d
+                    .Name("bar")
+                    .Location(DirectiveLocation.InputFieldDefinition))));
+
+            // assert
+            schema.ToString().MatchSnapshot();
+        }
+
+        [Fact]
+        public void Declare_Directive_By_Name_With_Argument()
+        {
+            // arrange
+            // act
+            var schema = CreateSchema(builder =>
+                builder.AddType(new FilterInputType<Foo>(d =>
+                {
+                    d.Filter(x => x.Bar)
+                        .BindExplicitly()
+                        .AllowEquals()
+                        .Directive("bar",
+                            new ArgumentNode("qux",
+                                new StringValueNode("foo")));
+                }))
+                .AddDirectiveType(new DirectiveType(d => d
+                    .Name("bar")
+                    .Location(DirectiveLocation.InputFieldDefinition)
+                    .Argument("qux")
+                    .Type<StringType>())));
+
+            // assert
+            schema.ToString().MatchSnapshot();
+        }
+
+        [Fact]
+        public void Declare_Directive_With_Clr_Type()
+        {
+            // arrange
+            // act
+            var schema = CreateSchema(builder =>
+                builder.AddType(new FilterInputType<Foo>(d =>
+                {
+                    d.Filter(x => x.Bar)
+                        .BindExplicitly()
+                        .AllowEquals()
+                        .Directive<Bar>();
+                }))
+                .AddDirectiveType(new DirectiveType<Bar>(d => d
+                    .Location(DirectiveLocation.InputFieldDefinition))));
+
+            // assert
+            schema.ToString().MatchSnapshot();
+        }
+
+        [Fact]
+        public void Declare_Directive_With_Clr_Instance()
+        {
+            // arrange
+            // act
+            var schema = CreateSchema(builder =>
+                builder.AddType(new FilterInputType<Foo>(d =>
+                {
+                    d.Filter(x => x.Bar)
+                        .BindExplicitly()
+                        .AllowEquals()
+                        .Directive(new Bar());
+                }))
+                .AddDirectiveType(new DirectiveType<Bar>(d => d
+                    .Location(DirectiveLocation.InputFieldDefinition))));
+
+            // assert
+            schema.ToString().MatchSnapshot();
+        }
+
+        [Fact]
+        public void Bind_Filter_Implicitly()
+        {
+            // arrange
+            // act
+            var schema = CreateSchema(
+                new FilterInputType<Foo>(descriptor =>
+                {
+                    descriptor
+                        .BindExplicitly()
+                        .Filter(x => x.Bar)
+                        .BindExplicitly()
+                        .BindImplicitly();
+                }));
+
+            // assert
+            schema.ToString().MatchSnapshot();
+        }
 
         public class Foo
         {
             public string Bar { get; set; }
+        }
+
+        public class Bar
+        {
+            public string Baz { get; set; }
         }
 
         public class FooFilterType
