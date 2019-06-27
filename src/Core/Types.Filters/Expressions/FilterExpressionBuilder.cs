7d1580cc0232c5876ec70f396e9847f2b7282b01
Michael Staib <michael.staib@hotmail.com>
2019-06-27 22:12:30 +0200

    Fixed null ref issues with the filter on inmemory lists

--- src/Core/Types.Filters/Expressions/FilterExpressionBuilder.cs
+++ src/Core/Types.Filters/Expressions/FilterExpressionBuilder.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
 
 namespace HotChocolate.Types.Filters.Expressions
 {
@@ -34,12 +32,24 @@ namespace HotChocolate.Types.Filters.Expressions
             return Expression.Equal(expression, Expression.Constant(false));
         }
 
-        public static Expression Equals(MemberExpression property, object value)
+        public static Expression Equals(
+            MemberExpression property,
+            object value)
         {
             return Expression.Equal(property, Expression.Constant(value));
         }
 
-        public static Expression In(MemberExpression property, Type genericType, object parsedValue)
+        public static Expression NotEquals(
+            MemberExpression property,
+            object value)
+        {
+            return Expression.NotEqual(property, Expression.Constant(value));
+        }
+
+        public static Expression In(
+            MemberExpression property,
+            Type genericType,
+            object parsedValue)
         {
             return Expression.Call(
                     typeof(Enumerable),
@@ -50,38 +60,69 @@ namespace HotChocolate.Types.Filters.Expressions
                 );
         }
 
-        public static Expression GreaterThan(MemberExpression property, object value)
+        public static Expression GreaterThan(
+            MemberExpression property,
+            object value)
         {
-            return Expression.GreaterThan(property, Expression.Constant(value));
+            return Expression.GreaterThan(
+                property,
+                Expression.Constant(value));
         }
 
-        public static Expression GreaterThanOrEqual(MemberExpression property, object value)
+        public static Expression GreaterThanOrEqual(
+            MemberExpression property,
+            object value)
         {
-            return Expression.GreaterThanOrEqual(property, Expression.Constant(value));
+            return Expression.GreaterThanOrEqual(
+                property,
+                Expression.Constant(value));
         }
 
-        public static Expression LowerThan(MemberExpression property, object value)
+        public static Expression LowerThan(
+            MemberExpression property,
+            object value)
         {
-            return Expression.LessThan(property, Expression.Constant(value));
+            return Expression.LessThan(
+                property,
+                Expression.Constant(value));
         }
 
-        public static Expression LowerThanOrEqual(MemberExpression property, object value)
+        public static Expression LowerThanOrEqual(
+            MemberExpression property,
+            object value)
         {
-            return Expression.LessThanOrEqual(property, Expression.Constant(value));
+            return Expression.LessThanOrEqual(
+                property,
+                Expression.Constant(value));
         }
 
-        public static Expression StartsWith(MemberExpression property, object value)
+        public static Expression StartsWith(
+            MemberExpression property,
+            object value)
         {
-            return Expression.Call(property, _startsWith, new[] { Expression.Constant(value) });
+            return Expression.AndAlso(
+                Expression.NotEqual(property, Expression.Constant(null)),
+                Expression.Call(property, _startsWith,
+                    new[] { Expression.Constant(value) }));
         }
 
-        public static Expression EndsWith(MemberExpression property, object value)
+        public static Expression EndsWith(
+            MemberExpression property,
+            object value)
         {
-            return Expression.Call(property, _endsWith, new[] { Expression.Constant(value) });
+            return Expression.AndAlso(
+                Expression.NotEqual(property, Expression.Constant(null)),
+                Expression.Call(property, _endsWith,
+                    new[] { Expression.Constant(value) }));
         }
-        public static Expression Contains(MemberExpression property, object value)
+        public static Expression Contains(
+            MemberExpression property,
+            object value)
         {
-            return Expression.Call(property, _contains, new[] { Expression.Constant(value) });
+            return Expression.AndAlso(
+                Expression.NotEqual(property, Expression.Constant(null)),
+                Expression.Call(property, _contains,
+                    new[] { Expression.Constant(value) }));
         }
     }
 }
