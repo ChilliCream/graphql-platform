58dd5af051a6862a9f485a75b77d70e26e9a27d6
Michael Staib <michael.staib@hotmail.com>
2019-07-01 11:13:57 +0200

    Fixed compile errros

---
+++ src/Server/AspNetCore/Subscriptions/QueryRequestDto.cs
@@ -0,0 +1,19 @@
+#if !ASPNETCLASSIC
+
+using Newtonsoft.Json.Linq;
+
+namespace HotChocolate.AspNetCore.Subscriptions
+{
+    internal class QueryRequestDto
+    {
+        public string OperationName { get; set; }
+
+        public string NamedQuery { get; set; }
+
+        public string Query { get; set; }
+
+        public JObject Variables { get; set; }
+    }
+}
+
+#endif
