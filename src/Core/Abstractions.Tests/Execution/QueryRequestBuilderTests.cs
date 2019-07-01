34f513f480ba410ef0eb3e4084edb248bcd1390c
Michael Staib <michael.staib@hotmail.com>
2019-07-01 17:40:55 +0200

    fixed more tests

--- src/Core/Abstractions.Tests/Execution/QueryRequestBuilderTests.cs
+++ src/Core/Abstractions.Tests/Execution/QueryRequestBuilderTests.cs
@@ -66,7 +66,7 @@ namespace HotChocolate.Execution.Tests
                     .Create();
 
             // assert
-            Assert.Equal("query",
+            Assert.Equal("querySource",
                 Assert.Throws<ArgumentException>(action).ParamName);
         }
 
