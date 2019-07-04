6e328ee4384d74e3410c9e1ca6157949736fa149
Michael Staib <michael@chillicream.com>
2019-07-04 14:33:28 +0200

    Fixed authorization description issue (#893)

--- src/Server/AspNetCore.Authorization/AuthorizeDirectiveType.cs
+++ src/Server/AspNetCore.Authorization/AuthorizeDirectiveType.cs
@@ -27,6 +27,22 @@ namespace HotChocolate.AspNetCore.Authorization
             descriptor.Location(DirectiveLocation.Object)
                 .Location(DirectiveLocation.FieldDefinition);
 
+#if !ASPNETCLASSIC
+            // TODO :resources
+            descriptor.Argument(t => t.Policy)
+                .Description(
+                    "The name of the authorzation policy that determines " +
+                    "access to the annotated resource.")
+                .Type<StringType>();
+#endif
+
+            // TODO :resources
+            descriptor.Argument(t => t.Roles)
+                .Description(
+                    "Roles that are allowed to access to the " +
+                    "annotated resource.")
+                .Type<ListType<NonNullType<StringType>>>();
+
             descriptor.Repeatable();
 
             descriptor.Use<AuthorizeMiddleware>();
