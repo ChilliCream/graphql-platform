192307da9c26a7ff71f1960ca51073569c353599
Michael Staib <michael@chillicream.com>
2019-07-05 16:17:59 +0200

    Visitor Refinements (#894)

--- src/Core/Language/Visitors/VisitorFnWrapper.cs
+++ src/Core/Language/Visitors/VisitorFnWrapper.cs
@@ -2,13 +2,6 @@ using System.Collections.Generic;
 
 namespace HotChocolate.Language
 {
-    public delegate VisitorAction VisitorFn<T>(
-        ISyntaxNode node,
-        ISyntaxNode parent,
-        IReadOnlyList<object> path,
-        IReadOnlyList<ISyntaxNode> ancestors)
-        where T : ISyntaxNode;
-
     internal class VisitorFnWrapper<T>
         : ISyntaxNodeVisitor<T>
         where T : ISyntaxNode
@@ -22,20 +15,28 @@ namespace HotChocolate.Language
             _leave = leave;
         }
 
-        public VisitorAction Enter(T node, ISyntaxNode parent, IReadOnlyList<object> path, IReadOnlyList<ISyntaxNode> ancestors)
+        public VisitorAction Enter(
+            T node,
+            ISyntaxNode parent,
+            IReadOnlyList<object> path,
+            IReadOnlyList<ISyntaxNode> ancestors)
         {
             if (_enter == null)
             {
-                return VisitorAction.Continue;
+                return VisitorAction.Default;
             }
             return _enter.Invoke(node, parent, path, ancestors);
         }
 
-        public VisitorAction Leave(T node, ISyntaxNode parent, IReadOnlyList<object> path, IReadOnlyList<ISyntaxNode> ancestors)
+        public VisitorAction Leave(
+            T node,
+            ISyntaxNode parent,
+            IReadOnlyList<object> path,
+            IReadOnlyList<ISyntaxNode> ancestors)
         {
             if (_leave == null)
             {
-                return VisitorAction.Continue;
+                return VisitorAction.Default;
             }
             return _leave.Invoke(node, parent, path, ancestors);
         }
