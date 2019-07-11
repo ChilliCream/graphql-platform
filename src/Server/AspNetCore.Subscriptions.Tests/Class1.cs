030fccf3cb4efe53e38d34aadb06811409135c81
Michael Staib <michael.staib@hotmail.com>
2019-07-11 11:28:32 +0200

    Added more tests

--- src/Server/AspNetCore.Subscriptions.Tests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AspNetCore.Subscriptions.Tests
-{
-    public class Class1
-    {
-    }
-}
--- src/Server/AspNetCore.Subscriptions/TestSettings.cs
+++ src/Server/AspNetCore.Subscriptions/TestSettings.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("HotChocolate.AspNetCore.Tests")]
+[assembly: InternalsVisibleTo("HotChocolate.AspNetCore.Subscriptions.Tests")]
