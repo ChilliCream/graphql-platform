43e79c2bf7ee2e233778cd76191aed38840f46e6
Michael Staib <michael.staib@hotmail.com>
2019-08-06 12:10:51 +0200

    Added custom scalar type

--- src/Stitching/Stitching.Tests/Middleware/ScalarTests.cs
+++ src/Stitching/Stitching.Tests/Middleware/ScalarTests.cs
@@ -7,6 +7,15 @@ using Snapshooter.Xunit;
 using HotChocolate.Execution;
 using HotChocolate.AspNetCore.Tests.Utilities;
 using Snapshooter;
+using HotChocolate.Types;
+using HotChocolate.Language;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.TestHost;
+using HotChocolate.Stitching.Schemas.Contracts;
+using HotChocolate.Stitching.Schemas.Customers;
+using HotChocolate.AspNetCore;
+using HotChocolate.Stitching.Schemas.SpecialCases;
+using Moq;
 
 namespace HotChocolate.Stitching
 {
@@ -72,5 +81,76 @@ namespace HotChocolate.Stitching
             // assert
             result.MatchSnapshot(new SnapshotNameExtension(fieldName));
         }
+
+        [Fact]
+        public async Task Custom_Scalar_Types()
+        {
+            // arrange
+            IHttpClientFactory clientFactory = CreateRemoteSchemas();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton(clientFactory);
+            serviceCollection.AddStitchedSchema(builder => builder
+                .AddSchemaFromHttp("special")
+                .AddSchemaConfiguration(c =>
+                    c.RegisterType<MyCustomScalarType>()));
+
+            IServiceProvider services =
+                serviceCollection.BuildServiceProvider();
+
+            IQueryExecutor executor = services
+                .GetRequiredService<IQueryExecutor>();
+            IExecutionResult result = null;
+
+            // act
+            using (IServiceScope scope = services.CreateScope())
+            {
+                IReadOnlyQueryRequest request =
+                    QueryRequestBuilder.New()
+                        .SetQuery("{ custom_scalar(bar: \"custom_string\") }")
+                        .SetServices(scope.ServiceProvider)
+                        .Create();
+
+                result = await executor.ExecuteAsync(request);
+            }
+
+            // assert
+            result.MatchSnapshot();
+        }
+
+        protected override IHttpClientFactory CreateRemoteSchemas(
+            Dictionary<string, HttpClient> connections)
+        {
+            TestServer server_contracts = TestServerFactory.Create(
+                ContractSchemaFactory.ConfigureServices,
+                app => app.UseGraphQL());
+
+            TestServer server_customers = TestServerFactory.Create(
+                CustomerSchemaFactory.ConfigureServices,
+                app => app.UseGraphQL());
+
+            TestServer server_special = TestServerFactory.Create(
+                SpecialCasesSchemaFactory.ConfigureServices,
+                app => app.UseGraphQL());
+
+            connections["contract"] = server_contracts.CreateClient();
+            connections["customer"] = server_customers.CreateClient();
+            connections["special"] = server_special.CreateClient();
+
+            var httpClientFactory = new Mock<IHttpClientFactory>();
+            httpClientFactory.Setup(t => t.CreateClient(It.IsAny<string>()))
+                .Returns(new Func<string, HttpClient>(n =>
+                {
+                    if (connections.ContainsKey(n))
+                    {
+                        return connections[n];
+                    }
+
+                    throw new Exception();
+                }));
+
+            return httpClientFactory.Object;
+        }
+
     }
 }
