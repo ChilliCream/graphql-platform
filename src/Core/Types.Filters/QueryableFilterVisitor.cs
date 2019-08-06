730578ff43541e9cd62f2ebc32c3397d90ca2ca2
Michael Staib <michael.staib@hotmail.com>
2019-08-06 14:35:46 +0200

    Reworked the input field to allow to check if something is set

--- src/Core/Types.Filters/QueryableFilterVisitor.cs
+++ src/Core/Types.Filters/QueryableFilterVisitor.cs
@@ -103,11 +103,6 @@ namespace HotChocolate.Types.Filters
         {
             base.Enter(node, parent, path, ancestors);
 
-            if(node.Value is NullValueNode)
-            {
-                return VisitorAction.Skip;
-            }
-
             if (Operations.Peek() is FilterOperationField field)
             {
                 // TODO : needed only if we allow objects
