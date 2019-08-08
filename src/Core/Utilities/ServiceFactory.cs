9f79e45e9809240f8e3f3a829654982fd8ba0dd6
Michael Staib <michael.staib@hotmail.com>
2019-08-08 11:27:18 +0200

    project cleanup

--- src/Core/Utilities/ServiceFactory.cs
+++ src/Core/Utilities/ServiceFactory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace HotChocolate.Runtime
+namespace HotChocolate.Utilities
 {
     public class ServiceFactory
         : IServiceProvider
