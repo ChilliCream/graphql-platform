1db1bc40ce3e417c8ad26f1ea368effb2ac24138
Michael Staib <michael.staib@hotmail.com>
2019-08-08 14:31:13 +0200

    Added mongo tests and fixed mongo filter issues

--- src/Core/Types.Filters.Mongo.Tests/MongoFilterTests.cs
+++ src/Core/Types.Filters.Mongo.Tests/MongoFilterTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System;
 using MongoDB.Driver;
 using MongoDB.Bson;
@@ -6,6 +7,7 @@ using HotChocolate.Execution;
 using Xunit;
 using System.Threading.Tasks;
 using Snapshooter.Xunit;
+using HotChocolate.Types.Relay;
 
 namespace HotChocolate.Types.Filters
 {
@@ -87,6 +89,44 @@ namespace HotChocolate.Types.Filters
             result.MatchSnapshot();
         }
 
+        [Fact]
+        public async Task GetItems_With_Paging_EqualsFilter_FirstItems_Is_Returned()
+        {
+            // arrange
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IMongoCollection<Model>>(sp =>
+            {
+                MongoClient client = new MongoClient();
+                IMongoDatabase database = client.GetDatabase(
+                    "db_" + Guid.NewGuid().ToString("N"));
+
+                var collection = database.GetCollection<Model>("col");
+                collection.InsertMany(new[]
+                {
+                    new Model { Foo = "abc", Bar = 1, Baz = true },
+                    new Model { Foo = "def", Bar = 2, Baz = false },
+                });
+                return collection;
+            });
+
+            ISchema schema = SchemaBuilder.New()
+                .AddQueryType<QueryType>()
+                .AddServices(serviceCollection.BuildServiceProvider())
+                .Create();
+
+            IQueryExecutor executor = schema.MakeExecutable();
+
+            IReadOnlyQueryRequest request = QueryRequestBuilder.New()
+                .SetQuery("{ paging(where: { foo: \"abc\" }) { nodes { foo } } }")
+                .Create();
+
+            // act
+            IExecutionResult result = await executor.ExecuteAsync(request);
+
+            // assert
+            result.MatchSnapshot();
+        }
+
         public class QueryType : ObjectType
         {
             protected override void Configure(IObjectTypeDescriptor descriptor)
@@ -94,6 +134,12 @@ namespace HotChocolate.Types.Filters
                 descriptor.Name("Query");
                 descriptor.Field("items")
                     .Type<ListType<ModelType>>()
+                    .UseFiltering<FilterInputType<Model>>()
+                    .Resolver(ctx =>
+                        ctx.Service<IMongoCollection<Model>>().AsQueryable());
+
+                descriptor.Field("paging")
+                    .UsePaging<ModelType>()
                     .UseFiltering<FilterInputType<Model>>()
                     .Resolver(ctx =>
                         ctx.Service<IMongoCollection<Model>>().AsQueryable());
