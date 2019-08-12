3764805db2821eb46b139f8d5fdbf4a9adb6cc6a
Michael Staib <michael.staib@hotmail.com>
2019-08-12 12:02:59 +0200

    demo code

---
+++ src/Client/Core.Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HotChocolate.Client;
+using Xunit;
+
+namespace Core.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            Demo.Foo();
+
+        }
+    }
+
+}
--- src/Client/Core/newclass.cs
+++ src/Client/Core/newclass.cs
@@ -1,3 +1,9 @@
+using System.Linq;
+using System;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace HotChocolate.Client
 {
     public interface IReadOnlyQueryRequest
@@ -24,25 +30,96 @@ namespace HotChocolate.Client
             CancellationToken cancellationToken);
     }
 
-    public interface IQueryableItem
+    public interface IQueryableValue
     {
         Expression Expression { get; }
     }
 
-    public interface IQueryableList
-        : IQueryableItem
+    public interface IQueryableValue<out T>
+        : IQueryableValue
     {
-
     }
+
+    public interface IQueryableList<out T>
+        : IQueryableValue<T>
+    {
+    }
+
+    public class QueryableValue<T>
+        : IQueryableValue<T>
+    {
+        public Expression Expression => throw new NotImplementedException();
+    }
+
+    public static class QueryableExtensions
+    {
+        public static IQueryableList<TResult> Select<TValue, TResult>(
+            this IQueryableList<TValue> source,
+            Expression<Func<TValue, TResult>> selector)
+                where TValue : IQueryableValue
+        {
+            return null;
+        }
+
+        public static IQueryableValue<TResult> Select<TValue, TResult>(
+            this IQueryableValue<TValue> source,
+            Expression<Func<TValue, TResult>> selector)
+                where TValue : IQueryableValue
+        {
+            return new QueryableValue<TResult>();
+        }
+    }
+
+    public static class Demo
+    {
+        public static void Foo()
+        {
+            var x = new Query().Select(t => new
+            {
+                fooFoo = t.Foo.Select(c => new
+                {
+                    c.Bar
+                })
+            });
+
+            Type type = x.GetType().GenericTypeArguments.First();
+        }
+    }
+
+
+
+    public class Query
+        : IQueryableValue<Query>
+    {
+        public Expression Expression => throw new NotImplementedException();
+
+        public Foo Foo { get; }
+    }
+
+    public class Foo
+        : IQueryableValue<Foo>
+    {
+        public Expression Expression => throw new NotImplementedException();
+
+        public string Bar { get; }
+    }
+
+    public class Bar : IQueryableValue<Bar>
+    {
+        public Expression Expression => throw new NotImplementedException();
+    }
+
+
+
 
     public interface IQueryCompiler
     {
-        ICompiledQuery<T> Compile(IQueryableItem queryable);
+        // ICompiledQuery<T> Compile(IQueryableValue queryable);
     }
 
     public interface IStreamCompiler
     {
-        ICompiledQuery<T> Compile(IQueryableItem queryable);
+        // ICompiledQuery<T> Compile(IQueryableValue queryable);
     }
 
     public interface ICompiledQuery<T>
