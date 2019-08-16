e88924752693b8fb81314375cb94045eba4adb03
Michael Staib <michael.staib@hotmail.com>
2019-08-16 10:26:31 +0200

    moved error codes to abstractions

--- src/Core/Core/Execution/Middleware/MiddlewareErrorCodes.cs
+++
@@ -1,9 +0,0 @@
-namespace HotChocolate.Execution
-{
-    internal static class MiddlewareErrorCodes
-    {
-        public const string Incomplete = "EXEC_MIDDLEWARE_INCOMPLETE";
-        public const string Timeout = "EXEC_TIMEOUT";
-        public const string QueryNotFound = "PERSISTED_QUERY_NOT_FOUND";
-    }
-}
