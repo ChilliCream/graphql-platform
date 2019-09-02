604e4913631c99c17136d331d39f49214c20dfd3
Michael Staib <michael.staib@hotmail.com>
2019-09-03 00:19:22 +0200

    fixed tests

--- src/Client/Generators.Tests/CodeModelGeneratorTests.cs
+++ src/Client/Generators.Tests/CodeModelGeneratorTests.cs
@@ -25,17 +25,68 @@ namespace StrawberryShake.Generators
         [Theory]
         public async Task Generate_Models(string queryFile)
         {
+            // arrange
+            var outputHandler = new TestOutputHandler();
 
+            // act
             await ClientGenerator.New()
-                .AddSchemaDocumentFromFile(
-                    "../../../../Demo/GraphQL/StarWars.graphql")
-                .AddQueryDocumentFromFile(
-                    "../../../../Demo/GraphQL/Queries.graphql")
-                .SetOutput(
-                    "../../../../Demo/GraphQL/Generated")
+                .AddQueryDocumentFromString(queryFile, FileResource.Open(queryFile))
+                .AddSchemaDocumentFromString("StarWars", FileResource.Open("StarWars.graphql"))
+                .SetOutput(outputHandler)
                 .BuildAsync();
 
+            // assert
+            outputHandler.Content.MatchSnapshot(
+                new SnapshotNameExtension(queryFile));
+        }
 
+        private class TestOutputHandler
+            : IFileHandler
+        {
+            private readonly List<GeneratorTask> _tasks = new List<GeneratorTask>();
+
+            public string Content { get; private set; }
+
+            public void Register(
+                ICodeDescriptor descriptor,
+                ICodeGenerator generator)
+            {
+                _tasks.Add(new GeneratorTask
+                {
+                    Descriptor = descriptor,
+                    Generator = new NamespaceGenerator(generator)
+                });
+            }
+
+            public async Task WriteAllAsync(ITypeLookup typeLookup)
+            {
+                var usedNames = new HashSet<string>();
+
+                using (var stream = new MemoryStream())
+                {
+                    using (var sw = new StreamWriter(stream, Encoding.UTF8))
+                    {
+                        using (var cw = new CodeWriter(sw))
+                        {
+                            foreach (GeneratorTask task in _tasks)
+                            {
+                                await task.Generator.WriteAsync(
+                                    cw, task.Descriptor, typeLookup);
+                                await cw.WriteLineAsync();
+                                await cw.WriteLineAsync();
+                            }
+                        }
+                    }
+
+                    Content = Encoding.UTF8.GetString(stream.ToArray());
+                }
+            }
+
+            private class GeneratorTask
+            {
+                public ICodeDescriptor Descriptor { get; set; }
+                public ICodeGenerator Generator { get; set; }
+            }
         }
     }
 }
