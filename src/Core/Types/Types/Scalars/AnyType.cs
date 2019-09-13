0d943ab71b80f07dae966a9bd43a327f143f6699
Michael Staib <michael.staib@hotmail.com>
2019-09-13 16:48:15 +0200

    Added serialization logic

--- src/Core/Types/Types/Scalars/AnyType.cs
+++ src/Core/Types/Types/Scalars/AnyType.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using HotChocolate.Configuration;
 using HotChocolate.Language;
 using HotChocolate.Utilities;
 
@@ -7,16 +9,26 @@ namespace HotChocolate.Types
     public sealed class AnyType
         : ScalarType
     {
-        private readonly ObjectValueToDictionaryConverter _converter =
+        private readonly ObjectValueToDictionaryConverter _objectValueToDictConverter =
             new ObjectValueToDictionaryConverter();
+        private ObjectToDictionaryConverter _objectToDictConverter;
+        private ITypeConversion _converter;
 
         public AnyType()
             : base("Any")
         {
-
         }
 
         public override Type ClrType => typeof(object);
+
+        protected override void OnCompleteType(
+            ICompletionContext context,
+            IDictionary<string, object> contextData)
+        {
+            _converter = context.Services.GetTypeConversion();
+            _objectToDictConverter = new ObjectToDictionaryConverter(_converter);
+            base.OnCompleteType(context, contextData);
+        }
 
         public override bool IsInstanceOfType(IValueNode literal)
         {
@@ -52,10 +64,10 @@ namespace HotChocolate.Types
                     return bvn.Value;
 
                 case ListValueNode lvn:
-                    return _converter.Convert(lvn);
+                    return _objectValueToDictConverter.Convert(lvn);
 
                 case ObjectValueNode ovn:
-                    return _converter.Convert(ovn);
+                    return _objectValueToDictConverter.Convert(ovn);
 
                 default:
                     return false;
@@ -63,6 +75,15 @@ namespace HotChocolate.Types
         }
 
         public override IValueNode ParseValue(object value)
+        {
+            if (value is null)
+            {
+                return NullValueNode.Default;
+            }
+            return ParseValue(value, new HashSet<object>());
+        }
+
+        private IValueNode ParseValue(object value, ISet<object> set)
         {
             if (value is null)
             {
@@ -95,18 +116,56 @@ namespace HotChocolate.Types
                     return new BooleanValueNode(b);
             }
 
-            throw new  NotImplementedException();
+            Type type = value.GetType();
+
+            if (type.IsValueType && _converter.TryConvert(
+                type, typeof(string), value, out object converted)
+                && converted is string c)
+            {
+                return new StringValueNode(c);
+            }
+
+            if (set.Contains(value))
+            {
+                if (value is IReadOnlyList<object> list)
+                {
+                    var valueList = new List<IValueNode>();
+                    foreach (object element in list)
+                    {
+                        valueList.Add(ParseValue(element, set));
+                    }
+                    return new ListValueNode(valueList);
+                }
+
+                if (value is IReadOnlyDictionary<string, object> dict)
+                {
+                    var fields = new List<ObjectFieldNode>();
+                    foreach (KeyValuePair<string, object> field in dict)
+                    {
+                        fields.Add(new ObjectFieldNode(
+                            field.Key,
+                            ParseValue(field.Value, set)));
+                    }
+                    return new ObjectValueNode(fields);
+                }
+
+                return ParseValue(_objectToDictConverter.Convert(value), set);
+            }
+
+            throw new ScalarSerializationException(
+                "Cycle detected in graph detected.");
         }
 
 
         public override object Serialize(object value)
         {
-            throw new NotImplementedException();
+            return _objectToDictConverter.Convert(value);
         }
 
         public override bool TryDeserialize(object serialized, out object value)
         {
-            throw new NotImplementedException();
+            value = serialized;
+            return true;
         }
     }
 }
---
+++ src/Core/Types/Utilities/ObjectToDictionaryConverter.cs
@@ -0,0 +1,127 @@
+using System.Collections.Concurrent;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace HotChocolate.Utilities
+{
+    internal class ObjectToDictionaryConverter
+    {
+        private readonly ITypeConversion _converter;
+        private readonly ConcurrentDictionary<Type, List<PropertyInfo>> _properties =
+            new ConcurrentDictionary<Type, List<PropertyInfo>>();
+
+        public ObjectToDictionaryConverter(ITypeConversion converter)
+        {
+            _converter = converter
+                ?? throw new ArgumentNullException(nameof(converter));
+        }
+
+        public object Convert(object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            object value = null;
+            Action<object> setValue = v => value = v;
+
+            return value;
+        }
+
+        private void VisitValue(
+            object obj,
+            Action<object> setValue,
+            ISet<object> processed)
+        {
+            if (obj is null)
+            {
+                setValue(null);
+            }
+
+            switch (obj)
+            {
+                case string _:
+                case short _:
+                case ushort _:
+                case int _:
+                case uint _:
+                case long _:
+                case ulong _:
+                case float _:
+                case double _:
+                case decimal _:
+                case bool _:
+                    setValue(obj);
+                    return;
+            }
+
+            Type type = obj.GetType();
+
+            if (type.IsValueType && _converter.TryConvert(
+                type, typeof(string), obj, out object converted)
+                && converted is string s)
+            {
+                setValue(s);
+            }
+            else if (obj is ICollection list)
+            {
+                VisitList(list, setValue, processed);
+            }
+            else
+            {
+                VisitObject(obj, setValue, processed);
+            }
+        }
+
+        private void VisitObject(
+            object obj,
+            Action<object> setValue,
+            ISet<object> processed)
+        {
+            if (processed.Add(obj))
+            {
+                var dict = new Dictionary<string, object>();
+                setValue(dict);
+
+                foreach (PropertyInfo property in GetProperties(obj))
+                {
+                    string name = property.GetGraphQLName();
+                    object value = property.GetValue(obj);
+                    Action<object> setField = v => dict[name] = v;
+                    VisitValue(value, setField, processed);
+                }
+            }
+        }
+
+        private void VisitList(
+            ICollection list,
+            Action<object> setValue,
+            ISet<object> processed)
+        {
+            var valueList = new List<object>();
+            setValue(valueList);
+
+            Action<object> addItem = item => valueList.Add(item);
+
+            foreach (object element in list)
+            {
+                VisitValue(element, addItem, processed);
+            }
+        }
+
+        private IReadOnlyList<PropertyInfo> GetProperties(object value)
+        {
+            Type type = value.GetType();
+            if (!_properties.TryGetValue(type, out List<PropertyInfo> properties))
+            {
+                properties = new List<PropertyInfo>(
+                    ReflectionUtils.GetProperties(type).Values);
+                _properties.TryAdd(type, properties);
+            }
+            return properties;
+        }
+    }
+}
