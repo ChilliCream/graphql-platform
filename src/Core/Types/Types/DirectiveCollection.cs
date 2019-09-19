d5f09c3510a850d5bfc674713abaa654adb211db
Michael Staib <michael.staib@hotmail.com>
2019-09-19 18:33:15 +0200

    Opened some more helpers

--- src/Core/Types/Types/DirectiveCollection.cs
+++ src/Core/Types/Types/DirectiveCollection.cs
@@ -19,7 +19,7 @@ namespace HotChocolate.Types
         private List<DirectiveDefinition> _definitions;
         private ILookup<NameString, IDirective> _lookup;
 
-        public DirectiveCollection(
+        internal DirectiveCollection(
             object source,
             IEnumerable<DirectiveDefinition> directiveDefinitions)
         {
@@ -186,6 +186,31 @@ namespace HotChocolate.Types
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+        public static DirectiveCollection CreateAndComplete(
+            ICompletionContext context,
+            object source,
+            IEnumerable<DirectiveDefinition> directiveDefinitions)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (directiveDefinitions is null)
+            {
+                throw new ArgumentNullException(nameof(directiveDefinitions));
+            }
+
+            var directives = new DirectiveCollection(
+                source, directiveDefinitions);
+            directives.CompleteCollection(context);
+            return directives;
         }
     }
 }
