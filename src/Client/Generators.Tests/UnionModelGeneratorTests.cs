b1473782627aec01edbf13ddf0b42b795233ef46
Michael Staib <michael.staib@hotmail.com>
2019-09-24 12:10:04 +0200

    Work in progress

--- src/Client/Generators.Tests/UnionModelGeneratorTests.cs
+++ src/Client/Generators.Tests/UnionModelGeneratorTests.cs
@@ -1,21 +1,18 @@
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ChilliCream.Testing;
 using HotChocolate;
 using HotChocolate.Language;
-using StrawberryShake.Generators.Descriptors;
-using Xunit;
 using HotChocolate.Types;
-using StrawberryShake.Generators.CSharp;
-using System.IO;
-using StrawberryShake.Generators.Utilities;
-using System.Text;
+using Xunit;
 using Snapshooter.Xunit;
+using StrawberryShake.Generators.Descriptors;
+using StrawberryShake.Generators.CSharp;
 
 namespace StrawberryShake.Generators
 {
     public class UnionModelGeneratorTests
+        : ModelGeneratorTestBase
     {
         [Fact]
         public async Task Union_Inline_Fragments()
@@ -240,66 +237,6 @@ namespace StrawberryShake.Generators
             string output = await WriteAllAsync(context.Descriptors, typeLookup);
 
             output.MatchSnapshot();
-        }
-
-        private static async Task<string> WriteAllAsync(
-            IReadOnlyCollection<ICodeDescriptor> descriptors,
-            ITypeLookup typeLookup)
-        {
-            var generators = new ICodeGenerator[]
-            {
-                new InterfaceGenerator(),
-                new ClassGenerator()
-            };
-
-            using (var stream = new MemoryStream())
-            {
-                using (var sw = new StreamWriter(stream, Encoding.UTF8))
-                {
-                    using (var cw = new CodeWriter(sw))
-                    {
-                        foreach (ICodeGenerator generator in generators)
-                        {
-                            foreach (ICodeDescriptor descriptor in descriptors)
-                            {
-                                if (generator.CanHandle(descriptor))
-                                {
-                                    await generator.WriteAsync(
-                                        cw, descriptor, typeLookup);
-                                    await cw.WriteLineAsync();
-                                }
-                            }
-                        }
-                    }
-                }
-                return Encoding.UTF8.GetString(stream.ToArray());
-            }
-        }
-
-        private class CollectFieldsVisitor
-            : QuerySyntaxWalker<Dictionary<FieldNode, string>>
-        {
-            protected override void VisitField(
-                FieldNode node,
-                Dictionary<FieldNode, string> context)
-            {
-                if (!context.ContainsKey(node))
-                {
-                    context.Add(node, "UNKNOWN");
-                }
-
-                base.VisitField(node, context);
-            }
-
-            public static IReadOnlyDictionary<FieldNode, string> MockLookup(
-                DocumentNode query,
-                IReadOnlyDictionary<FieldNode, string> knownFieldTypes)
-            {
-                var fieldTypes = knownFieldTypes.ToDictionary(t => t.Key, t => t.Value);
-                var visitor = new CollectFieldsVisitor();
-                visitor.Visit(query, fieldTypes);
-                return fieldTypes;
-            }
         }
     }
 }
