8b6d57deb7690a38be574349b5b0caa7146f4f9d
Senn Pascal (GIAPSE) <Pascal.Senn@gia.ch>
2019-09-29 23:54:25 +0200

    test(Filters): add integrations tests

--- src/Core/Types.Filters.Tests/QueryableFilterVisitorObjectTests.cs
+++ src/Core/Types.Filters.Tests/QueryableFilterVisitorObjectTests.cs
@@ -39,249 +39,181 @@ namespace HotChocolate.Types.Filters
             var b = new Foo { FooNested = new FooNested { Bar = "b" } };
             Assert.False(func(b));
         }
-        /*
+
         [Fact]
-        public void Create_StringNotEqual_Expression()
+        public void Create_ObjectStringEqualDeep_Expression()
         {
             // arrange
             var value = new ObjectValueNode(
-                new ObjectFieldNode("bar_not",
-                    new StringValueNode("a")));
+                new ObjectFieldNode("foo",
+                    new ObjectValueNode(
+                    new ObjectFieldNode("fooNested",
+                        new ObjectValueNode(
+                            new ObjectFieldNode("bar",
+                                new StringValueNode("a")
+                            )
+                        )
+                    )
+                )
+            )
+            );
 
-            var fooType = CreateType(new FooFilterType());
+            var fooType = CreateType(new EvenDeeperFilterType());
 
             // act
             var filter = new QueryableFilterVisitor(
                 fooType,
-                typeof(Foo),
+                typeof(EvenDeeper),
                 TypeConversion.Default);
             value.Accept(filter);
-            Func<Foo, bool> func = filter.CreateFilter<Foo>().Compile();
+            Func<EvenDeeper, bool> func = filter.CreateFilter<EvenDeeper>().Compile();
 
             // assert
-            var a = new Foo{FooNested =new FooNested { Bar = "a" }};
-            Assert.False(func(a));
-
-            var b = new Foo{FooNested =new FooNested { Bar = "b" }};
-            Assert.True(func(b));
-        }
-
-        [Fact]
-        public void Create_StringIn_Expression()
-        {
-            // arrange
-            var value = new ObjectValueNode(
-                new ObjectFieldNode("bar_in",
-                    new ListValueNode(new[]
-                    {
-                        new StringValueNode("a"),
-                        new StringValueNode("c")
-                    })));
-
-            var fooType = CreateType(new FooFilterType());
-
-            // act
-            var filter = new QueryableFilterVisitor(
-                fooType,
-                typeof(Foo),
-                TypeConversion.Default);
-            value.Accept(filter);
-            Func<Foo, bool> func = filter.CreateFilter<Foo>().Compile();
-
-            // assert
-            var a = new Foo{FooNested =new FooNested { Bar = "a" }};
+            var a = new EvenDeeper { Foo = new Foo { FooNested = new FooNested { Bar = "a" } } };
             Assert.True(func(a));
 
-            var b = new Foo{FooNested =new FooNested { Bar = "b" }};
+            var b = new EvenDeeper { Foo = new Foo { FooNested = new FooNested { Bar = "b" } } };
             Assert.False(func(b));
         }
 
         [Fact]
-        public void Create_StringNotIn_Expression()
+        public void Create_ObjectStringEqualRecursive_Expression()
         {
             // arrange
             var value = new ObjectValueNode(
-                new ObjectFieldNode("bar_not_in",
-                    new ListValueNode(new[]
-                    {
-                        new StringValueNode("a"),
-                        new StringValueNode("c")
-                    })));
+                new ObjectFieldNode("nested",
+                    new ObjectValueNode(
+                    new ObjectFieldNode("nested",
+                        new ObjectValueNode(
+                            new ObjectFieldNode("bar",
+                                new StringValueNode("a")
+                            )
+                        )
+                    )
+                )
+            )
+            );
 
-            var fooType = CreateType(new FooFilterType());
+            var fooType = CreateType(new FilterInputType<Recursive>());
 
             // act
             var filter = new QueryableFilterVisitor(
                 fooType,
-                typeof(Foo),
+                typeof(Recursive),
                 TypeConversion.Default);
             value.Accept(filter);
-            Func<Foo, bool> func = filter.CreateFilter<Foo>().Compile();
+            Func<Recursive, bool> func = filter.CreateFilter<Recursive>().Compile();
 
-            // assert
-            var a = new Foo{FooNested =new FooNested { Bar = "a" }};
-            Assert.False(func(a));
 
-            var b = new Foo{FooNested =new FooNested { Bar = "b" }};
-            Assert.True(func(b));
-        }
-
-        [Fact]
-        public void Create_StringContains_Expression()
-        {
-            // arrange
-            var value = new ObjectValueNode(
-                new ObjectFieldNode("bar_contains",
-                    new StringValueNode("a")));
-
-            var fooType = CreateType(new FooFilterType());
-
-            // act
-            var filter = new QueryableFilterVisitor(
-                fooType,
-                typeof(Foo),
-                TypeConversion.Default);
-            value.Accept(filter);
-            Func<Foo, bool> func = filter.CreateFilter<Foo>().Compile();
-
-            // assert
-            var a = new Foo{FooNested =new FooNested { Bar = "testatest" }};
+            var a = new Recursive { Nested = new Recursive { Nested = new Recursive { Bar = "a" } } };
             Assert.True(func(a));
 
-            var b = new Foo{FooNested =new FooNested { Bar = "testbtest" }};
+            var b = new Recursive { Nested = new Recursive { Nested = new Recursive { Bar = "b" } } };
             Assert.False(func(b));
+
         }
 
+
         [Fact]
-        public void Create_StringNoContains_Expression()
+        public void Create_ObjectStringEqualNull_Expression()
         {
             // arrange
             var value = new ObjectValueNode(
-                new ObjectFieldNode("bar_not_contains",
-                    new StringValueNode("a")));
+                new ObjectFieldNode("foo",
+                    new ObjectValueNode(
+                    new ObjectFieldNode("fooNested",
+                        new ObjectValueNode(
+                            new ObjectFieldNode("bar",
+                                new StringValueNode("a")
+                            )
+                        )
+                    )
+                )
+            )
+            );
 
-            var fooType = CreateType(new FooFilterType());
+            var fooType = CreateType(new EvenDeeperFilterType());
 
             // act
             var filter = new QueryableFilterVisitor(
                 fooType,
-                typeof(Foo),
+                typeof(EvenDeeper),
                 TypeConversion.Default);
             value.Accept(filter);
-            Func<Foo, bool> func = filter.CreateFilter<Foo>().Compile();
+            Func<EvenDeeper, bool> func = filter.CreateFilter<EvenDeeper>().Compile();
 
             // assert
-            var a = new Foo{FooNested =new FooNested { Bar = "testatest" }};
+            var a = new EvenDeeper { Foo = null };
             Assert.False(func(a));
 
-            var b = new Foo{FooNested =new FooNested { Bar = "testbtest" }};
-            Assert.True(func(b));
-        }
-
-        [Fact]
-        public void Create_StringStartsWith_Expression()
-        {
-            // arrange
-            var value = new ObjectValueNode(
-                new ObjectFieldNode("bar_starts_with",
-                    new StringValueNode("a")));
-
-            var fooType = CreateType(new FooFilterType());
-
-            // act
-            var filter = new QueryableFilterVisitor(
-                fooType,
-                typeof(Foo),
-                TypeConversion.Default);
-            value.Accept(filter);
-            Func<Foo, bool> func = filter.CreateFilter<Foo>().Compile();
-
-            // assert
-            var a = new Foo{FooNested =new FooNested { Bar = "ab" }};
-            Assert.True(func(a));
-
-            var b = new Foo{FooNested =new FooNested { Bar = "ba" }};
+            var b = new EvenDeeper { Foo = new Foo { FooNested = null } };
             Assert.False(func(b));
+
+            var c = new EvenDeeper { Foo = new Foo { FooNested = new FooNested { Bar = null } } };
+            Assert.False(func(c));
+
+            var d = new EvenDeeper { Foo = new Foo { FooNested = new FooNested { Bar = "a" } } };
+            Assert.True(func(d));
+
         }
 
+        /**
+         * As multiple hanlders for a single property can exists, it makes sense to test mutliple porperty filtering too
+         * Just to see if the null checks are wrapped around the whole object and not just around the one of the expressions.
+         * With the current visitor implementation this cannot be the case. Anyway, as code lifes is good to check twice. 
+         * */
         [Fact]
-        public void Create_StringNotStartsWith_Expression()
+        public void Create_ObjectStringEqualNullWithMultipleFilters_Expression()
         {
             // arrange
             var value = new ObjectValueNode(
-                new ObjectFieldNode("bar_not_starts_with",
-                    new StringValueNode("a")));
+                new ObjectFieldNode("foo",
+                    new ObjectValueNode(
+                    new ObjectFieldNode("fooNested",
+                        new ObjectValueNode(
+                            new ObjectFieldNode("bar",
+                                new StringValueNode("a")
+                            ),
 
-            var fooType = CreateType(new FooFilterType());
+                            new ObjectFieldNode("bar_not",
+                                new StringValueNode("c")
+                            )
+                        )
+                    )
+                )
+            )
+            );
+
+            var fooType = CreateType(new EvenDeeperFilterType());
 
             // act
             var filter = new QueryableFilterVisitor(
                 fooType,
-                typeof(Foo),
+                typeof(EvenDeeper),
                 TypeConversion.Default);
             value.Accept(filter);
-            Func<Foo, bool> func = filter.CreateFilter<Foo>().Compile();
+            Func<EvenDeeper, bool> func = filter.CreateFilter<EvenDeeper>().Compile();
 
             // assert
-            var a = new Foo{FooNested =new FooNested { Bar = "ab" }};
+            var a = new EvenDeeper { Foo = null };
             Assert.False(func(a));
 
-            var b = new Foo{FooNested =new FooNested { Bar = "ba" }};
-            Assert.True(func(b));
-        }
-
-        [Fact]
-        public void Create_StringEndsWith_Expression()
-        {
-            // arrange
-            var value = new ObjectValueNode(
-                new ObjectFieldNode("bar_ends_with",
-                    new StringValueNode("a")));
-
-            var fooType = CreateType(new FooFilterType());
-
-            // act
-            var filter = new QueryableFilterVisitor(
-                fooType,
-                typeof(Foo),
-                TypeConversion.Default);
-            value.Accept(filter);
-            Func<Foo, bool> func = filter.CreateFilter<Foo>().Compile();
-
-            // assert
-            var a = new Foo{FooNested =new FooNested { Bar = "ab" }};
-            Assert.False(func(a));
-
-            var b = new Foo{FooNested =new FooNested { Bar = "ba" }};
-            Assert.True(func(b));
-        }
-
-        [Fact]
-        public void Create_StringNotEndsWith_Expression()
-        {
-            // arrange
-            var value = new ObjectValueNode(
-                new ObjectFieldNode("bar_not_ends_with",
-                    new StringValueNode("a")));
-
-            var fooType = CreateType(new FooFilterType());
-
-            // act
-            var filter = new QueryableFilterVisitor(
-                fooType,
-                typeof(Foo),
-                TypeConversion.Default);
-            value.Accept(filter);
-            Func<Foo, bool> func = filter.CreateFilter<Foo>().Compile();
-
-            // assert
-            var a = new Foo{FooNested =new FooNested { Bar = "ab" }};
-            Assert.True(func(a));
-
-            var b = new Foo{FooNested =new FooNested { Bar = "ba" }};
+            var b = new EvenDeeper { Foo = new Foo { FooNested = null } };
             Assert.False(func(b));
+
+            var c = new EvenDeeper { Foo = new Foo { FooNested = new FooNested { Bar = null } } };
+            Assert.False(func(c));
+
+            var d = new EvenDeeper { Foo = new Foo { FooNested = new FooNested { Bar = "a" } } };
+            Assert.True(func(d));
+
         }
-        */
+
+        public class EvenDeeper
+        {
+            public Foo Foo { get; set; }
+        }
+
         public class Foo
         {
             public FooNested FooNested { get; set; }
@@ -290,6 +222,12 @@ namespace HotChocolate.Types.Filters
         {
             public string Bar { get; set; }
         }
+        public class Recursive
+        {
+            public Recursive Nested { get; set; }
+            public string Bar { get; set; }
+        }
+
 
         public class FooFilterType
             : FilterInputType<Foo>
@@ -298,6 +236,16 @@ namespace HotChocolate.Types.Filters
                 IFilterInputTypeDescriptor<Foo> descriptor)
             {
                 descriptor.Filter(t => t.FooNested).AllowObject(x => x.Filter(y => y.Bar));
+            }
+        }
+
+        public class EvenDeeperFilterType
+            : FilterInputType<EvenDeeper>
+        {
+            protected override void Configure(
+                IFilterInputTypeDescriptor<EvenDeeper> descriptor)
+            {
+                descriptor.Filter(t => t.Foo).AllowObject(x => x.Filter(y => y.FooNested).AllowObject(z => z.Filter(z => z.Bar)));
             }
         }
     }
