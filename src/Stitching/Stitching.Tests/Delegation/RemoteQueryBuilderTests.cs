d95ad3673673977d9601cfb8c91254655014c122
Nigel Sampson <nigel.sampson@pushpay.com>
2019-10-11 21:55:37 +1300

    Use operation name in stitched queries (#1124)

--- src/Stitching/Stitching.Tests/Delegation/RemoteQueryBuilderTests.cs
+++ src/Stitching/Stitching.Tests/Delegation/RemoteQueryBuilderTests.cs
@@ -45,7 +45,50 @@ namespace HotChocolate.Stitching.Delegation
 
             // act
             DocumentNode newQuery = RemoteQueryBuilder.New()
-                .SetOperation(OperationType.Query)
+                .SetOperation(null, OperationType.Query)
+                .SetSelectionPath(path)
+                .SetRequestField(field)
+                .AddVariable("fields_bar",
+                    new NamedTypeNode(null, new NameNode("String")))
+                .Build();
+
+            // assert
+            QuerySyntaxSerializer.Serialize(newQuery).MatchSnapshot();
+        }
+
+        [Fact]
+        public void BuildRemoteQueryCanOverrideOperationName()
+        {
+            // arrange
+            IImmutableStack<SelectionPathComponent> path =
+                SelectionPathParser.Parse("a.b.c.d(a: $fields:bar)");
+
+            DocumentNode initialQuery =
+                Utf8GraphQLParser.Parse(
+                    @"{
+                        foo {
+                          bar {
+                            baz {
+                              ... on Baz {
+                                qux
+                              }
+                            }
+                          }
+                        }
+                      }
+                    ");
+
+            FieldNode field = initialQuery.Definitions
+                .OfType<OperationDefinitionNode>().Single()
+                .SelectionSet.Selections
+                .OfType<FieldNode>().Single()
+                .SelectionSet.Selections
+                .OfType<FieldNode>().Single();
+
+
+            // act
+            DocumentNode newQuery = RemoteQueryBuilder.New()
+                .SetOperation(new NameNode(nameof(BuildRemoteQueryCanOverrideOperationName)), OperationType.Query)
                 .SetSelectionPath(path)
                 .SetRequestField(field)
                 .AddVariable("fields_bar",
