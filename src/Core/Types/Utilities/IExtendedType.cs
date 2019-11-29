3dcea548aa99155de7df7306abcbfbaa40f1958e
Michael Staib <michael@chillicream.com>
2019-11-29 12:26:29 +0100

    nullable support

---
+++ src/Core/Types/Utilities/IExtendedType.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+#nullable enable
+
+namespace HotChocolate.Utilities
+{
+    internal interface IExtendedType
+    {
+        Type Type { get; }
+
+        Type? Definition { get; }
+
+        ExtendedTypeKind Kind { get; }
+
+        bool IsGeneric { get; }
+
+        bool IsArray { get; }
+
+        bool IsInterface { get; }
+
+        bool IsNullable { get; }
+
+        IReadOnlyList<IExtendedType> TypeArguments { get; }
+
+        IReadOnlyList<IExtendedType> GetInterfaces();
+
+        IExtendedType WithIsNullable(bool isNullable);
+
+        IExtendedType WithTypeArguments(IReadOnlyList<IExtendedType> arguments);
+    }
+}
