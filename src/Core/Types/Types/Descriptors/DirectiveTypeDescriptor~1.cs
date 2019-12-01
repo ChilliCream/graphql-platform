7a1e5f895da720636270a5787cae2e8ee201afaf
Michael Staib <michael@chillicream.com>
2019-12-01 22:36:15 +0100

    reformatted file

--- src/Core/Types/Types/Descriptors/DirectiveTypeDescriptor~1.cs
+++ src/Core/Types/Types/Descriptors/DirectiveTypeDescriptor~1.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using HotChocolate.Utilities;
 using HotChocolate.Language;
+using HotChocolate.Properties;
 using HotChocolate.Resolvers;
 using HotChocolate.Types.Descriptors.Definitions;
-using HotChocolate.Properties;
-using System.Linq;
+using HotChocolate.Utilities;
 
 namespace HotChocolate.Types.Descriptors
 {
