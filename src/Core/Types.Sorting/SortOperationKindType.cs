6e971966da86603cf81ad6ed1562399a01287666
PascalSenn <pascal.senn@gia.ch>
2019-12-02 21:39:27 +0100

    Fixed explicit binding of sorting types.

--- src/Core/Types.Sorting/SortOperationKindType.cs
+++ src/Core/Types.Sorting/SortOperationKindType.cs
@@ -3,5 +3,11 @@ namespace HotChocolate.Types.Sorting
     public class SortOperationKindType
         : EnumType<SortOperationKind>
     {
+        protected override void Configure(IEnumTypeDescriptor<SortOperationKind> descriptor)
+        {
+            base.Configure(descriptor);
+            descriptor.Value(SortOperationKind.Asc);
+            descriptor.Value(SortOperationKind.Desc);
+        }
     }
 }
