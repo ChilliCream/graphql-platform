bb22f53e25aa469d71d1c0e6e06da243e10bbd80
Nigel Sampson <nigel.sampson@pushpay.com>
2019-12-11 15:47:28 +1300

    Add failing test

--- src/Stitching/Stitching.Tests/Middleware/TypeExtensionTests.cs
+++ src/Stitching/Stitching.Tests/Middleware/TypeExtensionTests.cs
@@ -7,6 +7,8 @@ using HotChocolate.Execution;
 using HotChocolate.Types;
 using System.Threading.Tasks;
 using HotChocolate.AspNetCore.Tests.Utilities;
+using System.Collections.Generic;
+using HotChocolate.Language;
 
 namespace HotChocolate.Stitching
 {
@@ -56,6 +58,42 @@ namespace HotChocolate.Stitching
 
             // assert
             Snapshot.Match(result);
+        }
+
+        [Fact]
+        public async Task AddObjectTypeExtensionWithResolver()
+        {
+            // arrange
+            IHttpClientFactory clientFactory = CreateRemoteSchemas();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton(clientFactory);
+            serviceCollection.AddStitchedSchema(builder =>
+                builder.AddSchemaFromHttp("contract")
+                    .AddSchemaFromHttp("customer")
+                    .AddSchemaConfiguration(c => c
+                        .RegisterType<PaginationAmountType>()
+                        .RegisterType(new ObjectTypeExtension(d => d
+                            .Name("Query")
+                            .Field<ObjectResolver>(r => r.GetStitchedCustomer())
+                            .Type(new NamedTypeNode("Customer"))))));
+
+            IServiceProvider services =
+                serviceCollection.BuildServiceProvider();
+
+            ISchema schema = services
+                .GetRequiredService<ISchema>();
+
+            Snapshot.Match(schema.ToString());
+        }
+
+        public class ObjectResolver
+        {
+            public IReadOnlyDictionary<string, object> GetStitchedCustomer() => new Dictionary<string, object> {
+                { "Id", "id" },
+                { "Name", "John Doe" },
+                { "Street", "Evergreen Tc" }
+            };
         }
 
         [Fact]
