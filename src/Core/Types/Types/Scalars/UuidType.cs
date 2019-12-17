f4b06718cb857d65e441326181f8064c3d06241a
Michael Staib <michael@chillicream.com>
2019-12-17 10:42:25 +0100

    Added format option to the uuid type

--- src/Core/Types/Types/Scalars/UuidType.cs
+++ src/Core/Types/Types/Scalars/UuidType.cs
@@ -3,46 +3,49 @@ using System;
 using HotChocolate.Language;
 using HotChocolate.Properties;
 
+#nullable enable
+
 namespace HotChocolate.Types
 {
     public sealed class UuidType
         : ScalarType<Guid, StringValueNode>
     {
-        private const char _format = 'N';
+        private readonly string _format;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="UuidType"/> class.
         /// </summary>
-        public UuidType()
-            : base(ScalarNames.Uuid)
+        public UuidType(char format = '\0')
+            : this(ScalarNames.Uuid, format)
         {
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="UuidType"/> class.
         /// </summary>
-        public UuidType(NameString name)
-            : base(name)
+        public UuidType(NameString name, char format = '\0')
+            : this(name, null, format)
         {
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="UuidType"/> class.
         /// </summary>
-        public UuidType(NameString name, string description)
+        public UuidType(NameString name, string? description, char format = '\0')
             : base(name)
         {
             Description = description;
+            _format = CreateFormatString(format);
         }
 
         protected override bool IsInstanceOfType(StringValueNode literal)
         {
-            return Utf8Parser.TryParse(literal.AsSpan(), out Guid _, out int _, _format);
+            return Utf8Parser.TryParse(literal.AsSpan(), out Guid _, out int _, _format[0]);
         }
 
         protected override Guid ParseLiteral(StringValueNode literal)
         {
-            if (Utf8Parser.TryParse(literal.AsSpan(), out Guid g, out int _, _format))
+            if (Utf8Parser.TryParse(literal.AsSpan(), out Guid g, out int _, _format[0]))
             {
                 return g;
             }
@@ -54,10 +57,10 @@ namespace HotChocolate.Types
 
         protected override StringValueNode ParseValue(Guid value)
         {
-            return new StringValueNode(value.ToString("N"));
+            return new StringValueNode(value.ToString(_format));
         }
 
-        public override bool TrySerialize(object value, out object serialized)
+        public override bool TrySerialize(object value, out object? serialized)
         {
             if (value is null)
             {
@@ -67,7 +70,7 @@ namespace HotChocolate.Types
 
             if (value is Guid uri)
             {
-                serialized = uri.ToString("N");
+                serialized = uri.ToString(_format);
                 return true;
             }
 
@@ -75,7 +78,7 @@ namespace HotChocolate.Types
             return false;
         }
 
-        public override bool TryDeserialize(object serialized, out object value)
+        public override bool TryDeserialize(object serialized, out object? value)
         {
             if (serialized is null)
             {
@@ -97,6 +100,28 @@ namespace HotChocolate.Types
 
             value = null;
             return false;
+        }
+
+        private static string CreateFormatString(char format)
+        {
+            if (format != '\0'
+                && format != 'N'
+                && format != 'D'
+                && format != 'B'
+                && format != 'P')
+            {
+                throw new ArgumentException(
+                    "Unknown format. Guid supports the following format chars: " +
+                    $"{{ `N`, `D`, `B`, `P` }}.{Environment.NewLine}" +
+                    "https://docs.microsoft.com/en-us/dotnet/api/" +
+                    "system.buffers.text.utf8parser.tryparse?" +
+                    "view=netcore-3.1#System_Buffers_Text_Utf8Parser_" +
+                    "TryParse_System_ReadOnlySpan_System_Byte__System_Guid__" +
+                    "System_Int32__System_Char_",
+                    nameof(format));
+            }
+
+            return format == '\0' ? "N" : format.ToString();
         }
     }
 }
