2e6847c088e638a6a4c0318e791fbeb309e7d8ac
Michael Staib <michael@chillicream.com>
2019-12-19 15:45:03 +0100

    Added enviornment repository tests

--- src/SchemaRegistry/Repositories.Mongo.Tests/EnvironmentRepositoryTests.cs
+++ src/SchemaRegistry/Repositories.Mongo.Tests/EnvironmentRepositoryTests.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
 using Squadron;
@@ -18,13 +19,59 @@ namespace MarshmallowPie.Repositories.Mongo
         }
 
         [Fact]
-        public async Task Foo()
+        public async Task GetEnvironments()
         {
             // arrange
+            var db = new MongoClient();
             IMongoCollection<Environment> collection =
-                _mongoResource.Client
-                    .GetDatabase("Foo")
-                    .GetCollection<Environment>("Baz");
+                _mongoResource.CreateCollection<Environment>();
+
+            var initial = new Environment("foo", "bar");
+            await collection.InsertOneAsync(initial, options: null, default);
+
+            var repository = new EnvironmentRepository(collection);
+
+            // act
+            Environment retrieved = repository.GetEnvironments()
+                .Where(t => t.Id == initial.Id)
+                .FirstOrDefault();
+
+            // assert
+            var maps = BsonClassMap.GetRegisteredClassMaps();
+            Assert.Equal(initial.Name, retrieved.Name);
+            Assert.Equal(initial.Description, retrieved.Description);
+        }
+
+        [Fact]
+        public async Task GetEnvironment()
+        {
+            // arrange
+            var db = new MongoClient();
+            IMongoCollection<Environment> collection =
+                _mongoResource.CreateCollection<Environment>();
+
+            var initial = new Environment("foo", "bar");
+            await collection.InsertOneAsync(initial, options: null, default);
+
+            var repository = new EnvironmentRepository(collection);
+
+            // act
+            Environment retrieved = await repository.GetEnvironmentAsync(initial.Id);
+
+            // assert
+            var maps = BsonClassMap.GetRegisteredClassMaps();
+            Assert.Equal(initial.Name, retrieved.Name);
+            Assert.Equal(initial.Description, retrieved.Description);
+        }
+
+        [Fact]
+        public async Task AddEnvironment()
+        {
+            // arrange
+            var db = new MongoClient();
+            IMongoCollection<Environment> collection =
+                _mongoResource.CreateCollection<Environment>();
+
 
             var repository = new EnvironmentRepository(collection);
             var environment = new Environment("foo", "bar");
@@ -36,9 +83,35 @@ namespace MarshmallowPie.Repositories.Mongo
             Environment retrieved = await collection.AsQueryable()
                 .Where(t => t.Id == environment.Id)
                 .FirstOrDefaultAsync();
+            var maps = BsonClassMap.GetRegisteredClassMaps();
             Assert.Equal(environment.Name, retrieved.Name);
             Assert.Equal(environment.Description, retrieved.Description);
         }
 
+        [Fact]
+        public async Task UpdateEnvironment()
+        {
+            // arrange
+            var db = new MongoClient();
+            IMongoCollection<Environment> collection =
+                _mongoResource.CreateCollection<Environment>();
+
+            var initial = new Environment("foo", "bar");
+            await collection.InsertOneAsync(initial, options: null, default);
+
+            var repository = new EnvironmentRepository(collection);
+
+            // act
+            var updated = new Environment(initial.Id, initial.Name, "abc");
+            await repository.UpdateEnvironmentAsync(updated);
+
+            // assert
+            Environment retrieved = await collection.AsQueryable()
+                .Where(t => t.Id == initial.Id)
+                .FirstOrDefaultAsync();
+            var maps = BsonClassMap.GetRegisteredClassMaps();
+            Assert.Equal(updated.Name, retrieved.Name);
+            Assert.Equal(updated.Description, retrieved.Description);
+        }
     }
 }
