01420f91fb9e59726babb8485e54ea9235f70c3f
Michael Staib <michael@chillicream.com>
2020-01-02 11:17:08 +0100

    Removed internals visible to

--- src/VisualStudio/StrawberryShake.Language/InternalsVisibleTo.cs
+++ src/VisualStudio/StrawberryShake.Language/InternalsVisibleTo.cs
@@ -1,7 +1,2 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("HotChocolate.Language.Tests")]
-[assembly: InternalsVisibleTo("HotChocolate.Stitching")]
-[assembly: InternalsVisibleTo("HotChocolate.Core")]
-[assembly: InternalsVisibleTo("HotChocolate.Types")]
-[assembly: InternalsVisibleTo("HotChocolate.Subscriptions")]
