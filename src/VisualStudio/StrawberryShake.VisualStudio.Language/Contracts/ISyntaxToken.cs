ba8aa9fa10c24f200287bc532bd32376acd56fda
Michael Staib <michael@chillicream.com>
2020-01-04 20:58:02 +0100

    added missing interface

---
+++ src/VisualStudio/StrawberryShake.VisualStudio.Language/Contracts/ISyntaxToken.cs
@@ -0,0 +1,14 @@
+namespace StrawberryShake.VisualStudio.Language
+{
+    public interface ISyntaxToken
+    {
+        TokenKind Kind { get; }
+        int Start { get; }
+        int End { get; }
+        int Line { get; }
+        int Column { get; }
+        string Value { get; }
+        SyntaxToken Previous { get; }
+        SyntaxToken Next { get; }
+    }
+}
