3017220bd9e924ce50d9426e510694e7b7f067b7
Michael Staib <michael.staib@hotmail.com>
2020-02-01 21:29:54 +0100

    reconfigured presisted queries

--- src/HotChocolate/Utilities/src/Utilities/Properties/UtilityResources.Designer.cs
+++ src/HotChocolate/Utilities/src/Utilities/Properties/UtilityResources.Designer.cs
@@ -10,7 +10,6 @@
 
 namespace HotChocolate.Utilities.Properties {
     using System;
-    using System.Reflection;
     
     
     /// <summary>
@@ -40,7 +39,7 @@ namespace HotChocolate.Utilities.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HotChocolate.Utilities.Properties.UtilityResources", typeof(UtilityResources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HotChocolate.Utilities.Properties.UtilityResources", typeof(UtilityResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
