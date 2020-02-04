f659a0052c638246d1292fa52598bda76a92d8f1
Senn Pascal <Pascal.Senn@gia.ch>
2020-02-04 18:02:29 +0100

    feat(Selection): add selection middleware

---
+++ src/Core/Types.Selection.Tests/SelectionTests.cs
@@ -0,0 +1,298 @@
+using System.Linq;
+using HotChocolate.Execution;
+using HotChocolate.Types.Selection.Extensions;
+using Xunit;
+
+namespace HotChocolate.Types.Selection
+{
+    public class SelectionTests
+    {
+        [Fact]
+        public void Execute_Selection_Fragment()
+        {
+            // arrange
+            Foo[] foos = new[]
+            {
+                Foo.Create("aa",1),
+                Foo.Create("bb",2),
+            };
+            IQueryable<Foo> resultCtx = null;
+            ISchema schema = SchemaBuilder.New()
+                .AddQueryType<Query>(
+                    d => d.Field(t => t.Foos)
+                        .Resolver(foos)
+                        .Use(next => async ctx =>
+                        {
+                            await next(ctx);
+                            resultCtx = ctx.Result as IQueryable<Foo>;
+                        })
+                        .UseSelection())
+                .Create();
+
+            IQueryExecutor executor = schema.MakeExecutable();
+
+            // act
+            executor.Execute(
+                "{ foos { ...test  } } fragment test on Foo { bar baz}");
+
+            // assert
+            Assert.NotNull(resultCtx);
+            Assert.Collection(resultCtx.ToArray(),
+                x =>
+                {
+                    Assert.Equal("aa", x.Bar);
+                    Assert.Equal(1, x.Baz);
+                    Assert.Null(x.Nested);
+                    Assert.Null(x.NestedCollection);
+                },
+                x =>
+                {
+                    Assert.Equal("bb", x.Bar);
+                    Assert.Equal(2, x.Baz);
+                    Assert.Null(x.Nested);
+                    Assert.Null(x.NestedCollection);
+                });
+        }
+
+        [Fact]
+        public void Execute_Selection_Scalar()
+        {
+            // arrange
+            Foo[] foos = new[]
+            {
+                Foo.Create("aa",1),
+                Foo.Create("bb",2),
+            };
+            IQueryable<Foo> resultCtx = null;
+            ISchema schema = SchemaBuilder.New()
+                .AddQueryType<Query>(
+                    d => d.Field(t => t.Foos)
+                        .Resolver(foos)
+                        .Use(next => async ctx =>
+                        {
+                            await next(ctx);
+                            resultCtx = ctx.Result as IQueryable<Foo>;
+                        })
+                        .UseSelection())
+                .Create();
+
+            IQueryExecutor executor = schema.MakeExecutable();
+
+            // act
+            executor.Execute(
+                "{ foos { bar } }");
+
+            // assert
+            Assert.NotNull(resultCtx);
+            Assert.Collection(resultCtx.ToArray(),
+                x =>
+                {
+                    Assert.Equal("aa", x.Bar);
+                    Assert.Equal(0, x.Baz);
+                    Assert.Null(x.Nested);
+                    Assert.Null(x.NestedCollection);
+                },
+                x =>
+                {
+                    Assert.Equal("bb", x.Bar);
+                    Assert.Equal(0, x.Baz);
+                    Assert.Null(x.Nested);
+                    Assert.Null(x.NestedCollection);
+                });
+        }
+
+        [Fact]
+        public void Execute_Selection_MultipleScalar()
+        {
+            // arrange
+            Foo[] foos = new[]
+            {
+                Foo.Create("aa",1),
+                Foo.Create("bb",2),
+            };
+            IQueryable<Foo> resultCtx = null;
+            ISchema schema = SchemaBuilder.New()
+                .AddQueryType<Query>(
+                    d => d.Field(t => t.Foos)
+                        .Resolver(foos)
+                        .Use(next => async ctx =>
+                        {
+                            await next(ctx);
+                            resultCtx = ctx.Result as IQueryable<Foo>;
+                        })
+                        .UseSelection())
+                .Create();
+
+            IQueryExecutor executor = schema.MakeExecutable();
+
+            // act
+            executor.Execute(
+                "{ foos { bar baz } }");
+
+            // assert
+            Assert.NotNull(resultCtx);
+            Assert.Collection(resultCtx.ToArray(),
+                x =>
+                {
+                    Assert.Equal("aa", x.Bar);
+                    Assert.Equal(1, x.Baz);
+                    Assert.Null(x.Nested);
+                    Assert.Null(x.NestedCollection);
+                },
+                x =>
+                {
+                    Assert.Equal("bb", x.Bar);
+                    Assert.Equal(2, x.Baz);
+                    Assert.Null(x.Nested);
+                    Assert.Null(x.NestedCollection);
+                });
+        }
+
+        [Fact]
+        public void Execute_Selection_Object()
+        {
+            // arrange
+            Foo[] foos = new[]
+            {
+                Foo.Create("aa",1),
+                Foo.Create("bb",2),
+            };
+            IQueryable<Foo> resultCtx = null;
+            ISchema schema = SchemaBuilder.New()
+                .AddQueryType<Query>(
+                    d => d.Field(t => t.Foos)
+                        .Resolver(foos)
+                        .Use(next => async ctx =>
+                        {
+                            await next(ctx);
+                            resultCtx = ctx.Result as IQueryable<Foo>;
+                        })
+                        .UseSelection())
+                .Create();
+
+            IQueryExecutor executor = schema.MakeExecutable();
+
+            // act
+            executor.Execute(
+                "{ foos { nested { bar } } }");
+
+            // assert
+            Assert.NotNull(resultCtx);
+            Assert.Collection(resultCtx.ToArray(),
+                x =>
+                {
+                    Assert.Null(x.Bar);
+                    Assert.Equal(0, x.Baz);
+                    Assert.NotNull(x.Nested);
+                    Assert.Equal("nestedaa", x.Nested.Bar);
+                    Assert.Equal(2, x.Nested.Baz);
+                    Assert.Null(x.NestedCollection);
+                },
+                x =>
+                {
+                    Assert.Null(x.Bar);
+                    Assert.Equal(0, x.Baz);
+                    Assert.NotNull(x.Nested);
+                    Assert.Equal("nestedbb", x.Nested.Bar);
+                    Assert.Equal(4, x.Nested.Baz);
+                    Assert.Null(x.NestedCollection);
+                });
+        }
+        [Fact]
+        public void Execute_Selection_Collection()
+        {
+            // arrange
+            Foo[] foos = new[]
+            {
+                Foo.Create("aa",1),
+                Foo.Create("bb",2),
+            };
+            IQueryable<Foo> resultCtx = null;
+            ISchema schema = SchemaBuilder.New()
+                .AddQueryType<Query>(
+                    d => d.Field(t => t.Foos)
+                        .Resolver(foos)
+                        .Use(next => async ctx =>
+                        {
+                            await next(ctx);
+                            resultCtx = ctx.Result as IQueryable<Foo>;
+                        })
+                        .UseSelection())
+                .Create();
+
+            IQueryExecutor executor = schema.MakeExecutable();
+
+            // act
+            executor.Execute(
+                "{ foos { nestedCollection { bar } } }");
+
+            // assert
+            Assert.NotNull(resultCtx);
+            Assert.Collection(resultCtx.ToArray(),
+                x =>
+                {
+                    Assert.Null(x.Bar);
+                    Assert.Equal(0, x.Baz);
+                    Assert.Null(x.Nested);
+                    Assert.Single(x.NestedCollection);
+                    Assert.Equal("nestedcollectionaa", x.NestedCollection[0].Bar);
+                    Assert.Equal(3, x.NestedCollection[0].Baz);
+                },
+                x =>
+                {
+                    Assert.Null(x.Bar);
+                    Assert.Equal(0, x.Baz);
+                    Assert.Null(x.Nested);
+                    Assert.Single(x.NestedCollection);
+                    Assert.Equal("nestedcollectionbb", x.NestedCollection[0].Bar);
+                    Assert.Equal(6, x.NestedCollection[0].Baz);
+                });
+        }
+    }
+
+
+    public class Query
+    {
+        public IQueryable<Foo> Foos { get; }
+    }
+
+    public class Foo
+    {
+        public string Bar { get; set; }
+
+        public int Baz { get; set; }
+
+        public NestedFoo Nested { get; set; }
+        public NestedFoo[] NestedCollection { get; set; }
+
+
+        public static Foo Create(string bar, int baz)
+        {
+            return new Foo
+            {
+                Bar = bar,
+                Baz = baz,
+                Nested = new NestedFoo()
+                {
+                    Bar = "nested" + bar,
+                    Baz = baz * 2
+                },
+                NestedCollection = new NestedFoo[]
+                   {
+                        new NestedFoo()
+                        {
+                            Bar = "nestedcollection" + bar,
+                            Baz = baz * 3
+                        },
+                   }
+            };
+        }
+    }
+
+    public class NestedFoo
+    {
+        public string Bar { get; set; }
+
+        public int Baz { get; set; }
+    }
+}
---
+++ src/Core/Types.Selection/SelectionMiddleware~1.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+using HotChocolate.Resolvers;
+using HotChocolate.Types.Relay;
+
+namespace HotChocolate.Types.Selection
+{
+    public class SelectionMiddleware<T>
+    {
+        private readonly FieldDelegate _next;
+
+        public SelectionMiddleware(FieldDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+
+        public async Task InvokeAsync(IMiddlewareContext context)
+        {
+            await _next(context).ConfigureAwait(false);
+
+            IQueryable<T> source = null;
+            IReadOnlyList<IFieldSelection> selection;
+            if (context.Result is PageableData<T> p)
+            {
+                // TODO add support for pagable data!
+                throw new Exception("NOT SUPPORTED");
+                source = p.Source;
+            }
+            else
+            {
+                if (context.Field.Type.NamedType() is ObjectType type)
+                {
+                    selection = context.CollectFields(type);
+                }
+                else
+                {
+                    throw new Exception("Illegal type");
+                }
+                p = null;
+            }
+
+            if (context.Result is IQueryable<T> q)
+            {
+                source = q;
+            }
+            else if (context.Result is IEnumerable<T> e)
+            {
+                source = e.AsQueryable();
+            }
+
+            IEnumerable<MemberInfo> members = selection.Select(x => x.Field.Member);
+            Expression<Func<T, T>> projection = SelectionExpressionBuilder.Project<T>(members);
+            context.Result = source.Select(projection);
+        }
+    }
+}
+
