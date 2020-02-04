924c991557c3ef3f5aef3ee191ef9339c6beda03
Senn Pascal <Pascal.Senn@gia.ch>
2020-02-04 15:26:03 +0100

    feat(Selection): add project

---
+++ src/Core/Types.Selection/SelectionMiddleware.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotChocolate.Types.Selection
+{
+    public class SelectionMiddleware
+    {
+    }
+}
