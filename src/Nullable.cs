d768b1d8123e5699c081a2725111e9dc62651832
Michael Staib <michael@chillicream.com>
2020-02-18 09:07:15 +0100

    Enabled nullable ref type support for .net core 2.1

--- src/Nullable.cs
+++ src/Nullable.cs
@@ -1,4 +1,4 @@
-#if NETSTANDARD2_1
+#if NETSTANDARD2_0 || NETCOREAPP2_1
 
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
