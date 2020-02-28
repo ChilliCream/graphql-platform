23037b8dd26f1ab08972e1414451796b4537aab9
Michael Staib <michael@chillicream.com>
2020-02-28 09:55:05 +0100

    added dataloader base classes

--- src/Core/Types/DataLoader/FetchGroupedDataLoader.cs
+++ src/Core/Types/DataLoader/FetchGroupedDataLoader.cs
@@ -3,44 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using GreenDonut;
 
 namespace HotChocolate.DataLoader
 {
     internal sealed class FetchGroupedDataLoader<TKey, TValue>
-        : DataLoaderBase<TKey, TValue[]>
+        : GroupedDataLoader<TKey, TValue>
     {
         private readonly FetchGroup<TKey, TValue> _fetch;
 
         public FetchGroupedDataLoader(FetchGroup<TKey, TValue> fetch)
-            : base(new DataLoaderOptions<TKey>
-            {
-                AutoDispatching = false,
-                Batching = true,
-                CacheSize = DataLoaderDefaults.CacheSize,
-                MaxBatchSize = DataLoaderDefaults.MaxBatchSize,
-                SlidingExpiration = TimeSpan.Zero
-            })
         {
             _fetch = fetch ?? throw new ArgumentNullException(nameof(fetch));
         }
 
-        protected override async Task<IReadOnlyList<Result<TValue[]>>>
-            FetchAsync(
-                IReadOnlyList<TKey> keys,
-                CancellationToken cancellationToken)
+        protected override Task<ILookup<TKey, TValue>> LoadGroupedBatchAsync(
+            IReadOnlyList<TKey> keys,
+            CancellationToken cancellationToken)
         {
-            ILookup<TKey, TValue> result = await _fetch(keys)
-                .ConfigureAwait(false);
-
-            var items = new Result<TValue[]>[keys.Count];
-
-            for (int i = 0; i < keys.Count; i++)
-            {
-                items[i] = result[keys[i]].ToArray();
-            }
-
-            return items;
+            return _fetch(keys);
         }
     }
 }
