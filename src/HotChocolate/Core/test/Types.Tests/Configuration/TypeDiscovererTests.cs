14802168c6aa1b059b4d055e407e3e30383aee1a
Michael Staib <michael@chillicream.com>
2020-03-05 13:22:57 +0100

    fixed more tests

--- src/HotChocolate/Core/test/Types.Tests/Configuration/TypeDiscovererTests.cs
+++ src/HotChocolate/Core/test/Types.Tests/Configuration/TypeDiscovererTests.cs
@@ -36,7 +36,7 @@ namespace HotChocolate.Configuration
             DiscoveredTypes result = typeDiscoverer.DiscoverTypes();
 
             // assert
-            Assert.Null(result.Errors);
+            Assert.Empty(result.Errors);
 
             result.Types
                 .Select(t => t.Type)
@@ -77,7 +77,7 @@ namespace HotChocolate.Configuration
             DiscoveredTypes result = typeDiscoverer.DiscoverTypes();
 
             // assert
-            Assert.Null(result.Errors);
+            Assert.Empty(result.Errors);
 
             result.Types
                 .Select(t => t.Type)
@@ -121,7 +121,7 @@ namespace HotChocolate.Configuration
             DiscoveredTypes result = typeDiscoverer.DiscoverTypes();
 
             // assert
-            Assert.Null(result.Errors);
+            Assert.Empty(result.Errors);
 
             result.Types
                 .Select(t => t.Type)
