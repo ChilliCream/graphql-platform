94e0186f698acaef49bb402ee0c12c1ffd432114
John Brandes <john.brandes0x@gmail.com>
2020-03-08 12:57:22 +0100

    Include error documentation and result documentation into the GraphQL description. (#1524, #1516)
    Co-authored-by: Moritz Brandes <mb@grannyandsmith.com>
    Co-authored-by: Michael Staib <michael@chillicream.com>

---
+++ src/HotChocolate/Core/src/Types/Types/Descriptors/Conventions/NoopDocumentationProvider.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Reflection;
+
+#nullable enable
+
+namespace HotChocolate.Types.Descriptors
+{
+    internal sealed class NoopDocumentationProvider
+        : IDocumentationProvider
+    {
+        public string? GetDescription(Type type) => null;
+
+        public string? GetDescription(MemberInfo member) => null;
+
+        public string? GetDescription(ParameterInfo parameter) => null;
+    }
+}
