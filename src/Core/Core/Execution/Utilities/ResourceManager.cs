0be12cd0e10cf3ad7b3782112293fefd098abd66
Michael Staib <michael@chillicream.com>
2020-03-10 01:08:47 +0100

    Added some more infos

--- src/Core/Core/Execution/Utilities/ResourceManager.cs
+++ src/Core/Core/Execution/Utilities/ResourceManager.cs
@@ -2,6 +2,31 @@ namespace HotChocolate.Execution
 {
     internal sealed class ResourceManager
     {
+        /*
+while(resolvers.Count > 0)
+{
+    bool waitForResource = true;
 
+    for(int i = 0; i < resolvers.Count; i++)
+    {
+        ResolverContext context = resolvers.Dequeue();
+        if(resourceManager.TryEnqueue(context))
+        {
+            waitForResource = false;
+        }
+        else
+        {
+            resolvers.Enqueue(context);
+        }
+    }
+
+    if(waitForResource)
+    {
+        await resourceManager.WaitForResourceAsync();
+    }
+}
+
+await resourceManager.CompleteAsync();
+        */
     }
 }
