a1be7ce5a4706b4474b4c35bdf7c879faf287f3b
Michael Staib <michael@chillicream.com>
2020-03-12 14:52:22 +0100

    Added tests

--- src/Core/Types/Types/Attributes/SubscribeAttribute.cs
+++ src/Core/Types/Types/Attributes/SubscribeAttribute.cs
@@ -4,7 +4,8 @@ using System;
 
 namespace HotChocolate.Types
 {
-    public class SubscribeAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+    public sealed class SubscribeAttribute : Attribute
     {
         public SubscribeAttribute(string resolverName)
         {
