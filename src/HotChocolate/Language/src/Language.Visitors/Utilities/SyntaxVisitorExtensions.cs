271712213ed198330d703eddc608bccff4086e97
Michael Staib <michael@chillicream.com>
2020-03-19 22:20:32 +0100

    Reorganized files

--- src/HotChocolate/Language/src/Language.Visitors/Utilities/SyntaxVisitorExtensions.cs
+++ src/HotChocolate/Language/src/Language.Visitors/Utilities/SyntaxVisitorExtensions.cs
@@ -2,12 +2,9 @@ namespace HotChocolate.Language.Visitors
 {
     public static class SyntaxVisitorExtensions
     {
-        private static readonly EmptySyntaxVisitorContext _empty =
-            new EmptySyntaxVisitorContext();
+        private static readonly EmptySyntaxVisitorContext _empty = new EmptySyntaxVisitorContext();
 
-        public static ISyntaxVisitorAction Visit(
-            this ISyntaxVisitor visitor,
-            ISyntaxNode node) =>
+        public static ISyntaxVisitorAction Visit(this ISyntaxVisitor visitor, ISyntaxNode node) =>
             visitor.Visit(node, _empty);
 
         private sealed class EmptySyntaxVisitorContext
