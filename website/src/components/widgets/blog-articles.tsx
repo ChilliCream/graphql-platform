8c860df86e85ea7bb122b0ec9759bd397d4eeedf
Rafael Staib <me@rafaelstaib.com>
2020-03-22 15:20:11 +0100

    Chore

--- website/src/components/widgets/blog-articles.tsx
+++ website/src/components/widgets/blog-articles.tsx
@@ -2,6 +2,9 @@ import Img, { FluidObject } from "gatsby-image";
 import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 import { GetBlogArticlesQuery } from "../../../graphql-types";
+import { ArticleTitle } from "../misc/blog-article-elements";
+import { BlogArticleMetadata } from "../misc/blog-article-metadata";
+import { BlogArticleTags } from "../misc/blog-article-tags";
 import { Link } from "../misc/link";
 
 interface BlogArticlesProperties {
@@ -21,6 +24,7 @@ export const BlogArticles: FunctionComponent<BlogArticlesProperties> = ({
               tag => tag && tag.length > 0
             ) as string[])
           : [];
+
         return (
           <Article key={`article-${node.id}`}>
             <Link to={node.frontmatter!.path!}>
@@ -32,24 +36,16 @@ export const BlogArticles: FunctionComponent<BlogArticlesProperties> = ({
                   }
                 />
               )}
-              <Title>{node.frontmatter!.title}</Title>
+              <ArticleTitle>{node.frontmatter!.title}</ArticleTitle>
             </Link>
-            <Metadata>
-              <AuthorLink to={node.frontmatter!.authorUrl!}>
-                <AuthorImage src={node.frontmatter!.authorImageUrl!} />
-                {node.frontmatter!.author}
-              </AuthorLink>{" "}
-              ・ {node.frontmatter!.date} ・ {node.fields!.readingTime!.text}
-            </Metadata>
-            {existingTags.length > 0 && (
-              <Tags>
-                {existingTags.map(tag => (
-                  <Tag key={tag}>
-                    <TagLink to={`/blog/tags/${tag}`}>{tag}</TagLink>
-                  </Tag>
-                ))}
-              </Tags>
-            )}
+            <BlogArticleMetadata
+              author={node.frontmatter!.author!}
+              authorImageUrl={node.frontmatter!.authorImageUrl!}
+              authorUrl={node.frontmatter!.authorUrl!}
+              date={node.frontmatter!.date!}
+              readingTime={node.fields!.readingTime!.text!}
+            />
+            <BlogArticleTags tags={existingTags} />
           </Article>
         );
       })}
@@ -73,69 +69,4 @@ const Article = styled.li`
   @media only screen and (min-width: 800px) {
     border: 1px solid #ccc;
   }
-`;
-
-const Title = styled.h1`
-  margin-top: 20px;
-  margin-right: 20px;
-  margin-left: 20px;
-  font-size: 1.667em;
-
-  @media only screen and (min-width: 800px) {
-    margin-right: 50px;
-    margin-left: 50px;
-  }
-`;
-
-const Metadata = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin: 0 20px 20px;
-  font-size: 0.778em;
-
-  @media only screen and (min-width: 800px) {
-    margin: 0 50px 20px;
-  }
-`;
-
-const AuthorLink = styled(Link)`
-  display: flex;
-  flex: 0 0 auto;
-  flex-direction: row;
-  align-items: center;
-  color: #666;
-`;
-
-const AuthorImage = styled.img`
-  flex: 0 0 auto;
-  margin-right: 0.5em;
-  border-radius: 15px;
-  width: 30px;
-`;
-
-const Tags = styled.ul`
-  margin: 0 20px 20px;
-  list-style-type: none;
-
-  @media only screen and (min-width: 800px) {
-    margin: 0 50px 20px;
-  }
-`;
-
-const Tag = styled.li`
-  display: inline-block;
-  margin: 0 5px 0 0;
-  border-radius: 4px;
-  padding: 0;
-  background-color: #f40010;
-  font-size: 0.722em;
-  letter-spacing: 0.05em;
-  color: #fff;
-`;
-
-const TagLink = styled(Link)`
-  display: block;
-  padding: 5px 15px;
-  color: #fff;
 `;
