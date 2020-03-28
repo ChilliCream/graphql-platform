4e4161498a4c654c04ef08e8f27a7a57c91246b4
Pascal Senn <senn.pasc@gmail.com>
2020-03-28 22:15:29 +0100

    fix merge issue

--- src/HotChocolate/Filters/src/Types.Selections/Handlers/FilterHandler.cs
+++ src/HotChocolate/Filters/src/Types.Selections/Handlers/FilterHandler.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Linq.Expressions;
 using HotChocolate.Language;
+using HotChocolate.Language.Visitors;
 using HotChocolate.Resolvers;
 using HotChocolate.Types.Filters;
 
@@ -21,7 +22,7 @@ namespace HotChocolate.Types.Selections.Handlers
                 var visitor = new QueryableFilterVisitor(
                     iot, fit.EntityType, context.Conversion, false);
 
-                filter.Accept(visitor);
+                visitor.Visit(filter);
 
                 return Expression.Call(
                     typeof(Enumerable),
