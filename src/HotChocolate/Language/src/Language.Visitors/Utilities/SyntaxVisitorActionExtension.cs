3f791b45cbabd1b9ccddc4dc2b109477e2e0801e
Pascal Senn <senn.pasc@gmail.com>
2020-04-09 15:21:50 +0200

    fixformat

--- src/HotChocolate/Language/src/Language.Visitors/Utilities/SyntaxVisitorActionExtension.cs
+++ src/HotChocolate/Language/src/Language.Visitors/Utilities/SyntaxVisitorActionExtension.cs
@@ -6,10 +6,12 @@ namespace HotChocolate.Language.Visitors
         {
             return action.Kind == SyntaxVisitorActionKind.Break;
         }
+
         public static bool IsContinue(this ISyntaxVisitorAction action)
         {
             return action.Kind == SyntaxVisitorActionKind.Continue;
         }
+        
         public static bool IsSkip(this ISyntaxVisitorAction action)
         {
             return action.Kind == SyntaxVisitorActionKind.Skip;
