926597701625d5ea79185c4c1db836f4cce4862f
Michael Staib <michael@michaels@chillicream.com>
2020-04-09 08:14:18 +0200

    wip

--- src/HotChocolate/Core/src/Validation/Options/IMaxComplexityOptionsAccessor.cs
+++ src/HotChocolate/Core/src/Validation/Options/IMaxComplexityOptionsAccessor.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using HotChocolate.Language;
 using HotChocolate.Types;
 
 namespace HotChocolate.Validation.Options
@@ -8,11 +11,16 @@ namespace HotChocolate.Validation.Options
         CostDirective? cost,
         int fieldDepth,
         int nodeDepth,
-        Func<string, object?> getVariable);
+        Func<string, object?> getVariable,
+        IMaxComplexityOptionsAccessor options);
 
     public interface IMaxComplexityOptionsAccessor
     {
+        int DefaultCost { get; }
+
         int? MaxAllowedComplexity { get; }
+
+        bool UseComplexityMultipliers { get; }
 
         ComplexityCalculation Calculation { get; }
     }
@@ -21,11 +29,67 @@ namespace HotChocolate.Validation.Options
     {
         public MaxComplexityOptions()
         {
-
+            ComplexityCalculation
         }
+
+        public int DefaultCost { get; set; }
 
         public int? MaxAllowedComplexity { get; set; }
 
+        public bool UseComplexityMultipliers { get; set; }
+
         public ComplexityCalculation Calculation { get; set; }
+
+        public static int DefaultCalculation(
+            IOutputField field,
+            FieldNode selection,
+            CostDirective? cost,
+            int fieldDepth,
+            int nodeDepth,
+            Func<string, object?> getVariable,
+            IMaxComplexityOptionsAccessor options)
+        {
+            if (cost is null)
+            {
+                return options.DefaultCost;
+            }
+
+            if (options.UseComplexityMultipliers)
+            {
+                if (cost.Multipliers.Count == 0)
+                {
+                    return cost.Complexity;
+                }
+
+                int complexity = 0;
+
+                for (int i = 0; i < cost.Multipliers.Count; i++)
+                {
+                    MultiplierPathString multiplier =  cost.Multipliers[i];
+                    ArgumentNode argument = selection.Arguments.FirstOrDefault(t => t.Name.Value.Equals(multiplier.Value));
+                    if (MultiplierHelper.TryGetMultiplierValue(
+                        context.FieldSelection, context.Variables,
+                        context.Cost.Multipliers[i], out int multiplier))
+                    {
+                        complexity += context.Cost.Complexity * multiplier;
+                    }
+                    else
+                    {
+                        complexity += context.Cost.Complexity;
+                    }
+                }
+
+                return complexity;
+            }
+        }
+    }
+
+    public interface IMaxExecutionDepthOptionsAccessor
+    {
+        /// <summary>
+        /// Gets the maximum allowed depth of a query. The default value is
+        /// <see langword="null"/>. The minimum allowed value is <c>1</c>.
+        /// </summary>
+        int? MaxAllowedExecutionDepth { get; }
     }
 }
