ff36be167c8df0198263c6b353b593ddca6877a6
Pascal Senn <senn.pasc@gmail.com>
2020-04-09 10:52:15 +0200

    feat(Validation): visitor pushs path only on continue

--- src/HotChocolate/Language/src/Language.Visitors/Utilities/SyntaxVisitorActionExtension.cs
+++ src/HotChocolate/Language/src/Language.Visitors/Utilities/SyntaxVisitorActionExtension.cs
@@ -6,5 +6,13 @@ namespace HotChocolate.Language.Visitors
         {
             return action.Kind == SyntaxVisitorActionKind.Break;
         }
+        public static bool IsContinue(this ISyntaxVisitorAction action)
+        {
+            return action.Kind == SyntaxVisitorActionKind.Continue;
+        }
+        public static bool IsSkip(this ISyntaxVisitorAction action)
+        {
+            return action.Kind == SyntaxVisitorActionKind.Skip;
+        }
     }
 }
