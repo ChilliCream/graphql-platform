ffa13416087776adbb75ffdbd5d0670273fc9f52
Pascal Senn <senn.pasc@gmail.com>
2020-04-09 21:53:35 +0200

    feat(Validation): add unqie input field names rule

--- src/HotChocolate/Core/test/Validation.Tests/InputObjectFieldUniquenessRuleTests.cs
+++ src/HotChocolate/Core/test/Validation.Tests/InputObjectFieldUniquenessRuleTests.cs
@@ -32,5 +32,118 @@ namespace HotChocolate.Validation
             ",
             error => Assert.Equal("Field `name` is ambiguous.", error.Message));
         }
+
+        [Fact]
+        public void InputObjectWithField()
+        {
+            ExpectValid(@"
+                {
+                    arguments {
+                        complexArgField(complexArg: {requiredField: true, f: true })
+                    }
+                }
+            ");
+        }
+
+        [Fact]
+        public void SameInputObjectWithinTwoArgs()
+        {
+            ExpectValid(@"
+                {
+                    arguments {
+                        complexArgField(
+                            complexArg1: {requiredField: true, f: true }, 
+                            complexArg2: {requiredField: true, f: true })
+                    }
+                }
+            ");
+        }
+
+        [Fact]
+        public void MultipleInputObjectFields()
+        {
+            ExpectValid(@"
+                {
+                    arguments {
+                        complexArgField(complexArg: {
+                            requiredField: true,
+                            f1: ""value"",
+                            f2: ""value"",
+                            f3: ""value"" })
+                    }
+                }
+            ");
+        }
+
+        [Fact]
+        public void AllowsForNestedInputObjectsWithSimilarFields()
+        {
+            ExpectValid(@"
+                {
+                    arguments {
+                        complexArgField(complexArg: {
+                            requiredField: true
+                            deep: {
+                                requiredField: true
+                                deep: {
+                                    requiredField: true
+                                    id: 1
+                                }
+                                id: 1
+                            }
+                            id: 1
+                        })
+                    }
+                }
+            ");
+        }
+
+        [Fact]
+        public void DuplicateInputObjectFields()
+        {
+            ExpectErrors(@"
+                {
+                    arguments {
+                        complexArgField(complexArg: {
+                            requiredField: true,
+                            f1: ""value"",
+                            f1: ""value"" })
+                    }
+                }
+            ");
+        }
+
+        [Fact]
+        public void ManyDuplicateInputObjectFields()
+        {
+            ExpectErrors(@"
+                {
+                    arguments {
+                        complexArgField(complexArg: {
+                            requiredField: true,
+                            f1: ""value"",
+                            f1: ""value"",
+                            f1: ""value"" })
+                    }
+                }
+            ");
+        }
+
+        [Fact]
+        public void NestedDuplicateInputObjectFields()
+        {
+            ExpectErrors(@"
+                {
+                    arguments {
+                        complexArgField(complexArg: {
+                            requiredField: true,
+                            deep: {
+                                requiredField:true
+                                f2: ""value"",
+                                f2: ""value"" }})
+                    }
+                }
+            ");
+        }
     }
 }
