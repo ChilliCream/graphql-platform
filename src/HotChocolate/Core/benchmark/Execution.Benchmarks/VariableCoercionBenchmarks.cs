b976fdf434e77cd31fac4220cf3828dde335ae61
Michael Staib <michael@chillicream.com>
2020-04-15 15:06:50 +0200

    Refined the benchmarks

--- src/HotChocolate/Core/benchmark/Execution.Benchmarks/VariableCoercionBenchmarks.cs
+++ src/HotChocolate/Core/benchmark/Execution.Benchmarks/VariableCoercionBenchmarks.cs
@@ -12,7 +12,7 @@ namespace HotChocolate.Execution.Benchmarks
     {
         private readonly VariableCoercionHelper _variableCoercionHelper;
         private readonly ISchema _schema;
-        private readonly  List<VariableDefinitionNode> _stringDefinition =
+        private readonly List<VariableDefinitionNode> _stringDefinition =
             new List<VariableDefinitionNode>
             {
                 new VariableDefinitionNode(
@@ -22,17 +22,37 @@ namespace HotChocolate.Execution.Benchmarks
                     new StringValueNode("def"),
                     Array.Empty<DirectiveNode>())
             };
-        private readonly  Dictionary<string, object> _stringLiteral =
+        private readonly List<VariableDefinitionNode> _objectDefinition =
+            new List<VariableDefinitionNode>
+            {
+                new VariableDefinitionNode(
+                    null,
+                    new VariableNode("abc"),
+                    new NamedTypeNode("ReviewInput"),
+                    new StringValueNode("def"),
+                    Array.Empty<DirectiveNode>())
+            };
+        private readonly Dictionary<string, object> _stringLiteral =
             new Dictionary<string, object>
             {
                 {"abc", new StringValueNode("foo")}
             };
-        private readonly  Dictionary<string, object> _string =
+        private readonly Dictionary<string, object> _objectLiteral =
+            new Dictionary<string, object>
+            {
+                {"abc", new ObjectValueNode(new ObjectFieldNode("stars", 5))}
+            };
+        private readonly Dictionary<string, object> _string =
             new Dictionary<string, object>
             {
                 {"abc","foo"}
             };
-        private readonly  Dictionary<string, VariableValue> _result =
+        private readonly Dictionary<string, object> _object =
+            new Dictionary<string, object>
+            {
+                {"abc", new Dictionary<string, object> { {"stars", 5} }}
+            };
+        private readonly Dictionary<string, VariableValue> _result =
             new Dictionary<string, VariableValue>();
 
         public VariableCoercionBenchmarks()
@@ -55,6 +75,22 @@ namespace HotChocolate.Execution.Benchmarks
             _result.Clear();
             _variableCoercionHelper.CoerceVariableValues(
                 _schema, _stringDefinition, _string, _result);
+        }
+
+        [Benchmark]
+        public void One_Object_Literal()
+        {
+            _result.Clear();
+            _variableCoercionHelper.CoerceVariableValues(
+                _schema, _objectDefinition, _objectLiteral, _result);
+        }
+
+        [Benchmark]
+        public void One_Object_Dictionary()
+        {
+            _result.Clear();
+            _variableCoercionHelper.CoerceVariableValues(
+                _schema, _objectDefinition, _object, _result);
         }
     }
 }
