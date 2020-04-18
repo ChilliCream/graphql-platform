7640cf56df93af77af879044eef5bb76a3fbbb3d
Pascal Senn <senn.pasc@gmail.com>
2020-04-19 01:17:39 +0200

    remove unused code

--- src/HotChocolate/Filters/test/Types.Filters.SqlLite.Tests/QueryableExtensions.cs
+++ src/HotChocolate/Filters/test/Types.Filters.SqlLite.Tests/QueryableExtensions.cs
@@ -1,10 +1,5 @@
-using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
-using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace HotChocolate.Types.Filters
 {
@@ -25,33 +20,5 @@ namespace HotChocolate.Types.Filters
 
             return descriptor;
         }
-
-
-        public static string ToSql<TEntity>(this IQueryable<TEntity> query) where TEntity : class
-        {
-            IEnumerator<TEntity> enumerator = query.Provider.Execute<IEnumerable<TEntity>>(
-                query.Expression).GetEnumerator();
-            var relationalCommandCache = enumerator.Private("_relationalCommandCache");
-            SelectExpression selectExpression
-                = relationalCommandCache.Private<SelectExpression>("_selectExpression");
-            IQuerySqlGeneratorFactory factory
-                = relationalCommandCache.Private<IQuerySqlGeneratorFactory>(
-                    "_querySqlGeneratorFactory");
-
-            QuerySqlGenerator sqlGenerator = factory.Create();
-            IRelationalCommand command = sqlGenerator.GetCommand(selectExpression);
-
-            return command.CommandText;
-        }
-
-        private static object Private(this object obj, string privateField)
-            => obj?.GetType()
-                .GetField(privateField, BindingFlags.Instance | BindingFlags.NonPublic)
-                ?.GetValue(obj);
-
-        private static T Private<T>(this object obj, string privateField)
-            => (T)obj?.GetType()
-                .GetField(privateField, BindingFlags.Instance | BindingFlags.NonPublic)
-                ?.GetValue(obj);
     }
 }
