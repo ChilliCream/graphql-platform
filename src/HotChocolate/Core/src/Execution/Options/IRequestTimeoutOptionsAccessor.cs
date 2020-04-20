cb7ecd1d8d8c8085935a48cfbedb7efd60214739
Michael Staib <michael@chillicream.com>
2020-04-20 15:39:44 +0200

    Moved options

--- src/HotChocolate/Core/src/Execution/Options/IRequestTimeoutOptionsAccessor.cs
+++ src/HotChocolate/Core/src/Execution/Options/IRequestTimeoutOptionsAccessor.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace HotChocolate.Execution.Configuration
+namespace HotChocolate.Execution.Options
 {
     /// <summary>
     /// Represents a dedicated options accessor to read the configured query
