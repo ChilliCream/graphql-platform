a7fa8a06691af44d728c11111ef6151ecbf2d8dd
Michael Staib <michael@chillicream.com>
2020-04-20 14:47:26 +0200

    Added more execution abstractions

---
+++ src/HotChocolate/Core/src/Execution/Properties/InternalsVisibleTo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("HotChocolate.Execution.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
--- src/HotChocolate/Core/src/Execution/RequestMiddleware.cs
+++ src/HotChocolate/Core/src/Execution/RequestMiddleware.cs
@@ -1,8 +1,7 @@
-using System.Threading.Tasks;
-
 namespace HotChocolate.Execution
 {
+    /// <summary>
+    /// Defines request middleware that can be added to the GraphQL request pipeline.
+    /// </summary>
     public delegate RequestDelegate RequestMiddleware(RequestDelegate next);
-
-    public delegate Task RequestDelegate(IRequestContext context);
 }
