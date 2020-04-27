2845c5660223b028b8818ba12d2024eed1df334a
Pascal Senn <senn.pasc@gmail.com>
2020-04-28 01:25:16 +0200

    style(Filters):fix style issues

--- src/HotChocolate/Filters/src/Types.Filters/Extensions/QueryableFilterVisitorContextExtensions.cs
+++ src/HotChocolate/Filters/src/Types.Filters/Extensions/QueryableFilterVisitorContextExtensions.cs
@@ -10,20 +10,20 @@ namespace HotChocolate.Types.Filters
     {
         public static void ReportError(
             this IQueryableFilterVisitorContext context,
-            IError error)
-                => context.Errors.Add(error);
+            IError error) =>
+                context.Errors.Add(error);
 
         public static QueryableClosure AddClosure(
             this IQueryableFilterVisitorContext context,
-            Type type)
-                => context.AddClosure(type, "_s" + context.Closures.Count, context.InMemory);
+            Type type) =>
+                context.AddClosure(type, "_s" + context.Closures.Count, context.InMemory);
 
         public static QueryableClosure AddIsNullClosure(
             this IQueryableFilterVisitorContext context,
             Type type)
         {
-            QueryableClosure closure
-                = context.AddClosure(type, "_s" + context.Closures.Count, false);
+            QueryableClosure closure =
+                context.AddClosure(type, "_s" + context.Closures.Count, false);
             context.GetLevel().Enqueue(FilterExpressionBuilder.Equals(closure.Parameter, null));
             return closure;
         }
@@ -40,45 +40,45 @@ namespace HotChocolate.Types.Filters
         }
 
         public static QueryableClosure GetClosure(
-            this IQueryableFilterVisitorContext context)
-                => context.Closures.Peek();
+            this IQueryableFilterVisitorContext context) =>
+                context.Closures.Peek();
 
         public static Expression GetInstance(
-            this IQueryableFilterVisitorContext context)
-                => context.Closures.Peek().Instance.Peek();
+            this IQueryableFilterVisitorContext context) =>
+                context.Closures.Peek().Instance.Peek();
 
         public static Queue<Expression> GetLevel(
-            this IQueryableFilterVisitorContext context)
-                => context.Closures.Peek().Level.Peek();
+            this IQueryableFilterVisitorContext context) =>
+                context.Closures.Peek().Level.Peek();
 
         public static void PushInstance(
-            this IQueryableFilterVisitorContext context, Expression nextExpression)
-                => context.Closures.Peek().Instance.Push(nextExpression);
+            this IQueryableFilterVisitorContext context, Expression nextExpression) =>
+                context.Closures.Peek().Instance.Push(nextExpression);
 
         public static void PushLevel(
-            this IQueryableFilterVisitorContext context, Queue<Expression> nextLevel)
-                => context.Closures.Peek().Level.Push(nextLevel);
+            this IQueryableFilterVisitorContext context, Queue<Expression> nextLevel) =>
+                context.Closures.Peek().Level.Push(nextLevel);
 
         public static Expression PopInstance(
-            this IQueryableFilterVisitorContext context)
-                => context.Closures.Peek().Instance.Pop();
+            this IQueryableFilterVisitorContext context) =>
+                context.Closures.Peek().Instance.Pop();
 
         public static Queue<Expression> PopLevel(
-            this IQueryableFilterVisitorContext context)
-                => context.Closures.Peek().Level.Pop();
+            this IQueryableFilterVisitorContext context) =>
+                context.Closures.Peek().Level.Pop();
 
         public static QueryableClosure PopClosure(
-            this IQueryableFilterVisitorContext context)
-                => context.Closures.Pop();
+            this IQueryableFilterVisitorContext context) =>
+                context.Closures.Pop();
 
         public static bool TryCreateLambda<TSource>(
             this IQueryableFilterVisitorContext context,
-            [NotNullWhen(true)]out Expression<Func<TSource, bool>>? expression)
-                => context.GetClosure().TryCreateLambda(out expression);
+            [NotNullWhen(true)]out Expression<Func<TSource, bool>>? expression) =>
+                context.GetClosure().TryCreateLambda(out expression);
 
         public static bool TryCreateLambda(
             this IQueryableFilterVisitorContext context,
-            [NotNullWhen(true)]out LambdaExpression? expression)
-                => context.GetClosure().TryCreateLambda(out expression);
+            [NotNullWhen(true)]out LambdaExpression? expression) =>
+                context.GetClosure().TryCreateLambda(out expression);
     }
 }
