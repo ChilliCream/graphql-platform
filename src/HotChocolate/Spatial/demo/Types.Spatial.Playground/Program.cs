fc106fe693735e595b428acd844ba3f992d639fc
Michael Staib <michael@chillicream.com>
2020-04-30 14:26:28 +0200

    references

--- src/HotChocolate/Spatial/demo/Types.Spatial.Playground/Program.cs
+++ src/HotChocolate/Spatial/demo/Types.Spatial.Playground/Program.cs
@@ -9,14 +9,14 @@ using Microsoft.Extensions.Logging;
 
 namespace Types.Spatial.Playground
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
--- src/HotChocolate/Spatial/src/Types.Spatial/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Types.Spatial
-{
-    public class Class1
-    {
-    }
-}
