92b184e7115208b6ce98357946e233e1fc51918d
Michael Staib <michael@chillicream.com>
2020-05-04 16:02:33 +0200

    Implement parse

--- src/HotChocolate/Spatial/src/Types.Spatial/Input/PointInputObject.cs
+++ src/HotChocolate/Spatial/src/Types.Spatial/Input/PointInputObject.cs
@@ -1,18 +1,35 @@
+using System.Collections.Generic;
 using System.Linq;
+using HotChocolate.Configuration;
 using HotChocolate.Language;
 using HotChocolate.Types;
+using HotChocolate.Types.Descriptors;
+using HotChocolate.Types.Descriptors.Definitions;
 using HotChocolate.Utilities.Serialization;
 using NetTopologySuite;
 using NetTopologySuite.Geometries;
 
 namespace Types.Spatial.Input
 {
-    public class PointInputObject: InputObjectType<Point>
+    public class PointInputObject : InputObjectType<Point>
     {
+        private const string _longitude = "longitude";
+        private const string _latitude = "latitude";
+        private const string _srid = "srid";
+
+        private IInputField _longitudeField;
+        private IInputField _latitudeField;
+        private IInputField _sridField;
+
+        public PointInputObject()
+        {
+            _longitudeField = default!;
+            _latitudeField = default!;
+            _sridField = default!;
+        }
+
         protected override void Configure(IInputObjectTypeDescriptor<Point> descriptor)
         {
-            base.Configure(descriptor);
-
             descriptor.BindFieldsExplicitly();
 
             // required fields
@@ -27,28 +44,67 @@ namespace Types.Spatial.Input
                 .Type<IntType>();
         }
 
-        public override object ParseLiteral(IValueNode literal)
+        public override object? ParseLiteral(IValueNode literal)
         {
-            if (literal is ObjectValueNode obj)
+            if (literal is NullValueNode)
             {
-                var srid = obj.Fields.FirstOrDefault(o => o.Name.Value == "longitude").Value.Value;
-
-                // var factory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: srid);
-
+                return null;
             }
-            return new Point(1337, 1337);
 
-            return base.ParseLiteral(literal);
+            if (literal is ObjectValueNode obj && obj.Fields.Count >= 2)
+            {
+                double? longitude = null;
+                double? latitude = null;
+                int? srid = null;
+
+                for (int i = 0; i < obj.Fields.Count; i++)
+                {
+                    ObjectFieldNode field = obj.Fields[i];
+
+                    switch (field.Name.Value)
+                    {
+                        case _longitude:
+                            longitude = (double)_longitudeField.Type.ParseLiteral(field.Value);
+                            break;
+                        case _latitude:
+                            latitude = (double)_latitudeField.Type.ParseLiteral(field.Value);
+                            break;
+                        case _srid:
+                            srid = (int)_sridField.Type.ParseLiteral(field.Value);
+                            break;
+                    }
+                }
+
+                if (longitude.HasValue && latitude.HasValue)
+                {
+                    return srid.HasValue
+                        ? throw new System.Exception("Factory goes here.")
+                        : new Point(longitude.Value, latitude.Value);
+                }
+            }
+
+            // TODO : resources
+            throw new InputObjectSerializationException("ERROR_MESSAGE");
         }
 
-        public override bool TrySerialize(object value, out object serialized)
+        public override bool TrySerialize(object value, out object? serialized)
         {
             return base.TrySerialize(value, out serialized);
         }
 
-        public override bool TryDeserialize(object serialized, out object value)
+        public override bool TryDeserialize(object serialized, out object? value)
         {
             return base.TryDeserialize(serialized, out value);
+        }
+
+        protected override void OnAfterCompleteType(
+            ICompletionContext context,
+            DefinitionBase definition,
+            IDictionary<string, object?> contextData)
+        {
+            _longitudeField = Fields[_longitude];
+            _latitudeField = Fields[_latitude];
+            _sridField = Fields[_srid];
         }
     }
 }
