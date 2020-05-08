63bee58f1bd248cc9fbfa868f209aa24671102a4
Rafael Staib <me@rafaelstaib.com>
2020-05-09 01:30:50 +0200

    Improvements for TOC and Aside (#1896)

--- website/src/components/widgets/doc-page.tsx
+++ website/src/components/widgets/doc-page.tsx
@@ -1,7 +1,9 @@
 import { graphql } from "gatsby";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useCallback } from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { DocPageFragment } from "../../../graphql-types";
+import { toggleAside, toggleTOC } from "../../state/common";
 import { ArticleComments } from "../misc/article-comments";
 import {
   Article,
@@ -10,11 +12,14 @@ import {
   ArticleTitle,
   ArticleWrapper,
 } from "../misc/article-elements";
-import { ArticleTableOfContent } from "../misc/article-table-of-content";
+import { ArticleSections } from "../misc/article-sections";
 import { DocPageAside } from "../misc/doc-page-aside";
 import { DocPageCommunity } from "../misc/doc-page-community";
 import { DocPageLegacy } from "../misc/doc-page-legacy";
 import { DocPageNavigation } from "../misc/doc-page-navigation";
+
+import ListAltIconSvg from "../../images/list-alt.svg";
+import NewspaperIconSvg from "../../images/newspaper.svg";
 
 interface DocPageProperties {
   data: DocPageFragment;
@@ -25,11 +30,20 @@ export const DocPage: FunctionComponent<DocPageProperties> = ({
   data,
   originPath,
 }) => {
+  const dispatch = useDispatch();
   const { fields, frontmatter, html } = data.file!.childMarkdownRemark!;
   const slug = fields!.slug!.substring(1);
   const path = `/docs/${slug}`;
   const selectedProduct = slug.substring(0, slug.indexOf("/"));
   const title = frontmatter!.title!;
+
+  const handleToggleTOC = useCallback(() => {
+    dispatch(toggleTOC());
+  }, []);
+
+  const handleToggleAside = useCallback(() => {
+    dispatch(toggleAside());
+  }, []);
 
   return (
     <Container>
@@ -42,6 +56,14 @@ export const DocPage: FunctionComponent<DocPageProperties> = ({
         <Article>
           <DocPageLegacy />
           <ArticleHeader>
+            <ResponsiveMenu>
+              <Button onClick={handleToggleTOC} className="toc-toggle">
+                <ListAltIconSvg /> Table of contents
+              </Button>
+              <Button onClick={handleToggleAside} className="aside-toggle">
+                <NewspaperIconSvg /> In this article
+              </Button>
+            </ResponsiveMenu>
             <ArticleTitle>{title}</ArticleTitle>
           </ArticleHeader>
           <ArticleContent dangerouslySetInnerHTML={{ __html: html! }} />
@@ -50,7 +72,7 @@ export const DocPage: FunctionComponent<DocPageProperties> = ({
       </ArticleWrapper>
       <DocPageAside>
         <DocPageCommunity data={data} originPath={originPath} />
-        <ArticleTableOfContent data={data.file!.childMarkdownRemark!} />
+        <ArticleSections data={data.file!.childMarkdownRemark!} />
       </DocPageAside>
     </Container>
   );
@@ -70,7 +92,7 @@ export const DocPageGraphQLFragment = graphql`
           title
         }
         html
-        ...ArticleTableOfContent
+        ...ArticleSections
       }
     }
     ...ArticleComments
@@ -91,5 +113,55 @@ const Container = styled.div`
 
   @media only screen and (min-width: 1300px) {
     max-width: 1300px;
+  }
+`;
+
+const ResponsiveMenu = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 30px 20px 20px;
+
+  @media only screen and (min-width: 800px) {
+    padding-right: 50px;
+    padding-left: 50px;
+  }
+
+  @media only screen and (min-width: 1050px) {
+    > .toc-toggle {
+      display: none;
+    }
+  }
+
+  @media only screen and (min-width: 1300px) {
+    display: none;
+  }
+`;
+
+const Button = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: #666;
+  transition: color 0.2s ease-in-out;
+
+  &.aside-toggle {
+    margin-left: auto;
+  }
+
+  &:hover {
+    color: #000;
+
+    > svg {
+      fill: #000;
+    }
+  }
+
+  > svg {
+    margin-right: 5px;
+    width: 16px;
+    height: 16px;
+    fill: #666;
+    transition: fill 0.2s ease-in-out;
   }
 `;
--- website/src/state/common/common.actions.ts
+++ website/src/state/common/common.actions.ts
@@ -8,9 +8,21 @@ export const hideCookieConsent = createAction(
   "[Common] Hide cookie consent message"
 );
 
+export const hideLegacyDocHeader = createAction(
+  "[Common] Hide legacy documentation message"
+);
+
 export const showCookieConsent = createAction(
   "[Common] Show cookie consent message"
 );
+
+export const showLegacyDocInfo = createAction(
+  "[Common] Show legacy documentation message"
+);
+
+export const toggleAside = createAction("[Common] Toggle aside pane");
+
+export const toggleTOC = createAction("[Common] Toggle table of contents pane");
 
 export const toggleNavigationGroup = createAction<{ readonly path: string }>(
   "[Common] Toggle navigation group"
