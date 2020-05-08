d0382f47dece559aade002e2a5936eef441cc6d9
Michael Staib <michael@chillicream.com>
2020-05-08 10:10:33 +0200

    started work

--- src/Core/Types/Types/Relay/IConnectionResolver.cs
+++ src/Core/Types/Types/Relay/IConnectionResolver.cs
@@ -5,6 +5,23 @@ namespace HotChocolate.Types.Relay
 {
     public interface IConnectionResolver
     {
-        Task<IConnection> ResolveAsync(CancellationToken cancellationToken);
+        Task<IConnection> ResolveAsync(
+            object source, 
+            int first, 
+            int last, 
+            string after, 
+            string before, 
+            CancellationToken cancellationToken);
+    }
+    
+    public interface IConnectionResolver<T> : IConnectionResolver
+    {
+        Task<IConnection> ResolveAsync(
+            T source, 
+            int first, 
+            int last, 
+            string after, 
+            string before, 
+            CancellationToken cancellationToken);
     }
 }
