e2a6a4bed89f55850edee174eb14d6b32738548e
Rafael Staib <me@rafaelstaib.com>
2020-05-13 23:17:08 +0200

    Synchronous BatchDispatcher and Tests (#1928)

--- src/HotChocolate/Core/src/Fetching/BatchScheduler.cs
+++ src/HotChocolate/Core/src/Fetching/BatchScheduler.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using System;
 using System.Collections.Concurrent;
 using System.Threading;
@@ -10,52 +11,24 @@ namespace HotChocolate.Fetching
         : IBatchScheduler
         , IBatchDispatcher
     {
-        private readonly object _lock = new object();
         private readonly ConcurrentQueue<Action> _queue = new ConcurrentQueue<Action>();
-        private bool _isDispatching;
 
-        public bool HasTasks => !_isDispatching && _queue.Count > 0;
+        public bool HasTasks => _queue.Count > 0;
 
         public event EventHandler? TaskEnqueued;
 
-        public Task DispatchAsync(CancellationToken cancellationToken)
+        public void Dispatch()
         {
-            if (!cancellationToken.IsCancellationRequested && !_isDispatching)
+            while(_queue.TryDequeue(out Action? dispatch))
             {
-                try
-                {
-                    Monitor.Enter(_lock, ref _isDispatching);
-
-                    while(!cancellationToken.IsCancellationRequested &&
-                        _queue.TryDequeue(out var dispatch))
-                    {
-                        dispatch();
-                    }
-                }
-                finally
-                {
-                    if (_isDispatching)
-                    {
-                        Monitor.Exit(_lock);
-                    }
-                }
+                dispatch();
             }
-
-            return Task.CompletedTask;
         }
 
         public void Schedule(Action dispatch)
         {
             _queue.Enqueue(dispatch);
-            RaiseTaskEnqueued();
-        }
-
-        private void RaiseTaskEnqueued()
-        {
-            if (TaskEnqueued != null)
-            {
-                TaskEnqueued(this, EventArgs.Empty);
-            }
+            TaskEnqueued?.Invoke(this, EventArgs.Empty);
         }
     }
 }
