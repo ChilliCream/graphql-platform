be56872484113ed6cd833538f3623c91cd437468
Pascal Senn <senn.pasc@gmail.com>
2020-05-13 16:59:16 +0200

    feat(Exec): add pop to obejct buffer

--- src/HotChocolate/Core/src/Execution/Utilities/ObjectBuffer.cs
+++ src/HotChocolate/Core/src/Execution/Utilities/ObjectBuffer.cs
@@ -6,7 +6,7 @@ namespace HotChocolate.Execution.Utilities
 {
     internal sealed class ObjectBuffer<T> where T : class, new()
     {
-        private readonly T[] _buffer;
+        private readonly T?[] _buffer;
         private readonly int _capacity;
         private readonly Action<T> _clean;
         private int _index;
@@ -34,9 +34,11 @@ namespace HotChocolate.Execution.Utilities
 
         public bool TryPop([NotNullWhen(true)] out T? obj)
         {
-            if (_index < _capacity)
+            var nextIndex = _index++;
+            if (nextIndex <= _capacity)
             {
-                obj = _buffer[_index++];
+                obj = _buffer[_index] ?? new T();
+                _buffer[_index] = null;
                 return true;
             }
 
@@ -58,7 +60,8 @@ namespace HotChocolate.Execution.Utilities
             var nextIndex = Interlocked.Increment(ref _index);
             if (nextIndex <= _capacity)
             {
-                obj = _buffer[nextIndex];
+                obj = _buffer[_index] ?? new T();
+                _buffer[_index] = null;
                 return true;
             }
 
@@ -66,13 +69,58 @@ namespace HotChocolate.Execution.Utilities
             return false;
         }
 
+
+        public void Push(T obj)
+        {
+            if (!TryPush(obj))
+            {
+                throw new InvalidOperationException("Buffer is full.");
+            }
+        }
+
+        public bool TryPush(T obj)
+        {
+            var nextIndex = _index--;
+            if (_index <= _capacity)
+            {
+                _clean(obj);
+                _buffer[nextIndex] = obj;
+                return true;
+            }
+            return false;
+        }
+
+        public void PopSafe(T obj)
+        {
+            if (!TryPush(obj))
+            {
+                throw new InvalidOperationException("Buffer is full.");
+            }
+        }
+
+        public bool TryPopSafe(T obj)
+        {
+            var nextIndex = Interlocked.Decrement(ref _index);
+            if (_index <= _capacity)
+            {
+                _clean(obj);
+                _buffer[nextIndex] = obj;
+                return true;
+            }
+            return false;
+        }
+
+
         public void Reset()
         {
             if (_index > 0)
             {
                 for (int i = 0; i < _index; i++)
                 {
-                    _clean(_buffer[i]);
+                    if (_buffer[i] != null)
+                    {
+                        _clean(_buffer[i]!);
+                    }
                 }
             }
             _index = 0;
