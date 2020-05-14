3c896b4a4e1b6b8b2a32f0400f2966e663642e43
John Merchant <john@jmercha.dev>
2020-05-15 00:42:13 +1000

    Reference missing projects

--- src/StrawberryShake/Tooling/src/dotnet-chilli/Program.cs
+++ src/StrawberryShake/Tooling/src/dotnet-chilli/Program.cs
@@ -1,11 +1,10 @@
-﻿using System;
-
-namespace StrawberryShake.Tools
+﻿namespace StrawberryShake.Tools
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // => Parser.Default.ParseArguments<InitOptions, UpdateOptions, CompileOptions, GenerateOptions, DownloadOptions, PublishOptions>(args);
         }
     }
 }
