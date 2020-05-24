b46bef2e90a146caed5a888509ec233735224fe0
Pascal Senn <senn.pasc@gmail.com>
2020-05-24 11:40:43 +0200

    fix: backport ambious nullability attribute

--- src/Core/Types/Utilities/NullableHelper.cs
+++ src/Core/Types/Utilities/NullableHelper.cs
@@ -12,8 +12,10 @@ namespace HotChocolate.Utilities
 {
     internal readonly ref struct NullableHelper
     {
-        private const string _nullableContextAttributeName = "NullableContextAttribute";
-        private const string _nullableAttributeName = "NullableAttribute";
+        private const string _nullableContextAttributeName =
+            "System.Runtime.CompilerServices.NullableContextAttribute";
+        private const string _nullableAttributeName =
+            "System.Runtime.CompilerServices.NullableAttribute";
 
         private readonly Type _type;
         private readonly Nullable _context;
@@ -235,7 +237,10 @@ namespace HotChocolate.Utilities
             IList<CustomAttributeData> attributes)
         {
             CustomAttributeData data = attributes.FirstOrDefault(t =>
-                t.AttributeType.Name.EqualsOrdinal(_nullableContextAttributeName));
+                string.Equals(
+                    t.AttributeType.FullName,
+                    _nullableContextAttributeName,
+                    StringComparison.Ordinal));
 
             if (data is { })
             {
@@ -251,7 +256,10 @@ namespace HotChocolate.Utilities
         {
             object[] attributes = method.ReturnTypeCustomAttributes.GetCustomAttributes(false);
             object attribute = attributes.FirstOrDefault(t =>
-                t.GetType().Name.EqualsOrdinal(_nullableAttributeName));
+                string.Equals(
+                    t.GetType().FullName,
+                    _nullableAttributeName,
+                    StringComparison.Ordinal));
 
             if (attribute is null)
             {
@@ -283,7 +291,10 @@ namespace HotChocolate.Utilities
             IList<CustomAttributeData> attributes)
         {
             CustomAttributeData data = attributes.FirstOrDefault(t =>
-                t.AttributeType.Name.EqualsOrdinal(_nullableAttributeName));
+                string.Equals(
+                    t.AttributeType.FullName,
+                    _nullableAttributeName,
+                    StringComparison.Ordinal));
 
             if (data is { })
             {
