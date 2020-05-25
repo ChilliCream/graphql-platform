42092071bf89c7941893d2293319ddb8a8e9be33
Pascal Senn <senn.pasc@gmail.com>
2020-05-25 02:52:50 +0200

    feat(filters): ceneralize operation handler

---
+++ src/HotChocolate/Filters/src/Types.Filters.Mongo/OperationHandlers/String/MongoOperationHandlers.Equals.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using HotChocolate.Language;
+using HotChocolate.Types.Filters.Expressions;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace HotChocolate.Types.Filters.Mongo
+{
+    public static partial class MongoOperationHandlers
+    {
+        public static bool Equals(
+            FilterOperation operation,
+            IInputType type,
+            IValueNode value,
+            FilterOperationField field,
+            IFilterVisitorContext<FilterDefinition<BsonDocument>> context,
+            [NotNullWhen(true)]out FilterDefinition<BsonDocument>? result)
+        {
+            object parsedValue = type.ParseLiteral(value);
+
+            if (!operation.IsNullable && parsedValue == null)
+            {
+                context.ReportError(
+                    ErrorHelper.CreateNonNullError(operation, type, value, context));
+
+                result = null;
+                return false;
+            }
+
+            if (type.IsInstanceOfType(value) &&
+                context is MongoFilterVisitorContext ctx)
+            {
+                if (operation.IsSimpleArrayType())
+                {
+                    result = new BsonDocument { { "$eq", BsonValue.Create(parsedValue) } };
+                }
+                else
+                {
+                    result = ctx.Builder.Eq(
+                        ctx.GetMongoFilterScope().GetPath(field), parsedValue);
+                }
+
+
+                return true;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public static bool NotEquals(
+            FilterOperation operation,
+            IInputType type,
+            IValueNode value,
+            FilterOperationField field,
+            IFilterVisitorContext<FilterDefinition<BsonDocument>> context,
+            [NotNullWhen(true)]out FilterDefinition<BsonDocument>? result)
+        {
+            object parsedValue = type.ParseLiteral(value);
+
+            if (!operation.IsNullable && parsedValue == null)
+            {
+                context.ReportError(
+                    ErrorHelper.CreateNonNullError(operation, type, value, context));
+
+                result = null;
+                return false;
+            }
+
+            if (type.IsInstanceOfType(value) &&
+                context is MongoFilterVisitorContext ctx)
+            {
+                if (operation.IsSimpleArrayType())
+                {
+                    result = new BsonDocument { { "$ne", BsonValue.Create(parsedValue) } };
+                }
+                else
+                {
+                    result = ctx.Builder.Ne(
+                        ctx.GetMongoFilterScope().GetPath(field), parsedValue);
+                }
+
+                return true;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
