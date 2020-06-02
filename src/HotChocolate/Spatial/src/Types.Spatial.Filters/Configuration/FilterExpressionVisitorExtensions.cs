598708242d00edf1661b67cf5ca807de51ea422f
Pascal Senn <senn.pasc@gmail.com>
2020-06-02 03:00:24 +0200

    feat(spatial): add extensions

--- src/HotChocolate/Spatial/src/Types.Spatial.Filters/Configuration/FilterExpressionVisitorExtensions.cs
+++ src/HotChocolate/Spatial/src/Types.Spatial.Filters/Configuration/FilterExpressionVisitorExtensions.cs
@@ -5,27 +5,35 @@ using System.Reflection;
 using HotChocolate.Types.Descriptors;
 using HotChocolate.Types.Filters;
 using HotChocolate.Types.Filters.Conventions;
+using HotChocolate.Types.Filters.Expressions;
 using NetTopologySuite.Geometries;
 
 namespace HotChocolate.Types.Spatial.Filters.Expressions
 {
     public static class FilterVisitorExtensions
     {
+        public static IFilterConventionDescriptor UseSpatialFilters(
+            this IFilterConventionDescriptor descriptor) =>
+            descriptor.SpatialFilterImplicitly().SpatialFilterExpression();
+
         public static IFilterConventionDescriptor SpatialFilterImplicitly(
             this IFilterConventionDescriptor descriptor) =>
-                descriptor.AddImplicitFilter(TryCreateGeometryFiler)
-                    .Type(SpatialFilterKind.Geometry)
-                    .Operation(SpatialFilterOperation.Distance)
-                        .Name((def, _) => def.Name + "_distance")
-                        .Description("")
-                        .And()
-                    .And();
+            descriptor.AddImplicitFilter(TryCreateGeometryFiler)
+                .Type(SpatialFilterKind.Geometry)
+                .Operation(SpatialFilterOperation.Distance)
+                    .Name((def, _) => def.Name + "_distance")
+                    .Description("")
+                    .And()
+                .And();
 
         public static IFilterConventionDescriptor SpatialFilterExpression(
             this IFilterConventionDescriptor descriptor) =>
-                descriptor.UseExpressionVisitor()
-                    .Kind(SpatialFilterKind.Geometry)
-                        .Operation(SpatialFilterOperation.Distance);
+            descriptor.UseExpressionVisitor()
+                .Kind(SpatialFilterKind.Geometry)
+                    .Operation(SpatialFilterOperation.Distance)
+                        .Handler(GemetryOperationHandlers.Distance).And()
+                        .And()
+                    .And();
 
         private static bool TryCreateGeometryFiler(
             IDescriptorContext context,
