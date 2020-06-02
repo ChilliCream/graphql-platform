ea8b09f735627e0a8895d418fcae2f3aaefff19d
Michael Staib <michael@chillicream.com>
2020-06-02 16:48:55 +0200

    wip

---
+++ src/HotChocolate/Core/src/Execution/Pipeline/WritePersistedQueryMiddleware.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HotChocolate.Execution.Instrumentation;
+using HotChocolate.Language;
+using HotChocolate.Validation;
+
+namespace HotChocolate.Execution.Pipeline
+{
+    internal sealed class WritePersistedQueryMiddleware
+    {
+        private const string _persistedQuery = "persistedQuery";
+        private const string _persisted = "persisted";
+        private const string _expectedValue = "expectedHashValue";
+        private const string _expectedType = "expectedHashType";
+        private const string _expectedFormat = "expectedHashFormat";
+        private const string _hash = "hash";
+        private readonly RequestDelegate _next;
+        private readonly IDiagnosticEvents _diagnosticEvents;
+        private readonly IWriteStoredQueries _persistedQueryStore;
+        private readonly string _hashName;
+        private readonly HashFormat _hashFormat;
+
+
+        public WritePersistedQueryMiddleware(
+            RequestDelegate next,
+            IDiagnosticEvents diagnosticEvents,
+            IWriteStoredQueries persistedQueryStore)
+        {
+            _next = next ??
+                throw new ArgumentNullException(nameof(next));
+            _diagnosticEvents = diagnosticEvents ??
+                throw new ArgumentNullException(nameof(diagnosticEvents));
+            _persistedQueryStore = persistedQueryStore ??
+                throw new ArgumentNullException(nameof(persistedQueryStore));
+        }
+
+        public async ValueTask InvokeAsync(IRequestContext context)
+        {
+            await _next(context).ConfigureAwait(false);
+
+            if (context.Document is { } &&
+                context.ValidationResult is { HasErrors: false } &&
+                context.Request.Extensions is { } && 
+                context.Request.Extensions.TryGetValue(_persistedQuery, out var s) && 
+                s is IReadOnlyDictionary<string, object> settings)
+            {
+                IQueryResultBuilder builder = QueryResultBuilder.FromResult(result);
+
+                // hash is found and matches the query key -> store the query
+                if (DoHashesMatch(settings, context.QueryKey, _hashName, out string userHash))
+                {
+                    // save the  query
+                    await _writeStoredQueries.WriteQueryAsync(
+                        context.QueryKey,
+                        context.Request.Query)
+                        .ConfigureAwait(false);
+
+                    // add persistence receipt to the result
+                    builder.SetExtension(
+                        _persistedQuery,
+                        new Dictionary<string, object>
+                        {
+                            { _hashName, userHash },
+                            { _persisted, true }
+                        });
+
+                    context.ContextData[ContextDataKeys.DocumentSaved] = true;
+                }
+                else
+                {
+                    builder.SetExtension(
+                        _persistedQuery,
+                        new Dictionary<string, object>
+                        {
+                            { _hashName, userHash },
+                            { _expectedValue, context.QueryKey },
+                            { _expectedType, _hashName },
+                            { _expectedFormat, _hashFormat.ToString() },
+                            { _persisted, false }
+                        });
+                }
+
+                context.Result = builder.Create();
+            }
+        }
+
+        private static bool DoHashesMatch(
+            IReadOnlyDictionary<string, object> settings,
+            string expectedHash,
+            string hashName,
+            out string userHash)
+        {
+            if (settings.TryGetValue(hashName, out object? h)
+                && h is string hash)
+            {
+                userHash = hash;
+                return hash.Equals(expectedHash, StringComparison.Ordinal);
+            }
+
+            userHash = null;
+            return false;
+        }
+    }
+}
