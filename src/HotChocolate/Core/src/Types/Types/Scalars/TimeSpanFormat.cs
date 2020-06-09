6346a2bf192056972bd8e54dbf3cedc35909ee19
glucaci <gabriel.lucaci.dev@gmail.com>
2020-06-09 21:08:13 +0200

    Refine comments and replace link

--- src/HotChocolate/Core/src/Types/Types/Scalars/TimeSpanFormat.cs
+++ src/HotChocolate/Core/src/Types/Types/Scalars/TimeSpanFormat.cs
@@ -3,11 +3,12 @@ namespace HotChocolate.Types
     public enum TimeSpanFormat
     {
         /// <summary>
-        /// https://www.w3.org/TR/xmlschema-2/#duration
+        /// TimeSpan ISO 8601 Format
+        /// https://tools.ietf.org/html/rfc3339
         /// </summary>
         Iso8601,
         /// <summary>
-        /// The Constant ("c") Format Specifier
+        /// TimeSpan .NET Constant ("c") Format
         /// https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-timespan-format-strings#the-constant-c-format-specifier
         /// </summary>
         DotNet
