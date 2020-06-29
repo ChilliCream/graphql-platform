909ca1c47c4a05d2e750f5dbcbe9dc5c483d25ea
Alexander Filatov <alexander.vl.filatov@gmail.com>
2020-06-29 22:53:31 +0300

    https://github.com/ChilliCream/hotchocolate/issues/2094 Offset (skip-take) Paging. Out of the box support
    - done

---
+++ src/HotChocolate/Core/src/Types/Types/OffsetPaging/ICollectionSlice.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace HotChocolate.Types.OffsetPaging
+{
+    public interface ICollectionSlice
+    {
+        IReadOnlyCollection<object> Nodes { get; }
+
+        int TotalCount { get; }
+    }
+}
---
+++ src/HotChocolate/Core/src/Types/Types/OffsetPaging/PagingCollectionMiddleware~1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotChocolate.Resolvers;
+
+namespace HotChocolate.Types.OffsetPaging
+{
+    public class PagingCollectionMiddleware<TClrType>
+    {
+        private readonly FieldDelegate _next;
+
+        public PagingCollectionMiddleware(FieldDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(IMiddlewareContext context)
+        {
+            await _next(context).ConfigureAwait(false);
+
+            IQueryable<TClrType> source = context.Result switch
+            {
+                IQueryable<TClrType> queryable => queryable,
+                IEnumerable<TClrType> enumerable => enumerable.AsQueryable(),
+                _ => null
+            };
+
+            if (source != null)
+            {
+                int? skip = context.Argument<int?>("skip");
+                int? take = context.Argument<int?>("take");
+
+                var totalCount = source.Count();
+
+                IQueryable<TClrType> slice = source;
+
+                if (skip != null)
+                    slice = slice.Skip(skip.Value);
+                if (take != null)
+                    slice = slice.Take(take.Value);
+
+                context.Result = new CollectionSlice<TClrType>(slice.ToList(), totalCount);
+            }
+        }
+    }
+}
