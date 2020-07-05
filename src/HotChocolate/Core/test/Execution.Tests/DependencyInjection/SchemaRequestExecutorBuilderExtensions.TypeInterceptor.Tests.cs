73a5eda24836a0954746bbdca82c3fa0ce8ebb7f
Michael Staib <michael@chillicream.com>
2020-07-05 20:34:41 +0200

    Added more tests

--- src/HotChocolate/Core/test/Execution.Tests/DependencyInjection/SchemaRequestExecutorBuilderExtensions.TypeInterceptor.Tests.cs
+++ src/HotChocolate/Core/test/Execution.Tests/DependencyInjection/SchemaRequestExecutorBuilderExtensions.TypeInterceptor.Tests.cs
@@ -119,5 +119,555 @@ namespace HotChocolate.DependencyInjection
             Assert.True(found);
             Assert.True(canHandleInvoked);
         }
+
+        [Fact]
+        public async Task OnAfterRegisterDependencies()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnAfterRegisterDependencies(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    }));
+
+            // assert
+            Assert.True(found);
+        }
+
+        [Fact]
+        public async Task OnAfterRegisterDependencies_Generic()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnAfterRegisterDependencies<ObjectTypeDefinition>(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    }));
+
+            // assert
+            Assert.True(found);
+        }
+
+        [Fact]
+        public async Task OnAfterRegisterDependencies_CanHandle()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+            bool canHandleInvoked = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnAfterRegisterDependencies(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    },
+                    canHandle: c => canHandleInvoked = true));
+
+            // assert
+            Assert.True(found);
+            Assert.True(canHandleInvoked);
+        }
+
+        [Fact]
+        public async Task OnAfterRegisterDependencies_Generic_CanHandle()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+            bool canHandleInvoked = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnAfterRegisterDependencies<ObjectTypeDefinition>(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    },
+                    canHandle: c => canHandleInvoked = true));
+
+            // assert
+            Assert.True(found);
+            Assert.True(canHandleInvoked);
+        }
+
+        [Fact]
+        public async Task OnBeforeCompleteName()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnBeforeCompleteName(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    }));
+
+            // assert
+            Assert.True(found);
+        }
+
+        [Fact]
+        public async Task OnBeforeCompleteName_Generic()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnBeforeCompleteName<ObjectTypeDefinition>(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    }));
+
+            // assert
+            Assert.True(found);
+        }
+
+        [Fact]
+        public async Task OnBeforeCompleteName_CanHandle()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+            bool canHandleInvoked = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnBeforeCompleteName(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    },
+                    canHandle: c => canHandleInvoked = true));
+
+            // assert
+            Assert.True(found);
+            Assert.True(canHandleInvoked);
+        }
+
+        [Fact]
+        public async Task OnBeforeCompleteName_Generic_CanHandle()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+            bool canHandleInvoked = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnBeforeCompleteName<ObjectTypeDefinition>(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    },
+                    canHandle: c => canHandleInvoked = true));
+
+            // assert
+            Assert.True(found);
+            Assert.True(canHandleInvoked);
+        }
+
+        [Fact]
+        public async Task OnAfterCompleteName()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnAfterCompleteName(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    }));
+
+            // assert
+            Assert.True(found);
+        }
+
+        [Fact]
+        public async Task OnAfterCompleteName_Generic()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnAfterCompleteName<ObjectTypeDefinition>(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    }));
+
+            // assert
+            Assert.True(found);
+        }
+
+        [Fact]
+        public async Task OnAfterCompleteName_CanHandle()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+            bool canHandleInvoked = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnAfterCompleteName(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    },
+                    canHandle: c => canHandleInvoked = true));
+
+            // assert
+            Assert.True(found);
+            Assert.True(canHandleInvoked);
+        }
+
+        [Fact]
+        public async Task OnAfterCompleteName_Generic_CanHandle()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+            bool canHandleInvoked = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnAfterCompleteName<ObjectTypeDefinition>(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    },
+                    canHandle: c => canHandleInvoked = true));
+
+            // assert
+            Assert.True(found);
+            Assert.True(canHandleInvoked);
+        }
+
+        [Fact]
+        public async Task OnBeforeCompleteType()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnBeforeCompleteType(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    }));
+
+            // assert
+            Assert.True(found);
+        }
+
+        [Fact]
+        public async Task OnBeforeCompleteType_Generic()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnBeforeCompleteType<ObjectTypeDefinition>(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    }));
+
+            // assert
+            Assert.True(found);
+        }
+
+        [Fact]
+        public async Task OnBeforeCompleteType_CanHandle()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+            bool canHandleInvoked = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnBeforeCompleteType(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    },
+                    canHandle: c => canHandleInvoked = true));
+
+            // assert
+            Assert.True(found);
+            Assert.True(canHandleInvoked);
+        }
+
+        [Fact]
+        public async Task OnBeforeCompleteType_Generic_CanHandle()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+            bool canHandleInvoked = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnBeforeCompleteType<ObjectTypeDefinition>(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    },
+                    canHandle: c => canHandleInvoked = true));
+
+            // assert
+            Assert.True(found);
+            Assert.True(canHandleInvoked);
+        }
+
+        [Fact]
+        public async Task OnAfterCompleteType()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnAfterCompleteType(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    }));
+
+            // assert
+            Assert.True(found);
+        }
+
+        [Fact]
+        public async Task OnAfterCompleteType_Generic()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnAfterCompleteType<ObjectTypeDefinition>(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    }));
+
+            // assert
+            Assert.True(found);
+        }
+
+        [Fact]
+        public async Task OnAfterCompleteType_CanHandle()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+            bool canHandleInvoked = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnAfterCompleteType(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    },
+                    canHandle: c => canHandleInvoked = true));
+
+            // assert
+            Assert.True(found);
+            Assert.True(canHandleInvoked);
+        }
+
+        [Fact]
+        public async Task OnAfterCompleteType_Generic_CanHandle()
+        {
+            // arrange
+            Snapshot.FullName();
+            bool found = false;
+            bool canHandleInvoked = false;
+
+            // act
+            await CreateSchemaAsync(c => c
+                .AddQueryType(d => d
+                    .Name("Query")
+                    .Field("foo")
+                    .Resolver("bar"))
+                .OnBeforeCompleteType<ObjectTypeDefinition>(
+                    (ctx, def, state) =>
+                    {
+                        if (def is { } && def.Name.Equals("Query"))
+                        {
+                            found = true;
+                        }
+                    },
+                    canHandle: c => canHandleInvoked = true));
+
+            // assert
+            Assert.True(found);
+            Assert.True(canHandleInvoked);
+        }
     }
 }
