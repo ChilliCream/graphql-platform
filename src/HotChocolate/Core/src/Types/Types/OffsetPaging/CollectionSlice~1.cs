60f02df320c2621517392d8a59597f247af2c062
Michael Staib <michael@chillicream.com>
2020-07-24 23:09:20 +0200

    Applied style changes.

--- src/HotChocolate/Core/src/Types/Types/OffsetPaging/CollectionSlice~1.cs
+++ src/HotChocolate/Core/src/Types/Types/OffsetPaging/CollectionSlice~1.cs
@@ -1,19 +1,20 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace HotChocolate.Types.OffsetPaging
 {
-    public class CollectionSlice<TClrType> : ICollectionSlice
+    public class CollectionSlice<TClrType>
+        : ICollectionSlice
     {
-        public IReadOnlyCollection<TClrType> Nodes { get; }
-
-        public int TotalCount { get; }
-
-        IReadOnlyCollection<object> ICollectionSlice.Nodes { get { return (IReadOnlyCollection<object>)Nodes; } }
-
         public CollectionSlice(IReadOnlyCollection<TClrType> slice, int totalCount)
         {
             Nodes = slice;
             TotalCount = totalCount;
         }
+        
+        public IReadOnlyCollection<TClrType> Nodes { get; }
+        
+        IReadOnlyCollection<object> ICollectionSlice.Nodes => (IReadOnlyCollection<object>)Nodes;
+
+        public int TotalCount { get; }
     }
 }
