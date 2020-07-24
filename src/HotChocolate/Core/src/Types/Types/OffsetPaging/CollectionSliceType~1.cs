ed885355d5d2968bc7f8a462e57d585046df661c
Michael Staib <michael@chillicream.com>
2020-07-24 23:07:07 +0200

    Changed  some formatting so that this fits with our 100 character rule.

--- src/HotChocolate/Core/src/Types/Types/OffsetPaging/CollectionSliceType~1.cs
+++ src/HotChocolate/Core/src/Types/Types/OffsetPaging/CollectionSliceType~1.cs
@@ -1,11 +1,18 @@
 ﻿namespace HotChocolate.Types.OffsetPaging
 {
-    public class CollectionSliceType<TSchemaType> : ObjectType<ICollectionSlice> where TSchemaType : class, IOutputType
+    public class CollectionSliceType<TSchemaType> 
+        : ObjectType<ICollectionSlice> 
+        where TSchemaType : class, IOutputType
     {
         protected override void Configure(IObjectTypeDescriptor<ICollectionSlice> descriptor)
         {
-            descriptor.Name(dependency => $"CollectionSliceOf{dependency.Name}").DependsOn<TSchemaType>();
-            descriptor.Field(i => i.Nodes).Type<ListType<TSchemaType>>();
+            descriptor
+                .Name(dependency => $"CollectionSliceOf{dependency.Name}")
+                .DependsOn<TSchemaType>();
+            
+            descriptor
+                .Field(i => i.Nodes)
+                .Type<ListType<TSchemaType>>();
         }
     }
 }
