5e070ffc69a104125369deec299bc51fa861eeb5
Michael Staib <michael@chillicream.com>
2020-09-03 22:09:24 +0200

    Removed unused variables from build

--- .build/Build.GitHub.cs
+++ .build/Build.GitHub.cs
@@ -4,13 +4,15 @@ using Nuke.Common;
 partial class Build : NukeBuild
 {
     [Parameter] readonly string GitHubToken;
-    [Parameter] readonly string GitHubRef = Environment.GetEnvironmentVariable("GITHUB_REF");
-    
-    [Parameter] readonly string GitHubRepository = Environment.GetEnvironmentVariable("GITHUB_REPOSITORY");
 
     /// <summary>
+    /// ChilliCream/hotchocolate
+    /// </summary>
+    [Parameter] readonly string GitHubRepository = Environment.GetEnvironmentVariable("GITHUB_REPOSITORY");
+
+    /// /// <summary>
     /// Unique identifier of your PR. Must correspond to the key of the PR in GitHub.
-    // E.G.: sonar.pullrequest.key=5
+    /// E.G.: sonar.pullrequest.key=5
     /// </summary>
     [Parameter] readonly string GitHubPRNumber = Environment.GetEnvironmentVariable("HC_GITHUB_PR_NR");
 
@@ -22,8 +24,8 @@ partial class Build : NukeBuild
 
 
     /// <summary>
-    /// The long-lived branch into which the PR will be merged. 
-    /// Default: master 
+    /// The long-lived branch into which the PR will be merged.
+    /// Default: master
     /// E.G.: sonar.pullrequest.base=master
     /// </summary>
     [Parameter] readonly string GitHubBaseRef = Environment.GetEnvironmentVariable("HC_GITHUB_BASE_REF");
