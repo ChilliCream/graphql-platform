4742f77dd3fb208f03721b7a898bce84e1035631
Pascal Senn <senn.pasc@gmail.com>
2020-09-12 03:32:09 +0200

    add tests for filtering with paging

--- src/HotChocolate/Data/test/Data.Filters.SqlServer.Tests/SchemaCache.cs
+++ src/HotChocolate/Data/test/Data.Filters.SqlServer.Tests/SchemaCache.cs
@@ -5,18 +5,20 @@ using HotChocolate.Execution;
 namespace HotChocolate.Data.Filters
 {
     public class SchemaCache
-        : FilterVisitorTestBase
-        , IDisposable
+        : FilterVisitorTestBase,
+          IDisposable
     {
         private readonly ConcurrentDictionary<(Type, Type, object), IRequestExecutor> _cache =
             new ConcurrentDictionary<(Type, Type, object), IRequestExecutor>();
 
-        public IRequestExecutor CreateSchema<T, TType>(T[] entities)
+        public IRequestExecutor CreateSchema<T, TType>(T[] entities, bool withPaging = false)
             where T : class
             where TType : FilterInputType<T>
         {
             (Type, Type, T[] entites) key = (typeof(T), typeof(TType), entities);
-            return _cache.GetOrAdd(key, (k) => base.CreateSchema<T, TType>(entities));
+            return _cache.GetOrAdd(
+                key,
+                k => base.CreateSchema<T, TType>(entities, withPaging: withPaging));
         }
 
         public void Dispose()
