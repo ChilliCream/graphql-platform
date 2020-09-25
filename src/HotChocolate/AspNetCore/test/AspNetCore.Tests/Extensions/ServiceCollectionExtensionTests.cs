57928350307aa1dfdc87dc0ee9d100a49e383ed1
Michael Staib <michael@chillicream.com>
2020-09-25 19:55:28 +0200

    Added extension tests

---
+++ src/HotChocolate/AspNetCore/test/AspNetCore.Tests/Extensions/ServiceCollectionExtensionTests.cs
@@ -0,0 +1,28 @@
+using HotChocolate.AspNetCore.Utilities;
+using Xunit;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public class ServiceCollectionExtensionTests
+    {
+        [Fact]
+        public static void AddHttpRequestSerializer_OfT()
+        {
+            // arrange
+            var serviceCollection = new ServiceCollection();
+
+            // act
+            HotChocolateAspNetCoreServiceCollectionExtensions
+                .AddHttpRequestSerializer<DefaultHttpResultSerializer>(serviceCollection);
+
+            // assert
+            Assert.Collection(
+                serviceCollection,
+                t =>
+                {
+                    Assert.Equal(typeof(IHttpResultSerializer), t.ServiceType);
+                    Assert.Equal(typeof(DefaultHttpResultSerializer), t.ImplementationType);
+                });
+        }
+    }
+}
