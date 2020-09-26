c4fb6dbb153c63599731087c6adf9f7a23ef8225
Michael Staib <michael@chillicream.com>
2020-09-26 19:52:36 +0200

    Update QueryableSortVisitorStringTests.cs

--- src/HotChocolate/Data/test/Data.Sorting.InMemory.Tests/QueryableSortVisitorStringTests.cs
+++ src/HotChocolate/Data/test/Data.Sorting.InMemory.Tests/QueryableSortVisitorStringTests.cs
@@ -36,19 +36,18 @@ namespace HotChocolate.Data.Sorting
             IRequestExecutor tester = _cache.CreateSchema<Foo, FooSortType>(_fooEntities);
 
             // act
-            // assert
             IExecutionResult res1 = await tester.ExecuteAsync(
                 QueryRequestBuilder.New()
                     .SetQuery("{ root(order: { bar: ASC}){ bar}}")
                     .Create());
 
-            res1.MatchSnapshot("ASC");
-
             IExecutionResult res2 = await tester.ExecuteAsync(
                 QueryRequestBuilder.New()
                     .SetQuery("{ root(order: { bar: DESC}){ bar}}")
                     .Create());
-
+                    
+            // assert
+            res1.MatchSnapshot("ASC");
             res2.MatchSnapshot("DESC");
         }
 
@@ -60,19 +59,18 @@ namespace HotChocolate.Data.Sorting
                 _fooNullableEntities);
 
             // act
-            // assert
             IExecutionResult res1 = await tester.ExecuteAsync(
                 QueryRequestBuilder.New()
                     .SetQuery("{ root(order: { bar: ASC}){ bar}}")
                     .Create());
-
-            res1.MatchSnapshot("ASC");
 
             IExecutionResult res2 = await tester.ExecuteAsync(
                 QueryRequestBuilder.New()
                     .SetQuery("{ root(order: { bar: DESC}){ bar}}")
                     .Create());
 
+            // assert
+            res1.MatchSnapshot("ASC");
             res2.MatchSnapshot("DESC");
         }
 
