bb97b6a99ce98124a6510657e303ca05de1df221
Frederic Birke <fred@freds.garden>
2020-09-29 11:15:02 +0200

    Add testcases for provides and requires

--- src/HotChocolate/ApolloFederation/test/ApolloFederation.Tests/Directives/RequiresDirectiveTests.cs
+++ src/HotChocolate/ApolloFederation/test/ApolloFederation.Tests/Directives/RequiresDirectiveTests.cs
@@ -1,5 +1,7 @@
 using System.Linq;
+using HotChocolate.ApolloFederation.Extensions;
 using HotChocolate.Types;
+using Snapshooter.Xunit;
 using Xunit;
 
 namespace HotChocolate.ApolloFederation.Directives
@@ -30,6 +32,160 @@ namespace HotChocolate.ApolloFederation.Directives
             Assert.Collection(directive.Locations,
                 t => Assert.Equal(DirectiveLocation.FieldDefinition, t));
 
+        }
+                [Fact]
+        public void AnnotateProvidesToFieldSchemaFirst()
+        {
+            Snapshot.FullName();
+
+            // arrange
+            ISchema schema = SchemaBuilder.New()
+                .AddDocumentFromString(
+                    @"
+                    type Review @key(fields: ""id"") {
+                        id: Int!
+                        product: Product! @requires(fields: ""id"")
+                    }
+
+                    type Product {
+                        name: String!
+                    }
+
+                    type Query {
+                        someField(a: Int): Review
+                    }
+                ")
+                .AddDirectiveType<KeyDirectiveType>()
+                .AddDirectiveType<RequiresDirectiveType>()
+                .AddType<FieldSetType>()
+                .Use(next => context => default)
+                .Create();
+
+            // act
+            ObjectType testType = schema.GetType<ObjectType>("Review");
+
+            // assert
+            Assert.Collection(testType.Fields.Single(field => field.Name.Value == "product").Directives,
+                providesDirective =>
+                {
+                    Assert.Equal(
+                        TypeNames.Requires,
+                        providesDirective.Name
+                    );
+                    Assert.Equal("fields", providesDirective.ToNode().Arguments[0].Name.ToString());
+                    Assert.Equal("\"id\"", providesDirective.ToNode().Arguments[0].Value.ToString());
+                }
+            );
+            schema.ToString().MatchSnapshot();
+        }
+
+        [Fact]
+        public void AnnotateProvidesToFieldCodeFirst()
+        {
+            Snapshot.FullName();
+
+            // arrange
+            var schema = Schema.Create(
+                t =>
+                {
+                    var productType = new ObjectType(
+                        o =>
+                        {
+                            o.Name("Product");
+                            o.Field("name")
+                                .Type<StringType>();
+                        }
+                    );
+
+                    var reviewType = new ObjectType(
+                        o =>
+                        {
+                            o.Name("Review")
+                                .Key("id");
+                            o.Field("id")
+                                .Type<IntType>();
+                            o.Field("product")
+                                .Requires("id")
+                                .Type(productType);
+                        }
+                    );
+                    t.RegisterType(reviewType);
+                    t.RegisterQueryType(new ObjectType(
+                        o => o.Name("Query")
+                            .Field("someField")
+                            .Argument("a", a => a.Type<IntType>())
+                            .Type(reviewType)
+                    ));
+
+                    t.RegisterDirective<KeyDirectiveType>();
+                    t.RegisterDirective<RequiresDirectiveType>();
+                    t.RegisterType<FieldSetType>();
+                    t.Use(next => context => default);
+                });
+
+            // act
+            ObjectType testType = schema.GetType<ObjectType>("Review");
+
+            // assert
+            Assert.Collection(testType.Fields.Single(field => field.Name.Value == "product").Directives,
+                providesDirective =>
+                {
+                    Assert.Equal(
+                        TypeNames.Requires,
+                        providesDirective.Name
+                    );
+                    Assert.Equal("fields", providesDirective.ToNode().Arguments[0].Name.ToString());
+                    Assert.Equal("\"id\"", providesDirective.ToNode().Arguments[0].Value.ToString());
+                }
+            );
+            schema.ToString().MatchSnapshot();
+        }
+
+        [Fact]
+        public void AnnotateProvidesToClassAttributePureCodeFirst()
+        {
+            Snapshot.FullName();
+
+            // arrange
+            var schema = SchemaBuilder.New()
+                .AddApolloFederation()
+                .AddQueryType<Query>()
+                .Create();
+
+            // act
+            ObjectType testType = schema.GetType<ObjectType>("Review");
+
+            // assert
+            Assert.Collection(testType.Fields.Single(field => field.Name.Value == "product").Directives,
+                providesDirective =>
+                {
+                    Assert.Equal(
+                        TypeNames.Requires,
+                        providesDirective.Name
+                    );
+                    Assert.Equal("fields", providesDirective.ToNode().Arguments[0].Name.ToString());
+                    Assert.Equal("\"id\"", providesDirective.ToNode().Arguments[0].Value.ToString());
+                }
+            );
+            schema.ToString().MatchSnapshot();
+        }
+
+        public class Query
+        {
+            public Review someField(int id) => default;
+        }
+
+        public class Review
+        {
+            [Key]
+            public int Id { get; set; }
+            [Requires("id")]
+            public Product Product { get; set; }
+        }
+
+        public class Product
+        {
+            public string Name { get; set; }
         }
     }
 }
