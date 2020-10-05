0adb1ea02a689f6b388b079634a66e7b2dae5708
Michael Staib <michael@chillicream.com>
2020-10-06 00:03:15 +0200

    Added more configuration api for schema stiching

--- src/HotChocolate/Stitching/src/Stitching/DependencyInjection/HotChocolateStitchingRequestExecutorExtensions.cs
+++ src/HotChocolate/Stitching/src/Stitching/DependencyInjection/HotChocolateStitchingRequestExecutorExtensions.cs
@@ -1,18 +1,17 @@
 using System;
-using System.Linq;
+using System.IO;
 using System.Net.Http;
-using System.Threading.Tasks;
-using GreenDonut;
 using HotChocolate;
 using HotChocolate.Execution;
 using HotChocolate.Execution.Configuration;
 using HotChocolate.Language;
 using HotChocolate.Stitching;
+using HotChocolate.Stitching.Merge;
+using HotChocolate.Stitching.Merge.Rewriters;
 using HotChocolate.Stitching.Pipeline;
 using HotChocolate.Stitching.Requests;
 using HotChocolate.Utilities.Introspection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using WellKnownContextData = HotChocolate.Stitching.WellKnownContextData;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -68,6 +67,13 @@ namespace Microsoft.Extensions.DependencyInjection
             this IRequestExecutorBuilder builder,
             NameString schemaName)
         {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            schemaName.EnsureNotEmpty(nameof(schemaName));
+
             // first we add a full GraphQL schema and executor that represents the remote schema.
             // This remote schema will be used by the stitching engine to execute queries against
             // this schema and also to lookup types in order correctly convert between scalars.
@@ -126,6 +132,264 @@ namespace Microsoft.Extensions.DependencyInjection
             builder.Services.TryAddScoped<IStitchingContext, StitchingContext>();
 
             return builder;
+        }
+
+        /// <summary>
+        /// Add a type merge rule in order to define how a type is merged.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="IRequestExecutorBuilder"/>.
+        /// </param>
+        /// <param name="mergeRuleFactory">
+        /// A factory that create the type merging rule.
+        /// </param>
+        /// <returns>
+        /// Returns the <see cref="IRequestExecutorBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="builder"/> is <c>null</c>, or
+        /// <paramref name="mergeRuleFactory"/> is <c>null</c>.
+        /// </exception>
+        public static IRequestExecutorBuilder AddTypeMergeRule(
+            this IRequestExecutorBuilder builder,
+            MergeTypeRuleFactory mergeRuleFactory)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (mergeRuleFactory == null)
+            {
+                throw new ArgumentNullException(nameof(mergeRuleFactory));
+            }
+
+            return builder.ConfigureSchema(
+                s => s.AddTypeMergeRules(mergeRuleFactory));
+        }
+
+        /// <summary>
+        /// Add a directive merge rule in order to define
+        /// how a directive is merged.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="IRequestExecutorBuilder"/>.
+        /// </param>
+        /// <param name="mergeRuleFactory">
+        /// A factory that create the directive merging rule.
+        /// </param>
+        /// <returns>
+        /// Returns the <see cref="IRequestExecutorBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="builder"/> is <c>null</c>, or
+        /// <paramref name="mergeRuleFactory"/> is <c>null</c>.
+        /// </exception>
+        public static IRequestExecutorBuilder AddDirectiveMergeRule(
+            this IRequestExecutorBuilder builder,
+            MergeDirectiveRuleFactory mergeRuleFactory)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (mergeRuleFactory == null)
+            {
+                throw new ArgumentNullException(nameof(mergeRuleFactory));
+            }
+
+            return builder.ConfigureSchema(
+                s => s.AddDirectiveMergeRules(mergeRuleFactory));
+        }
+
+        /// <summary>
+        /// Add a type definition rewriter in order to rewrite a
+        /// type definition on a remote schema document before
+        /// it is being merged.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="IRequestExecutorBuilder"/>.
+        /// </param>
+        /// <param name="rewriter">
+        /// The type definition rewriter.
+        /// </param>
+        /// <returns>
+        /// Returns the <see cref="IRequestExecutorBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="builder"/> is <c>null</c>, or
+        /// <paramref name="rewriter"/> is <c>null</c>.
+        /// </exception>
+        public static IRequestExecutorBuilder AddTypeRewriter(
+            this IRequestExecutorBuilder builder,
+            ITypeRewriter rewriter)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (rewriter == null)
+            {
+                throw new ArgumentNullException(nameof(rewriter));
+            }
+
+            return builder.ConfigureSchema(
+                s => s.AddTypeRewriter(rewriter));
+        }
+
+        /// <summary>
+        /// Add a document rewriter in order to rewrite
+        /// a remote schema document before it is being merged.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="IRequestExecutorBuilder"/>.
+        /// </param>
+        /// <param name="rewriter">
+        /// The document rewriter.
+        /// </param>
+        /// <returns>
+        /// Returns the <see cref="IRequestExecutorBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="builder"/> is <c>null</c>, or
+        /// <paramref name="rewriter"/> is <c>null</c>.
+        /// </exception>
+        public static IRequestExecutorBuilder AddDocumentRewriter(
+            this IRequestExecutorBuilder builder,
+            IDocumentRewriter rewriter)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (rewriter == null)
+            {
+                throw new ArgumentNullException(nameof(rewriter));
+            }
+
+            return builder.ConfigureSchema(
+                s => s.AddDocumentRewriter(rewriter));
+        }
+
+        /// <summary>
+        /// Adds a schema SDL that contains type extensions.
+        /// Extension documents can be used to extend merged types
+        /// or even replace them.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="IRequestExecutorBuilder"/>.
+        /// </param>
+        /// <param name="fileName">
+        /// The file name of the type extension document.
+        /// </param>
+        /// <returns>
+        /// Returns the <see cref="IRequestExecutorBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="builder"/> is <c>null</c>, or
+        /// <paramref name="fileName"/> is <c>null</c>.
+        /// </exception>
+        public static IRequestExecutorBuilder AddTypeExtensionsFromString(
+            this IRequestExecutorBuilder builder,
+            string fileName)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            return builder.ConfigureSchemaAsync(
+                async (s, ct) =>
+                {
+                    string content = await File
+                        .ReadAllTextAsync(fileName, ct)
+                        .ConfigureAwait(false);
+
+                    DocumentNode document =
+                        Utf8GraphQLParser.Parse(content);
+
+                    s.AddTypeExtensions(document);
+                });
+        }
+
+        /// <summary>
+        /// Add a document rewriter that is executed on
+        /// the merged schema document.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="IRequestExecutorBuilder"/>.
+        /// </param>
+        /// <param name="rewrite">
+        /// A delegate that is called to execute the
+        /// rewrite document logic.
+        /// </param>
+        /// <returns>
+        /// Returns the <see cref="IRequestExecutorBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="builder"/> is <c>null</c>, or
+        /// <paramref name="rewrite"/> is <c>null</c>.
+        /// </exception>
+        public static IRequestExecutorBuilder AddMergedDocumentRewriter(
+            this IRequestExecutorBuilder builder,
+            Func<DocumentNode, DocumentNode> rewrite)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (rewrite == null)
+            {
+                throw new ArgumentNullException(nameof(rewrite));
+            }
+
+            return builder.ConfigureSchema(
+                s => s.AddMergedDocRewriter(rewrite));
+        }
+
+        /// <summary>
+        /// Adds a schema visitor that is executed on
+        /// the merged schema document.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="IRequestExecutorBuilder"/>.
+        /// </param>
+        /// <param name="visit">
+        /// A delegate that is called to execute the
+        /// document visitation logic.
+        /// </param>
+        /// <returns>
+        /// Returns the <see cref="IRequestExecutorBuilder"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="builder"/> is <c>null</c>, or
+        /// <paramref name="visit"/> is <c>null</c>.
+        /// </exception>
+        public static IRequestExecutorBuilder AddMergedDocVisitor(
+            this IRequestExecutorBuilder builder,
+            Action<DocumentNode> visit)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (visit == null)
+            {
+                throw new ArgumentNullException(nameof(visit));
+            }
+
+            return builder.ConfigureSchema(
+                s => s.AddMergedDocVisitor(visit));
         }
     }
 }
--- src/HotChocolate/Stitching/src/Stitching/Extensions/ContextDataExtensions.cs
+++ src/HotChocolate/Stitching/src/Stitching/Extensions/ContextDataExtensions.cs
@@ -25,96 +25,6 @@ namespace HotChocolate.Stitching
                 "The mandatory remote executors have not been found.");
         }
 
-        public static IReadOnlyList<MergeTypeRuleFactory> GetTypeMergeRules(
-            this IDescriptorContext hasContextData)
-        {
-            if (hasContextData.ContextData.TryGetValue(TypeMergeRules, out object? o) &&
-                o is IReadOnlyList<MergeTypeRuleFactory> rules)
-            {
-                return rules;
-            }
-
-            return Array.Empty<MergeTypeRuleFactory>();
-        }
-
-        public static IReadOnlyList<MergeDirectiveRuleFactory> GetDirectiveMergeRules(
-            this IDescriptorContext hasContextData)
-        {
-            if (hasContextData.ContextData.TryGetValue(DirectiveMergeRules, out object? o) &&
-                o is IReadOnlyList<MergeDirectiveRuleFactory> rules)
-            {
-                return rules;
-            }
-
-            return Array.Empty<MergeDirectiveRuleFactory>();
-        }
-
-        public static IReadOnlyList<IDocumentRewriter> GetDocumentRewriter(
-            this IDescriptorContext hasContextData)
-        {
-            if (hasContextData.ContextData.TryGetValue(DocumentRewriter, out object? o) &&
-                o is IReadOnlyList<IDocumentRewriter> rules)
-            {
-                return rules;
-            }
-
-            return Array.Empty<IDocumentRewriter>();
-        }
-
-        public static IReadOnlyList<ITypeRewriter> GetTypeRewriter(
-            this IDescriptorContext hasContextData)
-        {
-            if (hasContextData.ContextData.TryGetValue(TypeRewriter, out object? o) &&
-                o is IReadOnlyList<ITypeRewriter> rules)
-            {
-                return rules;
-            }
-
-            return Array.Empty<ITypeRewriter>();
-        }
-
-        public static IReadOnlyList<DocumentNode> GetTypeExtensions(
-            this IDescriptorContext hasContextData)
-        {
-            if (hasContextData.ContextData.TryGetValue(TypeExtensions, out object? o) &&
-                o is IReadOnlyList<DocumentNode> rules)
-            {
-                return rules;
-            }
-
-            return Array.Empty<DocumentNode>();
-        }
-
-        public static IReadOnlyList<Func<DocumentNode, DocumentNode>> GetMergedDocRewriter(
-            this IDescriptorContext hasContextData)
-        {
-            if (hasContextData.ContextData.TryGetValue(MergedDocRewriter, out object? o) &&
-                o is IReadOnlyList<Func<DocumentNode, DocumentNode>> rules)
-            {
-                return rules;
-            }
-
-            return Array.Empty<Func<DocumentNode, DocumentNode>>();
-        }
-
-        public static IReadOnlyList<Action<DocumentNode>> GetMergedDocVisitors(
-            this IDescriptorContext hasContextData)
-        {
-            if (hasContextData.ContextData.TryGetValue(MergedDocRewriter, out object? o) &&
-                o is IReadOnlyList<Action<DocumentNode>> rules)
-            {
-                return rules;
-            }
-
-            return Array.Empty<Action<DocumentNode>>();
-        }
-
-        public static void AddRemoteExecutor(
-            this IHasContextData hasContextData,
-            NameString schemaName,
-            IRequestExecutor executor) =>
-            hasContextData.ContextData.AddRemoteExecutor(schemaName, executor);
-
         public static void AddRemoteExecutor(
             this IDictionary<string, object?> contextData,
             NameString schemaName,
@@ -163,5 +73,201 @@ namespace HotChocolate.Stitching
                         return dict;
                     });
         }
+
+        public static IReadOnlyList<MergeTypeRuleFactory> GetTypeMergeRules(
+            this IDescriptorContext hasContextData)
+        {
+            if (hasContextData.ContextData.TryGetValue(TypeMergeRules, out object? o) &&
+                o is IReadOnlyList<MergeTypeRuleFactory> rules)
+            {
+                return rules;
+            }
+
+            return Array.Empty<MergeTypeRuleFactory>();
+        }
+
+        public static ISchemaBuilder AddTypeMergeRules(
+            this ISchemaBuilder schemaBuilder,
+            MergeTypeRuleFactory mergeRuleFactory) =>
+            schemaBuilder.SetContextData(
+                TypeMergeRules,
+                current =>
+                {
+                    if (!(current is List<MergeTypeRuleFactory> list))
+                    {
+                        list = new List<MergeTypeRuleFactory>();
+                    }
+
+                    list.Add(mergeRuleFactory);
+                    return list;
+                });
+
+        public static IReadOnlyList<MergeDirectiveRuleFactory> GetDirectiveMergeRules(
+            this IDescriptorContext hasContextData)
+        {
+            if (hasContextData.ContextData.TryGetValue(DirectiveMergeRules, out object? o) &&
+                o is IReadOnlyList<MergeDirectiveRuleFactory> rules)
+            {
+                return rules;
+            }
+
+            return Array.Empty<MergeDirectiveRuleFactory>();
+        }
+
+        public static ISchemaBuilder AddDirectiveMergeRules(
+            this ISchemaBuilder schemaBuilder,
+            MergeDirectiveRuleFactory mergeRuleFactory) =>
+            schemaBuilder.SetContextData(
+                DirectiveMergeRules,
+                current =>
+                {
+                    if (!(current is List<MergeDirectiveRuleFactory> list))
+                    {
+                        list = new List<MergeDirectiveRuleFactory>();
+                    }
+
+                    list.Add(mergeRuleFactory);
+                    return list;
+                });
+
+        public static IReadOnlyList<ITypeRewriter> GetTypeRewriter(
+            this IDescriptorContext hasContextData)
+        {
+            if (hasContextData.ContextData.TryGetValue(TypeRewriter, out object? o) &&
+                o is IReadOnlyList<ITypeRewriter> rules)
+            {
+                return rules;
+            }
+
+            return Array.Empty<ITypeRewriter>();
+        }
+
+        public static ISchemaBuilder AddTypeRewriter(
+            this ISchemaBuilder schemaBuilder,
+            ITypeRewriter rewriter) =>
+            schemaBuilder.SetContextData(
+                TypeRewriter,
+                current =>
+                {
+                    if (!(current is List<ITypeRewriter> list))
+                    {
+                        list = new List<ITypeRewriter>();
+                    }
+
+                    list.Add(rewriter);
+                    return list;
+                });
+
+        public static IReadOnlyList<IDocumentRewriter> GetDocumentRewriter(
+            this IDescriptorContext hasContextData)
+        {
+            if (hasContextData.ContextData.TryGetValue(DocumentRewriter, out object? o) &&
+                o is IReadOnlyList<IDocumentRewriter> rules)
+            {
+                return rules;
+            }
+
+            return Array.Empty<IDocumentRewriter>();
+        }
+
+        public static ISchemaBuilder AddDocumentRewriter(
+            this ISchemaBuilder schemaBuilder,
+            IDocumentRewriter rewriter) =>
+            schemaBuilder.SetContextData(
+                DocumentRewriter,
+                current =>
+                {
+                    if (!(current is List<IDocumentRewriter> list))
+                    {
+                        list = new List<IDocumentRewriter>();
+                    }
+
+                    list.Add(rewriter);
+                    return list;
+                });
+
+        public static IReadOnlyList<DocumentNode> GetTypeExtensions(
+            this IDescriptorContext hasContextData)
+        {
+            if (hasContextData.ContextData.TryGetValue(TypeExtensions, out object? o) &&
+                o is IReadOnlyList<DocumentNode> rules)
+            {
+                return rules;
+            }
+
+            return Array.Empty<DocumentNode>();
+        }
+
+        public static ISchemaBuilder AddTypeExtensions(
+            this ISchemaBuilder schemaBuilder,
+            DocumentNode document) =>
+            schemaBuilder.SetContextData(
+                TypeExtensions,
+                current =>
+                {
+                    if (!(current is List<DocumentNode> list))
+                    {
+                        list = new List<DocumentNode>();
+                    }
+
+                    list.Add(document);
+                    return list;
+                });
+
+        public static IReadOnlyList<Func<DocumentNode, DocumentNode>> GetMergedDocRewriter(
+            this IDescriptorContext hasContextData)
+        {
+            if (hasContextData.ContextData.TryGetValue(MergedDocRewriter, out object? o) &&
+                o is IReadOnlyList<Func<DocumentNode, DocumentNode>> rules)
+            {
+                return rules;
+            }
+
+            return Array.Empty<Func<DocumentNode, DocumentNode>>();
+        }
+
+        public static ISchemaBuilder AddMergedDocRewriter(
+            this ISchemaBuilder schemaBuilder,
+            Func<DocumentNode, DocumentNode> rewrite) =>
+            schemaBuilder.SetContextData(
+                MergedDocRewriter,
+                current =>
+                {
+                    if (!(current is List<Func<DocumentNode, DocumentNode>> list))
+                    {
+                        list = new List<Func<DocumentNode, DocumentNode>>();
+                    }
+
+                    list.Add(rewrite);
+                    return list;
+                });
+
+        public static IReadOnlyList<Action<DocumentNode>> GetMergedDocVisitors(
+            this IDescriptorContext hasContextData)
+        {
+            if (hasContextData.ContextData.TryGetValue(MergedDocRewriter, out object? o) &&
+                o is IReadOnlyList<Action<DocumentNode>> rules)
+            {
+                return rules;
+            }
+
+            return Array.Empty<Action<DocumentNode>>();
+        }
+
+        public static ISchemaBuilder AddMergedDocVisitor(
+            this ISchemaBuilder schemaBuilder,
+            Action<DocumentNode> visit) =>
+            schemaBuilder.SetContextData(
+                MergedDocRewriter,
+                current =>
+                {
+                    if (!(current is List<Action<DocumentNode>> list))
+                    {
+                        list = new List<Action<DocumentNode>>();
+                    }
+
+                    list.Add(visit);
+                    return list;
+                });
     }
 }
