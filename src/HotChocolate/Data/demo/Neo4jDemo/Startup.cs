b9403d94f02468f697a451f44e628279717e33ec
Arif Hanif <ahanif@aeieng.com>
2020-10-07 00:24:00 -0400

    add demo project

---
+++ src/HotChocolate/Data/demo/Neo4jDemo/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using HotChocolate;
+
+namespace Neo4jDemo
+{
+    public class Startup
+    {
+        public class Query
+        {
+            public string Hello() => "World";
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGraphQLServer()
+                .AddQueryType<Query>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("GraphQL Server Launched!");
+                });
+            });
+        }
+    }
+}
