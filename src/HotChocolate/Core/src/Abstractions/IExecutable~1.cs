3ca2178e8c9cabad1e9f0680d42332c9d0b1620a
Pascal Senn <senn.pasc@gmail.com>
2020-10-16 17:10:52 +0200

    rename to executable

--- src/HotChocolate/Core/src/Abstractions/IExecutable~1.cs
+++ src/HotChocolate/Core/src/Abstractions/IExecutable~1.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace HotChocolate
 {
-    public interface IQuery<T> : IQuery
+    public interface IExecutable<T> : IExecutable
     {
         new ValueTask<IReadOnlyList<T>> ExecuteAsync(CancellationToken cancellationToken);
     }
