14821793717c67cde7ceb8ed7e6c6290e80edaab
Pascal Senn <senn.pasc@gmail.com>
2020-10-23 22:23:35 +0200

    feat: fix spatial filtering tests

--- src/HotChocolate/Spatial/test/Data.Tests/GeometryFilterTypeTests.cs
+++ src/HotChocolate/Spatial/test/Data.Tests/GeometryFilterTypeTests.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using HotChocolate.Types;
 using Snapshooter.Xunit;
+using Snapshooter;
 using Xunit;
 
 namespace HotChocolate.Data.Spatial.Filters.Tests
@@ -17,7 +18,101 @@ namespace HotChocolate.Data.Spatial.Filters.Tests
             ISchema schema = CreateSchema(s => s.AddType<GeometryFilterType>());
 
             // assert
-            schema.ToString().MatchSnapshot();
+#if NETCOREAPP2_1
+            schema.Print().MatchSnapshot(new SnapshotNameExtension("NETCOREAPP2_1"));
+#else
+            schema.Print().MatchSnapshot();
+#endif
+        }
+
+        [Fact]
+        public void LineStringFilterType_Native()
+        {
+            // arrange
+            // act
+            ISchema schema = CreateSchema(s => s.AddType<LineStringFilterType>());
+
+            // assert
+#if NETCOREAPP2_1
+            schema.Print().MatchSnapshot(new SnapshotNameExtension("NETCOREAPP2_1"));
+#else
+            schema.Print().MatchSnapshot();
+#endif
+        }
+
+        [Fact]
+        public void MultiLineStringFilterType_Native()
+        {
+            // arrange
+            // act
+            ISchema schema = CreateSchema(s => s.AddType<MultiLineStringFilterType>());
+
+            // assert
+#if NETCOREAPP2_1
+            schema.Print().MatchSnapshot(new SnapshotNameExtension("NETCOREAPP2_1"));
+#else
+            schema.Print().MatchSnapshot();
+#endif
+        }
+
+        [Fact]
+        public void PointFilterType_Native()
+        {
+            // arrange
+            // act
+            ISchema schema = CreateSchema(s => s.AddType<PointFilterType>());
+
+            // assert
+#if NETCOREAPP2_1
+            schema.Print().MatchSnapshot(new SnapshotNameExtension("NETCOREAPP2_1"));
+#else
+            schema.Print().MatchSnapshot();
+#endif
+        }
+
+        [Fact]
+        public void MultiPointFilterType_Native()
+        {
+            // arrange
+            // act
+            ISchema schema = CreateSchema(s => s.AddType<MultiPointFilterType>());
+
+            // assert
+#if NETCOREAPP2_1
+            schema.Print().MatchSnapshot(new SnapshotNameExtension("NETCOREAPP2_1"));
+#else
+            schema.Print().MatchSnapshot();
+#endif
+        }
+
+        [Fact]
+        public void PolygonFilterType_Native()
+        {
+            // arrange
+            // act
+            ISchema schema = CreateSchema(s => s.AddType<PolygonFilterType>());
+
+            // assert
+#if NETCOREAPP2_1
+            schema.Print().MatchSnapshot(new SnapshotNameExtension("NETCOREAPP2_1"));
+#else
+            schema.Print().MatchSnapshot();
+#endif
+        }
+
+        [Fact]
+        public void MultiPolygonFilterType_Native()
+        {
+            // arrange
+            // act
+            ISchema schema = CreateSchema(s => s.AddType<MultiPolygonFilterType>());
+
+            // assert
+#if NETCOREAPP2_1
+            schema.Print().MatchSnapshot(new SnapshotNameExtension("NETCOREAPP2_1"));
+#else
+            schema.Print().MatchSnapshot();
+#endif
         }
 
         public class FooDirective
