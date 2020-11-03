86c79b58e523c22bb224ddec5d5a5fd5d012a40b
Pascal Senn <senn.pasc@gmail.com>
2020-11-03 01:46:18 +0100

    feat: add docs

--- src/HotChocolate/Data/src/Data/Extensions/DataEnumerableExtensions.cs
+++ src/HotChocolate/Data/src/Data/Extensions/DataEnumerableExtensions.cs
@@ -5,11 +5,25 @@ namespace HotChocolate.Data
 {
     public static class DataEnumerableExtensions
     {
+        /// <summary>
+        /// Wraps the <see cref="IEnumerable{T}"/> with <see cref="QueryableExecutable{T}"/> to help
+        /// the execution engine to execute it more efficient
+        /// </summary>
+        /// <param name="source">The source of the <see cref="IExecutable"/></param>
+        /// <typeparam name="T">The type parameter</typeparam>
+        /// <returns>The wrapped object</returns>
         public static QueryableExecutable<T> AsExecutable<T>(this IEnumerable<T> source)
         {
             return new QueryableExecutable<T>(source.AsQueryable());
         }
 
+        /// <summary>
+        /// Wraps the <see cref="IQueryable"/> with <see cref="QueryableExecutable{T}"/> to help the
+        /// execution engine to execute it more efficient
+        /// </summary>
+        /// <param name="source">The source of the <see cref="IExecutable"/></param>
+        /// <typeparam name="T">The type parameter</typeparam>
+        /// <returns>The wrapped object</returns>
         public static QueryableExecutable<T> AsExecutable<T>(this IQueryable<T> source)
         {
             return new QueryableExecutable<T>(source);
