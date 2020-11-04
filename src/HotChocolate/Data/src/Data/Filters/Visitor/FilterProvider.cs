da9a820ee5f79bdca128d8044b4322403973aa14
Michael Staib <michael@chillicream.com>
2020-11-04 22:15:13 +0100

    Update FilterProvider.cs

--- src/HotChocolate/Data/src/Data/Filters/Visitor/FilterProvider.cs
+++ src/HotChocolate/Data/src/Data/Filters/Visitor/FilterProvider.cs
@@ -80,11 +80,11 @@ namespace HotChocolate.Data.Filters
             }
 
             IServiceProvider services = new DictionaryServiceProvider(
-                    (typeof(IFilterProvider), this),
-                    (typeof(IConventionContext), context),
-                    (typeof(IDescriptorContext), context.DescriptorContext),
-                    (typeof(IFilterConvention), _filterConvention),
-                    (typeof(ITypeInspector), context.DescriptorContext.TypeInspector))
+                (typeof(IFilterProvider), this),
+                (typeof(IConventionContext), context),
+                (typeof(IDescriptorContext), context.DescriptorContext),
+                (typeof(IFilterConvention), _filterConvention),
+                (typeof(ITypeInspector), context.DescriptorContext.TypeInspector))
                 .Include(context.Services);
 
             foreach ((Type Type, IFilterFieldHandler? Instance) handler in Definition.Handlers)
