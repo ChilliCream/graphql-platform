661ba06f31bff3009b050329e2c86787e8e0f70b
Michael Staib <michael@chillicream.com>
2020-11-04 22:41:16 +0100

    Update QueryableFilterVisitorOverlapsTests.cs

--- src/HotChocolate/Spatial/test/Data.Filters.SqlServer.Tests/QueryableFilterVisitorOverlapsTests.cs
+++ src/HotChocolate/Spatial/test/Data.Filters.SqlServer.Tests/QueryableFilterVisitorOverlapsTests.cs
@@ -8,38 +8,35 @@ using Xunit;
 namespace HotChocolate.Spatial.Data.Filters
 {
     public class QueryableFilterVisitorOverlapsTests
-        : SchemaCache,
-          IClassFixture<PostgreSqlResource<PostgisConfig>>
+        : SchemaCache
+        , IClassFixture<PostgreSqlResource<PostgisConfig>>
     {
-        private static readonly Polygon _truePolygon = new Polygon(
-            new LinearRing(
-                new[]
-                {
-                    new Coordinate(150, 150),
-                    new Coordinate(270, 150),
-                    new Coordinate(190, 70),
-                    new Coordinate(140, 20),
-                    new Coordinate(20, 20),
-                    new Coordinate(70, 70),
-                    new Coordinate(150, 150)
-                })
-        );
+        private static readonly Polygon _truePolygon = 
+            new Polygon(new LinearRing(new[]
+            {
+                new Coordinate(150, 150),
+                new Coordinate(270, 150),
+                new Coordinate(190, 70),
+                new Coordinate(140, 20),
+                new Coordinate(20, 20),
+                new Coordinate(70, 70),
+                new Coordinate(150, 150)
+            }));
 
-        private static readonly Polygon _falsePolygon = new Polygon(
-            new LinearRing(
-                new[]
-                {
-                    new Coordinate(1000, 1000),
-                    new Coordinate(100000, 1000),
-                    new Coordinate(100000, 100000),
-                    new Coordinate(1000, 100000),
-                    new Coordinate(1000, 1000),
-                })
-        );
+        private static readonly Polygon _falsePolygon = 
+            new Polygon(new LinearRing(new[]
+            {
+                new Coordinate(1000, 1000),
+                new Coordinate(100000, 1000),
+                new Coordinate(100000, 100000),
+                new Coordinate(1000, 100000),
+                new Coordinate(1000, 1000),
+            }));
 
         private static readonly Foo[] _fooEntities =
         {
-            new Foo { Id = 1, Bar = _truePolygon }, new Foo { Id = 2, Bar = _falsePolygon }
+            new Foo { Id = 1, Bar = _truePolygon },
+            new Foo { Id = 2, Bar = _falsePolygon }
         };
 
         public QueryableFilterVisitorOverlapsTests(PostgreSqlResource<PostgisConfig> resource)
@@ -59,28 +56,28 @@ namespace HotChocolate.Spatial.Data.Filters
                 QueryRequestBuilder.New()
                     .SetQuery(
                         @"{
-                        root(where: {
-                            bar: {
-                                overlaps: {
-                                    geometry: {
-                                        type: Polygon,
-                                        coordinates: [
-                                            [150 150],
-                                            [270 150],
-                                            [330 150],
-                                            [250 70],
-                                            [190 70],
-                                            [70 70],
-                                            [150 150]
-                                        ]
-                                    },
-                                    eq: true
+                            root(where: {
+                                bar: {
+                                    overlaps: {
+                                        geometry: {
+                                            type: Polygon,
+                                            coordinates: [
+                                                [150 150],
+                                                [270 150],
+                                                [330 150],
+                                                [250 70],
+                                                [190 70],
+                                                [70 70],
+                                                [150 150]
+                                            ]
+                                        },
+                                        eq: true
+                                    }
                                 }
+                            }){
+                                id
                             }
-                        }){
-                            id
-                        }
-                    }")
+                        }")
                     .Create());
 
             res1.MatchSqlSnapshot("true");
@@ -89,28 +86,28 @@ namespace HotChocolate.Spatial.Data.Filters
                 QueryRequestBuilder.New()
                     .SetQuery(
                         @"{
-                        root(where: {
-                            bar: {
-                                overlaps: {
-                                    geometry: {
-                                        type: Polygon,
-                                        coordinates: [
-                                            [150 150],
-                                            [270 150],
-                                            [330 150],
-                                            [250 70],
-                                            [190 70],
-                                            [70 70],
-                                            [150 150]
-                                        ]
-                                    },
-                                    eq: false
+                            root(where: {
+                                bar: {
+                                    overlaps: {
+                                        geometry: {
+                                            type: Polygon,
+                                            coordinates: [
+                                                [150 150],
+                                                [270 150],
+                                                [330 150],
+                                                [250 70],
+                                                [190 70],
+                                                [70 70],
+                                                [150 150]
+                                            ]
+                                        },
+                                        eq: false
+                                    }
                                 }
+                            }){
+                                id
                             }
-                        }){
-                            id
-                        }
-                    }")
+                        }")
                     .Create());
 
             res2.MatchSqlSnapshot("false");
