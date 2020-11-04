fbf366ff506ad352914785b294937785979b120f
Michael Staib <michael@chillicream.com>
2020-11-04 22:38:36 +0100

    Update QueryableFilterVisitorIntersectTests.cs

--- src/HotChocolate/Spatial/test/Data.Filters.SqlServer.Tests/QueryableFilterVisitorIntersectTests.cs
+++ src/HotChocolate/Spatial/test/Data.Filters.SqlServer.Tests/QueryableFilterVisitorIntersectTests.cs
@@ -8,36 +8,33 @@ using Xunit;
 namespace HotChocolate.Spatial.Data.Filters
 {
     public class QueryableFilterVisitorIntersectsTests
-        : SchemaCache,
-          IClassFixture<PostgreSqlResource<PostgisConfig>>
+        : SchemaCache
+        , IClassFixture<PostgreSqlResource<PostgisConfig>>
     {
-        private static readonly Polygon _truePolygon = new Polygon(
-            new LinearRing(
-                new[]
-                {
-                    new Coordinate(0, 0),
-                    new Coordinate(100, 0),
-                    new Coordinate(100, 100),
-                    new Coordinate(0, 100),
-                    new Coordinate(0, 0),
-                })
-        );
+        private static readonly Polygon _truePolygon = 
+            new Polygon(new LinearRing(new[]
+            {
+                new Coordinate(0, 0),
+                new Coordinate(100, 0),
+                new Coordinate(100, 100),
+                new Coordinate(0, 100),
+                new Coordinate(0, 0),
+            }));
 
-        private static readonly Polygon _falsePolygon = new Polygon(
-            new LinearRing(
-                new[]
-                {
-                    new Coordinate(1000, 1000),
-                    new Coordinate(100000, 1000),
-                    new Coordinate(100000, 100000),
-                    new Coordinate(1000, 100000),
-                    new Coordinate(1000, 1000),
-                })
-        );
+        private static readonly Polygon _falsePolygon = 
+            new Polygon(new LinearRing(new[]
+            {
+                new Coordinate(1000, 1000),
+                new Coordinate(100000, 1000),
+                new Coordinate(100000, 100000),
+                new Coordinate(1000, 100000),
+                new Coordinate(1000, 1000),
+            }));
 
         private static readonly Foo[] _fooEntities =
         {
-            new Foo { Id = 1, Bar = _truePolygon }, new Foo { Id = 2, Bar = _falsePolygon }
+            new Foo { Id = 1, Bar = _truePolygon },
+            new Foo { Id = 2, Bar = _falsePolygon }
         };
 
         public QueryableFilterVisitorIntersectsTests(PostgreSqlResource<PostgisConfig> resource)
@@ -57,20 +54,20 @@ namespace HotChocolate.Spatial.Data.Filters
                 QueryRequestBuilder.New()
                     .SetQuery(
                         @"{
-                        root(where: {
-                            bar: {
-                                intersects: {
-                                    geometry: {
-                                        type: Polygon,
-                                        coordinates: [[10 10], [10 90], [90 90], [90 10], [10 10]]
-                                    },
-                                    eq: true
+                            root(where: {
+                                bar: {
+                                    intersects: {
+                                        geometry: {
+                                            type: Polygon,
+                                            coordinates: [[10 10], [10 90], [90 90], [90 10], [10 10]]
+                                        },
+                                        eq: true
+                                    }
                                 }
+                            }){
+                                id
                             }
-                        }){
-                            id
-                        }
-                    }")
+                        }")
                     .Create());
 
             res1.MatchSqlSnapshot("true");
@@ -79,20 +76,20 @@ namespace HotChocolate.Spatial.Data.Filters
                 QueryRequestBuilder.New()
                     .SetQuery(
                         @"{
-                        root(where: {
-                            bar: {
-                                intersects: {
-                                    geometry: {
-                                        type: Polygon,
-                                        coordinates: [[10 10], [10 90], [90 90], [90 10], [10 10]]
-                                    },
-                                    eq: false
+                            root(where: {
+                                bar: {
+                                    intersects: {
+                                        geometry: {
+                                            type: Polygon,
+                                            coordinates: [[10 10], [10 90], [90 90], [90 10], [10 10]]
+                                        },
+                                        eq: false
+                                    }
                                 }
+                            }){
+                                id
                             }
-                        }){
-                            id
-                        }
-                    }")
+                        }")
                     .Create());
 
             res2.MatchSqlSnapshot("false");
