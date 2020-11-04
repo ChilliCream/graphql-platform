46d13bd990d2542fa189acc03fcbf93dff5769c3
Michael Staib <michael@chillicream.com>
2020-11-04 22:43:00 +0100

    Update QueryableFilterVisitorTouchesTests.cs

--- src/HotChocolate/Spatial/test/Data.Filters.SqlServer.Tests/QueryableFilterVisitorTouchesTests.cs
+++ src/HotChocolate/Spatial/test/Data.Filters.SqlServer.Tests/QueryableFilterVisitorTouchesTests.cs
@@ -8,36 +8,33 @@ using Xunit;
 namespace HotChocolate.Spatial.Data.Filters
 {
     public class QueryableFilterVisitorTouchesTests
-        : SchemaCache,
-          IClassFixture<PostgreSqlResource<PostgisConfig>>
+        : SchemaCache
+        , IClassFixture<PostgreSqlResource<PostgisConfig>>
     {
-        private static readonly Polygon _truePolygon = new Polygon(
-            new LinearRing(
-                new[]
-                {
-                    new Coordinate(140, 120),
-                    new Coordinate(160, 20),
-                    new Coordinate(20, 20),
-                    new Coordinate(20, 120),
-                    new Coordinate(140, 120)
-                })
-        );
+        private static readonly Polygon _truePolygon =
+            new Polygon(new LinearRing(new[]
+            {
+                new Coordinate(140, 120),
+                new Coordinate(160, 20),
+                new Coordinate(20, 20),
+                new Coordinate(20, 120),
+                new Coordinate(140, 120)
+            }));
 
-        private static readonly Polygon _falsePolygon = new Polygon(
-            new LinearRing(
-                new[]
-                {
-                    new Coordinate(1000, 1000),
-                    new Coordinate(100000, 1000),
-                    new Coordinate(100000, 100000),
-                    new Coordinate(1000, 100000),
-                    new Coordinate(1000, 1000),
-                })
-        );
+        private static readonly Polygon _falsePolygon =
+            new Polygon(new LinearRing(new[]
+            {
+                new Coordinate(1000, 1000),
+                new Coordinate(100000, 1000),
+                new Coordinate(100000, 100000),
+                new Coordinate(1000, 100000),
+                new Coordinate(1000, 1000),
+            }));
 
         private static readonly Foo[] _fooEntities =
         {
-            new Foo { Id = 1, Bar = _truePolygon }, new Foo { Id = 2, Bar = _falsePolygon }
+            new Foo { Id = 1, Bar = _truePolygon },
+            new Foo { Id = 2, Bar = _falsePolygon }
         };
 
         public QueryableFilterVisitorTouchesTests(PostgreSqlResource<PostgisConfig> resource)
@@ -57,26 +54,26 @@ namespace HotChocolate.Spatial.Data.Filters
                 QueryRequestBuilder.New()
                     .SetQuery(
                         @"{
-                        root(where: {
-                            bar: {
-                                touches: {
-                                    geometry: {
-                                        type: Polygon,
-                                        coordinates: [
-                                            [240 80],
-                                            [140 120],
-                                            [180 240],
-                                            [280 200],
-                                            [240 80]
-                                        ]
-                                    },
-                                    eq: true
+                            root(where: {
+                                bar: {
+                                    touches: {
+                                        geometry: {
+                                            type: Polygon,
+                                            coordinates: [
+                                                [240 80],
+                                                [140 120],
+                                                [180 240],
+                                                [280 200],
+                                                [240 80]
+                                            ]
+                                        },
+                                        eq: true
+                                    }
                                 }
+                            }){
+                                id
                             }
-                        }){
-                            id
-                        }
-                    }")
+                        }")
                     .Create());
 
             res1.MatchSqlSnapshot("true");
@@ -85,26 +82,26 @@ namespace HotChocolate.Spatial.Data.Filters
                 QueryRequestBuilder.New()
                     .SetQuery(
                         @"{
-                        root(where: {
-                            bar: {
-                                touches: {
-                                    geometry: {
-                                        type: Polygon,
-                                        coordinates: [
-                                            [240 80],
-                                            [140 120],
-                                            [180 240],
-                                            [280 200],
-                                            [240 80]
-                                        ]
-                                    },
-                                    eq: false
+                            root(where: {
+                                bar: {
+                                    touches: {
+                                        geometry: {
+                                            type: Polygon,
+                                            coordinates: [
+                                                [240 80],
+                                                [140 120],
+                                                [180 240],
+                                                [280 200],
+                                                [240 80]
+                                            ]
+                                        },
+                                        eq: false
+                                    }
                                 }
+                            }){
+                                id
                             }
-                        }){
-                            id
-                        }
-                    }")
+                        }")
                     .Create());
 
             res2.MatchSqlSnapshot("false");
