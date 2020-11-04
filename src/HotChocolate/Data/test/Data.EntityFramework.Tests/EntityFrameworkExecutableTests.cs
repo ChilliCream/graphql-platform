b6566ec6857042675f85b14223694cd585d31b7f
Michael Staib <michael@chillicream.com>
2020-11-04 11:52:31 +0100

    Update EntityFrameworkExecutableTests.cs

--- src/HotChocolate/Data/test/Data.EntityFramework.Tests/EntityFrameworkExecutableTests.cs
+++ src/HotChocolate/Data/test/Data.EntityFramework.Tests/EntityFrameworkExecutableTests.cs
@@ -20,7 +20,6 @@ namespace HotChocolate.Data
             _context = authorFixture.Context;
         }
 
-
         [Fact]
         public void Extensions_Should_ReturnEntityFrameworkExecutable_When_DBSet()
         {
