aca47bd46b53a922101bd500c063866277894945
Pascal Senn <pascal.senn@swisslife.ch>
2020-11-18 18:52:56 +0100

    feat: add tests

---
+++ src/HotChocolate/Stitching/test/Stitching.Tests/Configuration/ContextDataExtensionsTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace HotChocolate.Stitching.Configuration
+{
+    public class ContextDataExtensionsTest
+    {
+        [Fact]
+        public void AddNameLookup_Single()
+        {
+            // arrange
+            ISchemaBuilder schemaBuilder = SchemaBuilder.New().AddQueryType<Query>();
+
+            // act
+            schemaBuilder.AddNameLookup("OriginalType1", "NewType1", "Schema1");
+            schemaBuilder.AddNameLookup("OriginalType2", "NewType2", "Schema2");
+
+            // assert
+            IReadOnlyDictionary<(NameString, NameString), NameString> lookup =
+                schemaBuilder.Create().GetNameLookup();
+            Assert.Equal("OriginalType1", lookup[("NewType1", "Schema1")]);
+            Assert.Equal("OriginalType2", lookup[("NewType2", "Schema2")]);
+        }
+
+        [Fact]
+        public void AddNameLookup_Multiple()
+        {
+            // arrange
+            ISchemaBuilder schemaBuilder = SchemaBuilder.New().AddQueryType<Query>();
+            var dict = new Dictionary<(NameString, NameString), NameString>
+            {
+                { ("OriginalType1", "Schema1"), "NewType1" },
+                { ("OriginalType2", "Schema2"), "NewType2" }
+            };
+
+            // act
+            schemaBuilder.AddNameLookup(dict);
+
+            // assert
+            IReadOnlyDictionary<(NameString, NameString), NameString> lookup =
+                schemaBuilder.Create().GetNameLookup();
+            Assert.Equal("OriginalType1", lookup[("NewType1", "Schema1")]);
+            Assert.Equal("OriginalType2", lookup[("NewType2", "Schema2")]);
+        }
+
+        public class Query
+        {
+            public string Foo { get; }
+        }
+    }
+}
---
+++ src/HotChocolate/Stitching/test/Stitching.Tests/Configuration/HotChocolateStitchingRequestExecutorExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HotChocolate.Execution;
+using HotChocolate.Execution.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace HotChocolate.Stitching.Configuration
+{
+    public class HotChocolateStitchingRequestExecutorExtensionsTests
+    {
+        [Fact]
+        public async Task RewriteType()
+        {
+            // arrange
+            IRequestExecutorBuilder executorBuilder =
+                new ServiceCollection().AddGraphQL().AddQueryType<Query>();
+
+            // act
+            executorBuilder.RenameType("OriginalType1", "NewType1", "Schema1");
+            executorBuilder.RenameType("OriginalType2", "NewType2", "Schema2");
+
+            // assert
+            ISchema schema = await executorBuilder.BuildSchemaAsync();
+            IReadOnlyDictionary<(NameString, NameString), NameString> lookup =
+                schema.GetNameLookup();
+            Assert.Equal("OriginalType1", lookup[("NewType1", "Schema1")]);
+            Assert.Equal("OriginalType2", lookup[("NewType2", "Schema2")]);
+        }
+    }
+
+    public class Query
+    {
+        public string Foo { get; }
+    }
+}
