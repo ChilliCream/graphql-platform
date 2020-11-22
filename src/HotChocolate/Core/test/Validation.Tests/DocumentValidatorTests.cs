4844fdc7a3d8ea6efb973929a1c3a689a344c50d
Michael Staib <michael@chillicream.com>
2020-11-22 23:05:47 +0100

    Fixed Validation Resources (#2642)

--- src/HotChocolate/Core/test/Validation.Tests/DocumentValidatorTests.cs
+++ src/HotChocolate/Core/test/Validation.Tests/DocumentValidatorTests.cs
@@ -624,8 +624,8 @@ namespace HotChocolate.Validation
                 t =>
                 {
                     Assert.Equal(
-                        "The GraphQL document has an operation complexity of 2 " +
-                        "which exceeds the max allowed operation complexity of 1.",
+                        "The GraphQL document has an execution depth of 2 " +
+                        "which exceeds the max allowed execution depth of 1.",
                         t.Message);
                 });
         }
