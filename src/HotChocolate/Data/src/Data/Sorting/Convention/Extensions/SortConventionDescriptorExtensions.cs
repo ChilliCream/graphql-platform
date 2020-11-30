2f241687b0fb5117b6e45fc922e811034623a3b0
Pascal Senn <senn.pasc@gmail.com>
2020-11-30 01:30:35 +0100

    fix binding of structs on sorting typesg

--- src/HotChocolate/Data/src/Data/Sorting/Convention/Extensions/SortConventionDescriptorExtensions.cs
+++ src/HotChocolate/Data/src/Data/Sorting/Convention/Extensions/SortConventionDescriptorExtensions.cs
@@ -30,8 +30,18 @@ namespace HotChocolate.Data
                 throw new ArgumentNullException(nameof(descriptor));
             }
 
-            // bind string as it is a class to avoid SortType<string>
-            descriptor.BindRuntimeType<string, DefaultSortEnumType>();
+            // bind string and structs as it is a class to avoid SortFilterInputType<string>,..
+            descriptor
+                .BindRuntimeType<Guid?, DefaultSortEnumType>()
+                .BindRuntimeType<DateTime?, DefaultSortEnumType>()
+                .BindRuntimeType<DateTimeOffset?, DefaultSortEnumType>()
+                .BindRuntimeType<TimeSpan?, DefaultSortEnumType>()
+                .BindRuntimeType<Guid, DefaultSortEnumType>()
+                .BindRuntimeType<DateTime, DefaultSortEnumType>()
+                .BindRuntimeType<DateTimeOffset, DefaultSortEnumType>()
+                .BindRuntimeType<TimeSpan?, DefaultSortEnumType>()
+                .BindRuntimeType<string, DefaultSortEnumType>();
+
             descriptor.DefaultBinding<DefaultSortEnumType>();
 
             return descriptor;
