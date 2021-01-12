d22f7d64c0685db52f041053025247537fa9ade9
Arif Hanif <arif.hanif3@gmail.com>
2021-01-11 20:44:15 -0500

    start schema first demo project

--- src/HotChocolate/Data/demo/Neo4jDemo/Startup.cs
+++ src/HotChocolate/Data/demo/Neo4jDemo/Startup.cs
@@ -3,19 +3,44 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using HotChocolate;
 
 namespace Neo4jDemo
 {
     public class Startup
     {
+        // Book.cs
+        public class Book
+        {
+            public string Title { get; set; }
+
+            public string Author { get; set; }
+
+        }
+
+        // Query.cs
+        public class Query
+        {
+            public Book GetBook() => new Book { Title = "C# in depth", Author = "Jon Skeet" };
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddGraphQLServer()
+                .AddDocumentFromString(@"
+                    type Query {
+                      book(id: String): Book
+                    }
+
+                    type Book {
+                      title: String
+                      author: String
+                    }
+                ")
+                .BindComplexType<Query>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +59,7 @@ namespace Neo4jDemo
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
+                endpoints.MapGraphQL();
             });
         }
     }
