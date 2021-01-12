0e7d08d6a2fd4fe6f4c642e65c59e8fe9822136f
Michael Staib <michael@chillicream.com>
2021-01-12 15:36:57 +0100

    Introduced a way to intercept the initialization flow

--- src/HotChocolate/Core/src/Types/Types/Relay/RelayOptions.cs
+++ src/HotChocolate/Core/src/Types/Types/Relay/RelayOptions.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace HotChocolate.Types.Relay
 {
     /// <summary>
@@ -15,5 +17,12 @@ namespace HotChocolate.Types.Relay
         /// The name of the query field on a mutation payload (default: query).
         /// </summary>
         public NameString? QueryFieldName { get; set; }
+
+        /// <summary>
+        /// A predicate that defines if the query field shall be added to
+        /// the specified payload type.
+        /// </summary>
+        public Func<INamedType, bool> MutationPayloadPredicate { get; set; } =
+            type => type.Name.Value.EndsWith("Payload", StringComparison.Ordinal);
     }
 }
