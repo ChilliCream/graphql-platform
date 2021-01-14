1645f63666d0f29d19f2bbd2b9a6ebfa2e87f2ea
Frederic Birke <fred@freds.garden>
2021-01-14 23:49:21 +0100

    Finish first Iteration of ResultInfoGenerator

--- src/StrawberryShake/CodeGeneration/src/CodeGeneration.CSharp/Generators/ResultInfoGenerator.cs
+++ src/StrawberryShake/CodeGeneration/src/CodeGeneration.CSharp/Generators/ResultInfoGenerator.cs
@@ -12,40 +12,36 @@ namespace StrawberryShake.CodeGeneration.CSharp
 
         protected override Task WriteAsync(CodeWriter writer, TypeDescriptor typeDescriptor)
         {
-            AssertNonNull(writer, typeDescriptor);
+            AssertNonNull(
+                writer,
+                typeDescriptor
+            );
 
-            var entityIdsTypeReference = TypeReferenceBuilder.New()
-                .SetListType(ListType.List)
-                .SetName(WellKnownNames.EntityId);
+            var className = NamingConventions.ResultInfoNameFromTypeName(typeDescriptor.Name);
 
             ClassBuilder
                 .AddImplements(NamingConventions.ResultInfoNameFromTypeName(WellKnownNames.IOperationResultDataInfo))
-                .AddProperty(
-                    PropertyBuilder.New()
-                        .SetName(EntityIdsPropertyName)
-                        .SetType(entityIdsTypeReference)
-                )
-                .SetName(typeDescriptor.Name);
+                .SetName(className);
 
-            var entityIdsParamName = EntityIdsPropertyName.WithLowerFirstChar();
             ConstructorBuilder
                 .SetTypeName(typeDescriptor.Name)
-                .AddParameter(
-                    ParameterBuilder.New()
-                        .SetType(entityIdsTypeReference)
-                        .SetName(entityIdsParamName)
-                )
-                .AddCode(
-                    AssignmentBuilder.New()
-                        .SetLefthandSide(EntityIdsPropertyName)
-                        .SetRighthandSide(entityIdsParamName)
-                )
                 .SetAccessModifier(AccessModifier.Public);
 
+            var constructorCaller = MethodCallBuilder.New()
+                .SetPrefix("return new ")
+                .SetMethodName(className);
+
+            var withVersion = MethodBuilder.New()
+                .SetAccessModifier(AccessModifier.Public)
+                .SetReturnType(className)
+                .SetName($"{WellKnownNames.IOperationResultDataInfo}.WithVersion")
+                .AddParameter(ParameterBuilder.New()
+                    .SetType("ulong")
+                    .SetName("version"));
 
             foreach (var prop in typeDescriptor.Properties)
             {
-                var propTypeBuilder = prop.ToEntityIdBuilder();
+                var propTypeBuilder = prop.Type.ToEntityIdBuilder();
                 // Add Property to class
                 var propBuilder = PropertyBuilder
                     .New()
@@ -53,6 +49,7 @@ namespace StrawberryShake.CodeGeneration.CSharp
                     .SetType(propTypeBuilder)
                     .SetAccessModifier(AccessModifier.Public);
                 ClassBuilder.AddProperty(propBuilder);
+                constructorCaller.AddArgument(prop.Name);
 
                 // Add initialization of property to the constructor
                 var paramName = prop.Name.WithLowerFirstChar();
@@ -62,6 +59,27 @@ namespace StrawberryShake.CodeGeneration.CSharp
                 ConstructorBuilder.AddParameter(parameterBuilder);
                 ConstructorBuilder.AddCode(prop.Name + " = " + paramName + ";");
             }
+
+            ClassBuilder.AddProperty(PropertyBuilder.New()
+                .SetName("IOperationResultDataInfo.EntityIds")
+                .SetType("IReadOnlyCollection<EntityId>")
+                .AsLambda("_entityIds")
+            );
+
+            ClassBuilder.AddProperty(PropertyBuilder.New()
+                .SetName("IOperationResultDataInfo.Version")
+                .SetType("ulong")
+                .AsLambda("_version")
+            );
+
+
+            AddConstructorAssignedField("IReadOnlyCollection<EntityId>", "_entityIds");
+            constructorCaller.AddArgument("_entityIds");
+            AddConstructorAssignedField("ulong", "_version");
+            constructorCaller.AddArgument("_version");
+
+            withVersion.AddCode(constructorCaller);
+            ClassBuilder.AddMethod(withVersion);
 
             return CodeFileBuilder.New()
                 .SetNamespace(typeDescriptor.Namespace)
