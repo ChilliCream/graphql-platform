b25901fee1f8ddbe7889da92d226634fbec3ac5d
Michael Staib <michael@chillicream.com>
2021-01-15 14:35:40 +0100

    cleanup client model

--- src/StrawberryShake/CodeGeneration/src/CodeGeneration/Analyzers/Models/ClientModel.cs
+++ src/StrawberryShake/CodeGeneration/src/CodeGeneration/Analyzers/Models/ClientModel.cs
@@ -1,19 +1,41 @@
+using System;
 using System.Collections.Generic;
 
 namespace StrawberryShake.CodeGeneration.Analyzers.Models
 {
+    /// <summary>
+    /// The client model represents the client with all its operations and types.
+    /// </summary>
     public class ClientModel
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="ClientModel" />.
+        /// </summary>
+        /// <param name="operations">
+        /// The operations that the client has defined.
+        /// </param>
+        /// <param name="leafTypes">
+        /// The leaf types that are used.
+        /// </param>
+        /// <param name="inputObjectTypes">
+        /// The input types that could be passed in.
+        /// </param>
         public ClientModel(
             IReadOnlyList<OperationModel> operations,
             IReadOnlyList<LeafTypeModel> leafTypes,
             IReadOnlyList<InputObjectTypeModel> inputObjectTypes)
         {
-            Operations = operations;
-            LeafTypes = leafTypes;
-            InputObjectTypes = inputObjectTypes;
+            Operations = operations ?? 
+                throw new ArgumentNullException(nameof(operations));
+            LeafTypes = leafTypes ?? 
+                throw new ArgumentNullException(nameof(leafTypes));
+            InputObjectTypes = inputObjectTypes ?? 
+                throw new ArgumentNullException(nameof(inputObjectTypes));
         }
 
+        /// <summary>
+        /// Gets the operations
+        /// </summary>
         public IReadOnlyList<OperationModel> Operations { get; }
 
         /// <summary>
@@ -21,6 +43,9 @@ namespace StrawberryShake.CodeGeneration.Analyzers.Models
         /// </summary>
         public IReadOnlyList<LeafTypeModel> LeafTypes { get; }
 
+        /// <summary>
+        /// Gets the input objects that are needed.
+        /// </summary>
         public IReadOnlyList<InputObjectTypeModel> InputObjectTypes { get; }
     }
 }
