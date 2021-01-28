649a200b2995598198c92c7c4f1770bff99803ed
Greg Sieranski <git@wonbyte.com>
2021-01-28 00:05:44 -0600

    Update ThrowHelper.cs

--- src/HotChocolate/Core/src/Types.Scalars/ThrowHelper.cs
+++ src/HotChocolate/Core/src/Types.Scalars/ThrowHelper.cs
@@ -23,6 +23,28 @@ namespace HotChocolate.Types.Scalars
                     .Build(),
                 type);
         }
+        
+        public static SerializationException NonEmptyStringType_ParseLiteral_IsEmpty(IType type)
+        {
+            return new SerializationException(
+                ErrorBuilder.New()
+                    .SetMessage(ScalarResources.NonEmptyStringType_IsEmpty_ParseLiteral)
+                    .SetCode(ErrorCodes.Scalars.InvalidSyntaxFormat)
+                    .SetExtension("actualType", "NonEmptyString")
+                    .Build(),
+                type);
+        }
+
+        public static SerializationException NonEmptyStringType_ParseValue_IsEmpty(IType type)
+        {
+            return new SerializationException(
+                ErrorBuilder.New()
+                    .SetMessage(ScalarResources.NonEmptyStringType_IsEmpty_ParseValue)
+                    .SetCode(ErrorCodes.Scalars.InvalidRuntimeType)
+                    .SetExtension("actualType", "NonEmptyString")
+                    .Build(),
+                type);
+        }
 
         public static SerializationException PositiveIntType_ParseLiteral_ZeroOrLess(
             IType type)
