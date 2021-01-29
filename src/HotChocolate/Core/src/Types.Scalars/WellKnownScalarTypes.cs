ab3a6a72f1044e8186d15c6b784a112ec63cd7cf
Gregory Sieranski <git@wonbyte.com>
2021-01-29 13:23:50 -0600

    fix types i think

--- src/HotChocolate/Core/src/Types.Scalars/WellKnownScalarTypes.cs
+++ src/HotChocolate/Core/src/Types.Scalars/WellKnownScalarTypes.cs
@@ -2,10 +2,9 @@ namespace HotChocolate.Types
 {
     internal static class WellKnownScalarTypes
     {
-        public const string NonEmptyString = nameof(NonEmptyString);
+        public const string NonEmptyString = nameof(NonEmptyStringType);
         public const string PositiveInt = nameof(PositiveIntType);
-        public const string NegativeInt = nameof(NegativeInt);
-
+        public const string NegativeInt = nameof(NegativeIntType);
         public const string EmailAddress = nameof(EmailAddressType);
     }
 }
