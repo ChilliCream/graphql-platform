644e5c0b5b323698f71d19ac1a978bddf8f9a873
Gregory Sieranski <git@wonbyte.com>
2021-01-28 23:09:40 -0600

    Update WellKnownScalarTypes.cs

--- src/HotChocolate/Core/src/Types.Scalars/WellKnownScalarTypes.cs
+++ src/HotChocolate/Core/src/Types.Scalars/WellKnownScalarTypes.cs
@@ -3,9 +3,9 @@ namespace HotChocolate.Types
     internal static class WellKnownScalarTypes
     {
         public const string NonEmptyString = nameof(NonEmptyString);
-        public const string PositiveIntType = nameof(PositiveIntType);
+        public const string PositiveInt = nameof(PositiveIntType);
         public const string NegativeInt = nameof(NegativeInt);
 
-        public const string EmailAddress = nameof(EmaillAddress);
+        public const string EmailAddress = nameof(EmailAddressType);
     }
 }
