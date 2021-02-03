c644d237de8dbe4ac8a8b98d5582d8a6d8dbcb1d
Greg Sieranski <git@wonbyte.com>
2021-02-03 12:53:57 -0600

    Adds EmailAddress Scalar (#2989)
     Co-authored-by: Pascal Senn <senn.pasc@gmail.com>

--- src/HotChocolate/Core/src/Types.Scalars/ScalarResources.Designer.cs
+++ src/HotChocolate/Core/src/Types.Scalars/ScalarResources.Designer.cs
@@ -9,21 +9,21 @@
 
 namespace HotChocolate.Types.Scalars {
     using System;
-    
-    
+
+
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class ScalarResources {
-        
+
         private static System.Resources.ResourceManager resourceMan;
-        
+
         private static System.Globalization.CultureInfo resourceCulture;
-        
+
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal ScalarResources() {
         }
-        
+
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Resources.ResourceManager ResourceManager {
             get {
@@ -34,7 +34,7 @@ namespace HotChocolate.Types.Scalars {
                 return resourceMan;
             }
         }
-        
+
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Globalization.CultureInfo Culture {
             get {
@@ -44,58 +44,82 @@ namespace HotChocolate.Types.Scalars {
                 resourceCulture = value;
             }
         }
-        
+
+        internal static string EmailAddress_Description {
+            get {
+                return ResourceManager.GetString("EmailAddress_Description", resourceCulture);
+            }
+        }
+
+        internal static string EmailAddress_IsInvalid_ParseLiteral {
+            get {
+                return ResourceManager.GetString("EmailAddress_IsInvalid_ParseLiteral", resourceCulture);
+            }
+        }
+
+        internal static string EmailAddress_IsInvalid_ParseValue {
+            get {
+                return ResourceManager.GetString("EmailAddress_IsInvalid_ParseValue", resourceCulture);
+            }
+        }
+
         internal static string NegativeIntType_Description {
             get {
                 return ResourceManager.GetString("NegativeIntType_Description", resourceCulture);
             }
         }
-        
+
         internal static string NegativeIntType_IsNotNegative_ParseLiteral {
             get {
                 return ResourceManager.GetString("NegativeIntType_IsNotNegative_ParseLiteral", resourceCulture);
             }
         }
-        
+
         internal static string NegativeIntType_IsNotNegative_ParseValue {
             get {
                 return ResourceManager.GetString("NegativeIntType_IsNotNegative_ParseValue", resourceCulture);
             }
         }
-        
+
         internal static string NonEmptyStringType_Description {
             get {
                 return ResourceManager.GetString("NonEmptyStringType_Description", resourceCulture);
             }
         }
-        
+
         internal static string NonEmptyStringType_IsEmpty_ParseLiteral {
             get {
                 return ResourceManager.GetString("NonEmptyStringType_IsEmpty_ParseLiteral", resourceCulture);
             }
         }
-        
+
         internal static string NonEmptyStringType_IsEmpty_ParseValue {
             get {
                 return ResourceManager.GetString("NonEmptyStringType_IsEmpty_ParseValue", resourceCulture);
             }
         }
-        
+
         internal static string PositiveIntType_Description {
             get {
                 return ResourceManager.GetString("PositiveIntType_Description", resourceCulture);
             }
         }
-        
+
         internal static string PositiveIntType_ZeroOrLess_ParseLiteral {
             get {
                 return ResourceManager.GetString("PositiveIntType_ZeroOrLess_ParseLiteral", resourceCulture);
             }
         }
-        
+
         internal static string PositiveIntType_ZeroOrLess_ParseValue {
             get {
                 return ResourceManager.GetString("PositiveIntType_ZeroOrLess_ParseValue", resourceCulture);
+            }
+        }
+
+        internal static string EmailAddress_ValidationPattern {
+            get {
+                return ResourceManager.GetString("EmailAddress_ValidationPattern", resourceCulture);
             }
         }
     }
--- src/HotChocolate/Core/src/Types.Scalars/WellKnownScalarTypes.cs
+++ src/HotChocolate/Core/src/Types.Scalars/WellKnownScalarTypes.cs
@@ -2,8 +2,9 @@ namespace HotChocolate.Types
 {
     internal static class WellKnownScalarTypes
     {
-        public const string NonEmptyString = nameof(NonEmptyString);
-        public const string PositiveIntType = nameof(PositiveIntType);
+        public const string EmailAddress = nameof(EmailAddress);
         public const string NegativeInt = nameof(NegativeInt);
+        public const string NonEmptyString = nameof(NonEmptyString);
+        public const string PositiveInt = nameof(PositiveInt);
     }
 }
