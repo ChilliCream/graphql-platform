872979af24f0351baa8b66b2dcc9b9c1e67cef1a
Pascal Senn <senn.pasc@gmail.com>
2021-02-08 22:04:31 +0100

    feat: add long timespan url and uuid serializer

--- src/StrawberryShake/Client/src/Core/ThrowHelper.cs
+++ src/StrawberryShake/Client/src/Core/ThrowHelper.cs
@@ -1,3 +1,6 @@
+using System;
+using StrawberryShake.Serialization;
+
 namespace StrawberryShake
 {
     internal static class ThrowHelper
@@ -7,5 +10,27 @@ namespace StrawberryShake
             string scalarType) =>
             new(new ClientError(
                 $"The runtime value is expected to be {runtimeType} for {scalarType}."));
+
+        internal static GraphQLClientException UrlFormatter_CouldNotParseUri(string value) =>
+            new(new ClientError(
+                $"The URL serializer could not parse value{value}. Invalid format. "));
+
+        internal static GraphQLClientException UrlFormatter_DoesNotStartWithSlash(string uri) =>
+            new(new ClientError(
+                $"The relative uri {uri} does not start with '/'"));
+
+        internal static GraphQLClientException TimeSpanSerializer_CouldNotParseValue(
+            string value,
+            TimeSpanFormat format) =>
+            new(new ClientError(
+                $"The TimeSpan serializer could not parse value {value}. The provided value was " +
+                $"not in format {format.ToString()}"));
+
+        internal static GraphQLClientException TimeSpanSerializer_CouldNotFormatValue(
+            TimeSpan value,
+            TimeSpanFormat format) =>
+            new(new ClientError(
+                $"The TimeSpan serializer could not serialize value {value}. The provided value " +
+                $"was not in format {format.ToString()}"));
     }
 }
