e77a6dfd3200a59f693a44d2420a96a5efc28894
Michael Staib <michael@chillicream.com>
2021-02-09 20:21:06 +0100

    Updated Nuke

--- .build/Build.Publish.cs
+++ .build/Build.Publish.cs
@@ -1,42 +1,12 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using Nuke.Common;
 using Nuke.Common.CI;
-using Nuke.Common.CI.AppVeyor;
-using Nuke.Common.CI.AzurePipelines;
-using Nuke.Common.CI.GitHubActions;
-using Nuke.Common.CI.TeamCity;
-using Nuke.Common.Execution;
-using Nuke.Common.Git;
 using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
-using Nuke.Common.Tools.Coverlet;
-using Nuke.Common.Tools.DotCover;
 using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Tools.GitHub;
-using Nuke.Common.Tools.GitVersion;
-using Nuke.Common.Tools.InspectCode;
 using Nuke.Common.Tools.NuGet;
-using Nuke.Common.Tools.ReportGenerator;
-using Nuke.Common.Tools.Slack;
-using Nuke.Common.Tools.SonarScanner;
-using Nuke.Common.Utilities;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.ChangeLog.ChangelogTasks;
-using static Nuke.Common.ControlFlow;
-using static Nuke.Common.Gitter.GitterTasks;
-using static Nuke.Common.IO.CompressionTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
-using static Nuke.Common.Tools.Git.GitTasks;
-using static Nuke.Common.Tools.InspectCode.InspectCodeTasks;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.Tools.ReportGenerator.ReportGeneratorTasks;
-using static Nuke.Common.Tools.Slack.SlackTasks;
-using static Nuke.Common.Tools.SonarScanner.SonarScannerTasks;
 using static Helpers;
 using static Nuke.Common.Tools.NuGet.NuGetTasks;
 
@@ -51,7 +21,7 @@ partial class Build : NukeBuild
         .DependsOn(Restore)
         .Produces(PackageDirectory / "*.nupkg")
         .Produces(PackageDirectory / "*.snupkg")
-        .Requires(() => Configuration.Equals(Configuration.Release))
+        .Requires(() => Configuration.Equals("Release"))
         .Executes(() =>
         {
             if (!InvokedTargets.Contains(Restore))
@@ -100,7 +70,7 @@ partial class Build : NukeBuild
         .Consumes(Pack)
         .Requires(() => NuGetSource)
         .Requires(() => NuGetApiKey)
-        .Requires(() => Configuration.Equals(Configuration.Release))
+        .Requires(() => Configuration.Equals("Release"))
         .Executes(() =>
         {
             IReadOnlyCollection<AbsolutePath> packages = PackageDirectory.GlobFiles("*.nupkg");
