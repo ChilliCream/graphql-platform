24d5de92e06dad2850b6a1a23ac963641edd1c22
Pascal Senn <senn.pasc@gmail.com>
2021-02-11 14:58:11 +0100

    feat: cleanup

--- src/HotChocolate/Core/src/Types.Scalars/WellKnownScalarTypes.cs
+++ src/HotChocolate/Core/src/Types.Scalars/WellKnownScalarTypes.cs
@@ -6,6 +6,12 @@ namespace HotChocolate.Types
         public const string NegativeInt = nameof(NegativeInt);
         public const string NonEmptyString = nameof(NonEmptyString);
         public const string NonNegativeInt = nameof(NonNegativeInt);
+<<<<<<< HEAD
+=======
+        public const string NonPositiveFloat = nameof(NonPositiveFloat);
+        public const string NonPositiveInt = nameof(NonPositiveInt);
+        public const string NonNegativeFloat = nameof(NonNegativeFloat);
+>>>>>>> 2b4974314 (feat: cleanup)
         public const string PhoneNumber = nameof(PhoneNumber);
         public const string PositiveInt = nameof(PositiveInt);
     }
