88b0852cfa84822c808f857009c9fd31b659d6ab
Pascal Senn <senn.pasc@gmail.com>
2021-03-07 23:19:48 +0100

    feat: sort throwhelper

--- src/HotChocolate/Core/src/Types.Scalars/ThrowHelper.cs
+++ src/HotChocolate/Core/src/Types.Scalars/ThrowHelper.cs
@@ -112,6 +112,28 @@ namespace HotChocolate.Types.Scalars
                 type);
         }
 
+        public static SerializationException MacAddressType_ParseValue_IsInvalid(IType type)
+        {
+            return new SerializationException(
+                ErrorBuilder.New()
+                    .SetMessage(ScalarResources.MacAddressType_IsInvalid_ParseValue)
+                    .SetCode(ErrorCodes.Scalars.InvalidRuntimeType)
+                    .SetExtension("actualType", WellKnownScalarTypes.MacAddress)
+                    .Build(),
+                type);
+        }
+
+        public static SerializationException MacAddressType_ParseLiteral_IsInvalid(IType type)
+        {
+            return new SerializationException(
+                ErrorBuilder.New()
+                    .SetMessage(ScalarResources.MacAddressType_IsInvalid_ParseLiteral)
+                    .SetCode(ErrorCodes.Scalars.InvalidSyntaxFormat)
+                    .SetExtension("actualType", WellKnownScalarTypes.MacAddress)
+                    .Build(),
+                type);
+        }
+
         public static SerializationException NegativeFloatType_ParseLiteral_IsNotNegative(
             IType type)
         {
@@ -367,28 +389,6 @@ namespace HotChocolate.Types.Scalars
                             ScalarResources.RegexType_IsInvalid_ParseLiteral,
                             name))
                     .SetCode(ErrorCodes.Scalars.InvalidSyntaxFormat)
-                    .Build(),
-                type);
-        }
-
-        public static SerializationException MacAddressType_ParseValue_IsInvalid(IType type)
-        {
-            return new SerializationException(
-                ErrorBuilder.New()
-                    .SetMessage(ScalarResources.MacAddressType_IsInvalid_ParseValue)
-                    .SetCode(ErrorCodes.Scalars.InvalidRuntimeType)
-                    .SetExtension("actualType", WellKnownScalarTypes.MacAddress)
-                    .Build(),
-                type);
-        }
-
-        public static SerializationException MacAddressType_ParseLiteral_IsInvalid(IType type)
-        {
-            return new SerializationException(
-                ErrorBuilder.New()
-                    .SetMessage(ScalarResources.MacAddressType_IsInvalid_ParseLiteral)
-                    .SetCode(ErrorCodes.Scalars.InvalidSyntaxFormat)
-                    .SetExtension("actualType", WellKnownScalarTypes.MacAddress)
                     .Build(),
                 type);
         }
