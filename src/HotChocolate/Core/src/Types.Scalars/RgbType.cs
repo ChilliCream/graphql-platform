98c87667314a35738c67a2338270e31523a12cc5
Pascal Senn <senn.pasc@gmail.com>
2021-03-11 23:08:21 +0100

    fix: format

--- src/HotChocolate/Core/src/Types.Scalars/RgbType.cs
+++ src/HotChocolate/Core/src/Types.Scalars/RgbType.cs
@@ -6,13 +6,15 @@ namespace HotChocolate.Types.Scalars
 {
     /// <summary>
     /// The `Rgb` scalar type represents a valid CSS RGB color
-    /// <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/color_value#rgb()_and_rgba()">MDN CSS Color</a>
+    /// <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/color_value#rgb()_and_rgba()">
+    /// MDN CSS Color
+    /// </a>
     /// </summary>
     public class RgbType : RegexType
     {
         private const string _validationPattern =
-            "^rgb\\(\\s*(-?\\d+|-?\\d*\\.\\d+(?=%))(%?)\\s*,\\s*(-?\\d+|-?\\d*\\.\\d+(?=%))(\\2)\\s*," +
-            "\\s*(-?\\d+|-?\\d*\\.\\d+(?=%))(\\2)\\s*\\)$";
+            "^rgb\\(\\s*(-?\\d+|-?\\d*\\.\\d+(?=%))(%?)\\s*,\\s*(-?\\d+|-?\\d*\\.\\d+(?=%))("+
+            "\\2)\\s*,\\s*(-?\\d+|-?\\d*\\.\\d+(?=%))(\\2)\\s*\\)$";
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RgbType"/> class.
@@ -37,6 +39,5 @@ namespace HotChocolate.Types.Scalars
         {
             return ThrowHelper.RgbType_ParseValue_IsInvalid(this);
         }
-
     }
 }
