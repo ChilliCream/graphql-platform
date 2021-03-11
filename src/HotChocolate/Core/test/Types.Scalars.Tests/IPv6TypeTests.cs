afcec23741e8069d5f40a6999e6fc0a4174d78ad
Pascal Senn <senn.pasc@gmail.com>
2021-03-11 22:42:15 +0100

    fix: format

--- src/HotChocolate/Core/test/Types.Scalars.Tests/IPv6TypeTests.cs
+++ src/HotChocolate/Core/test/Types.Scalars.Tests/IPv6TypeTests.cs
@@ -157,14 +157,26 @@ namespace HotChocolate.Types.Scalars
         [InlineData(typeof(StringValueNode), "2001:db8::7/32", "2001:db8::7/32")]
         [InlineData(typeof(StringValueNode), "a:b:c:d:e::1.2.3.4/13", "a:b:c:d:e::1.2.3.4/13")]
         [InlineData(typeof(StringValueNode), "a:b:c:d:e::1.2.3.4/64", "a:b:c:d:e::1.2.3.4/64")]
-        [InlineData(typeof(StringValueNode), "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/0", "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/0")]
-        [InlineData(typeof(StringValueNode), "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/32", "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/32")]
-        [InlineData(typeof(StringValueNode), "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/128", "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/128")]
-        [InlineData(typeof(StringValueNode), "1080:0:0:0:8:800:200C:417A/27", "1080:0:0:0:8:800:200C:417A/27")]
+        [InlineData(typeof(StringValueNode),
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/0",
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/0")]
+        [InlineData(typeof(StringValueNode),
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/32",
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/32")]
+        [InlineData(typeof(StringValueNode),
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/128",
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/128")]
+        [InlineData(typeof(StringValueNode),
+            "1080:0:0:0:8:800:200C:417A/27",
+            "1080:0:0:0:8:800:200C:417A/27")]
         [InlineData(typeof(StringValueNode), "2001:db8::7", "2001:db8::7")]
         [InlineData(typeof(StringValueNode), "a:b:c:d:e::1.2.3.4", "a:b:c:d:e::1.2.3.4")]
-        [InlineData(typeof(StringValueNode), "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210", "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210")]
-        [InlineData(typeof(StringValueNode), "1080:0:0:0:8:800:200C:417A", "1080:0:0:0:8:800:200C:417A")]
+        [InlineData(typeof(StringValueNode),
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210",
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210")]
+        [InlineData(typeof(StringValueNode),
+            "1080:0:0:0:8:800:200C:417A",
+            "1080:0:0:0:8:800:200C:417A")]
         [InlineData(typeof(StringValueNode), "::1:2:3:4:5:6:7", "::1:2:3:4:5:6:7")]
         [InlineData(typeof(StringValueNode), "::1:2:3:4:5:6", "::1:2:3:4:5:6")]
         [InlineData(typeof(StringValueNode), "1::1:2:3:4:5:6", "1::1:2:3:4:5:6")]
@@ -247,13 +259,17 @@ namespace HotChocolate.Types.Scalars
         [InlineData("2001:db8::7/32", "2001:db8::7/32")]
         [InlineData("a:b:c:d:e::1.2.3.4/13", "a:b:c:d:e::1.2.3.4/13")]
         [InlineData("a:b:c:d:e::1.2.3.4/64", "a:b:c:d:e::1.2.3.4/64")]
-        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/0", "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/0")]
-        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/32", "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/32")]
-        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/128", "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/128")]
+        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/0",
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/0")]
+        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/32",
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/32")]
+        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/128",
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/128")]
         [InlineData("1080:0:0:0:8:800:200C:417A/27", "1080:0:0:0:8:800:200C:417A/27")]
         [InlineData("2001:db8::7", "2001:db8::7")]
         [InlineData("a:b:c:d:e::1.2.3.4", "a:b:c:d:e::1.2.3.4")]
-        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210", "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210")]
+        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210",
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210")]
         [InlineData("1080:0:0:0:8:800:200C:417A", "1080:0:0:0:8:800:200C:417A")]
         [InlineData("::1:2:3:4:5:6:7", "::1:2:3:4:5:6:7")]
         [InlineData("::1:2:3:4:5:6", "::1:2:3:4:5:6")]
@@ -332,13 +348,17 @@ namespace HotChocolate.Types.Scalars
         [InlineData("2001:db8::7/32", "2001:db8::7/32")]
         [InlineData("a:b:c:d:e::1.2.3.4/13", "a:b:c:d:e::1.2.3.4/13")]
         [InlineData("a:b:c:d:e::1.2.3.4/64", "a:b:c:d:e::1.2.3.4/64")]
-        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/0", "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/0")]
-        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/32", "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/32")]
-        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/128", "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/128")]
+        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/0",
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/0")]
+        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/32",
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/32")]
+        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/128",
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210/128")]
         [InlineData("1080:0:0:0:8:800:200C:417A/27", "1080:0:0:0:8:800:200C:417A/27")]
         [InlineData("2001:db8::7", "2001:db8::7")]
         [InlineData("a:b:c:d:e::1.2.3.4", "a:b:c:d:e::1.2.3.4")]
-        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210", "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210")]
+        [InlineData("FEDC:BA98:7654:3210:FEDC:BA98:7654:3210",
+            "FEDC:BA98:7654:3210:FEDC:BA98:7654:3210")]
         [InlineData("1080:0:0:0:8:800:200C:417A", "1080:0:0:0:8:800:200C:417A")]
         [InlineData("::1:2:3:4:5:6:7", "::1:2:3:4:5:6:7")]
         [InlineData("::1:2:3:4:5:6", "::1:2:3:4:5:6")]
