4e0d9522f14ef60bd073c0022545d812ccc9b0d4
Gregory Sieranski <git@wonbyte.com>
2021-03-16 18:20:34 -0500

    wip

--- src/HotChocolate/Core/test/Types.Scalars.Tests/LocalTimeTypeTests.cs
+++ src/HotChocolate/Core/test/Types.Scalars.Tests/LocalTimeTypeTests.cs
@@ -71,13 +71,13 @@ namespace HotChocolate.Types.Scalars
             // arrange
             var dateTimeType = new LocalTimeType();
             var literal = new StringValueNode(
-                "06/28/2018 23:46:14");
+                "2018-06-29T08:46:14+04:00");
             var expectedDateTime = new DateTimeOffset(
                 new DateTime(2018, 6, 29, 8, 46, 14),
                 new TimeSpan(4, 0, 0));
 
             // act
-            var dateTime = dateTimeType
+            var dateTime = (DateTimeOffset)dateTimeType
                 .ParseLiteral(literal);
 
             // assert
