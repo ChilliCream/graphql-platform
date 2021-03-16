56ef246b4755462617ed4cd247fc3dd3b4d93822
Michael Staib <michael@chillicream.com>
2021-03-16 08:49:16 +0100

    Ignore indexer by by default. (#3289)

--- src/StrawberryShake/Client/test/Persistence.LiteDB.Tests/LiteDBPersistenceTests.cs
+++ src/StrawberryShake/Client/test/Persistence.LiteDB.Tests/LiteDBPersistenceTests.cs
@@ -36,12 +36,18 @@ namespace StrawberryShake.Persistence.SQLLite
                     session.SetEntity(new EntityId("ABC", 1), new MockEntity("abc"));
                 });
 
+                await Task.Delay(250, ct.Token);
+
                 var count = 0;
 
                 while (!ct.IsCancellationRequested && count == 0)
                 {
-                    await Task.Delay(50, ct.Token);
                     count = db.GetCollection(LiteDBPersistence.Entities).Count();
+
+                    if (count == 0)
+                    {
+                        await Task.Delay(50, ct.Token);
+                    }
                 }
 
                 Assert.Equal(1, count);
@@ -77,12 +83,18 @@ namespace StrawberryShake.Persistence.SQLLite
                         session.SetEntity(new EntityId("ABC", 1), new MockEntity("abc"));
                     });
 
+                    await Task.Delay(250, ct.Token);
+
                     var count = 0;
 
                     while (!ct.IsCancellationRequested && count == 0)
                     {
-                        await Task.Delay(50, ct.Token);
                         count = db.GetCollection("entities").Count();
+
+                        if (count == 0)
+                        {
+                            await Task.Delay(50, ct.Token);
+                        }
                     }
 
                     Assert.Equal(1, count);
