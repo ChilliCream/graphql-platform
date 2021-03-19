9b2499dedc60f5af6f3c7de079dbc3e0a8ac7209
Michael Staib <michael@chillicream.com>
2021-03-19 08:06:44 +0100

    added server test case

--- src/HotChocolate/AspNetCore/test/AspNetCore.Tests/HttpPostMiddlewareTests.cs
+++ src/HotChocolate/AspNetCore/test/AspNetCore.Tests/HttpPostMiddlewareTests.cs
@@ -778,5 +778,73 @@ namespace HotChocolate.AspNetCore
             // assert
             result.MatchSnapshot();
         }
+
+        [Fact]
+        public async Task Fragments()
+        {
+            // arrange
+            TestServer server = CreateStarWarsServer();
+
+            // act
+             ClientQueryResult result =
+                await server.PostAsync(new ClientQueryRequest
+                {
+                    Query = @"
+                        query ($if: Boolean!) {
+                            human(id: ""1000"") {
+                                ... Human1 @include(if: $if)
+                                ... Human2 @skip(if: $if)
+                            }
+                        }
+                        fragment Human1 on Human {
+                            friends {
+                                edges {
+                                    ... FriendEdge1
+                                }
+                            }
+                        }
+                        fragment FriendEdge1 on CharacterEdge {
+                            node {
+                                __typename
+                                friends {
+                                    nodes {
+                                        __typename
+                                        ... Human3
+                                    }
+                                }
+                            }
+                        }
+                        fragment Human2 on Human {
+                            friends {
+                                edges {
+                                    node {
+                                        __typename
+                                        ... Human3
+                                    }
+                                }
+                            }
+                        }
+                        fragment Human3 on Human {
+                            name # this works
+
+                            # this is returned as an empty object but should be populated
+                            # interestingly it seems like this passes in this test, but
+                            # if run against the StarWars asp.net core sample app, this
+                            # is an empty object!
+                            otherHuman {
+                                __typename
+                                name
+                            }
+                        }
+                        ",
+                    Variables = new Dictionary<string, object>
+                    {
+                        { "if", false }
+                    }
+                });
+
+            // assert
+            result.MatchSnapshot();
+        }
     }
 }
