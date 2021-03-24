58de659db2d10821b024cd127946d4702bf3830b
Gregory Sieranski <git@wonbyte.com>
2021-03-23 22:37:03 -0500

    some updates

--- src/HotChocolate/Core/src/Types.Scalars/UtcOffsetType.cs
+++ src/HotChocolate/Core/src/Types.Scalars/UtcOffsetType.cs
@@ -27,7 +27,6 @@ namespace HotChocolate.Types
                 {new TimeSpan(-3,0,0), "-03:00"},
                 {new TimeSpan(-2,0,0), "-02:00"},
                 {new TimeSpan(-1,0,0), "-01:00"},
-                {new TimeSpan(-1,0,0), "-01:00"},
                 {new TimeSpan(0,0,0), "00:00"},
                 {new TimeSpan(1,0,0), "+01:00"},
                 {new TimeSpan(2,0,0), "+02:00"},
@@ -42,7 +41,7 @@ namespace HotChocolate.Types
                 {new TimeSpan(6,30,0), "+06:30"},
                 {new TimeSpan(7,0,0), "+07:00"},
                 {new TimeSpan(8,0,0), "+08:00"},
-                {new TimeSpan(8,0,0), "+08:45"},
+                {new TimeSpan(8,45,0), "+08:45"},
                 {new TimeSpan(9,0,0), "+09:00"},
                 {new TimeSpan(9,30,0), "+09:30"},
                 {new TimeSpan(10,0,0), "+10:00"},
@@ -55,7 +54,48 @@ namespace HotChocolate.Types
             };
 
         private static readonly Dictionary<string, TimeSpan> _offsetToTimeSpan =
-            new() {["-12:00"] = new TimeSpan(-12, 0, 0)};
+            new()
+            {
+                ["-12:00"] = new TimeSpan(-12, 0, 0),
+                ["-11:00"] = new TimeSpan(-11, 0, 0),
+                ["-10:00"] = new TimeSpan(-10, 0, 0),
+                ["-09:30"] = new TimeSpan(-9, 30, 0),
+                ["-09:00"] = new TimeSpan(-9, 0, 0),
+                ["-08:00"] = new TimeSpan(-8, 0, 0),
+                ["-07:00"] = new TimeSpan(-7, 0, 0),
+                ["-06:00"] = new TimeSpan(-6, 0, 0),
+                ["-05:00"] = new TimeSpan(-5, 0, 0),
+                ["-04:00"] = new TimeSpan(-4, 0, 0),
+                ["-03:30"] = new TimeSpan(-3, 30, 0),
+                ["-03:00"] = new TimeSpan(-3, 0, 0),
+                ["-02:00"] = new TimeSpan(-2, 0, 0),
+                ["-01:00"] = new TimeSpan(-1, 0, 0),
+                ["-00:00"] = new TimeSpan(0, 0, 0),
+                ["+00:00"] = new TimeSpan(0, 0, 0),
+                ["+01:00"] = new TimeSpan(1, 0, 0),
+                ["+02:00"] = new TimeSpan(2, 0, 0),
+                ["+03:00"] = new TimeSpan(3, 0, 0),
+                ["+03:30"] = new TimeSpan(3, 30, 0),
+                ["+04:00"] = new TimeSpan(4, 0, 0),
+                ["+04:30"] = new TimeSpan(4, 30, 0),
+                ["+05:00"] = new TimeSpan(5, 0, 0),
+                ["+05:30"] = new TimeSpan(5, 30, 0),
+                ["+05:45"] = new TimeSpan(5, 45, 0),
+                ["+06:00"] = new TimeSpan(6, 0, 0),
+                ["+06:30"] = new TimeSpan(6, 30, 0),
+                ["+07:00"] = new TimeSpan(7, 0, 0),
+                ["+08:00"] = new TimeSpan(8, 0, 0),
+                ["+08:45"] = new TimeSpan(8, 45, 0),
+                ["+09:00"] = new TimeSpan(9, 0, 0),
+                ["+09:30"] = new TimeSpan(9, 30, 0),
+                ["+10:00"] = new TimeSpan(10, 0, 0),
+                ["+10:30"] = new TimeSpan(10, 30, 0),
+                ["+11:00"] = new TimeSpan(11, 0, 0),
+                ["+12:00"] = new TimeSpan(12, 0, 0),
+                ["+12:45"] = new TimeSpan(12, 45, 0),
+                ["+13:00"] = new TimeSpan(13, 0, 0),
+                ["+14:00"] = new TimeSpan(14, 0, 0),
+            };
 
         /// <summary>
         /// Initializes a new instance of the <see cref="UtcOffsetType"/> class.
@@ -105,7 +145,7 @@ namespace HotChocolate.Types
         {
             if (_timeSpanToOffset.TryGetValue(runtimeValue, out var found))
             {
-                return new StringValueNode(found);
+                return new(found);
             }
 
             throw ThrowHelper.UtcOffset_ParseValue_IsInvalid(this);
@@ -137,8 +177,8 @@ namespace HotChocolate.Types
                 case string s when _offsetToTimeSpan.TryGetValue(s, out TimeSpan timeSpan):
                     runtimeValue = timeSpan;
                     return true;
-                case TimeSpan ts:
-                    runtimeValue = ts;
+                case TimeSpan timeSpan when _timeSpanToOffset.TryGetValue(timeSpan, out var s):
+                    runtimeValue = s;
                     return true;
                 default:
                     runtimeValue = null;
