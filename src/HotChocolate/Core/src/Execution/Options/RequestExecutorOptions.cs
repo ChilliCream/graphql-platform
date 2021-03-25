7afd97e903645f49bc1dce57ef5a4b88c8e57c59
Michael Staib <michael@chillicream.com>
2021-03-25 16:54:13 +0100

    docus

--- src/HotChocolate/Core/src/Execution/Options/RequestExecutorOptions.cs
+++ src/HotChocolate/Core/src/Execution/Options/RequestExecutorOptions.cs
@@ -42,7 +42,7 @@ namespace HotChocolate.Execution.Options
             Debugger.IsAttached;
 
         /// <summary>
-        /// Gets or sets the maximum amount of queries that can be cached. The
+        /// Gets or sets the maximum amount of compiled queries that can be cached. The
         /// default value is <c>100</c>. The minimum allowed value is
         /// <c>10</c>.
         /// </summary>
