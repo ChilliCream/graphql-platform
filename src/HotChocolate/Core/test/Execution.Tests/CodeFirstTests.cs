ff78ace344e7b90f2a78bfdfda75293a934f2a41
Michael Staib <michael@chillicream.com>
2021-03-26 16:43:19 +0100

    Introduced result object extensions

--- src/HotChocolate/Core/test/Execution.Tests/CodeFirstTests.cs
+++ src/HotChocolate/Core/test/Execution.Tests/CodeFirstTests.cs
@@ -8,7 +8,6 @@ using HotChocolate.Tests;
 using HotChocolate.Types;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
-using Snapshooter.Xunit;
 using Xunit;
 
 namespace HotChocolate.Execution
---
+++ src/StrawberryShake/Client/src/Core/Extensions/OperationResultExtensions.cs
@@ -0,0 +1,75 @@
+using System;
+
+namespace StrawberryShake.Extensions
+{
+    /// <summary>
+    /// This class provides extension methods for the operation result.
+    /// </summary>
+    public static class OperationResultExtensions
+    {
+        /// <summary>
+        /// Ensures that the operation result has no errors and throws a
+        /// <see cref="GraphQLClientException"/> when the operation result
+        /// has errors.
+        /// </summary>
+        /// <param name="result">The operation result.</param>
+        /// <exception cref="ArgumentNullException">
+        /// The operation result is null.
+        /// </exception>
+        /// <exception cref="GraphQLClientException">
+        /// The operation result has errors.
+        /// </exception>
+        public static void EnsureNoErrors(this IOperationResult result)
+        {
+            if (result is null)
+            {
+                throw new ArgumentNullException(nameof(result));
+            }
+
+            if (result.Errors.Count > 0)
+            {
+                throw new GraphQLClientException(result.Errors);
+            }
+        }
+
+        /// <summary>
+        /// Indicates if the operation result has errors.
+        /// </summary>
+        /// <param name="result">The operation result.</param>
+        /// <returns>
+        /// <c>true</c>, if the result has errors; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// The operation result is null.
+        /// </exception>
+        public static bool HasErrors(this IOperationResult result)
+        {
+            if (result is null)
+            {
+                throw new ArgumentNullException(nameof(result));
+            }
+
+            return result.Errors.Count > 0;
+        }
+
+        /// <summary>
+        /// Indicates if the operation result has errors.
+        /// </summary>
+        /// <param name="result">The operation result.</param>
+        /// <returns>
+        /// <c>true</c>, if the result has errors; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// The operation result is null.
+        /// </exception>
+        public static bool IsSuccessResult(this IOperationResult result)
+        {
+            if (result is null)
+            {
+                throw new ArgumentNullException(nameof(result));
+            }
+
+            return result.Errors.Count == 0;
+        }
+    }
+}
