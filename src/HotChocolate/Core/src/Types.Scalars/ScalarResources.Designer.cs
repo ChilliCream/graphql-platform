f9a2a5660a1a1e7cdfa7a6487d8b4d76d01ec1f0
Pascal Senn <senn.pasc@gmail.com>
2021-03-28 19:13:33 +0200

    feat: cleanup

--- src/HotChocolate/Core/src/Types.Scalars/ScalarResources.Designer.cs
+++ src/HotChocolate/Core/src/Types.Scalars/ScalarResources.Designer.cs
@@ -8,6 +8,9 @@
 //------------------------------------------------------------------------------
 
 namespace HotChocolate.Types {
+    using System;
+    
+    
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
