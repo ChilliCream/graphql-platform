4d5da85c745e17c8584a0a17899fdef6d44c4a89
Gregory Sieranski <git@wonbyte.com>
2021-04-02 00:15:10 -0500

    more tests

--- src/HotChocolate/Core/test/Types.Scalars.Tests/LocalCurrencyTypeTests.cs
+++ src/HotChocolate/Core/test/Types.Scalars.Tests/LocalCurrencyTypeTests.cs
@@ -13,7 +13,7 @@ namespace HotChocolate.Types
         public void LocalCurrency_EnsureLocalCurrencyTypeKindIsCorrect()
         {
             // arrange
-            var type = new LocalCurrencyType("US","en-Us");
+            var type = new LocalCurrencyType("Germany","de-DE");
 
             // act
             TypeKind kind = type.Kind;
@@ -40,7 +40,7 @@ namespace HotChocolate.Types
         {
             // arrange
             ScalarType scalar = CreateType<LocalCurrencyType>();
-            var valueSyntax = new StringValueNode("10.99");
+            var valueSyntax = new StringValueNode("$10.99");
 
             // act
             var result = scalar.IsInstanceOfType(valueSyntax);
@@ -54,7 +54,7 @@ namespace HotChocolate.Types
         {
             // arrange
             ScalarType scalar = new LocalCurrencyType("Germany", "de-De");
-            var valueSyntax = new StringValueNode("10,99");
+            var valueSyntax = new StringValueNode("10,99 €");
 
             // act
             var result = scalar.IsInstanceOfType(valueSyntax);
@@ -68,7 +68,7 @@ namespace HotChocolate.Types
         {
             // arrange
             ScalarType scalar = CreateType<LocalCurrencyType>();
-            var valueSyntax = new StringValueNode("24.99");
+            var valueSyntax = new StringValueNode("$24.99");
             var expectedResult = (decimal) 24.99;
 
             // act
@@ -83,7 +83,7 @@ namespace HotChocolate.Types
         {
             // arrange
             ScalarType scalar = new LocalCurrencyType("Germany", "de-DE");
-            var valueSyntax = new StringValueNode("24,99");
+            var valueSyntax = new StringValueNode("24,99 €");
             var expectedResult = (decimal) 24.99;
 
             // act
@@ -98,7 +98,7 @@ namespace HotChocolate.Types
         [InlineData("UK", "en-GB")]
         [InlineData("Switzerland", "de-CH")]
         [Theory]
-        public void LocalTime_ParseLiteralStringValueDifferentCulture(string name, string cultureName)
+        public void LocalCurrency_ParseLiteralStringValueDifferentCulture(string name, string cultureName)
         {
             // arrange
             ScalarType scalar = new LocalCurrencyType(name, cultureName);
@@ -110,6 +110,216 @@ namespace HotChocolate.Types
 
             // assert
             Assert.Equal(expectedDateTime, dateTime);
+        }
+
+        [Fact]
+        protected void LocalCurrency_ExpectParseLiteralToThrowSerializationException()
+        {
+            // arrange
+            ScalarType scalar = CreateType<LocalCurrencyType>();
+            var valueSyntax = new StringValueNode("foo");
+
+            // act
+            Exception? result = Record.Exception(() => scalar.ParseLiteral(valueSyntax));
+
+            // assert
+            Assert.IsType<SerializationException>(result);
+        }
+
+        [Fact]
+        protected void LocalCurrency_ExpectParseValueToMatchDecimal()
+        {
+            // arrange
+            ScalarType scalar = CreateType<LocalCurrencyType>();
+            var valueSyntax = (decimal)24.95;
+
+            // act
+            IValueNode result = scalar.ParseValue(valueSyntax);
+
+            // assert
+            Assert.Equal(typeof(StringValueNode), result.GetType());
+        }
+
+        [Fact]
+        protected void LocalCurrency_ExpectParseValueToThrowSerializationException()
+        {
+            // arrange
+            ScalarType scalar = CreateType<LocalCurrencyType>();
+            var runtimeValue = new StringValueNode("foo");
+
+            // act
+            Exception? result = Record.Exception(() => scalar.ParseValue(runtimeValue));
+
+            // assert
+            Assert.IsType<SerializationException>(result);
+        }
+
+        [Fact]
+        protected void LocalCurrency_ExpectSerializeDecimalToMatch()
+        {
+            // arrange
+            ScalarType scalar = new LocalCurrencyType();
+            var dec = (decimal) 9.99;
+            const string expectedValue = "$9.99";
+
+            // act
+            string serializedValue = (string)scalar.Serialize(dec)!;
+
+            // assert
+            Assert.Equal(expectedValue, serializedValue);
+        }
+
+        [Fact]
+        protected void LocalCurrency_ExpectDeserializeNullToMatch()
+        {
+            // arrange
+            ScalarType scalar = new LocalCurrencyType();
+
+            // act
+            var success = scalar.TryDeserialize(null, out object? deserialized);
+
+            // assert
+            Assert.True(success);
+            Assert.Null(deserialized);
+        }
+
+        [Fact]
+        public void LocalCurrency_ExpectDeserializeNullableDecimalToDecimal()
+        {
+            // arrange
+            ScalarType scalar = new LocalCurrencyType();
+            decimal? dec = null;
+
+            // act
+            var success = scalar.TryDeserialize(dec, out object? deserialized);
+
+            // assert
+            Assert.True(success);
+            Assert.Null(deserialized);
+        }
+
+        [Fact]
+        protected void LocalCurrency_ExpectDeserializeStringToMatch()
+        {
+            // arrange
+            ScalarType scalar = CreateType<LocalCurrencyType>();
+            var runtimeValue = 7.99m;
+
+            // act
+            var deserializedValue = (decimal)scalar.Deserialize("$7.99")!;
+
+            // assert
+            Assert.Equal(runtimeValue, deserializedValue);
+        }
+
+        [Fact]
+        protected void LocalCurrency_ExpectDeserializeDecimalToMatch()
+        {
+            // arrange
+            ScalarType scalar = CreateType<LocalCurrencyType>();
+            object resultValue = 0.99m;
+
+
+            // act
+            object? result = scalar.Deserialize(resultValue);
+
+            // assert
+            Assert.Equal(resultValue, result);
+        }
+
+        [Fact]
+        public void LocalCurrency_ExpectDeserializeInvalidStringToDecimal()
+        {
+            // arrange
+            ScalarType scalar = new LocalCurrencyType();
+
+            // act
+            var success = scalar.TryDeserialize("abc", out object? _);
+
+            // assert
+            Assert.False(success);
+        }
+
+        [Fact]
+        public void LocalCurrency_ExpectDeserializeNullToNull()
+        {
+            // arrange
+            ScalarType scalar = new LocalCurrencyType();
+
+            // act
+            var success = scalar.TryDeserialize(null, out object? deserialized);
+
+            // assert
+            Assert.True(success);
+            Assert.Null(deserialized);
+        }
+
+        [Fact]
+        protected void LocalCurrency_ExpectSerializeToThrowSerializationException()
+        {
+            // arrange
+            ScalarType scalar = CreateType<LocalCurrencyType>();
+
+            // act
+            Exception? result = Record.Exception(() => scalar.Serialize("foo"));
+
+            // assert
+            Assert.IsType<SerializationException>(result);
+        }
+
+        [Fact]
+        protected void LocalCurrency_ExpectDeserializeToThrowSerializationException()
+        {
+            // arrange
+            ScalarType scalar = CreateType<LocalCurrencyType>();
+            object runtimeValue = new IntValueNode(1);
+
+            // act
+            Exception? result = Record.Exception(() => scalar.Deserialize(runtimeValue));
+
+            // assert
+            Assert.IsType<SerializationException>(result);
+        }
+
+        [Fact]
+        protected void LocalCurrency_ExpectParseResultToMatchNull()
+        {
+            // arrange
+            ScalarType scalar = new LocalCurrencyType();
+
+            // act
+            IValueNode result = scalar.ParseResult(null);
+
+            // assert
+            Assert.Equal(typeof(NullValueNode), result.GetType());
+        }
+
+        [Fact]
+        protected void LocalTime_ExpectParseResultToMatchStringValue()
+        {
+            // arrange
+            ScalarType scalar = new LocalCurrencyType();
+            const string valueSyntax = "$9.99";
+
+            // act
+            IValueNode result = scalar.ParseResult(valueSyntax);
+
+            // assert
+            Assert.Equal(typeof(StringValueNode), result.GetType());
+        }
+
+        [Fact]
+        protected void LocalCurrency_ExpectParseResultToThrowSerializationException()
+        {
+            // arrange
+            ScalarType scalar = new LocalCurrencyType();
+            IValueNode runtimeValue = new IntValueNode(1);
+
+            // act
+            Exception? result = Record.Exception(() => scalar.ParseResult(runtimeValue));
+
+            // assert
+            Assert.IsType<SerializationException>(result);
         }
     }
 }
