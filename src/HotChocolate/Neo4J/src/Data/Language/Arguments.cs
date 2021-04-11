e3098a3ace8f7276a92f675ab841fda0b7e8b5ec
Arif Hanif <ahanif@aeieng.com>
2021-04-10 23:32:14 -0400

    Update Arguments.cs

--- src/HotChocolate/Neo4J/src/Data/Language/Arguments.cs
+++ src/HotChocolate/Neo4J/src/Data/Language/Arguments.cs
@@ -1,11 +1,11 @@
 namespace HotChocolate.Data.Neo4J.Language
 {
     /// <summary>
-    /// Specialized list of expressions that represent the arguments on a procedure call.
+    /// Specialized list of expressions that represent the arguments of a procedure call.
     /// </summary>
     public class Arguments : TypedSubtree<Expression>, ITypedSubtree
     {
-        public override ClauseKind Kind { get; } = ClauseKind.Arguments;
+        public override ClauseKind Kind => ClauseKind.Arguments;
         public Arguments(params Expression[] children) : base(children) { }
 
         protected override IVisitable PrepareVisit(Expression child) =>
