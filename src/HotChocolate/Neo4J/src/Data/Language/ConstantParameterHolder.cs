9a9448dc3310cf8cdbe441e0b46ee88256bb989f
Arif Hanif <ahanif@aeieng.com>
2021-04-10 23:33:50 -0400

    Create ConstantParameterHolder.cs

---
+++ src/HotChocolate/Neo4J/src/Data/Language/ConstantParameterHolder.cs
@@ -0,0 +1,18 @@
+﻿namespace HotChocolate.Data.Neo4J.Language
+{
+    public class ConstantParameterHolder
+    {
+        private readonly object _value;
+        private readonly Expression _literalValue;
+
+        public ConstantParameterHolder(object value)
+        {
+            _value = value;
+            _literalValue = Cypher.LiteralOf(value);
+        }
+
+        public object GetValue() => _value;
+
+        public string AsString() => _literalValue.ToString();
+    }
+}
