8abe21dc15b194e50f813bf774455edea904fadc
PascalSenn <senn.pasc@gmail.com>
2021-04-19 17:18:31 +0200

    Fixed memory leak caused by query list variable params (#3541)
    Co-authored-by: davidbrazel2 <47366627+davidbrazel2@users.noreply.github.com>

--- src/StrawberryShake/Client/src/Core/Helper/ComparisonHelper.cs
+++ src/StrawberryShake/Client/src/Core/Helper/ComparisonHelper.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -39,6 +40,58 @@ namespace StrawberryShake.Helper
                 while (e1.MoveNext())
                 {
                     if (!(e2.MoveNext() && SequenceEqual(e1.Current, e2.Current)))
+                    {
+                        return false;
+                    }
+                }
+
+                return !e2.MoveNext();
+            }
+
+            return false;
+        }
+
+        public static bool SequenceEqual(
+            IEnumerable? first,
+            IEnumerable? second)
+        {
+            if (first is null && second is null)
+            {
+                return true;
+            }
+
+            if (first is not null && second is not null)
+            {
+                IEnumerator e1 = first.GetEnumerator();
+                IEnumerator e2 = second.GetEnumerator();
+
+                while (e1.MoveNext())
+                {
+                    if (!e2.MoveNext())
+                    {
+                        return false;
+                    }
+
+                    if (e1.Current is null && e2.Current is not null)
+                    {
+                        return false;
+                    }
+
+                    if (e1.Current is not null && e2.Current is null)
+                    {
+                        return false;
+                    }
+
+                    if (e1.Current is IEnumerable i1 &&
+                        e2.Current is IEnumerable i2 &&
+                        !SequenceEqual(i1, i2))
+                    {
+                        return false;
+                    }
+
+                    if (e1.Current is not null &&
+                        e2.Current is not null &&
+                        !e1.Current.Equals(e2.Current))
                     {
                         return false;
                     }
