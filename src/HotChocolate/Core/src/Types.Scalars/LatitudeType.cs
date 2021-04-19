3c8a2a54ac88c86088adaf20f85114d104da98e8
Gregory Sieranski <git@wonbyte.com>
2021-04-19 11:46:15 -0500

    updates

--- src/HotChocolate/Core/src/Types.Scalars/LatitudeType.cs
+++ src/HotChocolate/Core/src/Types.Scalars/LatitudeType.cs
@@ -7,7 +7,7 @@ namespace HotChocolate.Types
 {
     /// <summary>
     /// The `LatitudeType` scalar represents a valid decimal degrees latitude number.
-    /// <a>https://en.wikipedia.org/wiki/Latitude</a>
+    /// <a href="https://en.wikipedia.org/wiki/Latitude">Read More</a>
     /// </summary>
     public class LatitudeType : ScalarType<double, StringValueNode>
     {
@@ -40,11 +40,11 @@ namespace HotChocolate.Types
             {
                 null => NullValueNode.Default,
 
-                string s when Latitude.IsSexagesimal(s) &&
-                              Latitude.TryDeserializeFromString(s, out var value) =>
-                    ParseValue(value),
+                string s when Latitude.TryDeserialize(s, out var value) => ParseValue(value),
 
-                decimal d => ParseValue(d),
+                double d => ParseValue(d),
+
+                int d => ParseValue(d),
 
                 _ => throw ThrowHelper.LatitudeType_ParseValue_IsInvalid(this)
             };
@@ -53,8 +53,7 @@ namespace HotChocolate.Types
         /// <inheritdoc />
         protected override double ParseLiteral(StringValueNode valueSyntax)
         {
-            if (Latitude.TryDeserializeFromString(valueSyntax.Value, out var value) &&
-                value != null)
+            if (Latitude.TryDeserialize(valueSyntax.Value, out var value) && value is not null)
             {
                 return value.Value;
             }
@@ -65,40 +64,38 @@ namespace HotChocolate.Types
         /// <inheritdoc />
         protected override StringValueNode ParseValue(double runtimeValue)
         {
-            if (runtimeValue is < Latitude._minLat or > Latitude._maxLat)
+            if (runtimeValue is > Latitude.Min and < Latitude.Max)
             {
                 return new StringValueNode(Latitude.ToPrecision(runtimeValue));
             }
+
             throw ThrowHelper.LatitudeType_ParseValue_IsInvalid(this);
         }
 
         private static class Latitude
         {
-            internal const double _minLat = -90.0;
-            internal const double _maxLat = 90.0;
-            // https://en.wikipedia.org/wiki/Decimal_degrees#Precision
-            internal const int _maxPrecision = 8;
+            public const double Min = -90.0;
+            public const double Max = 90.0;
+            private const int _maxPrecision = 8;
 
             private const string SexagesimalRegex =
-                "^([0-9]{1,3})°\\s*([0-9]{1,3}(?:\\.(?:[0-9]{1,}))?)['′]\\s*(([0-9]{1,3}(\\.([0-9]{1,}))" +
-                "?)[\"″]\\s*)?([NEOSW]?)$";
+                "^([0-9]{1,3})°\\s*([0-9]{1,3}(?:\\.(?:[0-9]{1,}))?)['′]\\s*(([0-9]{1,3}" +
+                "(\\.([0-9]{1,}))?)[\"″]\\s*)?([NEOSW]?)$";
 
-            private static readonly Regex _rx = new(
-                SexagesimalRegex,
-                RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            private static readonly Regex _rx =
+                new(SexagesimalRegex, RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-            internal static bool IsSexagesimal(string s)
-            {
-                return _rx.IsMatch(s);
-            }
-
-            internal static bool TryDeserializeFromString(string serialized, out double? value)
+            public static bool TryDeserialize(string serialized, out double? value)
             {
                 MatchCollection coords = _rx.Matches(serialized);
                 if (coords.Count > 0)
                 {
-                    var minute = double.TryParse(coords[0].Groups[2].Value, out var min) ? min / 60 : 0;
-                    var second = double.TryParse(coords[0].Groups[4].Value, out var sec) ? sec / 3600 : 0;
+                    var minute = double.TryParse(coords[0].Groups[2].Value, out var min)
+                        ? min / 60
+                        : 0;
+                    var second = double.TryParse(coords[0].Groups[4].Value, out var sec)
+                        ? sec / 3600
+                        : 0;
                     var degree = double.Parse(coords[0].Groups[1].Value);
                     var result = degree + minute + second;
 
@@ -111,8 +108,9 @@ namespace HotChocolate.Types
                 return false;
             }
 
-            internal static string ToPrecision(double d) =>
-                Math.Round(d, _maxPrecision, MidpointRounding.AwayFromZero).ToString(CultureInfo.InvariantCulture);
+            public static string ToPrecision(double d) =>
+                Math.Round(d, _maxPrecision, MidpointRounding.AwayFromZero)
+                    .ToString(CultureInfo.InvariantCulture);
         }
     }
 }
