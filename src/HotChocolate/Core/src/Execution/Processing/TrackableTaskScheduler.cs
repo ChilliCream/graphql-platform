9a02ebce9604f0e67fce2781ca0af557d89b5629
Marte <marte.tassyns@outlook.be>
2021-04-27 08:53:18 +0200

    experiment for higher performance of TrackableTaskScheduler (no locking when starting tasks and reduced locking when executing them)

--- src/HotChocolate/Core/src/Execution/Processing/TrackableTaskScheduler.cs
+++ src/HotChocolate/Core/src/Execution/Processing/TrackableTaskScheduler.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace HotChocolate.Execution.Processing
@@ -13,18 +16,22 @@ namespace HotChocolate.Execution.Processing
     {
         private readonly object _lock = new();
         /// <summary>The queue holding pending tasks</summary>
-        private readonly ConcurrentQueue<Task> _queue = new ConcurrentQueue<Task>();
+        private readonly Channel<Task> _queue = Channel.CreateUnbounded<Task>(new UnboundedChannelOptions {
+            AllowSynchronousContinuations = true,
+            SingleReader = false,
+            SingleWriter = false
+        });
         /// <summary>The underlying task scheduler to which all work should be scheduled.</summary>
         private readonly TaskScheduler _underlyingScheduler;
         /// <summary>The maximum number of tasks processors that can be active at the same time.</summary>
         private readonly int _processingTaskMax = Environment.ProcessorCount;
         /// <summary>The amount of time in milliseconds a single processing task can keep the underlying scheduler occupied</summary>
         /// <remarks>If a task takes longer it will not quit, it will just result in a new processing task after it completes</remarks>
-        private readonly long _processingTaskTimeout = 50;
-        /// <summary>The actual number of tasks processors that are currently active.</summary>
+        private readonly int _processingTaskTimeout = 50;
+        /// <summary>The number of tasks processors that are currently processing a task.</summary>
         private int _processingTaskCount = 0;
         /// <summary>If > 0, no more work will be accepted and the active processing tasks will stop as soon as possible</summary>
-        private int _shutdown = 0;
+        private CancellationTokenSource _shutdown = new();
         /// <summary>This event is triggered whenever the processing comes to a complete stop (no items left in queue, and nothing running)</summary>
         private event EventHandler? ProcessingHalted;
 
@@ -32,6 +39,7 @@ namespace HotChocolate.Execution.Processing
         public TrackableTaskScheduler(TaskScheduler underlyingScheduler)
         {
             _underlyingScheduler = underlyingScheduler;
+            StartupProcessors();
         }
 
         /// <returns>true if the scheduler has no remaining work (either on queue or in progress)</returns>
@@ -102,89 +110,108 @@ namespace HotChocolate.Execution.Processing
         /// </summary>
         public void Complete()
         {
-            Interlocked.Increment(ref _shutdown);
+            _queue.Writer.Complete();
+            _shutdown.Cancel();
         }
 
         protected override IEnumerable<Task>? GetScheduledTasks()
         {
-            return _queue;
+            // for debugging only, try to use internal member of UnboundedQueue
+            var itemsField = _queue.GetType()
+                .GetField("_items", BindingFlags.Instance | BindingFlags.NonPublic);
+            return itemsField?.GetValue(_queue) as IEnumerable<Task> ?? new List<Task>();
         }
 
         protected override void QueueTask(Task task)
         {
             Debug.Assert(task != null, "Infrastructure should have provided a non-null task.");
-            Debug.Assert(_shutdown == 0, "After completion no more work should be scheduled");
-            lock (_lock)
-            {
-                _queue.Enqueue(task);
-                ProcessAsyncIfNecessary();
-            }
+            Debug.Assert(!_shutdown.IsCancellationRequested, "After completion no more work should be scheduled");
+            _queue.Writer.WriteAsync(task);
         }
 
         protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
         {
-            return TryExecuteTask(task);
+            return SafeExecuteTask(task);
         }
 
         /// <remarks>This method assumes that the caller takes a lock on _lock</remarks>
-        private void ProcessAsyncIfNecessary()
+        private void StartupProcessors()
         {
-            if (_shutdown == 0 && _processingTaskCount < _processingTaskMax && _queue.Count > 0)
+            for(int i = 0; i < _processingTaskMax; ++i)
             {
-                // Launch concurrent task processing, up to the allowed limit
-                for (int i = _queue.Count; i > 0 && _processingTaskCount < _processingTaskMax; --i)
+                var options = TaskCreationOptions.DenyChildAttach | TaskCreationOptions.PreferFairness;
+                Task.Factory.StartNew(() => ProcessTasks(), default, options, _underlyingScheduler);
+            }
+        }
+
+        private async ValueTask ProcessTasks()
+        {
+            while (!_shutdown.IsCancellationRequested)
+            {
+                try
                 {
+                    var ctxSource = CancellationTokenSource.CreateLinkedTokenSource(_shutdown.Token);
+                    ctxSource.CancelAfter(_processingTaskTimeout);
+
+                    Task task = await _queue.Reader.ReadAsync(ctxSource.Token);
                     Interlocked.Increment(ref _processingTaskCount);
                     try
                     {
-                        var options = TaskCreationOptions.DenyChildAttach | TaskCreationOptions.PreferFairness;
-                        var processingTask = new Task(ProcessTasks, default, options);
-                        processingTask.Start(_underlyingScheduler);
+                        SafeExecuteTask(task);
+                        while (!ctxSource.IsCancellationRequested && _queue.Reader.TryRead(out task))
+                        {
+                            SafeExecuteTask(task);
+                        }
                     }
-                    catch
+                    finally
                     {
-                        Interlocked.Decrement(ref _processingTaskCount);
-                        throw;
+                        var value = Interlocked.Decrement(ref _processingTaskCount);
+                        if (value < 0)
+                        {
+                            Debug.Fail("Inconstistent processing task count, investigate why this is happening");
+                            value = Interlocked.CompareExchange(ref _processingTaskCount, 0, value);
+                        }
+                        if (value == 0)
+                        {
+                            lock (_lock)
+                            {
+                                ProcessingHalted?.Invoke(this, EventArgs.Empty);
+                            }
+                        }
                     }
+                    
+                    if (!_shutdown.IsCancellationRequested)
+                    {
+                         // yield to give other tasks a chance to run fairly as well 
+                         await Task.Yield();
+                    }
+                }
+                catch(TaskCanceledException)
+                {
+                    // expected, ignore
+                }
+                catch(Exception e)
+                {
+                    Debug.Fail($"unexpected exception: {e}");
                 }
             }
         }
 
-        private void ProcessTasks()
+        private bool SafeExecuteTask(Task task)
         {
-            try
+            // Execute the task. If the scheduler was previously faulted,
+            // this task could have been faulted when it was queued; ignore such tasks.
+            bool result;
+            if (task.IsCompleted)
             {
-                var stopwatch = new Stopwatch();
-                stopwatch.Start();
-
-                Task task;
-                while (_shutdown == 0 && stopwatch.ElapsedMilliseconds < _processingTaskTimeout && _queue.TryDequeue(out task))
-                {
-                    // Execute the task. If the scheduler was previously faulted,
-                    // this task could have been faulted when it was queued; ignore such tasks.
-                    if (!task.IsCompleted)
-                    {
-                        TryExecuteTask(task);
-                        Debug.Assert(task.IsCompleted, "After TaskScheduler.TryExecuteTask the task should be completed");
-                    }
-                }
+                result = true;
             }
-            finally
+            else
             {
-                lock(_lock)
-                {
-                    var value = Interlocked.Decrement(ref _processingTaskCount);
-                    if (value < 0)
-                    {
-                        // should not happen, but auto recover if it does just in case
-                        Interlocked.CompareExchange(ref _processingTaskCount, 0, value);
-                    }
-
-                    ProcessAsyncIfNecessary();
-
-                    if (_processingTaskCount == 0) ProcessingHalted?.Invoke(this, EventArgs.Empty);
-                }
+                result = TryExecuteTask(task);
+                Debug.Assert(task.IsCompleted, "After TaskScheduler.TryExecuteTask the task should be completed");
             }
+            return result;
         }
     }
 }
