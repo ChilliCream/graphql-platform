9ea438f431ebf68878c3cf3f7fb01d10564f5de4
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2021-04-30 20:37:51 +0200

    Improvements

--- publish-api.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Marking all unshipped APIs as shipped..."
-
-find ./src -name "PublicAPI.Unshipped.txt" -exec sh -c 'echo "$1"; basedir="$(dirname "$1")"; cp "$1" "$basedir/PublicAPI.Shipped.txt"' _ {} \;
-
-echo "Marked all unshipped APIs as shipped!"
