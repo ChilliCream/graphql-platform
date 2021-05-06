10691a1b32c736927641e52e92ede20d05a31c3c
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2021-05-06 21:13:11 +0200

    Use Implements in internal code

--- src/HotChocolate/Core/test/StarWars/Types/HumanType.cs
+++ src/HotChocolate/Core/test/StarWars/Types/HumanType.cs
@@ -10,7 +10,7 @@ namespace HotChocolate.StarWars.Types
     {
         protected override void Configure(IObjectTypeDescriptor<Human> descriptor)
         {
-            descriptor.Interface<CharacterType>();
+            descriptor.Implements<CharacterType>();
 
             descriptor.Field(t => t.Id)
                 .Type<NonNullType<IdType>>();
