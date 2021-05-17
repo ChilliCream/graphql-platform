82bc367141144e095a7524f02820e2133504be75
Rafael Staib <me@rafaelstaib.com>
2021-05-17 21:33:30 +0200

    Disabled dead links check for now

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -60,7 +60,10 @@ module.exports = {
               backgroundColor: "transparent",
             },
           },
-          "gatsby-remark-check-links",
+          // todo: enable and fix
+          // note: disabled for now, because # hash links are not recognized.
+          // we probably need this plugin again gatsby-remark-autolink-headers.
+          //`gatsby-remark-check-links`,
         ],
       },
     },
