61e6140bd04f29047ea5e435867359dc5bd0b0d7
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2021-05-22 21:42:55 +0200

    Use current branch for the diff-shipped-api.ps1 script

--- scripts/diff-shipped-api.ps1
+++ scripts/diff-shipped-api.ps1
@@ -1,13 +1,17 @@
 [CmdletBinding(PositionalBinding = $false)]
 param (
     [string]$from,
-    [string]$to = "main"
+    [string]$to
 )
 
 Set-StrictMode -version 2.0
 $ErrorActionPreference = "Stop"
 
 try {
+    if ([string]::IsNullOrWhiteSpace($to)) {
+        $to = git branch --show-current
+    }
+
     Write-Host "Diffing '$from' to '$to'..."
 
     git --no-pager diff --minimal -U0 --word-diff "$from" "$to" -- "../src/**/PublicAPI.Shipped.txt"
