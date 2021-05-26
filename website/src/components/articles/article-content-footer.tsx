d4abc70e5bb716364758d4df15c037903a8b7ad4
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2021-05-26 12:56:34 +0200

    Display last updated on documentation entries
    Conflicts:
    	website/src/components/doc-page/doc-page.tsx

---
+++ website/src/components/articles/article-content-footer.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+interface Props {
+  lastUpdated: string;
+  lastAuthorName: string;
+}
+
+export const ArticleContentFooter: FC<Props> = ({
+  lastUpdated,
+  lastAuthorName,
+}) => {
+  return (
+    <Footer>
+      <Small>
+        Last updated on <strong>{lastUpdated}</strong> by{" "}
+        <strong>{lastAuthorName}</strong>
+      </Small>
+    </Footer>
+  );
+};
+
+const Small = styled.small`
+  font-style: italic;
+  font-size: 0.7em;
+`;
+
+const Footer = styled.div`
+  text-align: right;
+  margin-top: 30px;
+`;
