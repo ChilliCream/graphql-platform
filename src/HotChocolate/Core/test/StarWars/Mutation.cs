6d929b5dc36476940e0a45cd0f2a046283fffdf2
Michael Staib <michael@chillicream.com>
2021-05-28 20:27:14 +0200

    Started on integration tests

--- src/HotChocolate/Core/test/StarWars/Mutation.cs
+++ src/HotChocolate/Core/test/StarWars/Mutation.cs
@@ -25,5 +25,35 @@ namespace HotChocolate.StarWars
             await eventSender.SendAsync(episode, review);
             return review;
         }
+
+        /// <summary>
+        /// Creates a review for a given Star Wars episode.
+        /// </summary>
+        /// <param name="episode">The episode to review.</param>
+        /// <param name="review">The review.</param>
+        /// <param name="repository">The review repository.</param>
+        /// <param name="eventSender">The event sending service.</param>
+        /// <returns>The created review.</returns>
+        public async Task<ReviewPayload> CreateReview2(
+            ReviewInput input,
+            [Service]ReviewRepository repository,
+            [Service]ITopicEventSender eventSender)
+        {
+            repository.AddReview(input.Episode, input.Review);
+            await eventSender.SendAsync(input.Episode, input.Review);
+            return new ReviewPayload { Review = input.Review};
+        }
+    }
+
+    public class ReviewInput
+    {
+        public Episode Episode { get; set; }
+
+        public Review Review { get; set; }
+    }
+
+    public class ReviewPayload
+    {
+        public Review Review { get; set; }
     }
 }
