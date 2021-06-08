0b00e8fcdff2c4aaf2c9acae6e9028b6d9699c80
Michael Staib <michael@chillicream.com>
2021-06-08 11:10:37 +0200

    Fixed an issue with streams where the stream could not be cancelled

--- src/HotChocolate/Core/test/Execution.Tests/Pipeline/TimeoutMiddlewareTests.cs
+++ src/HotChocolate/Core/test/Execution.Tests/Pipeline/TimeoutMiddlewareTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using HotChocolate.Execution.Instrumentation;
@@ -8,6 +9,7 @@ using HotChocolate.Execution.Options;
 using HotChocolate.Execution.Processing;
 using HotChocolate.Language;
 using HotChocolate.Tests;
+using HotChocolate.Types;
 using HotChocolate.Utilities;
 using HotChocolate.Validation;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,12 +34,57 @@ namespace HotChocolate.Execution.Pipeline
                 .MatchSnapshotAsync();
         }
 
+        [Fact]
+        public async Task Ensure_That_Combined_Token_Is_Not_Disposed_On_Stream()
+        {
+            using var cts = new CancellationTokenSource();
+
+            IExecutionResult result = await new ServiceCollection()
+                .AddGraphQL()
+                .AddQueryType<TimeoutQuery>()
+                .AddSubscriptionType<Subscriptions>()
+                .ExecuteRequestAsync("subscription { onFoo }", cancellationToken: cts.Token);
+
+            SubscriptionResult stream = Assert.IsType<SubscriptionResult>(result);
+
+            IAsyncEnumerable<IQueryResult> enumerable = stream.ReadResultsAsync();
+            IAsyncEnumerator<IQueryResult> enumerator = enumerable.GetAsyncEnumerator(
+                CancellationToken.None);
+
+            // we should get the first result!
+            await enumerator.MoveNextAsync();
+            enumerator.Current.ToJson().MatchSnapshot();
+
+            cts.Cancel();
+
+            Assert.False(await enumerator.MoveNextAsync(), "the stream should be canceled.");
+        }
+
         public class TimeoutQuery
         {
             public async Task<string> Timeout(CancellationToken cancellationToken)
             {
                 await Task.Delay(TimeSpan.FromSeconds(30), cancellationToken);
                 return "Hello";
+            }
+        }
+
+        public class Subscriptions
+        {
+            [SubscribeAndResolve]
+            public async IAsyncEnumerable<string> OnFoo(
+                [EnumeratorCancellation] CancellationToken cancellationToken)
+            {
+                if (!cancellationToken.IsCancellationRequested)
+                {
+                    yield return await Task.FromResult("a");
+                }
+
+                if (!cancellationToken.IsCancellationRequested)
+                {
+                    yield return "b";
+                    throw new Exception("we should not get here in this test case.");
+                }
             }
         }
     }
