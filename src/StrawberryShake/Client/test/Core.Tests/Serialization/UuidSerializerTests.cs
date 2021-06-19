f45f53250e3f5ca5dd198f15bfe7519a84f2c322
Marco Hamann <marco.hamann@dmgmori.com>
2021-06-19 12:17:38 +0200

    add unit tests for UuidSerializer

---
+++ src/StrawberryShake/Client/test/Core.Tests/Serialization/UuidSerializerTests.cs
@@ -0,0 +1,53 @@
+using System;
+using StrawberryShake;
+using StrawberryShake.Serialization;
+using Xunit;
+
+namespace Core.Tests.Serialization
+{
+    public class UuidSerializerTests
+    {
+        [Theory]
+        [InlineData("D")]
+        [InlineData("N")]
+        [InlineData("B")]
+        [InlineData("P")]
+        [InlineData("X")]
+        public void Parse_Different_Formats(string format)
+        {
+            // arrange
+            var serializer = new UUIDSerializer();
+            var guid = Guid.NewGuid();
+
+            // act
+            Guid result = serializer.Parse(guid.ToString(format));
+
+            // assert
+            Assert.Equal(guid, result);
+        }
+
+        [Fact]
+        public void Parse_Exception()
+        {
+            // arrange
+            var serializer = new UUIDSerializer();
+
+            // assert
+            Assert.Throws<GraphQLClientException>(() => serializer.Parse(string.Empty));
+        }
+
+        [Fact]
+        public void Format()
+        {
+            // arrange
+            var serializer = new UUIDSerializer();
+            var guid = Guid.NewGuid();
+
+            // act
+            var result = serializer.Format(guid);
+
+            // assert
+            Assert.Equal(guid.ToString(), result);
+        }
+    }
+}
