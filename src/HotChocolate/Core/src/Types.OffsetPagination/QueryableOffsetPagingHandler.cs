527436012dfa45ff20c50733cabc98f4b57697ae
Pascal Senn <senn.pasc@gmail.com>
2021-06-27 23:21:23 +0200

    feat: move pagination to new extension methods

--- src/HotChocolate/Core/src/Types.OffsetPagination/QueryableOffsetPagingHandler.cs
+++ src/HotChocolate/Core/src/Types.OffsetPagination/QueryableOffsetPagingHandler.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using HotChocolate.Resolvers;
 
@@ -29,7 +28,7 @@ namespace HotChocolate.Types.Pagination
             return source switch
             {
                 IQueryable<TItemType> q => ResolveAsync(context, q, arguments),
-                IEnumerable<TItemType> e => ResolveAsync(context,e.AsQueryable(),arguments),
+                IEnumerable<TItemType> e => ResolveAsync(context, e.AsQueryable(), arguments),
                 IExecutable<TItemType> ex => SliceAsync(context, ex.Source, arguments),
                 _ => throw new GraphQLException("Cannot handle the specified data source.")
             };
@@ -40,34 +39,14 @@ namespace HotChocolate.Types.Pagination
             IQueryable<TItemType> queryable,
             OffsetPagingArguments arguments = default)
         {
-            IQueryable<TItemType> original = queryable;
-
-            if (arguments.Skip.HasValue)
-            {
-                queryable = queryable.Skip(arguments.Skip.Value);
-            }
-
-            queryable = queryable.Take(arguments.Take + 1);
-            List<TItemType> items =
-                await ExecuteQueryableAsync(queryable, context.RequestAborted)
-                    .ConfigureAwait(false);
-            var pageInfo = new CollectionSegmentInfo(
-                items.Count == arguments.Take + 1,
-                (arguments.Skip ?? 0) > 0);
-
-            if (items.Count > arguments.Take)
-            {
-                items.RemoveAt(arguments.Take);
-            }
-
-            Func<CancellationToken, ValueTask<int>> getTotalCount =
-                ct => throw new InvalidOperationException();
+            OffsetPagingHelper.CountAsync<IQueryable<TItemType>> getTotalCount =
+                (_, _) => throw new InvalidOperationException();
 
             // TotalCount is one of the heaviest operations. It is only necessary to load totalCount
             // when it is enabled (IncludeTotalCount) and when it is contained in the selection set.
             if (IncludeTotalCount &&
                 context.Field.Type is ObjectType objectType &&
-                context.Selection.SyntaxNode.SelectionSet is {} selectionSet)
+                context.Selection.SyntaxNode.SelectionSet is { } selectionSet)
             {
                 IReadOnlyList<IFieldSelection> selections = context
                     .GetSelections(objectType, selectionSet, true);
@@ -86,49 +65,19 @@ namespace HotChocolate.Types.Pagination
                 // count in a variable, to pass it into the clojure
                 if (includeTotalCount)
                 {
-                    var captureCount = original.Count();
-                    getTotalCount = ct => new ValueTask<int>(captureCount);
+                    var captureCount = queryable.Count();
+                    getTotalCount = (_, _) => new ValueTask<int>(captureCount);
                 }
             }
 
-            return new CollectionSegment(
-                (IReadOnlyCollection<object>)items,
-                pageInfo,
-                getTotalCount);
-        }
-
-        protected virtual async ValueTask<List<TItemType>> ExecuteQueryableAsync(
-            IQueryable<TItemType> queryable,
-            CancellationToken cancellationToken)
-        {
-            var list = new List<TItemType>();
-
-            if (queryable is IAsyncEnumerable<TItemType> enumerable)
-            {
-                await foreach (TItemType item in enumerable.WithCancellation(cancellationToken)
-                    .ConfigureAwait(false))
-                {
-                    list.Add(item);
-                }
-            }
-            else
-            {
-                await Task.Run(() =>
-                {
-                    foreach (TItemType item in queryable)
-                    {
-                        if (cancellationToken.IsCancellationRequested)
-                        {
-                            break;
-                        }
-
-                        list.Add(item);
-                    }
-
-                }).ConfigureAwait(false);
-            }
-
-            return list;
+            return await OffsetPagingHelper.ApplyPagination(
+                queryable,
+                arguments,
+                (x, skip) => x.Skip(skip),
+                (x, take) => x.Take(take),
+                OffsetPagingHelper.ExecuteEnumerable,
+                getTotalCount,
+                context.RequestAborted);
         }
     }
 }
