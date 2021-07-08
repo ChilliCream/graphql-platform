cfa079abc66c1da305008fd87a4c3bce8de1d489
Michael Staib <michael@chillicream.com>
2021-07-08 08:13:21 +0200

    Added documentation

--- src/HotChocolate/Core/src/Execution/DependencyInjection/RequestExecutorBuilderExtensions.ResolverCompiler.cs
+++ src/HotChocolate/Core/src/Execution/DependencyInjection/RequestExecutorBuilderExtensions.ResolverCompiler.cs
@@ -1,14 +1,23 @@
 using System;
-using System.Linq.Expressions;
-using System.Reflection;
 using HotChocolate.Execution.Configuration;
-using HotChocolate.Internal;
-using HotChocolate.Resolvers;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static partial class RequestExecutorBuilderExtensions
     {
+        /// <summary>
+        /// Configures the resolver compiler.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="IRequestExecutorBuilder"/>.
+        /// </param>
+        /// <param name="configure">
+        /// A delegate that is to configure the resolver compiler.
+        /// </param>
+        /// <returns>
+        /// An <see cref="IRequestExecutorBuilder"/> that can be used to configure a schema
+        /// and its execution.
+        /// </returns>
         public static IRequestExecutorBuilder ConfigureResolverCompiler(
             this IRequestExecutorBuilder builder,
             Action<IResolverCompilerBuilder> configure)
@@ -16,50 +25,5 @@ namespace Microsoft.Extensions.DependencyInjection
             configure(new DefaultResolverCompilerBuilder(builder));
             return builder;
         }
-    }
-
-    public static class ResolverCompilerBuilderExtensions
-    {
-        public static IResolverCompilerBuilder AddParameter<T>(
-            this IResolverCompilerBuilder builder,
-            Expression<Func<IResolverContext, T>> expression,
-            Func<ParameterInfo, bool>? canHandle = null)
-        {
-            if (canHandle is null)
-            {
-                builder.RequestExecutorBuilder.Services.TryAddParameterExpressionBuilder(
-                    _ => new CustomParameterExpressionBuilder<T>(expression));
-            }
-            else
-            {
-                builder.RequestExecutorBuilder.Services.TryAddParameterExpressionBuilder(
-                    _ => new CustomParameterExpressionBuilder<T>(expression, canHandle));
-            }
-            return builder;
-        }
-
-        public static IResolverCompilerBuilder AddService<TService>(
-            this IResolverCompilerBuilder builder)
-        {
-            builder.RequestExecutorBuilder.Services
-                .TryAddParameterExpressionBuilder<
-                    CustomServiceParameterExpressionBuilder<TService>>();
-            return builder;
-        }
-    }
-
-    public interface IResolverCompilerBuilder
-    {
-        IRequestExecutorBuilder RequestExecutorBuilder { get; }
-    }
-
-    internal sealed class DefaultResolverCompilerBuilder : IResolverCompilerBuilder
-    {
-        public DefaultResolverCompilerBuilder(IRequestExecutorBuilder requestExecutorBuilder)
-        {
-            RequestExecutorBuilder = requestExecutorBuilder;
-        }
-
-        public IRequestExecutorBuilder RequestExecutorBuilder { get; }
     }
 }
