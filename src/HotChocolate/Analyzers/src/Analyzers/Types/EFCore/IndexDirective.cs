041fb32d44ff03bbc2c019f06812244073a6f447
Ben McCallum <ben.mccallum@live.com.au>
2021-07-10 15:40:13 +0200

    Flatten classes to make iterating faster

--- src/HotChocolate/Analyzers/src/Analyzers/Types/EFCore/IndexDirective.cs
+++ src/HotChocolate/Analyzers/src/Analyzers/Types/EFCore/IndexDirective.cs
@@ -1,7 +1,19 @@
+using HotChocolate.Types;
+
 namespace HotChocolate.Analyzers.Types.EFCore
 {
     public class IndexDirective
     {
 
+    }
+
+    public class IndexDirectiveType : DirectiveType<IndexDirective>
+    {
+        protected override void Configure(IDirectiveTypeDescriptor<IndexDirective> descriptor)
+        {
+            descriptor
+                .Name("index")
+                .Location(DirectiveLocation.FieldDefinition);
+        }
     }
 }
