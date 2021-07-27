76ba0982ec0709a7c1fd986a0f1967edcbdc9dee
Michael Staib <michael@chillicream.com>
2021-07-28 00:55:15 +0200

    Added Base Setup

---
+++ src/StrawberryShake/SourceGenerator/test/CodeGeneration.CSharp.Analyzers.Worker.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CodeGeneration.CSharp.Analyzers.Worker.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
