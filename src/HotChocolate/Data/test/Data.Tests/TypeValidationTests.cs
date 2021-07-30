19cbb0d24dc4990a95468dde213e8f27fab16e2f
Michael Staib <michael@chillicream.com>
2021-07-30 09:48:47 +0200

    Fixed tests

--- src/HotChocolate/Data/test/Data.Tests/TypeValidationTests.cs
+++ src/HotChocolate/Data/test/Data.Tests/TypeValidationTests.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using HotChocolate.Data;
+using System.Reflection;
+using HotChocolate.Types;
+using HotChocolate.Types.Descriptors;
 using Snapshooter.Xunit;
 using Xunit;
 
-namespace HotChocolate.Types
+namespace HotChocolate.Data.Test
 {
     public class TypeValidationTests
     {
@@ -20,7 +21,13 @@ namespace HotChocolate.Types
                     .AddSorting()
                     .Create();
 
-            Assert.Throws<SchemaException>(Action).Message.MatchSnapshot();
+            SchemaException exception = Assert.Throws<SchemaException>(Action);
+
+            Assert.Collection(
+                exception.Errors,
+                error => Assert.Equal("HC0050", error.Code));
+
+            exception.Message.MatchSnapshot();
         }
 
         [Fact]
@@ -34,7 +41,26 @@ namespace HotChocolate.Types
                     .AddSorting()
                     .Create();
 
-            Assert.Throws<SchemaException>(Action).Message.MatchSnapshot();
+            SchemaException exception = Assert.Throws<SchemaException>(Action);
+
+            Assert.Collection(
+                exception.Errors,
+                error => Assert.Equal("HC0050", error.Code));
+            
+            exception.Message.MatchSnapshot();
+        }
+
+        [Fact]
+        public void EnsureCorrectlyOrderedMiddlewarePassValidation()
+        {
+            SchemaBuilder.New()
+                .AddQueryType<CorrectMiddlewarePipeline>()
+                .AddProjections()
+                .AddFiltering()
+                .AddSorting()
+                .Create()
+                .ToString()
+                .MatchSnapshot();
         }
 
         public class InvalidMiddlewarePipeline1
@@ -50,6 +76,35 @@ namespace HotChocolate.Types
             [UseFiltering]
             [UseProjection]
             public IEnumerable<string> GetBars() => throw new NotImplementedException();
+        }
+
+        public class CorrectMiddlewarePipeline
+        {
+            [UsePaging]
+            [UseSomeOther]
+            [UseProjection]
+            [UseSomeOther]
+            [UseFiltering]
+            [UseSomeOther]
+            [UseSorting]
+            [UseSomeOther]
+            public IEnumerable<Foo> GetBars() => throw new NotImplementedException();
+        }
+
+        public class Foo
+        {
+            public string? Bar { get; set; }
+        }
+
+        public class UseSomeOtherAttribute : ObjectFieldDescriptorAttribute
+        {
+            public override void OnConfigure(
+                IDescriptorContext context,
+                IObjectFieldDescriptor descriptor,
+                MemberInfo member)
+            {
+                descriptor.Use(_ => _);
+            }
         }
     }
 }
