b41ab2e2bf33c7958a4c8d269deef868bc4bba16
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2021-07-31 14:22:05 +0200

    Support EnumTypeExtension

--- src/HotChocolate/Core/src/Types/Types/Descriptors/Extensions/EnumTypeDescriptorExtensions.cs
+++ src/HotChocolate/Core/src/Types/Types/Descriptors/Extensions/EnumTypeDescriptorExtensions.cs
@@ -21,5 +21,23 @@ namespace HotChocolate.Types
             descriptor.Value(value).Ignore();
             return descriptor;
         }
+
+        public static IEnumTypeDescriptor Ignore<T>(
+            this IEnumTypeDescriptor descriptor,
+            T value)
+        {
+            if (descriptor is null)
+            {
+                throw new ArgumentNullException(nameof(descriptor));
+            }
+
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            descriptor.Value(value).Ignore();
+            return descriptor;
+        }
     }
 }
