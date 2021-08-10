61cd85ee305cae7a509c0a388cc7777bc4a163fd
Michael Staib <michael@chillicream.com>
2021-08-10 23:11:52 +0200

    Added some xml docs

--- src/HotChocolate/Core/src/Types/Types/Descriptors/Extensions/EnumTypeDescriptorExtensions.cs
+++ src/HotChocolate/Core/src/Types/Types/Descriptors/Extensions/EnumTypeDescriptorExtensions.cs
@@ -2,8 +2,32 @@ using System;
 
 namespace HotChocolate.Types
 {
+    /// <summary>
+    /// Provides extensions to the <see cref="IEnumTypeDescriptor"/> and
+    /// <see cref="IEnumTypeDescriptor{T}"/>.
+    /// </summary>
     public static class EnumTypeDescriptorExtensions
     {
+        /// <summary>
+        /// Ignores the given enum value for the schema creation.
+        /// This enum will not be included into the GraphQL schema.
+        /// </summary>
+        /// <param name="descriptor">
+        /// The enum type descriptor.
+        /// </param>
+        /// <param name="value">
+        /// The enum value that shall be ignored.
+        /// </param>
+        /// <typeparam name="T">
+        /// The enum value type.
+        /// </typeparam>
+        /// <returns>
+        /// Returns the enum type descriptor for configuration chaining.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="descriptor"/> is <c>null</c> or
+        /// <paramref name="value"/> is <c>null</c>.
+        /// </exception>
         public static IEnumTypeDescriptor<T> Ignore<T>(
             this IEnumTypeDescriptor<T> descriptor,
             T value)
@@ -22,6 +46,26 @@ namespace HotChocolate.Types
             return descriptor;
         }
 
+        /// <summary>
+        /// Ignores the given enum value for the schema creation.
+        /// This enum will not be included into the GraphQL schema.
+        /// </summary>
+        /// <param name="descriptor">
+        /// The enum type descriptor.
+        /// </param>
+        /// <param name="value">
+        /// The enum value that shall be ignored.
+        /// </param>
+        /// <typeparam name="T">
+        /// The enum value type.
+        /// </typeparam>
+        /// <returns>
+        /// Returns the enum type descriptor for configuration chaining.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="descriptor"/> is <c>null</c> or
+        /// <paramref name="value"/> is <c>null</c>.
+        /// </exception>
         public static IEnumTypeDescriptor Ignore<T>(
             this IEnumTypeDescriptor descriptor,
             T value)
