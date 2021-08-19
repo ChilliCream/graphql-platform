061b818d415551e80cf62c8542789ff8365488d9
Michael Staib <michael@chillicream.com>
2021-08-19 10:12:19 +0200

    Updated scripts

--- build.sh
+++ build.sh
@@ -9,16 +9,22 @@ SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 # CONFIGURATION
 ###########################################################################
 
-BUILD_PROJECT_FILE="$SCRIPT_DIR/.build/Build.csproj"
+BUILD_PROJECT_FILE="$SCRIPT_DIR/build/_build.csproj"
 TEMP_DIRECTORY="$SCRIPT_DIR/.nuke/temp"
 
-DOTNET_GLOBAL_FILE="$SCRIPT_DIR//global.json"
+DOTNET_GLOBAL_FILE="$SCRIPT_DIR/global.json"
 DOTNET_INSTALL_URL="https://dot.net/v1/dotnet-install.sh"
 DOTNET_CHANNEL="Current"
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_MULTILEVEL_LOOKUP=0
+export DOTNET_ROLL_FORWARD="Major"
+export NUKE_TELEMETRY_OPTOUT=1
+export MSBUILDDISABLENODEREUSE=1
+
+#export NUKE_ENTERPRISE_SOURCE="https://nuget.pkg.github.com/nuke-build/index.json"
+#export NUKE_ENTERPRISE_USERNAME="nuke-bot"
 
 ###########################################################################
 # EXECUTION
@@ -27,6 +33,15 @@ export DOTNET_MULTILEVEL_LOOKUP=0
 function FirstJsonValue {
     perl -nle 'print $1 if m{"'"$1"'": "([^"]+)",?}' <<< "${@:2}"
 }
+
+# Print environment variables
+# WARNING: Make sure that secrets are actually scrambled in build log
+# env | sort
+
+# Check if any dotnet is installed
+if [[ -x "$(command -v dotnet)" ]]; then
+    dotnet --info
+fi
 
 # If dotnet CLI is installed globally and it matches requested version, use for execution
 if [ -x "$(command -v dotnet)" ] && dotnet --version &>/dev/null; then
@@ -37,10 +52,6 @@ else
     mkdir -p "$TEMP_DIRECTORY"
     curl -Lsfo "$DOTNET_INSTALL_FILE" "$DOTNET_INSTALL_URL"
     chmod +x "$DOTNET_INSTALL_FILE"
-
-    "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --version "2.1.816" --no-path
-    "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --version "3.1.409" --no-path
-    "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --version "5.0.300" --no-path
 
     # If global.json exists, load expected version
     if [[ -f "$DOTNET_GLOBAL_FILE" ]]; then
@@ -62,5 +73,9 @@ fi
 
 echo "Microsoft (R) .NET Core SDK version $("$DOTNET_EXE" --version)"
 
-"$DOTNET_EXE" build "$BUILD_PROJECT_FILE" /nodeReuse:false /p:UseSharedCompilation=false -nologo -clp:NoSummary --verbosity quiet
+#if [[ ! -z ${NUKE_ENTERPRISE_PASSWORD+x} && "$NUKE_ENTERPRISE_PASSWORD" != "" ]]; then
+#    "$DOTNET_EXE" nuget add source "$NUKE_ENTERPRISE_SOURCE" --username "$NUKE_ENTERPRISE_USERNAME" --password "$NUKE_ENTERPRISE_PASSWORD" --store-password-in-clear-text
+#fi
+
+"$DOTNET_EXE" build "$BUILD_PROJECT_FILE" /nodeReuse:false /p:UseSharedCompilation=false -nologo -clp:NoSummary
 "$DOTNET_EXE" run --project "$BUILD_PROJECT_FILE" --no-build -- "$@"
