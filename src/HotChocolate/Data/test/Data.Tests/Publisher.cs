07d21d5996a79da522471d9a162736f7c9a126e7
Pascal Senn <senn.pasc@gmail.com>
2021-08-25 00:48:38 +0200

    Fix format

--- src/HotChocolate/Data/test/Data.Tests/Publisher.cs
+++ src/HotChocolate/Data/test/Data.Tests/Publisher.cs
@@ -5,11 +5,15 @@ namespace HotChocolate.Data
     public class Publisher
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public string Address { get; set; }
+
         public string Zipcode { get; set; }
 
         public virtual ICollection<Book> Books { get; set; }
+
         public virtual ICollection<Author> Authors { get; set; }
     }
 }
