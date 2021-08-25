058d4590bf3ca589e2043f811b6e3f9df3b4260f
Michael Staib <michael@chillicream.com>
2021-08-25 13:19:09 +0200

    Added HTTP Benchmarks (#4120)

--- src/HotChocolate/Benchmarks/src/Benchmarks.Execution/HttpQueryBenchmarks.cs
+++ src/HotChocolate/Benchmarks/src/Benchmarks.Execution/HttpQueryBenchmarks.cs
@@ -1,28 +1,15 @@
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using HotChocolate.ConferencePlanner;
-using HotChocolate.Execution;
 
 namespace HotChocolate.Benchmarks
 {
     [RPlotExporter, CategoriesColumn, RankColumn, MeanColumn, MedianColumn, MemoryDiagnoser]
-    public class QueryBenchmarks : BenchmarkBase
+    public class HttpQueryBenchmarks : BenchmarkBase
     {
         [Benchmark]
         public async Task Sessions_TitleAndAbstract_10_Items() =>
-            await BenchmarkAsync(@"
-                {
-                    sessions(first: 10) {
-                        nodes {
-                            title
-                            abstract
-                        }
-                    }
-                }
-            ");
-
-        public async Task Print_Sessions_TitleAndAbstract_10_Items() =>
-            await PrintQueryPlanAsync(@"
+            await BenchmarkServerAsync(@"
                 {
                     sessions(first: 10) {
                         nodes {
@@ -35,22 +22,7 @@ namespace HotChocolate.Benchmarks
 
         [Benchmark]
         public async Task Sessions_TitleAndAbstractAndTrackName_10_Items() =>
-            await BenchmarkAsync(@"
-                {
-                    sessions(first: 10) {
-                        nodes {
-                            title
-                            abstract
-                            track {
-                                name
-                            }
-                        }
-                    }
-                }
-            ");
-
-        public async Task Print_Sessions_TitleAndAbstractAndTrackName_10_Items() =>
-            await PrintQueryPlanAsync(@"
+            await BenchmarkServerAsync(@"
                 {
                     sessions(first: 10) {
                         nodes {
@@ -66,19 +38,13 @@ namespace HotChocolate.Benchmarks
 
         [Benchmark]
         public async Task Sessions_Medium() =>
-            await BenchmarkAsync(SessionMediumQuery);
-
-        public async Task Print_Sessions_Medium() =>
-            await PrintQueryPlanAsync(SessionMediumQuery);
+            await BenchmarkServerAsync(SessionMediumQuery);
 
         public string SessionMediumQuery { get; } = Resources.SessionMediumQuery;
 
         [Benchmark]
         public async Task Sessions_Large() =>
-            await BenchmarkAsync(SessionLargeQuery);
-
-        public async Task Print_Sessions_Large() =>
-            await PrintQueryPlanAsync(SessionLargeQuery);
+            await BenchmarkServerAsync(SessionLargeQuery);
 
         public string SessionLargeQuery { get; } = Resources.SessionLargeQuery;
     }
