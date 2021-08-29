b0bfa7c8f4de102988f2a57f9545a2a9753c7d08
Arif Hanif <arif.hanif3@gmail.com>
2021-08-29 19:17:49 -0400

    products dropdown

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -12,6 +12,19 @@ module.exports = {
         link: `/platform`,
       },
       {
+        name: `Products`,
+        items: [
+          {
+            name: `Banana Cake Pop`,
+            link: `/products/banana-cake-pop`,
+            icon: `
+              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                  <path fill="currentColor" d="M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M15,18V16H6V18H15M18,14V12H6V14H18Z" />
+              </svg>`,
+          },
+        ]
+      },
+      {
         name: `Docs`,
         link: `/docs/hotchocolate/`,
       },
