d9958303a7ef4816b2fd6d050f74ff955d122e8e
Michael Staib <michael@chillicream.com>
2021-09-07 15:14:47 +0200

    Added more tests

--- src/HotChocolate/Core/src/Abstractions/Execution/ResultValue.cs
+++ src/HotChocolate/Core/src/Abstractions/Execution/ResultValue.cs
@@ -1,43 +1,81 @@
 using System;
+using HotChocolate.Properties;
+using HotChocolate.Utilities;
 
 #nullable enable
 
 namespace HotChocolate.Execution
 {
-    public readonly struct ResultValue : IEquatable<ResultValue?>
+    /// <summary>
+    /// Represents an entry in a <see cref="IResultMap"/>
+    /// </summary>
+    public readonly struct ResultValue : IEquatable<ResultValue>
     {
-        public ResultValue(string name, object? value, bool isNullable)
+        /// <summary>
+        /// Creates a new result value.
+        /// </summary>
+        /// <param name="name">The name of the entry.</param>
+        /// <param name="value">The value of the entry.</param>
+        /// <param name="isNullable">
+        /// Specifies if the <paramref name="value"/> is allowed to be null.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// <see cref="name"/> is <c>null</c> or <see cref="String.Empty"/>.
+        /// </exception>
+        public ResultValue(string name, object? value, bool isNullable = true)
         {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException(
+                    AbstractionResources.ResultValue_NameIsNullOrEmpty,
+                    nameof(name));
+            }
+
             Name = name;
             Value = value;
             IsNullable = isNullable;
-            IsInitialized = true;
         }
 
+        /// <summary>
+        /// Gets the name of this <see cref="IResultMap"/> entry.
+        /// </summary>
         public string Name { get; }
 
+        /// <summary>
+        /// Gets the value of this <see cref="IResultMap"/> entry.
+        /// </summary>
         public object? Value { get; }
 
+        /// <summary>
+        /// Specifies if <see cref="Value"/> is allowed to be empty.
+        /// </summary>
         public bool IsNullable { get; }
 
-        public bool IsInitialized { get; }
+        /// <summary>
+        /// Specifies if this entry is fully initialized.
+        /// </summary>
+        public bool IsInitialized => Name is not null!;
 
+        /// <summary>Indicates whether this instance and a specified object are equal.</summary>
+        /// <param name="obj">The object to compare with the current instance.</param>
+        /// <returns>
+        /// <see langword="true" /> if <paramref name="obj" /> and this instance are the same type
+        /// and represent the same value; otherwise, <see langword="false" />.
+        /// </returns>
         public override bool Equals(object? obj)
-        {
-            return obj is ResultValue value &&
-                IsInitialized == value.IsInitialized &&
-                Name == value.Name &&
-                Value == value.Value;
-        }
+            => obj is ResultValue value && Equals(value);
 
-        public bool Equals(ResultValue? other)
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>
+        /// <see langword="true" /> if the current object is equal to the
+        /// <paramref name="other" /> parameter; otherwise, <see langword="false" />.
+        /// </returns>
+        public bool Equals(ResultValue other)
         {
-            if (other is null)
-            {
-                return false;
-            }
-
-            if (IsInitialized != other.Value.IsInitialized)
+            if (IsInitialized != other.IsInitialized)
             {
                 return false;
             }
@@ -47,10 +85,15 @@ namespace HotChocolate.Execution
                 return true;
             }
 
-            return Name == other.Value.Name &&
-                   Value == other.Value.Value;
+            return Name.EqualsOrdinal(other.Name) && Equals(Value, other.Value);
         }
 
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A 32-bit signed integer that is the hash code for this instance.
+        /// </returns>
         public override int GetHashCode()
         {
             unchecked
--- src/HotChocolate/Core/test/Execution.Tests/Processing/ResultMapTests.cs
+++ src/HotChocolate/Core/test/Execution.Tests/Processing/ResultMapTests.cs
@@ -1,7 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Globalization;
-using Microsoft.Extensions.FileProviders.Physical;
 using Xunit;
 
 namespace HotChocolate.Execution.Processing
