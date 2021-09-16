97cdc3435ca3d9129b7e5a4beb498b6eb1b727e1
Michael Staib <michael@chillicream.com>
2021-09-16 11:25:04 +0000

    Updated Banana Cake Pop to preview 14 (#4238)

--- src/HotChocolate/AspNetCore/src/AspNetCore/Resources/static/js/15.097304fd.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonp@banana-cake-pop/main"]=this["webpackJsonp@banana-cake-pop/main"]||[]).push([[15],{625:function(a,n,p){"use strict";p.r(n);var t=p(385);p.d(n,"default",(function(){return t.b}))}}]);
