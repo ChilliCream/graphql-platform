213ca9966afc52edd1b8818b2c7ac1c12c8bfb76
Michael Staib <michael@chillicream.com>
2021-09-24 08:48:12 +0000

    Fixed issue with the UUID parser when using the Utf8Parser (#4264)

--- src/HotChocolate/Core/test/Types.Tests/Types/Scalars/UuidTypeTests.cs
+++ src/HotChocolate/Core/test/Types.Tests/Types/Scalars/UuidTypeTests.cs
@@ -86,7 +86,7 @@ namespace HotChocolate.Types
             var uuidType = new UuidType();
 
             // act
-            object? serializedValue = uuidType.Serialize(null);
+            var serializedValue = uuidType.Serialize(null);
 
             // assert
             Assert.Null(serializedValue);
@@ -113,7 +113,7 @@ namespace HotChocolate.Types
             var uuidType = new UuidType();
 
             // act
-            var success = uuidType.TryDeserialize(null, out object? o);
+            var success = uuidType.TryDeserialize(null, out var o);
 
             // assert
             Assert.True(success);
@@ -128,7 +128,7 @@ namespace HotChocolate.Types
             var guid = Guid.NewGuid();
 
             // act
-            var success = uuidType.TryDeserialize(guid.ToString("N"), out object? o);
+            var success = uuidType.TryDeserialize(guid.ToString("N"), out var o);
 
             // assert
             Assert.True(success);
@@ -143,7 +143,7 @@ namespace HotChocolate.Types
             var guid = Guid.NewGuid();
 
             // act
-            var success = uuidType.TryDeserialize(guid, out object? o);
+            var success = uuidType.TryDeserialize(guid, out var o);
 
             // assert
             Assert.True(success);
@@ -219,7 +219,7 @@ namespace HotChocolate.Types
             NullValueNode literal = NullValueNode.Default;
 
             // act
-            object? value = uuidType.ParseLiteral(literal);
+            var value = uuidType.ParseLiteral(literal);
 
             // assert
             Assert.Null(value);
@@ -283,7 +283,7 @@ namespace HotChocolate.Types
         }
 
         [Fact]
-        public void EnsureDateTypeKindIsCorret()
+        public void EnsureDateTypeKindIsCorrect()
         {
             // arrange
             var type = new UuidType();
@@ -374,12 +374,108 @@ namespace HotChocolate.Types
             void Action() => new UuidType(defaultFormat: 'z');
 
             // assert
-            #if NETCOREAPP2_1
+#if NETCOREAPP2_1
             Assert.Throws<ArgumentException>(Action).Message
                 .MatchSnapshot(new SnapshotNameExtension("NETCOREAPP2_1"));
-            #else
+#else
             Assert.Throws<ArgumentException>(Action).Message.MatchSnapshot();
-            #endif
+#endif
+        }
+
+        [InlineData(false)]
+        [InlineData(true)]
+        [Theory]
+        public void Parse_Guid_String_With_Appended_String(bool enforceFormat)
+        {
+            // arrange
+            var input = new StringValueNode("fbdef721-93c5-4267-8f92-ca27b60aa51f-foobar");
+            var uuidType = new UuidType(defaultFormat: 'D', enforceFormat: enforceFormat);
+
+            // act
+            void Fail() => uuidType.ParseLiteral(input);
+
+            // assert
+            Assert.Throws<SerializationException>(Fail);
+        }
+
+        [InlineData(false)]
+        [InlineData(true)]
+        [Theory]
+        public void Parse_Guid_Valid_Input(bool enforceFormat)
+        {
+            // arrange
+            var input = new StringValueNode("fbdef721-93c5-4267-8f92-ca27b60aa51f");
+            var uuidType = new UuidType(defaultFormat: 'D', enforceFormat: enforceFormat);
+
+            // act
+            var guid = (Guid)uuidType.ParseLiteral(input)!;
+
+            // assert
+            Assert.Equal(input.Value, guid.ToString("D"));
+        }
+
+        [InlineData(false)]
+        [InlineData(true)]
+        [Theory]
+        public void Deserialize_Guid_String_With_Appended_String(bool enforceFormat)
+        {
+            // arrange
+            var input = "fbdef721-93c5-4267-8f92-ca27b60aa51f-foobar";
+            var uuidType = new UuidType(defaultFormat: 'D', enforceFormat: enforceFormat);
+
+            // act
+            void Fail() => uuidType.Deserialize(input);
+
+            // assert
+            Assert.Throws<SerializationException>(Fail);
+        }
+
+        [InlineData(false)]
+        [InlineData(true)]
+        [Theory]
+        public void Deserialize_Guid_Valid_Format(bool enforceFormat)
+        {
+            // arrange
+            var input = "fbdef721-93c5-4267-8f92-ca27b60aa51f";
+            var uuidType = new UuidType(defaultFormat: 'D', enforceFormat: enforceFormat);
+
+            // act
+            var guid = (Guid)uuidType.Deserialize(input)!;
+
+            // assert
+            Assert.Equal(input, guid.ToString("D"));
+        }
+
+        [InlineData(false)]
+        [InlineData(true)]
+        [Theory]
+        public void IsInstanceOf_Guid_String_With_Appended_String(bool enforceFormat)
+        {
+            // arrange
+            var input = new StringValueNode("fbdef721-93c5-4267-8f92-ca27b60aa51f-foobar");
+            var uuidType = new UuidType(defaultFormat: 'D', enforceFormat: enforceFormat);
+
+            // act
+            var result = uuidType.IsInstanceOfType(input);
+
+            // assert
+            Assert.False(result);
+        }
+
+        [InlineData(false)]
+        [InlineData(true)]
+        [Theory]
+        public void IsInstanceOf_Guid_Valid_Format(bool enforceFormat)
+        {
+            // arrange
+            var input = new StringValueNode("fbdef721-93c5-4267-8f92-ca27b60aa51f");
+            var uuidType = new UuidType(defaultFormat: 'D', enforceFormat: enforceFormat);
+
+            // act
+            var result = uuidType.IsInstanceOfType(input);
+
+            // assert
+            Assert.True(result);
         }
     }
 }
