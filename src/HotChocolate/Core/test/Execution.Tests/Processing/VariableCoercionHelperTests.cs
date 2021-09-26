539a4e0df883ff80cb47693514071ede5d998a8e
Michael Staib <michael@chillicream.com>
2021-09-26 09:40:31 +0200

    fixed non null test for lists

--- src/HotChocolate/Core/test/Execution.Tests/Processing/VariableCoercionHelperTests.cs
+++ src/HotChocolate/Core/test/Execution.Tests/Processing/VariableCoercionHelperTests.cs
@@ -676,7 +676,7 @@ namespace HotChocolate.Execution.Processing
                     }
 
                     input FooInput {
-                        enum: TestEnum
+                        enum: TestEnum!
                     }
 
                     enum TestEnum {
