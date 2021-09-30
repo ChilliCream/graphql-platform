0fb76a1462dd44fd3d55cd726d0b61d1706cfd1c
Michael Staib <michael@chillicream.com>
2021-09-30 11:34:22 +0200

    Started work on the schema building process

---
+++ src/HotChocolate/Stitching2/src/Stitching/SchemaBuilding/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using HotChocolate.Language;
+
+namespace HotChocolate.Stitching.SchemaBuilding
+{
+    public class Class1
+    {
+    }
+
+    public class NodeIdentifier
+    {
+
+    }
+
+    public class TypeIdentifier : NodeIdentifier
+    {
+
+    }
+
+    public class FieldIdentified : NodeIdentifier
+    {
+            
+    }
+
+
+    public class SchemaConfiguration
+    {
+        public HashSet<OperationType> IncludedOperations { get; } = new();
+
+        public HashSet<string> IgnoreDirectiveDeclarations { get; } = new();
+    }
+}
