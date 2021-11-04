387493b551c3e019234438136b191b9516dbdff2
PascalSenn <senn.pasc@gmail.com>
2021-11-04 21:46:32 +0100

    Added better error messages to data (#4370)

--- src/HotChocolate/Data/src/Data/Projections/Expressions/QueryableProjectionProvider.cs
+++ src/HotChocolate/Data/src/Data/Projections/Expressions/QueryableProjectionProvider.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using HotChocolate.Resolvers;
+using HotChocolate.Types;
 
 namespace HotChocolate.Data.Projections.Expressions
 {
