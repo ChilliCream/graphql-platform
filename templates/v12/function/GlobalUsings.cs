e45702cb55676161a5addb3305ba6d5ad0414727
Michael Staib <michael@chillicream.com>
2021-11-04 18:39:25 +0100

    Fixed Azure Functions Template

--- templates/v12/function/GlobalUsings.cs
+++ templates/v12/function/GlobalUsings.cs
@@ -3,4 +3,4 @@ global using HotChocolate.Types;
 global using HotChocolate.AzureFunctions;
 global using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 global using Microsoft.Extensions.DependencyInjection;
-global using HotChocolate.Template.Server;
+global using HotChocolate.Template.AzureFunctions;
