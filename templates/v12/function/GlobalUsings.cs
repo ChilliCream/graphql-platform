246ee9e0b4190bfc42663d674f000aed4f443be2
Michael Staib <michael@chillicream.com>
2021-11-10 17:57:54 +0100

    Fixed Reworked global Usings in Templates

--- templates/v12/function/GlobalUsings.cs
+++ templates/v12/function/GlobalUsings.cs
@@ -1,6 +1,1 @@
-global using HotChocolate;
-global using HotChocolate.Types;
-global using HotChocolate.AzureFunctions;
-global using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-global using Microsoft.Extensions.DependencyInjection;
 global using HotChocolate.Template.AzureFunctions;
