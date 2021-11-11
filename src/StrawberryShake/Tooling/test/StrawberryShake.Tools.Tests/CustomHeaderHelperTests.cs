b5c14b6ac0da4cb6822a2841fa7083562671bbdb
Richard van Heest <rvanheest@users.noreply.github.com>
2021-11-11 23:04:07 +0100

    only split on first occurrence of '='

---
+++ src/StrawberryShake/Tooling/test/StrawberryShake.Tools.Tests/CustomHeaderHelperTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace StrawberryShake.Tools.Tests
+{
+    public class CustomHeaderHelperTests
+    {
+        [Fact]
+        public void ParseHeadersArgument_Test()
+        {
+            // arrange
+            var arguments = new List<string?>
+            {
+                "myheader=myvalue",
+                "header-name=a-value-one,a-value-two",
+                "not-a-key-value-header",
+                null,
+                "headername=mybase64=.encodedvalue=.",
+            };
+
+            // act
+            Dictionary<string, IEnumerable<string>> headers = CustomHeaderHelper.ParseHeadersArgument(arguments);
+
+            // assert
+            Assert.Equal(3, headers.Count);
+            Assert.Equal(new [] {"myvalue"}, headers["myheader"]);
+            Assert.Equal(new [] {"a-value-one", "a-value-two"}, headers["header-name"]);
+            Assert.False(headers.ContainsKey("not-a-key-value-header"));
+            Assert.Equal(new [] {"mybase64=.encodedvalue=."}, headers["headername"]);
+        }
+    }
+}
