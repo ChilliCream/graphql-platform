f9efb55d4e81faf9b684c646c8bb3e51c18c6664
Michael Staib <michael@chillicream.com>
2021-11-25 15:48:49 +0100

    Reworked Build

--- build.sh
+++ build.sh
@@ -38,28 +38,13 @@ else
     curl -Lsfo "$DOTNET_INSTALL_FILE" "$DOTNET_INSTALL_URL"
     chmod +x "$DOTNET_INSTALL_FILE"
 
-    DOTNET_DIRECTORY="$TEMP_DIRECTORY/dotnet-unix"
-
     # install older frameworks for tests
-    "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --version "2.1.816" --no-path
-    "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --version "3.1.409" --no-path
-    "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --version "5.0.401" --no-path
+    "$DOTNET_INSTALL_FILE" --version "2.1.816" --no-path
+    "$DOTNET_INSTALL_FILE" --version "3.1.409" --no-path
+    "$DOTNET_INSTALL_FILE" --version "5.0.401" --no-path
+    "$DOTNET_INSTALL_FILE" --version "5.0.100" --no-path
 
-    # If global.json exists, load expected version
-    if [[ -f "$DOTNET_GLOBAL_FILE" ]]; then
-        DOTNET_VERSION=$(FirstJsonValue "version" "$(cat "$DOTNET_GLOBAL_FILE")")
-        if [[ "$DOTNET_VERSION" == ""  ]]; then
-            unset DOTNET_VERSION
-        fi
-    fi
-
-    # Install by channel or version
-    if [[ -z ${DOTNET_VERSION+x} ]]; then
-        "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --channel "$DOTNET_CHANNEL" --no-path
-    else
-        "$DOTNET_INSTALL_FILE" --install-dir "$DOTNET_DIRECTORY" --version "$DOTNET_VERSION" --no-path
-    fi
-    export DOTNET_EXE="$DOTNET_DIRECTORY/dotnet"
+    export DOTNET_EXE="$(command -v dotnet)"
 fi
 
 echo "Microsoft (R) .NET Core SDK version $("$DOTNET_EXE" --version)"
