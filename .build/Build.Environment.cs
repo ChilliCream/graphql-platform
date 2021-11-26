b0f3cf3f115ff7cad26d240ea2857b7956dec2be
Michael Staib <michael@chillicream.com>
2021-11-26 14:58:40 +0100

    Process more tests in parallel

--- .build/Build.Environment.cs
+++ .build/Build.Environment.cs
@@ -7,7 +7,7 @@ partial class Build
     const string Net50 = "net5.0";
     const string Net60 = "net6.0";
 
-    readonly int DegreeOfParallelism = System.Environment.ProcessorCount * 2;
+    readonly int DegreeOfParallelism = System.Environment.ProcessorCount * 3;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath AllSolutionFile => SourceDirectory / "All.sln";
