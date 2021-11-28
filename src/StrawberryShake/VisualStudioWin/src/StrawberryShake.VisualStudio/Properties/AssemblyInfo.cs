398ef41ef22e5defc8ede7e1dcd5785520daed8d
Michael Staib <michael@chillicream.com>
2021-11-28 14:21:53 +0100

    Updated project to v2022

--- src/StrawberryShake/VisualStudioWin/src/StrawberryShake.VisualStudio/Properties/AssemblyInfo.cs
+++ src/StrawberryShake/VisualStudioWin/src/StrawberryShake.VisualStudio/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("12.4.0.*")]
-[assembly: AssemblyFileVersion("12.4.0.0")]
+[assembly: AssemblyVersion("12.3.2.*")]
+[assembly: AssemblyFileVersion("12.3.2.0")]
