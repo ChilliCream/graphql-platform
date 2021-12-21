3d87c3e1825e7daa3668f20df5c74f441f426281
Michael Staib <michael@chillicream.com>
2021-12-21 21:40:08 +0100

    Filter

--- .build/Build.Tests.cs
+++ .build/Build.Tests.cs
@@ -137,7 +137,7 @@ partial class Build
             .EnableCollectCoverage()
             .SetCoverletOutputFormat(CoverletOutputFormat.opencover)
             .SetProcessArgumentConfigurator(a => a.Add("--collect:\"XPlat Code Coverage\""))
-            .SetExcludeByFile("*.Generated.cs,**/*.Test/*.cs,**/*.Tests/*.cs")
+            .SetExcludeByFile("*.Generated.cs")
             .SetFramework(Net60)
             .CombineWith(projects, (_, v) => _
                 .SetProjectFile(v)
