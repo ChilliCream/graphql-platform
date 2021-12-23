39ba80328732b4714c69f8f6308e7b5a79314921
Michael Staib <michael@chillicream.com>
2021-12-23 21:41:18 +0100

    cleanup

--- src/HotChocolate/Core/test/Validation.Tests/Types/CatOrDogType.cs
+++ src/HotChocolate/Core/test/Validation.Tests/Types/CatOrDogType.cs
@@ -12,39 +12,4 @@ namespace HotChocolate.Validation.Types
             descriptor.Type<DogType>();
         }
     }
-
-    public class PetInputType : InputObjectType
-    {
-        protected override void Configure(IInputObjectTypeDescriptor descriptor)
-        {
-            descriptor.Name("PetInput").OneOf();
-
-            descriptor.Field("cat").Type<CatInputType>();
-            descriptor.Field("dog").Type<DogInputType>();
-        }
-    }
-
-    public class CatInputType : InputObjectType
-    {
-        protected override void Configure(IInputObjectTypeDescriptor descriptor)
-        {
-            descriptor.Name("CatInput");
-
-            descriptor.Field("name").Type("String!");
-            descriptor.Field("nickname").Type("String");
-            descriptor.Field("meowVolume").Type("Int");
-        }
-    }
-
-    public class DogInputType : InputObjectType
-    {
-        protected override void Configure(IInputObjectTypeDescriptor descriptor)
-        {
-            descriptor.Name("DogInput");
-
-            descriptor.Field("name").Type("String!");
-            descriptor.Field("nickname").Type("String");
-            descriptor.Field("barkVolume").Type("Int");
-        }
-    }
 }
