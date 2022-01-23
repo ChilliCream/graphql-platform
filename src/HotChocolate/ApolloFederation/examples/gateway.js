5658ad65dad5b7dee1db56690dc6bf5de69dba1f
Michael Staib <michael@chillicream.com>
2022-01-24 00:56:17 +0100

    Cleanup

--- src/HotChocolate/ApolloFederation/examples/gateway.js
+++ src/HotChocolate/ApolloFederation/examples/gateway.js
@@ -10,7 +10,7 @@ const gateway = new ApolloGateway({
   serviceList: [
     { name: "accounts", url: "http://localhost:5001/graphql" },
     { name: "reviews", url: "http://localhost:5002/graphql" },
-    { name: "products", url: "http://localhost:5003/graphql" }
+    { name: "products", url: "http://localhost:5003/graphql" },
     { name: "inventory", url: "http://localhost:5004/graphql" }
   ],
 
