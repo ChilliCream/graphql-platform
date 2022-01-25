9db4e4a330bd6ea60e135e8362e30f208d575b4b
Michael Staib <michael@chillicream.com>
2022-01-25 22:55:02 +0100

    Added descriptions

--- src/HotChocolate/ApolloFederation/src/ApolloFederation/ReferenceResolvers/IEntityResolverDescriptor.cs
+++ src/HotChocolate/ApolloFederation/src/ApolloFederation/ReferenceResolvers/IEntityResolverDescriptor.cs
@@ -6,35 +6,146 @@ using HotChocolate.Types;
 
 namespace HotChocolate.ApolloFederation;
 
+/// <summary>
+/// The entity descriptor allows to specify a reference resolver.
+/// </summary>
 public interface IEntityResolverDescriptor
 {
+    /// <summary>
+    /// Resolve an entity from its representation.
+    /// </summary>
+    /// <param name="fieldResolver">
+    /// The resolver.
+    /// </param>
+    /// <returns>
+    /// Returns the descriptor for configuration chaining.
+    /// </returns>
     IObjectTypeDescriptor ResolveReference(
         FieldResolverDelegate fieldResolver);
 
+    /// <summary>
+    /// Resolve an entity from its representation.
+    /// </summary>
+    /// <param name="method">
+    /// The reference resolver selector.
+    /// </param>
+    /// <typeparam name="TResolver">
+    /// The type on which the reference resolver is located.
+    /// </typeparam>
+    /// <returns>
+    /// Returns the descriptor for configuration chaining.
+    /// </returns>
     IObjectTypeDescriptor ResolveReferenceWith<TResolver>(
         Expression<Func<TResolver, object>> method);
 
+    /// <summary>
+    /// Resolve an entity from its representation.
+    /// </summary>
+    /// <typeparam name="TResolver">
+    /// The type on which the reference resolver is located.
+    /// </typeparam>
+    /// <returns>
+    /// Returns the descriptor for configuration chaining.
+    /// </returns>
     IObjectTypeDescriptor ResolveReferenceWith<TResolver>();
 
+    /// <summary>
+    /// Resolve an entity from its representation.
+    /// </summary>
+    /// <param name="method">
+    /// The reference resolver.
+    /// </param>
+    /// <returns>
+    /// Returns the descriptor for configuration chaining.
+    /// </returns>
     IObjectTypeDescriptor ResolveReferenceWith(MethodInfo method);
 
+    /// <summary>
+    /// Resolve an entity from its representation.
+    /// </summary>
+    /// <param name="type">
+    /// The type on which the reference resolver is located.
+    /// </param>
+    /// <returns>
+    /// Returns the descriptor for configuration chaining.
+    /// </returns>
     IObjectTypeDescriptor ResolveReferenceWith(Type type);
 }
 
+/// <summary>
+/// The entity descriptor allows to specify a reference resolver.
+/// </summary>
 public interface IEntityResolverDescriptor<TEntity>
 {
+    /// <summary>
+    /// Resolve an entity from its representation.
+    /// </summary>
+    /// <param name="fieldResolver">
+    /// The resolver.
+    /// </param>
+    /// <returns>
+    /// Returns the descriptor for configuration chaining.
+    /// </returns>
     IObjectTypeDescriptor ResolveReference(
         FieldResolverDelegate fieldResolver);
 
+    /// <summary>
+    /// Resolve an entity from its representation.
+    /// </summary>
+    /// <param name="method">
+    /// The reference resolver selector.
+    /// </param>
+    /// <returns>
+    /// Returns the descriptor for configuration chaining.
+    /// </returns>
     IObjectTypeDescriptor ResolveReference(
         Expression<Func<TEntity, object>> method);
 
+    /// <summary>
+    /// Resolve an entity from its representation.
+    /// </summary>
+    /// <param name="method">
+    /// The reference resolver selector.
+    /// </param>
+    /// <typeparam name="TResolver">
+    /// The type on which the reference resolver is located.
+    /// </typeparam>
+    /// <returns>
+    /// Returns the descriptor for configuration chaining.
+    /// </returns>
     IObjectTypeDescriptor ResolveReferenceWith<TResolver>(
         Expression<Func<TResolver, object>> method);
 
+    /// <summary>
+    /// Resolve an entity from its representation.
+    /// </summary>
+    /// <typeparam name="TResolver">
+    /// The type on which the reference resolver is located.
+    /// </typeparam>
+    /// <returns>
+    /// Returns the descriptor for configuration chaining.
+    /// </returns>
     IObjectTypeDescriptor ResolveReferenceWith<TResolver>();
 
+    /// <summary>
+    /// Resolve an entity from its representation.
+    /// </summary>
+    /// <param name="method">
+    /// The reference resolver.
+    /// </param>
+    /// <returns>
+    /// Returns the descriptor for configuration chaining.
+    /// </returns>
     IObjectTypeDescriptor ResolveReferenceWith(MethodInfo method);
 
+    /// <summary>
+    /// Resolve an entity from its representation.
+    /// </summary>
+    /// <param name="type">
+    /// The type on which the reference resolver is located.
+    /// </param>
+    /// <returns>
+    /// Returns the descriptor for configuration chaining.
+    /// </returns>
     IObjectTypeDescriptor ResolveReferenceWith(Type type);
 }
