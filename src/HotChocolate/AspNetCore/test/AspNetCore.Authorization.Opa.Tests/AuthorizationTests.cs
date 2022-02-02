e97e2b78c2fff54f7ab3ca2a3e6611db521d8670
queil <queil@users.noreply.github.com>
2022-02-02 19:43:02 +0000

    Added more tests

--- src/HotChocolate/AspNetCore/test/AspNetCore.Authorization.Opa.Tests/AuthorizationTests.cs
+++ src/HotChocolate/AspNetCore/test/AspNetCore.Authorization.Opa.Tests/AuthorizationTests.cs
@@ -59,6 +59,30 @@ public class AuthorizationTests : ServerTestBase, IAsyncLifetime
     [Theory]
     [ClassData(typeof(AuthorizationTestData))]
     [ClassData(typeof(AuthorizationAttributeTestData))]
+    public async Task Policy_NotFound(Action<IRequestExecutorBuilder> configure)
+    {
+        // arrange
+        TestServer server = CreateTestServer(
+            builder =>
+            {
+                configure(builder);
+                builder.Services.AddAuthorization();
+
+            },
+            SetUpHttpContext);
+
+        // act
+        ClientQueryResult result =
+            await server.PostAsync(new ClientQueryRequest { Query = "{ age }" });
+
+        // assert
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        result.MatchSnapshot();
+    }
+
+    [Theory]
+    [ClassData(typeof(AuthorizationTestData))]
+    [ClassData(typeof(AuthorizationAttributeTestData))]
     public async Task Policy_NotAuthorized(Action<IRequestExecutorBuilder> configure)
     {
         // arrange
@@ -72,7 +96,44 @@ public class AuthorizationTests : ServerTestBase, IAsyncLifetime
             SetUpHttpContext + (Action<HttpContext>)(c =>
             {
                 c.Request.Headers["Authorization"] =
-                    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+                    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lI" +
+                    "iwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+            }));
+
+        var hasAgeDefinedPolicy = await File.ReadAllTextAsync("policies/has_age_defined.rego");
+        using var client = new HttpClient { BaseAddress = new Uri("http://127.0.0.1:8181") };
+
+        HttpResponseMessage putPolicyResponse = await client.PutAsync("/v1/policies/has_age_defined", new StringContent(hasAgeDefinedPolicy));
+        putPolicyResponse.EnsureSuccessStatusCode();
+
+        // act
+        ClientQueryResult result =
+            await server.PostAsync(new ClientQueryRequest { Query = "{ age }" });
+
+        // assert
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        result.MatchSnapshot();
+    }
+
+    [Theory]
+    [ClassData(typeof(AuthorizationTestData))]
+    [ClassData(typeof(AuthorizationAttributeTestData))]
+    public async Task Policy_Authorized(Action<IRequestExecutorBuilder> configure)
+    {
+        // arrange
+        TestServer server = CreateTestServer(
+            builder =>
+            {
+                configure(builder);
+                builder.Services.AddAuthorization();
+
+            },
+            SetUpHttpContext + (Action<HttpContext>)(c =>
+            {
+                c.Request.Headers["Authorization"] =
+                    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lI" +
+                    "iwiaWF0IjoxNTE2MjM5MDIyLCJiaXJ0aGRhdGUiOiIxNy0xMS0yMDAwIn0.p88IUnrabPMh6LVi4DIYsDeZozjfj4Ofwg" +
+                    "jXBglnxac";
             }));
 
         var hasAgeDefinedPolicy = await File.ReadAllTextAsync("policies/has_age_defined.rego");
