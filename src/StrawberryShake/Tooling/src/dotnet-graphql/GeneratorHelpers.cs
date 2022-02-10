8d179c3941be9b6f42047c0e2411057068215b5c
Michael Staib <michael@chillicream.com>
2022-02-11 00:29:33 +0100

    cleanup

--- src/StrawberryShake/Tooling/src/dotnet-graphql/GeneratorHelpers.cs
+++ src/StrawberryShake/Tooling/src/dotnet-graphql/GeneratorHelpers.cs
@@ -1,19 +1,19 @@
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.IO.SearchOption;
+
 namespace StrawberryShake.Tools;
 
 public static class GeneratorHelpers
 {
     public static string GetCodeGenServerLocation()
-    {
-        var s = Path.Combine(
-            Path.GetDirectoryName(typeof(GeneratorHelpers).Assembly.Location)!,
+        => Combine(
+            GetDirectoryName(typeof(GeneratorHelpers).Assembly.Location)!,
             "..", "..", "..", "build", "gen", "BerryCodeGen.dll");
-        Console.WriteLine(s);
-        return s;
-    }
 
     public static string[] GetConfigFiles(string path)
-        => Directory.GetFiles(path, ".graphqlrc.json", SearchOption.AllDirectories);
+        => GetFiles(path, ".graphqlrc.json", AllDirectories);
 
     public static string[] GetDocuments(string path)
-        => Directory.GetFiles(path, "*.graphql", SearchOption.AllDirectories);
+        => GetFiles(path, "*.graphql", AllDirectories);
 }
