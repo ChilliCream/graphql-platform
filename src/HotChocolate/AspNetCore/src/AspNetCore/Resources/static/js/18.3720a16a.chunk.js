fbf152796d26ce87268d2d0867f5727d8673b496
Michael Staib <michael@chillicream.com>
2022-02-23 20:51:56 +0100

    Added BCP preview 24 (#4794)

---
+++ src/HotChocolate/AspNetCore/src/AspNetCore/Resources/static/js/18.3720a16a.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonp@banana-cake-pop/main"]=this["webpackJsonp@banana-cake-pop/main"]||[]).push([[18],{1129:function(a,n,p){"use strict";p.r(n);var t=p(103);p.d(n,"default",(function(){return t.a}))}}]);
