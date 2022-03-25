1d50e1997e615e8f03f93b31df3f3f1d90f69b1e
Michael Staib <michael@chillicream.com>
2022-03-25 16:26:13 +0100

    Added tests.

--- src/HotChocolate/Stitching/test/Stitching.Types.Tests/DefaultObjectTypeDefinitionMergerTests.cs
+++ src/HotChocolate/Stitching/test/Stitching.Types.Tests/DefaultObjectTypeDefinitionMergerTests.cs
@@ -35,4 +35,42 @@ public class DefaultObjectTypeDefinitionMergerTests
             t => Assert.Equal("Abc", Assert.IsType<SourceBinding>(t).Source));
         b.Definition.ToString().MatchSnapshot();
     }
+
+    [Fact]
+    public void Same_Field_In_Each_Schema_Version()
+    {
+        // arrange
+        ObjectTypeDefinition.TryParse(
+            @"type Foo {
+                a: String
+            }",
+            "Abc",
+            out ObjectTypeDefinition? a);
+
+        ObjectTypeDefinition.TryParse(
+            @"type Foo {
+                a: String
+            }",
+            "Def",
+            out ObjectTypeDefinition? b);
+
+        // act
+        var merger = new DefaultObjectTypeDefinitionMerger();
+        merger.MergeInto(a!, b!);
+
+        // assert
+        Assert.Collection(
+            b!.Bindings,
+            t =>
+            {
+                Assert.Equal("Def", Assert.IsType<SourceBinding>(t).Source);
+                Assert.Equal("Foo.a", Assert.IsType<SourceBinding>(t).Target.ToString());
+            },
+            t =>
+            {
+                Assert.Equal("Abc", Assert.IsType<SourceBinding>(t).Source);
+                Assert.Equal("Foo.a", Assert.IsType<SourceBinding>(t).Target.ToString());
+            });
+        b.Definition.ToString().MatchSnapshot();
+    }
 }
