12025e96ad90512aa4b42b7aed93abe3d06bf018
Michael Staib <michael@chillicream.com>
2022-03-25 14:35:20 +0100

    wip

---
+++ src/HotChocolate/Stitching/test/Stitching.Types.Tests/DefaultObjectTypeDefinitionMergerTests.cs
@@ -0,0 +1,38 @@
+using HotChocolate.Stitching.Types.Bindings;
+using Snapshooter.Xunit;
+using Xunit;
+
+namespace HotChocolate.Stitching.Types;
+
+public class DefaultObjectTypeDefinitionMergerTests
+{
+    [Fact]
+    public void Merge_Simple_Type()
+    {
+        // arrange
+        ObjectTypeDefinition.TryParse(
+            @"type Foo {
+                a: String
+            }",
+            "Abc",
+            out ObjectTypeDefinition? a);
+
+        ObjectTypeDefinition.TryParse(
+            @"type Foo {
+                b: String
+            }",
+            "Def",
+            out ObjectTypeDefinition? b);
+
+        // act
+        var merger = new DefaultObjectTypeDefinitionMerger();
+        merger.MergeInto(a!, b!);
+
+        // assert
+        Assert.Collection(
+            b!.Bindings,
+            t => Assert.Equal("Def", Assert.IsType<SourceBinding>(t).Source),
+            t => Assert.Equal("Abc", Assert.IsType<SourceBinding>(t).Source));
+        b!.Definition.MatchSnapshot();
+    }
+}
