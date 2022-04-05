137f2e9f01777b3b770d16c5446d205857f4790c
Vadim Hatsura <vadim.hatsura@gmail.com>
2022-04-05 22:40:49 +0200

    rework ObjectFieldNode
    add tests

---
+++ src/HotChocolate/Language/test/Language.SyntaxTree.Tests/ObjectFieldNodeTests.cs
@@ -0,0 +1,133 @@
+using Xunit;
+
+namespace HotChocolate.Language.SyntaxTree;
+
+public class ObjectFieldNodeTests
+{
+    [Fact]
+    public void EqualsObjectFieldNode_SameLocation()
+    {
+        // arrange
+        var a = new ObjectFieldNode(TestLocations.Location1, new NameNode("aa"), new FloatValueNode(1.2));
+        var b = new ObjectFieldNode(TestLocations.Location1, new NameNode("aa"), new FloatValueNode(1.2));
+        var c = new ObjectFieldNode(TestLocations.Location1, new NameNode("aa"), new FloatValueNode(2.1));
+        var d = new ObjectFieldNode(TestLocations.Location1, new NameNode("dd"), new FloatValueNode(1.2));
+
+        // act
+        var aaResult = a.Equals(a);
+        var abResult = a.Equals(b);
+        var acResult = a.Equals(c);
+        var adResult = a.Equals(d);
+        var aNullResult = a.Equals(default);
+
+        // assert
+        Assert.True(aaResult);
+        Assert.True(abResult);
+        Assert.False(acResult);
+        Assert.False(adResult);
+        Assert.False(aNullResult);
+    }
+
+    [Fact]
+    public void EqualsObjectFieldNode_DifferentLocations()
+    {
+        // arrange
+        var a = new ObjectFieldNode(TestLocations.Location1, new NameNode("aa"), new FloatValueNode(1.2));
+        var b = new ObjectFieldNode(TestLocations.Location2, new NameNode("aa"), new FloatValueNode(1.2));
+        var c = new ObjectFieldNode(TestLocations.Location3, new NameNode("aa"), new FloatValueNode(2.1));
+        var d = new ObjectFieldNode(TestLocations.Location3, new NameNode("dd"), new FloatValueNode(1.2));
+
+        // act
+        var aaResult = a.Equals(a);
+        var abResult = a.Equals(b);
+        var acResult = a.Equals(c);
+        var adResult = a.Equals(d);
+        var aNullResult = a.Equals(default);
+
+        // assert
+        Assert.True(aaResult);
+        Assert.True(abResult);
+        Assert.False(acResult);
+        Assert.False(adResult);
+        Assert.False(aNullResult);
+    }
+
+    [Fact]
+    public void CompareGetHashCode_WithLocation()
+    {
+        // arrange
+        var a = new ObjectFieldNode(TestLocations.Location1, new NameNode("aa"), new FloatValueNode(1.2));
+        var b = new ObjectFieldNode(TestLocations.Location2, new NameNode("aa"), new FloatValueNode(1.2));
+        var c = new ObjectFieldNode(TestLocations.Location1, new NameNode("aa"), new FloatValueNode(2.1));
+        var d = new ObjectFieldNode(TestLocations.Location2, new NameNode("dd"), new FloatValueNode(1.2));
+        var e = new ObjectFieldNode(TestLocations.Location3, new NameNode("dd"), new FloatValueNode(1.2));
+
+        // act
+        var aHash = a.GetHashCode();
+        var bHash = b.GetHashCode();
+        var cHash = c.GetHashCode();
+        var dHash = d.GetHashCode();
+        var eHash = e.GetHashCode();
+
+        // assert
+        Assert.Equal(aHash, bHash);
+        Assert.NotEqual(aHash, cHash);
+        Assert.NotEqual(aHash, dHash);
+
+        Assert.NotEqual(cHash, dHash);
+        Assert.Equal(dHash, eHash);
+    }
+
+
+    [Fact]
+    public void Create_Float()
+    {
+        // arrange
+        // act
+        var obj = new ObjectFieldNode("abc", 1.2);
+
+        // assert
+        Assert.Equal("abc", obj.Name.Value);
+        Assert.Equal("1.2", Assert.IsType<FloatValueNode>(obj.Value).Value);
+    }
+
+    [Fact]
+    public void Create_Int()
+    {
+        // arrange
+        // act
+        var obj = new ObjectFieldNode("abc", 1);
+
+        // assert
+        Assert.Equal("abc", obj.Name.Value);
+        Assert.Equal("1", Assert.IsType<IntValueNode>(obj.Value).Value);
+    }
+
+    [InlineData(true)]
+    [InlineData(false)]
+    [Theory]
+    public void Create_Bool(bool value)
+    {
+        // arrange
+        // act
+        var obj = new ObjectFieldNode("abc", value);
+
+        // assert
+        Assert.Equal("abc", obj.Name.Value);
+        Assert.Equal(value, Assert.IsType<BooleanValueNode>(obj.Value).Value);
+    }
+
+    [Fact]
+    public void Create_String()
+    {
+        // arrange
+        // act
+        var obj = new ObjectFieldNode("abc", "def");
+
+        // assert
+        Assert.Equal("abc", obj.Name.Value);
+        Assert.Equal("def", Assert.IsType<StringValueNode>(obj.Value).Value);
+    }
+}
+
+
