7ada9de00f9117e0cc7dcdc8d0e6e43c0fe1b666
Michael Staib <michael@chillicream.com>
2022-04-07 23:04:18 +0200

    cleanup

--- src/HotChocolate/Language/src/Language.SyntaxTree/OperationTypeDefinitionNode.cs
+++ src/HotChocolate/Language/src/Language.SyntaxTree/OperationTypeDefinitionNode.cs
@@ -18,14 +18,17 @@ public sealed class OperationTypeDefinitionNode
         Type = type ?? throw new ArgumentNullException(nameof(type));
     }
 
+    /// <inheritdoc />
     public SyntaxKind Kind => SyntaxKind.OperationTypeDefinition;
 
+    /// <inheritdoc />
     public Location? Location { get; }
 
     public OperationType Operation { get; }
 
     public NamedTypeNode Type { get; }
 
+    /// <inheritdoc />
     public IEnumerable<ISyntaxNode> GetNodes()
     {
         yield return Type;
@@ -52,15 +55,38 @@ public sealed class OperationTypeDefinitionNode
     /// </returns>
     public string ToString(bool indented) => SyntaxPrinter.Print(this, indented);
 
-    public OperationTypeDefinitionNode WithLocation(Location? location) =>
-        new(location, Operation, Type);
+    /// <summary>
+    /// Creates a new node from the current instance and replaces the
+    /// <see cref="Location" /> with <paramref name="location" />.
+    /// </summary>
+    /// <param name="location">
+    /// The location that shall be used to replace the current location.
+    /// </param>
+    /// <returns>
+    /// Returns the new node with the new <paramref name="location" />.
+    /// </returns>
+    public OperationTypeDefinitionNode WithLocation(Location? location)
+        => new(location, Operation, Type);
 
-    public OperationTypeDefinitionNode WithOperation(OperationType operation) =>
-        new(Location, operation, Type);
+    public OperationTypeDefinitionNode WithOperation(OperationType operation)
+        => new(Location, operation, Type);
 
-    public OperationTypeDefinitionNode WithType(NamedTypeNode type) =>
-        new(Location, Operation, type);
+    public OperationTypeDefinitionNode WithType(NamedTypeNode type)
+        => new(Location, Operation, type);
 
+    /// <summary>
+    /// Determines whether the specified <see cref="OperationTypeDefinitionNode"/>
+    /// is equal to the current <see cref="OperationTypeDefinitionNode"/>.
+    /// </summary>
+    /// <param name="other">
+    /// The <see cref="OperationTypeDefinitionNode"/> to compare with the current
+    /// <see cref="OperationTypeDefinitionNode"/>.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if the specified <see cref="OperationTypeDefinitionNode"/> is equal
+    /// to the current <see cref="OperationTypeDefinitionNode"/>;
+    /// otherwise, <c>false</c>.
+    /// </returns>
     public bool Equals(OperationTypeDefinitionNode? other)
     {
         if (ReferenceEquals(null, other))
@@ -76,9 +102,56 @@ public sealed class OperationTypeDefinitionNode
         return Operation == other.Operation && Type.Equals(other.Type);
     }
 
-    public override bool Equals(object? obj) => ReferenceEquals(this, obj) ||
-                                                obj is OperationTypeDefinitionNode other &&
-                                                Equals(other);
+    /// <summary>
+    /// Determines whether the specified <see cref="object"/> is equal to
+    /// the current <see cref="OperationTypeDefinitionNode"/>.
+    /// </summary>
+    /// <param name="obj">
+    /// The <see cref="object"/> to compare with the current
+    /// <see cref="OperationTypeDefinitionNode"/>.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if the specified <see cref="object"/> is equal to the
+    /// current <see cref="OperationTypeDefinitionNode"/>; otherwise, <c>false</c>.
+    /// </returns>
+    public override bool Equals(object? obj)
+        => ReferenceEquals(this, obj) ||
+            (obj is OperationTypeDefinitionNode other && Equals(other));
 
-    public override int GetHashCode() => HashCode.Combine((int)Operation, Type, Kind);
+    /// <summary>
+    /// Serves as a hash function for a <see cref="OperationTypeDefinitionNode"/>
+    /// object.
+    /// </summary>
+    /// <returns>
+    /// A hash code for this instance that is suitable for use in
+    /// hashing algorithms and data structures such as a hash table.
+    /// </returns>
+    public override int GetHashCode()
+        => HashCode.Combine(Kind, Operation, Type);
+
+    /// <summary>
+    /// The equal operator.
+    /// </summary>
+    /// <param name="left">The left parameter</param>
+    /// <param name="right">The right parameter</param>
+    /// <returns>
+    /// <c>true</c> if <paramref name="left"/> and <paramref name="right"/> are equal.
+    /// </returns>
+    public static bool operator ==(
+        OperationTypeDefinitionNode? left,
+        OperationTypeDefinitionNode? right)
+        => Equals(left, right);
+
+    /// <summary>
+    /// The not equal operator.
+    /// </summary>
+    /// <param name="left">The left parameter</param>
+    /// <param name="right">The right parameter</param>
+    /// <returns>
+    /// <c>true</c> if <paramref name="left"/> and <paramref name="right"/> are not equal.
+    /// </returns>
+    public static bool operator !=(
+        OperationTypeDefinitionNode? left,
+        OperationTypeDefinitionNode? right)
+        => !Equals(left, right);
 }
