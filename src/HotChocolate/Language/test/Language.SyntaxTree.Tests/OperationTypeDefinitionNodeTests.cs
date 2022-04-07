a9301adad8a74a18260024a87e91abed8cff64fd
Michael Staib <michael@chillicream.com>
2022-04-07 23:17:33 +0200

    Fixed tests

--- src/HotChocolate/Language/test/Language.SyntaxTree.Tests/OperationTypeDefinitionNodeTests.cs
+++ src/HotChocolate/Language/test/Language.SyntaxTree.Tests/OperationTypeDefinitionNodeTests.cs
@@ -8,27 +8,29 @@ public class OperationTypeDefinitionNodeTests
     public void EqualsOperationTypeDefinitionNode_SameLocation()
     {
         // arrange
-        var a = new OperationTypeDefinitionNode(TestLocations.Location1, OperationType.Query,
+        var a = new OperationTypeDefinitionNode(
+            TestLocations.Location1,
+            OperationType.Query,
             new NamedTypeNode("a"));
-        var b = new OperationTypeDefinitionNode(TestLocations.Location1, OperationType.Query,
+        var b = new OperationTypeDefinitionNode(
+            TestLocations.Location1,
+            OperationType.Query,
             new NamedTypeNode("a"));
-        var c = new OperationTypeDefinitionNode(TestLocations.Location1, OperationType.Query,
+        var c = new OperationTypeDefinitionNode(
+            TestLocations.Location1,
+            OperationType.Query,
             new NamedTypeNode("b"));
-        var d = new OperationTypeDefinitionNode(TestLocations.Location1, OperationType.Mutation,
-            new NamedTypeNode("a"));
 
         // act
         var aaResult = a.Equals(a);
         var abResult = a.Equals(b);
         var acResult = a.Equals(c);
-        var adResult = a.Equals(d);
         var aNullResult = a.Equals(default);
 
         // assert
         Assert.True(aaResult);
         Assert.True(abResult);
         Assert.False(acResult);
-        Assert.False(adResult);
         Assert.False(aNullResult);
     }
 
@@ -36,27 +38,29 @@ public class OperationTypeDefinitionNodeTests
     public void EqualsOperationTypeDefinitionNode_DifferentLocations()
     {
         // arrange
-        var a = new OperationTypeDefinitionNode(TestLocations.Location1, OperationType.Query,
+        var a = new OperationTypeDefinitionNode(
+            TestLocations.Location1,
+            OperationType.Query,
             new NamedTypeNode("a"));
-        var b = new OperationTypeDefinitionNode(TestLocations.Location2, OperationType.Query,
+        var b = new OperationTypeDefinitionNode(
+            TestLocations.Location2,
+            OperationType.Query,
             new NamedTypeNode("a"));
-        var c = new OperationTypeDefinitionNode(TestLocations.Location3, OperationType.Query,
+        var c = new OperationTypeDefinitionNode(
+            TestLocations.Location3,
+            OperationType.Query,
             new NamedTypeNode("b"));
-        var d = new OperationTypeDefinitionNode(TestLocations.Location3, OperationType.Mutation,
-            new NamedTypeNode("a"));
 
         // act
         var aaResult = a.Equals(a);
         var abResult = a.Equals(b);
         var acResult = a.Equals(c);
-        var adResult = a.Equals(d);
         var aNullResult = a.Equals(default);
 
         // assert
         Assert.True(aaResult);
         Assert.True(abResult);
         Assert.False(acResult);
-        Assert.False(adResult);
         Assert.False(aNullResult);
     }
 
@@ -64,15 +68,21 @@ public class OperationTypeDefinitionNodeTests
     public void CompareGetHashCode_WithLocations()
     {
         // arrange
-        var a = new OperationTypeDefinitionNode(TestLocations.Location1, OperationType.Query,
+        var a = new OperationTypeDefinitionNode(
+            TestLocations.Location1,
+            OperationType.Query,
             new NamedTypeNode("a"));
-        var b = new OperationTypeDefinitionNode(TestLocations.Location2, OperationType.Query,
+        var b = new OperationTypeDefinitionNode(
+            TestLocations.Location2,
+            OperationType.Query,
             new NamedTypeNode("a"));
-        var c = new OperationTypeDefinitionNode(TestLocations.Location1, OperationType.Query,
+        var c = new OperationTypeDefinitionNode(
+            TestLocations.Location1,
+            OperationType.Query,
             new NamedTypeNode("b"));
-        var d = new OperationTypeDefinitionNode(TestLocations.Location2, OperationType.Mutation,
-            new NamedTypeNode("a"));
-        var e = new OperationTypeDefinitionNode(TestLocations.Location3, OperationType.Mutation,
+        var d = new OperationTypeDefinitionNode(
+            TestLocations.Location3,
+            OperationType.Mutation,
             new NamedTypeNode("a"));
 
         // act
@@ -80,13 +90,10 @@ public class OperationTypeDefinitionNodeTests
         var bHash = b.GetHashCode();
         var cHash = c.GetHashCode();
         var dHash = d.GetHashCode();
-        var eHash = e.GetHashCode();
 
         // assert
         Assert.Equal(aHash, bHash);
         Assert.NotEqual(aHash, cHash);
-        Assert.NotEqual(aHash, dHash);
-        Assert.NotEqual(bHash, dHash);
-        Assert.Equal(dHash, eHash);
+        Assert.Equal(cHash, dHash);
     }
 }
