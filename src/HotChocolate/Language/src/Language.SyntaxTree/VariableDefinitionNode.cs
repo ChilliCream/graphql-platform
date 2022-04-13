fb62bae9d10228349d1a8bae1241789be4a4df98
Michael Staib <michael@chillicream.com>
2022-04-13 10:21:17 +0200

    refinements

--- src/HotChocolate/Language/src/Language.SyntaxTree/VariableDefinitionNode.cs
+++ src/HotChocolate/Language/src/Language.SyntaxTree/VariableDefinitionNode.cs
@@ -4,11 +4,32 @@ using HotChocolate.Language.Utilities;
 
 namespace HotChocolate.Language;
 
+/// <summary>
+/// The variable definition syntax of a GraphQL operation.
+/// </summary>
 public sealed class VariableDefinitionNode
     : ISyntaxNode
     , IHasDirectives
     , IEquatable<VariableDefinitionNode>
 {
+    /// <summary>
+    /// Initializes a new instance of <see cref="VariableDefinitionNode"/>.
+    /// </summary>
+    /// <param name="location">
+    /// The location of the syntax node within the original source text.
+    /// </param>
+    /// <param name="variable">
+    /// The variable.
+    /// </param>
+    /// <param name="type">
+    /// The variable type.
+    /// </param>
+    /// <param name="defaultValue">
+    /// The variables default value.
+    /// </param>
+    /// <param name="directives">
+    /// The directives of this variable declaration.
+    /// </param>
     public VariableDefinitionNode(
         Location? location,
         VariableNode variable,
@@ -23,18 +44,33 @@ public sealed class VariableDefinitionNode
         Directives = directives ?? throw new ArgumentNullException(nameof(directives));
     }
 
+    /// <inheritdoc />
     public SyntaxKind Kind => SyntaxKind.VariableDefinition;
 
+    /// <inheritdoc />
     public Location? Location { get; }
 
+    /// <summary>
+    /// Gets the variable.
+    /// </summary>
     public VariableNode Variable { get; }
 
+    /// <summary>
+    /// Gets the variable type.
+    /// </summary>
     public ITypeNode Type { get; }
 
+    /// <summary>
+    /// Gets the variables default value.
+    /// </summary>
     public IValueNode? DefaultValue { get; }
 
+    /// <summary>
+    /// Gets the directives of this variable declaration.
+    /// </summary>
     public IReadOnlyList<DirectiveNode> Directives { get; }
 
+    /// <inheritdoc />
     public IEnumerable<ISyntaxNode> GetNodes()
     {
         yield return Variable;
@@ -72,17 +108,69 @@ public sealed class VariableDefinitionNode
     /// </returns>
     public string ToString(bool indented) => SyntaxPrinter.Print(this, indented);
 
+    /// <summary>
+    /// Creates a new node from the current instance and replaces the
+    /// <see cref="Location" /> with <paramref name="location" />.
+    /// </summary>
+    /// <param name="location">
+    /// The location that shall be used to replace the current location.
+    /// </param>
+    /// <returns>
+    /// Returns the new node with the new <paramref name="location" />.
+    /// </returns>
     public VariableDefinitionNode WithLocation(Location? location)
         => new(location, Variable, Type, DefaultValue, Directives);
 
+    /// <summary>
+    /// Creates a new node from the current instance and replaces the
+    /// <see cref="Variable" /> with <paramref name="variable" />.
+    /// </summary>
+    /// <param name="variable">
+    /// The variable that shall be used to replace the current <see cref="Variable" />.
+    /// </param>
+    /// <returns>
+    /// Returns the new node with the new <paramref name="variable" />.
+    /// </returns>
     public VariableDefinitionNode WithVariable(VariableNode variable)
         => new(Location, variable, Type, DefaultValue, Directives);
 
+    /// <summary>
+    /// Creates a new node from the current instance and replaces the
+    /// <see cref="Type" /> with <paramref name="type" />.
+    /// </summary>
+    /// <param name="type">
+    /// The type that shall be used to replace the current <see cref="Type" />.
+    /// </param>
+    /// <returns>
+    /// Returns the new node with the new <paramref name="type" />.
+    /// </returns>
     public VariableDefinitionNode WithType(ITypeNode type)
         => new(Location, Variable, type, DefaultValue, Directives);
 
+    /// <summary>
+    /// Creates a new node from the current instance and replaces the
+    /// <see cref="DefaultValue" /> with <paramref name="defaultValue" />.
+    /// </summary>
+    /// <param name="defaultValue">
+    /// The defaultValue that shall be used to replace the current <see cref="DefaultValue" />.
+    /// </param>
+    /// <returns>
+    /// Returns the new node with the new <paramref name="defaultValue" />.
+    /// </returns>
     public VariableDefinitionNode WithDefaultValue(IValueNode? defaultValue)
         => new(Location, Variable, Type, defaultValue, Directives);
+
+    /// <summary>
+    /// Creates a new node from the current instance and replaces the
+    /// <see cref="Directives" /> with <paramref name="directives" />.
+    /// </summary>
+    /// <param name="directives">
+    /// The directives that shall be used to replace the current
+    /// <see cref="Directives" />.
+    /// </param>
+    /// <returns>
+    /// Returns the new node with the new <paramref name="directives" />.
+    /// </returns>
 
     public VariableDefinitionNode WithDirectives(IReadOnlyList<DirectiveNode> directives)
         => new(Location, Variable, Type, DefaultValue, directives);
