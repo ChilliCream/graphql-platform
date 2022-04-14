0e26c2810916138734e57670a0d296253a057093
Michael Staib <michael@chillicream.com>
2022-04-14 11:23:23 +0200

    edits

--- src/HotChocolate/Language/src/Language.SyntaxTree/SchemaDefinitionNodeBase.cs
+++ src/HotChocolate/Language/src/Language.SyntaxTree/SchemaDefinitionNodeBase.cs
@@ -3,28 +3,50 @@ using System.Collections.Generic;
 
 namespace HotChocolate.Language;
 
+/// <summary>
+/// Represents the base class for <see cref="SchemaDefinitionNode"/> and
+/// <see cref="SchemaExtensionNode"/>.
+/// </summary>
 public abstract class SchemaDefinitionNodeBase
     : IHasDirectives
     , IEquatable<SchemaDefinitionNodeBase>
 {
+    /// <summary>
+    /// Initializes a new instance of <see cref="SchemaDefinitionNodeBase"/>.
+    /// </summary>
+    /// <param name="location">
+    /// The location of the syntax node within the original source text.
+    /// </param>
+    /// <param name="directives">
+    /// The applied directives.
+    /// </param>
+    /// <param name="operationTypes">
+    /// The operation types.
+    /// </param>
     protected SchemaDefinitionNodeBase(
         Location? location,
         IReadOnlyList<DirectiveNode> directives,
         IReadOnlyList<OperationTypeDefinitionNode> operationTypes)
     {
         Location = location;
-        Directives = directives
-            ?? throw new ArgumentNullException(nameof(directives));
-        OperationTypes = operationTypes
-            ?? throw new ArgumentNullException(nameof(operationTypes));
+        Directives = directives ?? throw new ArgumentNullException(nameof(directives));
+        OperationTypes = operationTypes ?? throw new ArgumentNullException(nameof(operationTypes));
     }
 
+    /// <inheritdoc cref="ISyntaxNode.Kind" />
     public abstract SyntaxKind Kind { get; }
 
+    /// <inheritdoc cref="ISyntaxNode.Location" />
     public Location? Location { get; }
 
+    /// <summary>
+    /// Gets the applied directives.
+    /// </summary>
     public IReadOnlyList<DirectiveNode> Directives { get; }
 
+    /// <summary>
+    /// Gets the schema operation types.
+    /// </summary>
     public IReadOnlyList<OperationTypeDefinitionNode> OperationTypes { get; }
 
     /// <summary>
@@ -98,11 +120,27 @@ public abstract class SchemaDefinitionNodeBase
         return hashCode.ToHashCode();
     }
 
+    /// <summary>
+    /// The equal operator.
+    /// </summary>
+    /// <param name="left">The left parameter</param>
+    /// <param name="right">The right parameter</param>
+    /// <returns>
+    /// <c>true</c> if <paramref name="left"/> and <paramref name="right"/> are equal.
+    /// </returns>
     public static bool operator ==(
         SchemaDefinitionNodeBase? left,
         SchemaDefinitionNodeBase? right)
         => Equals(left, right);
 
+    /// <summary>
+    /// The not equal operator.
+    /// </summary>
+    /// <param name="left">The left parameter</param>
+    /// <param name="right">The right parameter</param>
+    /// <returns>
+    /// <c>true</c> if <paramref name="left"/> and <paramref name="right"/> are not equal.
+    /// </returns>
     public static bool operator !=(
         SchemaDefinitionNodeBase? left,
         SchemaDefinitionNodeBase? right)
