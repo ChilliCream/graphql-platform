ad668d74ef12062eb2210c0714743628c1672e26
PascalSenn <senn.pasc@gmail.com>
2022-04-16 22:21:00 +0200

    Adds expression support for filtering (#4877)

--- src/HotChocolate/Data/src/Data/Filters/IFilterFieldDefinition.cs
+++ src/HotChocolate/Data/src/Data/Filters/IFilterFieldDefinition.cs
@@ -1,14 +1,23 @@
+using System.Linq.Expressions;
 using System.Collections.Generic;
 using System.Reflection;
+using HotChocolate.Types;
+using HotChocolate.Types.Descriptors.Definitions;
 
 namespace HotChocolate.Data.Filters;
 
 public interface IFilterFieldDefinition
+    : IDefinition
+    , IHasDirectiveDefinition
+    , IHasIgnore
+    , IHasScope
 {
     MemberInfo? Member { get; }
 
     IFilterFieldHandler? Handler { get; }
 
+    Expression? Expression { get; }
+    
     string? Scope { get; }
 
     List<int> AllowedOperations { get; }
