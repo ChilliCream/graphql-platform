ca0d44601a038dfd0895b375067f68d1dc911a19
PascalSenn <senn.pasc@gmail.com>
2022-04-17 20:44:25 +0200

    Migrated MongoDb to file scoped namespaces (#4972)

--- src/HotChocolate/MongoDb/test/Data.MongoDb.Filters.Tests/Extensions/TestExtensions.cs
+++ src/HotChocolate/MongoDb/test/Data.MongoDb.Filters.Tests/Extensions/TestExtensions.cs
@@ -2,24 +2,23 @@ using HotChocolate.Execution;
 using Snapshooter;
 using Snapshooter.Xunit;
 
-namespace HotChocolate.Data.MongoDb.Filters
+namespace HotChocolate.Data.MongoDb.Filters;
+
+public static class TestExtensions
 {
-    public static class TestExtensions
+    public static void MatchDocumentSnapshot(
+        this IExecutionResult? result,
+        string snapshotName)
     {
-        public static void MatchDocumentSnapshot(
-            this IExecutionResult? result,
-            string snapshotName)
+        if (result is { })
         {
-            if (result is { })
+            result.ToJson().MatchSnapshot(new SnapshotNameExtension(snapshotName));
+            if (result.ContextData is { } &&
+                result.ContextData.TryGetValue("query", out object? queryResult) &&
+                queryResult is string queryString &&
+                !string.IsNullOrWhiteSpace(queryString))
             {
-                result.ToJson().MatchSnapshot(new SnapshotNameExtension(snapshotName));
-                if (result.ContextData is { } &&
-                    result.ContextData.TryGetValue("query", out object? queryResult) &&
-                    queryResult is string queryString &&
-                    !string.IsNullOrWhiteSpace(queryString))
-                {
-                    queryString.MatchSnapshot(new SnapshotNameExtension(snapshotName + "_query"));
-                }
+                queryString.MatchSnapshot(new SnapshotNameExtension(snapshotName + "_query"));
             }
         }
     }
