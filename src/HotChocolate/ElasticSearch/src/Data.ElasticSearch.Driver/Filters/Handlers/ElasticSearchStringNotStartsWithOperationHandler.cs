8a889c0a0fc52d03ab5bdef51e108486558e6e63
frzmohammadali <frz.mohammadali.me@outlook.com>
2022-05-01 20:11:08 +0100

    Added StartsWith and NotStartsWith handlers

---
+++ src/HotChocolate/ElasticSearch/src/Data.ElasticSearch.Driver/Filters/Handlers/ElasticSearchStringNotStartsWithOperationHandler.cs
@@ -0,0 +1,42 @@
+using HotChocolate.Configuration;
+using HotChocolate.Data.Filters;
+using HotChocolate.Language;
+using HotChocolate.Types;
+using static HotChocolate.Data.Filters.DefaultFilterOperations;
+
+namespace HotChocolate.Data.ElasticSearch.Filters;
+
+/// <summary>
+/// This filter operation handler maps a NotStartsWith operation field to a
+/// <see cref="ISearchOperation"/>
+/// </summary>
+public class ElasticSearchStringNotStartsWithOperationHandler
+    : ElasticSearchStringStartsWithOperationHandler
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="ElasticSearchStringNotStartsWithOperationHandler"/>
+    /// </summary>
+    public ElasticSearchStringNotStartsWithOperationHandler(InputParser inputParser)
+        : base(inputParser)
+    {
+    }
+
+    /// <inheritdoc />
+    public override bool CanHandle(
+        ITypeCompletionContext context,
+        IFilterInputTypeDefinition typeDefinition,
+        IFilterFieldDefinition fieldDefinition)
+        => context.Type is StringOperationFilterInputType &&
+            fieldDefinition is FilterOperationFieldDefinition { Id: NotStartsWith };
+
+    /// <inheritdoc />
+    public override ISearchOperation HandleOperation(
+        ElasticSearchFilterVisitorContext context,
+        IFilterOperationField field,
+        IValueNode value,
+        object? parsedValue)
+    {
+        ISearchOperation operation = base.HandleOperation(context, field, value, parsedValue);
+        return ElasticSearchOperationHelpers.Negate(operation);
+    }
+}
