4830f35fea283a419fd458e80d474ec5805c2648
Pascal Senn <senn.pasc@gmail.com>
2022-05-11 21:37:16 +0200

    Release task in node resolver after use

--- src/HotChocolate/Core/src/Types/Types/Relay/NodeFieldTypeInterceptor.cs
+++ src/HotChocolate/Core/src/Types/Types/Relay/NodeFieldTypeInterceptor.cs
@@ -130,7 +130,6 @@ internal sealed class NodeFieldTypeInterceptor : TypeInterceptor
         {
             ListValueNode list = context.ArgumentLiteral<ListValueNode>(Ids);
             Task<object?>[] tasks = ArrayPool<Task<object?>>.Shared.Rent(list.Items.Count);
-            tasks.AsSpan().Slice(0, list.Items.Count).Clear();
             var result = new object?[list.Items.Count];
 
             try
@@ -192,7 +191,7 @@ internal sealed class NodeFieldTypeInterceptor : TypeInterceptor
             }
             finally
             {
-                ArrayPool<Task<object?>>.Shared.Return(tasks);
+                ArrayPool<Task<object?>>.Shared.Return(tasks, true);
             }
         }
         else
