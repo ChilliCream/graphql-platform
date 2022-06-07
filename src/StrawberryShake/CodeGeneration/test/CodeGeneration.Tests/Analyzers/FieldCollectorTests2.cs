fa9d4bdd89e63d33d56df5b330ef87db0f2a12a4
Michael Staib <michael@chillicream.com>
2022-06-07 10:27:21 -0500

    edits

--- src/StrawberryShake/CodeGeneration/test/CodeGeneration.Tests/Analyzers/FieldCollectorTests2.cs
+++ src/StrawberryShake/CodeGeneration/test/CodeGeneration.Tests/Analyzers/FieldCollectorTests2.cs
@@ -112,6 +112,71 @@ public class FieldCollectorTests2
         Format(fragmentTypeModels).MatchSnapshot();
     }
 
+    [Fact]
+    public async Task Collect_Multiple_Fragments_Two_Levels()
+    {
+        // arrange
+        var schema =
+            await new ServiceCollection()
+                .AddGraphQL()
+                .AddDocumentFromString(FileResource.Open("CoinApi.graphql"))
+                .AddType<UploadType>()
+                .UseField(_ => _ => default)
+                .BuildSchemaAsync();
+
+        DocumentNode document = Utf8GraphQLParser.Parse(
+            @"fragment AssetList_List on AssetsConnection {
+                nodes {
+                    name
+                    price {
+                        lastPrice
+                    }
+                }
+            }
+
+            fragment Index_Symbols on AssetsConnection {
+                nodes {
+                    symbol
+                }
+            }
+
+            fragment NavigationControls_Page on PageInfo {
+                hasNextPage
+                endCursor
+            }
+
+            fragment Connection on AssetsConnection {
+                ... AssetList_List
+                ... Index_Symbols
+                pageInfo {
+                    ... NavigationControls_Page
+                }
+            }
+
+            query GetAssets($after: String = null) {
+                assets(after: $after order: { price: { marketCap: DESC } }) {
+                    ... Connection
+                }
+            }
+
+            subscription OnPriceChange {
+                onPriceChange {
+                    lastPrice
+                }
+            }");
+
+        var fragmentIndex = FragmentIndexer.Index(schema, document);
+        var fragmentCache = new Dictionary<string, Fragment>();
+        var collector = new FieldCollector2(schema, fragmentIndex, fragmentCache);
+        var context = new DocumentAnalyzerContext(schema, document);
+
+        // act
+        var fragmentTypeModels = collector.CollectFragments(context);
+
+        // assert
+        Format(fragmentTypeModels).MatchSnapshot();
+    }
+
     private string Format(IEnumerable<OutputTypeModel> typeModels)
     {
         var output = new StringBuilder();
@@ -145,7 +210,7 @@ public class FieldCollectorTests2
         if (typeModel.Implements.Count > 0)
         {
             output.Append("    : ");
-            output.Append(string.Join("\n    ,", typeModel.Implements.Select(t => t.Name)));
+            output.Append(string.Join("\n    , ", typeModel.Implements.Select(t => t.Name)));
             output.Append('\n');
         }
 
