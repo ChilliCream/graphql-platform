bd1163e126b34f3fa472507402f724e47d524dff
wassim-k <wassim.khalil9@gmail.com>
2022-06-10 00:53:35 +1000

    Always use NtsGeometryServices instance for geometry creation (#5048)

--- src/HotChocolate/Spatial/test/Data.Filters.SqlServer.Tests/QueryableFilterVisitorTouchesTests.cs
+++ src/HotChocolate/Spatial/test/Data.Filters.SqlServer.Tests/QueryableFilterVisitorTouchesTests.cs
@@ -7,9 +7,9 @@ using Xunit;
 
 namespace HotChocolate.Data.Filters.Spatial;
 
+[Collection("Postgres")]
 public class QueryableFilterVisitorTouchesTests
     : SchemaCache
-    , IClassFixture<PostgreSqlResource<PostgisConfig>>
 {
     private static readonly Polygon _truePolygon =
         new Polygon(new LinearRing(new[]
