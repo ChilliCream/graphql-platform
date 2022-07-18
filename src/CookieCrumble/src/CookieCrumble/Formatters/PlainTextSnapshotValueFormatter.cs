adca5cd617195d7473db616ebd56f4f6b8c0a45e
Michael Staib <michael@chillicream.com>
2022-07-18 20:42:59 +0200

    updated snapshots

--- src/CookieCrumble/src/CookieCrumble/Formatters/PlainTextSnapshotValueFormatter.cs
+++ src/CookieCrumble/src/CookieCrumble/Formatters/PlainTextSnapshotValueFormatter.cs
@@ -11,23 +11,6 @@ internal sealed class PlainTextSnapshotValueFormatter : ISnapshotValueFormatter
     {
         if (value?.ToString() is { } s)
         {
-            var serialized = s.AsSpan();
-            var buffer = ArrayPool<char>.Shared.Rent(serialized.Length);
-            var span = buffer.AsSpan()[..serialized.Length];
-            var written = 0;
-
-            for (var i = 0; i < serialized.Length; i++)
-            {
-                if (serialized[i] is not '\r')
-                {
-                    span[written++] = serialized[i];
-                }
-            }
-
-            span = span[..written];
-            snapshot.Append(span);
-
-            ArrayPool<char>.Shared.Return(buffer);
             snapshot.Append(s);
         }
     }
