c6dce90e935e7ec6279a8bc337d7d0db0feb9b1e
Michael Staib <michael@chillicream.com>
2022-07-20 21:19:08 +0200

    edits

--- src/HotChocolate/Fusion/src/Core/Dummy.cs
+++ src/HotChocolate/Fusion/src/Core/Dummy.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using HotChocolate.Execution.Processing;
 using HotChocolate.Language;
@@ -6,33 +8,91 @@ namespace HotChocolate.Fusion;
 
 public class QueryPlanBuilder
 {
-    private readonly Schema _schema = default!;
+    private readonly Schema _schema;
 
-    public void Foo(IOperation operation, ISelectionSet selectionSet, ObjectType type)
+
+    public QueryPlanBuilder(Schema schema)
+    {
+        _schema = schema;
+    }
+
+    public void CollectSelectionsBySchema1(
+        IOperation operation,
+        ISelectionSet selectionSet,
+        ObjectType typeContext)
     {
         var selections = new List<ISelection>(selectionSet.Selections);
+        var schemaName = ResolveBestMatchingSchema(operation, selections, typeContext);
+        var selectionSyntaxList = new List<ISelectionNode>();
 
-        while (selections.Count > 0)
+        foreach (var selection in selections)
         {
-            var bestScore = 0;
-            var bestSchema = _schema.Bindings[0];
+            var field = typeContext.Fields[selection.Field.Name];
 
-            foreach (var schemaName in _schema.Bindings)
+            if (field.Bindings.TryGetValue(schemaName, out var binding))
             {
-                var score = CalculateSchemaScore(operation, selections, type, schemaName);
-
-                if (score > bestScore)
-                {
-                    bestScore = score;
-                    bestSchema = schemaName;
-                }
-            }
-
-            foreach (var selection in selectionSet.Selections)
-            {
-
+                selectionSyntaxList.Add(CreateSelectionSyntax(selection, field, binding, null));
             }
         }
+
+
+    }
+
+    private ISelectionNode CreateSelectionSyntax(
+        ISelection selection,
+        ObjectField field,
+        MemberBinding binding,
+        FetchDefinition? resolver)
+    {
+        if (resolver is null)
+        {
+            var alias = !selection.ResponseName.Equals(binding.Name)
+                ? new NameNode(selection.ResponseName)
+                : null;
+
+            return new FieldNode(
+                null,
+                new(binding.Name),
+                alias,
+                null,
+                Array.Empty<DirectiveNode>(),
+                Array.Empty<ArgumentNode>(),
+                null);
+        }
+
+        throw new NotImplementedException();
+    }
+
+
+    private string ResolveBestMatchingSchema(
+        IOperation operation,
+        IReadOnlyList<ISelection> selections,
+        ObjectType typeContext)
+    {
+        var bestScore = 0;
+        var bestSchema = _schema.Bindings[0];
+
+        foreach (var schemaName in _schema.Bindings)
+        {
+            var score = CalculateSchemaScore(operation, selections, typeContext, schemaName);
+
+            if (score > bestScore)
+            {
+                bestScore = score;
+                bestSchema = schemaName;
+            }
+        }
+
+        return bestSchema;
+    }
+
+    private void CreateRequest(
+        IOperation operation,
+        IReadOnlyList<ISelection> selections,
+        ObjectType typeContext,
+        string schemaName)
+    {
+
     }
 
     private int CalculateSchemaScore(
@@ -67,6 +127,8 @@ public class QueryPlanBuilder
 
         return score;
     }
+
+
 }
 
 public class QueryPlan : ExecutionNode
@@ -75,12 +137,12 @@ public class QueryPlan : ExecutionNode
 
 public sealed class RequestNode : ExecutionNode
 {
-    public RequestNode(RequestTemplate template)
+    public RequestNode(RequestHandler handler)
     {
-        Template = template;
+        Handler = handler;
     }
 
-    public RequestTemplate Template { get; }
+    public RequestHandler Handler { get; }
 }
 
 public abstract class ExecutionNode
@@ -114,16 +176,8 @@ public abstract class ExecutionNode
     }
 }
 
-public class RequestTemplate
+public class RequestHandler
 {
-    public RequestTemplate(
-        IReadOnlyList<string> requires,
-        IReadOnlyList<string> exports,
-        DocumentNode document)
-    {
-
-    }
-
     public IReadOnlyList<string> Requires { get; }
 
     public IReadOnlyList<string> Exports { get; }
@@ -134,6 +188,9 @@ public class RequestTemplate
         => throw new NotImplementedException();
 
     public IReadOnlyList<IValueNode> ExtractExports(JsonElement response)
+        => throw new NotImplementedException();
+
+    public void ExtractResult(JsonElement response, ObjectResult parent)
         => throw new NotImplementedException();
 }
 
@@ -151,55 +208,165 @@ public interface IType
     string Name { get; }
 }
 
-public class Schema
+public sealed class Schema
 {
-    public IReadOnlyList<string> Bindings { get; }
+    private readonly string[] _bindings;
+    private readonly Dictionary<string, IType> _types;
 
-    public IType this[string typeName] => throw new NotImplementedException();
+    public Schema(IEnumerable<string> bindings, IEnumerable<IType> types)
+    {
+        _bindings = bindings.ToArray();
+        _types = types.ToDictionary(t => t.Name, StringComparer.Ordinal);
+    }
 
-    public T GetType<T>(string typeName) where T : IType => throw new NotImplementedException();
+    public IReadOnlyList<string> Bindings => _bindings;
+
+    public T GetType<T>(string typeName) where T : IType
+    {
+        if (_types.TryGetValue(typeName, out var type) && type is T casted)
+        {
+            return casted;
+        }
+
+        throw new InvalidOperationException("Type not found.");
+    }
 }
 
-public class ObjectType : IType
+public sealed class ObjectType : IType
 {
+    public ObjectType(string name,
+        IEnumerable<MemberBinding> bindings,
+        IEnumerable<FetchDefinition> resolvers,
+        IEnumerable<ObjectField> fields)
+    {
+        Name = name;
+        Bindings = new MemberBindingCollection(bindings);
+        Resolvers = new FetchDefinitionCollection(resolvers);
+        Fields = new ObjectFieldCollection(fields);
+    }
+
     public string Name { get; }
 
     public MemberBindingCollection Bindings { get; }
+
+    public FetchDefinitionCollection Resolvers { get; }
+
+    // public VariableDefinitionCollection Variables { get; }
 
     public ObjectFieldCollection Fields { get; }
+
 }
 
-public class ObjectField
+public sealed class ObjectField
 {
+    public ObjectField(
+        string name,
+        IEnumerable<MemberBinding> bindings,
+        IEnumerable<FetchDefinition> resolvers)
+    {
+        Name = name;
+        Bindings = new MemberBindingCollection(bindings);
+        Resolvers = new FetchDefinitionCollection(resolvers);
+    }
+
     public string Name { get; }
 
     public MemberBindingCollection Bindings { get; }
+
+    public FetchDefinitionCollection Resolvers { get; }
 }
 
-public class ObjectFieldCollection
+public sealed class ObjectFieldCollection : IEnumerable<ObjectField>
+{
+    private readonly Dictionary<string, ObjectField> _fields;
+
+    public ObjectFieldCollection(IEnumerable<ObjectField> fields)
+    {
+        _fields = fields.ToDictionary(t => t.Name, StringComparer.Ordinal);
+    }
+
+    public int Count => _fields.Count;
+
+    public ObjectField this[string fieldName] => _fields[fieldName];
+
+    public bool TryGetValue(string fieldName, [NotNullWhen(true)] out ObjectField? value)
+        => _fields.TryGetValue(fieldName, out value);
+
+    public IEnumerator<ObjectField> GetEnumerator() => _fields.Values.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+public sealed class MemberBindingCollection : IEnumerable<MemberBinding>
+{
+    private readonly Dictionary<string, MemberBinding> _bindings;
+
+    public MemberBindingCollection(IEnumerable<MemberBinding> bindings)
+    {
+        _bindings = bindings.ToDictionary(t => t.SchemaName, StringComparer.Ordinal);
+    }
+
+    public int Count => _bindings.Count;
+
+    // public MemberBinding this[string schemaName] => throw new NotImplementedException();
+
+    public bool TryGetValue(string schemaName, [NotNullWhen(true)] out MemberBinding? value)
+        => _bindings.TryGetValue(schemaName, out value);
+
+    public bool ContainsSchema(string schemaName) => _bindings.ContainsKey(schemaName);
+
+    public IEnumerator<MemberBinding> GetEnumerator() => _bindings.Values.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+public sealed class FetchDefinitionCollection : IEnumerable<FetchDefinition>
+{
+    private readonly Dictionary<string, FetchDefinition[]> _fetchDefinitions;
+
+    public FetchDefinitionCollection(IEnumerable<FetchDefinition> fetchDefinitions)
+    {
+        _fetchDefinitions = fetchDefinitions
+            .GroupBy(t => t.SchemaName)
+            .ToDictionary(t => t.Key, t => t.ToArray(), StringComparer.Ordinal);
+    }
+
+    public int Count => _fetchDefinitions.Count;
+
+    // public IReadOnlyList<FetchDefinition> this[string schemaName] => throw new NotImplementedException();
+
+    public bool TryGetValue(string schemaName, out IReadOnlyList<FetchDefinition>? values)
+    {
+        if (_fetchDefinitions.TryGetValue(schemaName, out var temp))
+        {
+            values = temp;
+            return true;
+        }
+
+        values = null;
+        return false;
+    }
+
+    public bool ContainsSchema(string schemaName) => _fetchDefinitions.ContainsKey(schemaName);
+
+    public IEnumerator<FetchDefinition> GetEnumerator()
+        => _fetchDefinitions.Values.SelectMany(t => t).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+public class VariableDefinitionCollection
 {
     public int Count { get; }
 
-    public ObjectField this[string fieldName] => throw new NotImplementedException();
+    public VariableDefinition this[string variableName] => throw new NotImplementedException();
 
-    public bool TryGetValue(string fieldName, out ObjectField value)
-    {
-        throw new NotImplementedException();
-    }
-}
-
-public class MemberBindingCollection
-{
-    public int Count { get; }
-
-    public MemberBinding this[string schema] => throw new NotImplementedException();
-
-    public bool TryGetValue(string schema, out MemberBinding value)
+    public bool TryGetValue(string variableName, out VariableDefinition value)
     {
         throw new NotImplementedException();
     }
 
-    public bool ContainsSchema(string schema) => throw new NotImplementedException();
+    public bool ContainsSchema(string variableName) => throw new NotImplementedException();
 }
 
 
@@ -211,25 +378,67 @@ public class MemberBinding
     /// <summary>
     /// Initializes a new instance of <see cref="MemberBinding"/>.
     /// </summary>
-    /// <param name="schema">
+    /// <param name="schemaName">
     /// The schema to which the type system member is bound to.
     /// </param>
     /// <param name="name">
-    /// The name which the type system member has in the <see cref="Schema"/>.
+    /// The name which the type system member has in the <see cref="SchemaName"/>.
     /// </param>
-    public MemberBinding(string schema, string name)
+    public MemberBinding(string schemaName, string name)
     {
-        Schema = schema;
+        SchemaName = schemaName;
         Name = name;
     }
 
     /// <summary>
     /// Gets the schema to which the type system member is bound to.
     /// </summary>
-    public string Schema { get; }
+    public string SchemaName { get; }
 
     /// <summary>
-    /// Gets the name which the type system member has in the <see cref="Schema"/>.
+    /// Gets the name which the type system member has in the <see cref="SchemaName"/>.
     /// </summary>
     public string Name { get; }
+}
+
+public class FetchDefinition
+{
+    public FetchDefinition(
+        string schemaName,
+        SelectionSetNode select,
+        FragmentSpreadNode? placeholder,
+        IReadOnlyList<string> requires)
+    {
+        SchemaName = schemaName;
+        Select = select;
+        Placeholder = placeholder;
+        Requires = requires;
+    }
+
+    /// <summary>
+    /// Gets the schema to which the type system member is bound to.
+    /// </summary>
+    public string SchemaName { get; }
+
+    public SelectionSetNode Select { get; }
+
+    public FragmentSpreadNode? Placeholder { get; }
+
+    public IReadOnlyList<string> Requires { get; }
+}
+
+public class VariableDefinition
+{
+    public VariableDefinition(string name, IType type, SelectionSetNode select)
+    {
+        Name = name;
+        Type = type;
+        Select = select;
+    }
+
+    public string Name { get; }
+
+    public IType Type { get; }
+
+    public SelectionSetNode Select { get; }
 }
--- src/HotChocolate/Fusion/test/Core.Tests/UnitTest1.cs
+++ src/HotChocolate/Fusion/test/Core.Tests/UnitTest1.cs
@@ -1,10 +1,119 @@
+using HotChocolate.Execution;
+using HotChocolate.Execution.Processing;
+using HotChocolate.Fusion;
+using HotChocolate.Language;
+using Microsoft.Extensions.DependencyInjection;
+using static HotChocolate.Language.Utf8GraphQLParser;
+
 namespace test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public async Task Test1()
     {
+        // arrange
+        var sdl = @"
+            type Query {
+                personById(id: ID!) : Person
+            }
+
+            type Person {
+                id: ID!
+                name: String!
+                bio: String
+            }";
+
+        var schema = await new ServiceCollection()
+            .AddGraphQL()
+            .AddDocumentFromString(sdl)
+            .UseField(n => n)
+            .BuildSchemaAsync();
+
+        var distributedQueryType = new ObjectType(
+            "Query",
+            new[]
+            {
+                new MemberBinding("a", "Query"),
+                new MemberBinding("b", "Query"),
+            },
+            Array.Empty<FetchDefinition>(),
+            new[]
+            {
+                new ObjectField(
+                    "personById",
+                    new[] { new MemberBinding("a", "personById"), },
+                    new[]
+                    {
+                        new FetchDefinition(
+                            "a",
+                            Syntax.ParseSelectionSet("{ personById }"),
+                            null,
+                            Array.Empty<string>())
+                    })
+            });
+
+        var distributedPersonType = new ObjectType(
+            "Person",
+            new[]
+            {
+                new MemberBinding("a", "Person"),
+                new MemberBinding("b", "Person"),
+            },
+            Array.Empty<FetchDefinition>(),
+            new[]
+            {
+                new ObjectField(
+                    "id",
+                    new[]
+                    {
+                        new MemberBinding("a", "id"),
+                        new MemberBinding("b", "id"),
+                    },
+                    Array.Empty<FetchDefinition>()),
+                new ObjectField(
+                    "name",
+                    new[] { new MemberBinding("a", "name"), },
+                    Array.Empty<FetchDefinition>()),
+                new ObjectField(
+                    "bio",
+                    new[] { new MemberBinding("b", "bio"), },
+                    Array.Empty<FetchDefinition>())
+            });
+
+        var distributedSchema = new Schema(
+            new[] { "a", "b" },
+            new[]
+            {
+                distributedQueryType,
+                distributedPersonType
+            });
+
+        var request =
+            Parse(
+                @"{
+                    personById(id: 1) {
+                        id
+                        name
+                        bio
+                    }
+                }");
+
+        var operationCompiler = new OperationCompiler(new());
+        var operation = operationCompiler.Compile(
+            "abc",
+            (OperationDefinitionNode)request.Definitions.First(),
+            schema.QueryType,
+            request,
+            schema);
+
+        // act
+        var queryPlanBuilder = new QueryPlanBuilder(distributedSchema);
+
+        queryPlanBuilder.CollectSelectionsBySchema1(
+            operation,
+            operation.RootSelectionSet,
+            distributedQueryType);
 
     }
 }
