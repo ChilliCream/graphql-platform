cd0bf5dbbcc9384bc91ba28915bd9b68aff650a0
Pascal Senn <senn.pasc@gmail.com>
2022-08-01 09:44:46 +0200

    Cleanup

--- src/HotChocolate/Data/src/Data/Filters/Convention/Extensions/FilterConventionDescriptorExtensions.cs
+++ src/HotChocolate/Data/src/Data/Filters/Convention/Extensions/FilterConventionDescriptorExtensions.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using HotChocolate.Data.Filters;
-using HotChocolate.Data.Filters.Expressions;
-using HotChocolate.Types;
 
 namespace HotChocolate.Data;
 
