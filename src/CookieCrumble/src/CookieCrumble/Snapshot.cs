3f1d571289c2af6bc04929032eafe3b9541821e5
Michael Staib <michael@chillicream.com>
2022-08-25 13:04:23 +0200

    Fixed compile issue

--- src/CookieCrumble/src/CookieCrumble/Snapshot.cs
+++ src/CookieCrumble/src/CookieCrumble/Snapshot.cs
@@ -27,7 +27,9 @@ public sealed class Snapshot
             new SchemaSnapshotValueFormatter(),
             new ExceptionSnapshotValueFormatter(),
             new SchemaErrorSnapshotValueFormatter(),
+#if NET6_0_OR_GREATER
             new QueryPlanSnapshotValueFormatter(),
+#endif
         });
     private static readonly JsonSnapshotValueFormatter _defaultFormatter = new();
 
