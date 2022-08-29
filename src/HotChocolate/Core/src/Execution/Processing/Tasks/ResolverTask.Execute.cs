051bc83f37aa726bbcc8cd7c65f3afd24d3a962c
Michael Staib <michael@chillicream.com>
2022-08-29 15:41:14 +0200

    more docs

--- src/HotChocolate/Core/src/Execution/Processing/Tasks/ResolverTask.Execute.cs
+++ src/HotChocolate/Core/src/Execution/Processing/Tasks/ResolverTask.Execute.cs
@@ -258,6 +258,15 @@ internal sealed partial class ResolverTask
         return list;
     }
 
+    /// <summary>
+    /// In most cases a resolver task is rented and returned to its pool after execution.
+    /// The execute method itself will return the task.
+    ///
+    /// But there are a couple of edge cases where we rent a dummy task and do not execute it.
+    /// In these we do want to return it manually.
+    ///
+    /// Caution: This method is unsafe and could lead to double returns to the pool.
+    /// </summary>
     public async ValueTask CompleteUnsafeAsync()
     {
         if (!this.IsCompleted())
