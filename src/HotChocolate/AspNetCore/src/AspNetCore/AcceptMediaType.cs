6d7534527ec69d0d4ac86fb78af1d6074733981f
Michael Staib <michael@chillicream.com>
2022-09-09 08:34:00 +0200

    xml docs

--- src/HotChocolate/AspNetCore/src/AspNetCore/AcceptMediaType.cs
+++ src/HotChocolate/AspNetCore/src/AspNetCore/AcceptMediaType.cs
@@ -3,8 +3,29 @@ using Microsoft.Extensions.Primitives;
 
 namespace HotChocolate.AspNetCore;
 
+/// <summary>
+/// Representation of a single media type entry from the accept header.
+/// </summary>
 public readonly struct AcceptMediaType
 {
+    /// <summary>
+    /// Initializes a new instance of <see cref="AcceptMediaType"/>.
+    /// </summary>
+    /// <param name="type">
+    /// The type of the media type header entry.
+    /// </param>
+    /// <param name="subType">
+    /// The subtype of the media type header entry.
+    /// </param>
+    /// <param name="quality">
+    /// The value of the quality parameter `q`.
+    /// </param>
+    /// <param name="charset">
+    /// The charset.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Type or subtype are empty.
+    /// </exception>
     internal AcceptMediaType(
         StringSegment type,
         StringSegment subType,
@@ -67,15 +88,51 @@ public readonly struct AcceptMediaType
         }
     }
 
+    /// <summary>
+    /// Gets the media type kind which is an enum representing well-known media type.
+    /// </summary>
     public AcceptMediaTypeKind Kind { get; }
 
+    /// <summary>
+    /// Gets the type of the <see cref="AcceptMediaType"/>.
+    /// </summary>
+    /// <example>
+    /// For the media type <c>"application/json"</c>,
+    /// the property gives the value <c>"application"</c>.
+    /// </example>
+    /// <remarks>
+    /// See <see href="https://tools.ietf.org/html/rfc6838#section-4.2"/>
+    /// for more details on the type.
+    /// </remarks>
     public string Type { get; }
 
+    /// <summary>
+    /// Gets the subtype of the <see cref="AcceptMediaType"/>.
+    /// </summary>
+    /// <example>
+    /// For the media type <c>"application/vnd.example+json"</c>, the property gives the value
+    /// <c>"vnd.example+json"</c>.
+    /// </example>
+    /// <remarks>
+    /// See <see href="https://tools.ietf.org/html/rfc6838#section-4.2"/>
+    /// for more details on the subtype.
+    /// </remarks>
     public string SubType { get; }
 
+    /// <summary>
+    /// Gets or sets the value of the quality parameter. Returns null
+    /// if there is no quality.
+    /// </summary>
     public double? Quality { get; }
 
+    /// <summary>
+    /// Gets or sets the value of the charset parameter.
+    /// Returns <c>null</c> if there is no charset.
+    /// </summary>
     public string? Charset { get; }
 
+    /// <summary>
+    /// Defines if the charset is UTF-8.
+    /// </summary>
     public bool IsUtf8 { get; }
 }
--- src/HotChocolate/AspNetCore/src/AspNetCore/AcceptMediaTypeKind.cs
+++ src/HotChocolate/AspNetCore/src/AspNetCore/AcceptMediaTypeKind.cs
@@ -1,13 +1,47 @@
 namespace HotChocolate.AspNetCore;
 
+/// <summary>
+/// Representation of well-known media kinds. We use this to avoid constant string comparison.
+/// </summary>
 public enum AcceptMediaTypeKind
 {
+    /// <summary>
+    /// Not a well-known meda type.
+    /// </summary>
     Unknown = 0,
+
+    /// <summary>
+    /// *.*
+    /// </summary>
     All,
+
+    /// <summary>
+    /// application/*
+    /// </summary>
     AllApplication,
+
+    /// <summary>
+    /// multipart/*
+    /// </summary>
     AllMultiPart,
+
+    /// <summary>
+    /// application/graphql-response+json
+    /// </summary>
     ApplicationGraphQL,
+
+    /// <summary>
+    /// application/json
+    /// </summary>
     ApplicationJson,
+
+    /// <summary>
+    /// multipart/mixed
+    /// </summary>
     MultiPartMixed,
+
+    /// <summary>
+    /// text/event-stream
+    /// </summary>
     EventStream
 }
