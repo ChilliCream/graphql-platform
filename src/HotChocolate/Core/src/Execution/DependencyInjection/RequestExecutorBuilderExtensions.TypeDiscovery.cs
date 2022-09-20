a809c73dd520a6d949c92a2ed987836294ac89bd
Michael Staib <michael@chillicream.com>
2022-09-20 21:08:38 +0200

    xml docs

--- src/HotChocolate/Core/src/Execution/DependencyInjection/RequestExecutorBuilderExtensions.TypeDiscovery.cs
+++ src/HotChocolate/Core/src/Execution/DependencyInjection/RequestExecutorBuilderExtensions.TypeDiscovery.cs
@@ -12,6 +12,26 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static partial class RequestExecutorBuilderExtensions
 {
+    /// <summary>
+    /// Registers a new type discovery handler with the type initialization.
+    /// </summary>
+    /// <param name="builder">
+    /// The <see cref="IRequestExecutorBuilder"/>.
+    /// </param>
+    /// <param name="factory">
+    /// The factory to create the type discovery handler.
+    /// </param>
+    /// <typeparam name="T">
+    /// The type discovery handler type.
+    /// </typeparam>
+    /// <returns>
+    /// An <see cref="IRequestExecutorBuilder"/> that can be used to configure a schema
+    /// and its execution.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// - <paramref name="builder"/> is <c>null</c>
+    /// - <paramref name="factory"/> is <c>null</c>
+    /// </exception>
     public static IRequestExecutorBuilder AddTypeDiscoveryHandler<T>(
         this IRequestExecutorBuilder builder,
         Func<IDescriptorContext, T> factory)
