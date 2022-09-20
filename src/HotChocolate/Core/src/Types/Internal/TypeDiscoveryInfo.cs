b0bd7b7f704a1a15cdc9893626b2bfadf85929f3
Michael Staib <michael@chillicream.com>
2022-09-20 21:24:00 +0200

    fixed

--- src/HotChocolate/Core/src/Types/Internal/TypeDiscoveryInfo.cs
+++ src/HotChocolate/Core/src/Types/Internal/TypeDiscoveryInfo.cs
@@ -7,10 +7,21 @@ using HotChocolate.Types.Descriptors;
 
 namespace HotChocolate.Internal;
 
+/// <summary>
+/// Provides addition metadata about a <see cref="ExtendedTypeReference"/>.
+/// </summary>
 public readonly ref struct TypeDiscoveryInfo
 {
+    /// <summary>
+    /// Initializes a new instance of <see cref="TypeDiscoveryInfo"/>.
+    /// </summary>
     public TypeDiscoveryInfo(ExtendedTypeReference typeReference)
     {
+        if (typeReference is null)
+        {
+            throw new ArgumentNullException(nameof(typeReference));
+        }
+
         RuntimeType = typeReference.Type.Type;
         IsPublic = IsPublicInternal(typeReference);
         IsComplex = IsComplexTypeInternal(typeReference, IsPublic);
@@ -21,20 +32,44 @@ public readonly ref struct TypeDiscoveryInfo
         Context = typeReference.Context;
     }
 
+    /// <summary>
+    /// Gets the runtime type.
+    /// </summary>
     public Type RuntimeType { get; }
 
+    /// <summary>
+    /// The the type attribute if one was annotated to the <see cref="RuntimeType"/>.
+    /// </summary>
     public ITypeAttribute? Attribute { get; }
 
+    /// <summary>
+    /// Specifies if the <see cref="RuntimeType"/> is an interface.
+    /// </summary>
     public bool IsInterface { get; }
 
+    /// <summary>
+    /// Specifies if the <see cref="RuntimeType"/> is a complex type.
+    /// </summary>
     public bool IsComplex { get; }
 
+    /// <summary>
+    /// Specifies if the <see cref="RuntimeType"/> is abstract.
+    /// </summary>
     public bool IsAbstract { get; }
 
+    /// <summary>
+    /// Specifies if the <see cref="RuntimeType"/> is an enum.
+    /// </summary>
     public bool IsEnum { get; }
 
+    /// <summary>
+    /// Specifies if the <see cref="RuntimeType"/> is public.
+    /// </summary>
     public bool IsPublic { get; }
 
+    /// <summary>
+    /// Specifies the <see cref="TypeContext"/> of the type reference.
+    /// </summary>
     public TypeContext Context { get; }
 
     private static ITypeAttribute? GetTypeAttributeInternal(
