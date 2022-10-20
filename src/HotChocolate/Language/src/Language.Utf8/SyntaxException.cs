ce091a757abef09ef920094983777ca32e52e7c9
Pascal Senn <senn.pasc@gmail.com>
2022-10-20 11:26:42 +0200

    Slice source text in SyntaxException

--- src/HotChocolate/Language/src/Language.Utf8/SyntaxException.cs
+++ src/HotChocolate/Language/src/Language.Utf8/SyntaxException.cs
@@ -4,20 +4,22 @@ using System.Text;
 namespace HotChocolate.Language;
 
 [Serializable]
-public class SyntaxException
-    : Exception
+public class SyntaxException : Exception
 {
-    internal SyntaxException(Utf8GraphQLReader reader, string message)
-        : base(message)
+    private const int _sourceTextRangeSize = 512;
+
+    internal SyntaxException(Utf8GraphQLReader reader, string message) : base(message)
     {
         Position = reader.Position;
         Line = reader.Line;
         Column = reader.Column;
-        SourceText = Encoding.UTF8.GetString(reader.GraphQLData.ToArray());
+        SourceTextOffset = CalculateSourceTextOffset(reader);
+        SourceText = SliceSourceText(reader, SourceTextOffset);
     }
 
-    internal SyntaxException(Utf8GraphQLReader reader, string message, params object[] args)
-        : this(reader, string.Format(message, args))
+    internal SyntaxException(Utf8GraphQLReader reader, string message, params object[] args) : this(
+        reader,
+        string.Format(message, args))
     {
     }
 
@@ -28,4 +30,37 @@ public class SyntaxException
     public int Column { get; }
 
     public string SourceText { get; }
+
+    public int SourceTextOffset { get; set; }
+
+    private static int CalculateSourceTextOffset(Utf8GraphQLReader reader)
+    {
+        if (reader.GraphQLData.Length <= _sourceTextRangeSize ||
+            reader.Position <= _sourceTextRangeSize / 2)
+        {
+            return 0;
+        }
+
+        if (reader.GraphQLData.Length - reader.Position <= _sourceTextRangeSize / 2)
+        {
+            return reader.GraphQLData.Length - _sourceTextRangeSize;
+        }
+
+        return reader.Position - _sourceTextRangeSize / 2;
+    }
+
+    private static string SliceSourceText(Utf8GraphQLReader reader, int offset)
+    {
+        var offsetLength = reader.GraphQLData.Length - offset;
+        if (offsetLength > _sourceTextRangeSize)
+        {
+            offsetLength = _sourceTextRangeSize;
+        }
+
+        var slice = reader.GraphQLData
+            .Slice(offset, offsetLength)
+            .ToArray();
+
+        return Encoding.UTF8.GetString(slice);
+    }
 }
