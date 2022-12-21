1f2acff0771e6072bf5c595148a9d3f4079d7cfc
Marco Hamann <marcohamann89@hotmail.de>
2022-12-21 05:13:19 +0100

    add comparable handlers for elastic search range filtering

---
+++ src/HotChocolate/ElasticSearch/src/Data.ElasticSearch.Driver/Filters/Handlers/Comparable/ElasticSearchComparableLowerThanHandler.cs
@@ -0,0 +1,42 @@
+﻿using HotChocolate.Data.Filters;
+using HotChocolate.Language;
+using HotChocolate.Types;
+
+namespace HotChocolate.Data.ElasticSearch.Filters.Comparable;
+
+public class ElasticSearchComparableLowerThanHandler : ElasticSearchComparableOperationHandler
+{
+    /// <inheritdoc />
+    public ElasticSearchComparableLowerThanHandler(InputParser inputParser) : base(inputParser)
+    {
+    }
+
+    /// <inheritdoc />
+    protected override int Operation => DefaultFilterOperations.LowerThan;
+
+    /// <inheritdoc />
+    public override ISearchOperation HandleOperation(ElasticSearchFilterVisitorContext context, IFilterOperationField field,
+        IValueNode value, object? parsedValue)
+    {
+        return parsedValue switch
+        {
+            double doubleVal => new RangeOperation<double>(context.GetPath(), ElasticSearchOperationKind.Filter)
+            {
+                LowerThan = new RangeOperationValue<double>(doubleVal)
+            },
+            float floatValue => new RangeOperation<double>(context.GetPath(), ElasticSearchOperationKind.Filter)
+            {
+                LowerThan = new RangeOperationValue<double>(floatValue)
+            },
+            string stringValue => new RangeOperation<string>(context.GetPath(), ElasticSearchOperationKind.Filter)
+            {
+                LowerThan = new RangeOperationValue<string>(stringValue)
+            },
+            DateTime dateTimeVal => new RangeOperation<DateTime>(context.GetPath(), ElasticSearchOperationKind.Filter)
+            {
+                LowerThan = new RangeOperationValue<DateTime>(dateTimeVal)
+            },
+            _ => throw new InvalidOperationException()
+        };
+    }
+}
