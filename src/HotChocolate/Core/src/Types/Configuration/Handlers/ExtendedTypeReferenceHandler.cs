9ef4d37123e025eb9b719c4345866ed44427028f
Michael Staib <michael@chillicream.com>
2023-01-09 00:54:52 +0100

    cleanup

--- src/HotChocolate/Core/src/Types/Configuration/Handlers/ExtendedTypeReferenceHandler.cs
+++ src/HotChocolate/Core/src/Types/Configuration/Handlers/ExtendedTypeReferenceHandler.cs
@@ -90,28 +90,3 @@ internal sealed class ExtendedTypeReferenceHandler : ITypeRegistrarHandler
     private static bool IsTypeSystemObject(Type type) =>
         typeof(TypeSystemObjectBase).IsAssignableFrom(type);
 }
-
-internal sealed class ExtendedTypeDirectiveReferenceHandler : ITypeRegistrarHandler
-{
-    private readonly ITypeInspector _typeInspector;
-
-    public ExtendedTypeDirectiveReferenceHandler(ITypeInspector typeInspector)
-    {
-        _typeInspector = typeInspector;
-    }
-
-
-    public TypeReferenceKind Kind => TypeReferenceKind.DirectiveExtendedType;
-
-    public void Handle(ITypeRegistrar typeRegistrar, ITypeReference typeReference)
-    {
-        var typeRef = (ExtendedTypeDirectiveReference)typeReference;
-
-        if (_typeInspector.TryCreateTypeInfo(typeRef.Type, out var typeInfo) &&
-            !ExtendedType.Tools.IsSchemaType(typeInfo.NamedType) &&
-            !typeRegistrar.IsResolved(typeRef))
-        {
-            typeRegistrar.MarkUnresolved(typeRef);
-        }
-    }
-}
