645cd17f0fb56ec451be70a5afc78f88ec0ea3a3
Michael Staib <michael@chillicream.com>
2023-01-31 18:44:46 +0100

    Improve constructor binding (#5742)

--- src/HotChocolate/Core/src/Types/Types/Helpers/TypeMemHelper.cs
+++ src/HotChocolate/Core/src/Types/Types/Helpers/TypeMemHelper.cs
@@ -17,10 +17,13 @@ internal static class TypeMemHelper
     private static Dictionary<string, ObjectFieldDefinition>? _objectFieldDefinitionMap;
     private static Dictionary<string, InputFieldDefinition>? _inputFieldDefinitionMap;
     private static Dictionary<string, InputField>? _inputFieldMap;
+    private static Dictionary<string, InputField>? _inputFieldMapOrdinalIgnoreCase;
     private static Dictionary<string, DirectiveArgument>? _directiveArgumentMap;
+    private static Dictionary<string, DirectiveArgument>? _directiveArgumentMapOrdinalIgnoreCase;
     private static Dictionary<ParameterInfo, string>? _argumentNameMap;
     private static HashSet<MemberInfo>? _memberSet;
     private static HashSet<string>? _nameSet;
+    private static HashSet<string>? _nameSetOrdinalIgnoreCase;
 
     public static Dictionary<string, ObjectFieldDefinition> RentObjectFieldDefinitionMap()
         => Interlocked.Exchange(ref _objectFieldDefinitionMap, null) ??
@@ -46,20 +49,46 @@ internal static class TypeMemHelper
         => Interlocked.Exchange(ref _inputFieldMap, null) ??
             new Dictionary<string, InputField>(StringComparer.Ordinal);
 
+    public static Dictionary<string, InputField> RentInputFieldMapOrdinalIgnoreCase()
+        => Interlocked.Exchange(ref _inputFieldMapOrdinalIgnoreCase, null) ??
+            new Dictionary<string, InputField>(StringComparer.OrdinalIgnoreCase);
+
     public static void Return(Dictionary<string, InputField> map)
     {
         map.Clear();
-        Interlocked.CompareExchange(ref _inputFieldMap, map, null);
+
+        if (map.Comparer.Equals(StringComparer.Ordinal))
+        {
+            Interlocked.CompareExchange(ref _inputFieldMap, map, null);
+        }
+
+        if (map.Comparer.Equals(StringComparer.OrdinalIgnoreCase))
+        {
+            Interlocked.CompareExchange(ref _inputFieldMapOrdinalIgnoreCase, map, null);
+        }
     }
 
     public static Dictionary<string, DirectiveArgument> RentDirectiveArgumentMap()
         => Interlocked.Exchange(ref _directiveArgumentMap, null) ??
             new Dictionary<string, DirectiveArgument>(StringComparer.Ordinal);
 
+    public static Dictionary<string, DirectiveArgument> RentDirectiveArgumentMapOrdinalIgnoreCase()
+        => Interlocked.Exchange(ref _directiveArgumentMapOrdinalIgnoreCase, null) ??
+            new Dictionary<string, DirectiveArgument>(StringComparer.OrdinalIgnoreCase);
+
     public static void Return(Dictionary<string, DirectiveArgument> map)
     {
         map.Clear();
-        Interlocked.CompareExchange(ref _directiveArgumentMap, map, null);
+
+        if (map.Comparer.Equals(StringComparer.Ordinal))
+        {
+            Interlocked.CompareExchange(ref _directiveArgumentMap, map, null);
+        }
+
+        if (map.Comparer.Equals(StringComparer.OrdinalIgnoreCase))
+        {
+            Interlocked.CompareExchange(ref _directiveArgumentMapOrdinalIgnoreCase, map, null);
+        }
     }
 
     public static HashSet<MemberInfo> RentMemberSet()
@@ -76,10 +105,23 @@ internal static class TypeMemHelper
         => Interlocked.Exchange(ref _nameSet, null) ??
             new HashSet<string>(StringComparer.Ordinal);
 
+    public static HashSet<string> RentNameSetOrdinalIgnoreCase()
+        => Interlocked.Exchange(ref _nameSetOrdinalIgnoreCase, null) ??
+            new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
     public static void Return(HashSet<string> set)
     {
         set.Clear();
-        Interlocked.CompareExchange(ref _nameSet, set, null);
+
+        if (set.Comparer.Equals(StringComparer.Ordinal))
+        {
+            Interlocked.CompareExchange(ref _nameSet, set, null);
+        }
+
+        if (set.Comparer.Equals(StringComparer.OrdinalIgnoreCase))
+        {
+            Interlocked.CompareExchange(ref _nameSetOrdinalIgnoreCase, set, null);
+        }
     }
 
     public static Dictionary<ParameterInfo, string> RentArgumentNameMap()
@@ -101,9 +143,12 @@ internal static class TypeMemHelper
         Interlocked.Exchange(ref _objectFieldDefinitionMap, null);
         Interlocked.Exchange(ref _inputFieldDefinitionMap, null);
         Interlocked.Exchange(ref _inputFieldMap, null);
+        Interlocked.Exchange(ref _inputFieldMapOrdinalIgnoreCase, null);
         Interlocked.Exchange(ref _directiveArgumentMap, null);
+        Interlocked.Exchange(ref _directiveArgumentMapOrdinalIgnoreCase, null);
         Interlocked.Exchange(ref _argumentNameMap, null);
         Interlocked.Exchange(ref _memberSet, null);
         Interlocked.Exchange(ref _nameSet, null);
+        Interlocked.Exchange(ref _nameSetOrdinalIgnoreCase, null);
     }
 }
