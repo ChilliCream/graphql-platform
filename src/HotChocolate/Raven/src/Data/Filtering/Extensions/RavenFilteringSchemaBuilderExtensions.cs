29266703221039022c8aa0526c5265d56d77d401
Pascal Senn <senn.pasc@gmail.com>
2023-02-06 13:07:32 +0100

    Adds missing filtering operations for raven

--- src/HotChocolate/Raven/src/Data/Filtering/Extensions/RavenFilteringSchemaBuilderExtensions.cs
+++ src/HotChocolate/Raven/src/Data/Filtering/Extensions/RavenFilteringSchemaBuilderExtensions.cs
@@ -1,19 +1,57 @@
+using System.Reflection;
 using HotChocolate.Data.Filters;
 using HotChocolate.Data.Raven.Filtering;
+using HotChocolate.Types;
+using HotChocolate.Types.Descriptors;
 
 namespace HotChocolate.Data;
 
 public static class RavenFilteringSchemaBuilderExtensions
 {
     public static ISchemaBuilder AddRavenFiltering(this ISchemaBuilder schemaBuilder)
-        => schemaBuilder
-            .AddFiltering(
-                t => t.ConfigureFilterType<IListFilterInputType>(
-                        x => x.Operation(DefaultFilterOperations.All).Ignore())
-                    .AddDefaultOperations()
-                    .BindDefaultTypes()
-                    .UseRavenQueryableFilterProvider());
+        => schemaBuilder.AddFiltering<RavenConvention>();
+}
 
-    private static void UseRavenQueryableFilterProvider(this IFilterConventionDescriptor descriptor)
-        => descriptor.Provider<RavenQueryableFilterProvider>();
+internal sealed class RavenConvention : FilterConvention
+{
+    private ITypeInspector _typeInspector = null!;
+
+    protected override void Configure(IFilterConventionDescriptor descriptor)
+    {
+        descriptor
+            .AddDefaultOperations()
+            .BindDefaultTypes()
+            .Provider<RavenQueryableFilterProvider>();
+    }
+
+    protected override void Complete(IConventionContext context)
+    {
+        base.Complete(context);
+
+        _typeInspector = context.DescriptorContext.TypeInspector;
+    }
+
+    public override ExtendedTypeReference GetFieldType(MemberInfo member)
+    {
+        if (member is null)
+        {
+            throw new ArgumentNullException(nameof(member));
+        }
+
+        var runtimeType = _typeInspector.GetReturnType(member, true);
+
+        if (runtimeType.IsArrayOrList)
+        {
+            if (runtimeType.ElementType is { } &&
+                TryCreateFilterType(runtimeType.ElementType, out var elementType))
+            {
+                return _typeInspector.GetTypeRef(
+                    typeof(RavenListFilterInputType<>).MakeGenericType(elementType),
+                    TypeContext.Input,
+                    Scope);
+            }
+        }
+
+        return base.GetFieldType(member);
+    }
 }
