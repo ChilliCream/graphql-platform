c4081575d62675f38c94f5df8645faba13b2e5d0
Michael Staib <michael@chillicream.com>
2023-02-08 16:28:37 +0100

    Added more tests

--- src/HotChocolate/Core/test/Execution.Tests/Integration/Spec/MutationTests.cs
+++ src/HotChocolate/Core/test/Execution.Tests/Integration/Spec/MutationTests.cs
@@ -1,6 +1,7 @@
+using System.Threading.Tasks;
 using CookieCrumble;
-using Microsoft.Extensions.DependencyInjection;
 using HotChocolate.Types;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HotChocolate.Execution.Integration.Spec;
 
@@ -13,7 +14,7 @@ public class MutationTests
             await new ServiceCollection()
                 .AddGraphQLServer()
                 .AddQueryType(d => d.Field("a").Resolve("b"))
-                .AddMutationType<Mutation>()
+                .AddMutationType<Mutation1>()
                 .ExecuteRequestAsync(
                     """
                     mutation {
@@ -33,7 +34,40 @@ public class MutationTests
             """);
     }
 
-    public class Mutation
+    [Fact]
+    public async Task Ensure_Mutations_Child_Fields_Are_Scoped_To_Its_Parent()
+    {
+        var result =
+            await new ServiceCollection()
+                .AddGraphQLServer()
+                .AddQueryType(d => d.Field("a").Resolve("b"))
+                .AddMutationType<Mutation2>()
+                .ExecuteRequestAsync(
+                    """
+                    mutation {
+                        a { a b }
+                        b { a b }
+                    }
+                    """);
+
+        result.MatchInlineSnapshot(
+            """
+            {
+              "data": {
+                "a": {
+                  "a": true,
+                  "b": true
+                },
+                "b": {
+                  "a": true,
+                  "b": true
+                }
+              }
+            }
+            """);
+    }
+
+    public class Mutation1
     {
         private int _order;
         private bool _a;
@@ -46,7 +80,7 @@ public class MutationTests
             {
                 if (_b)
                 {
-                    throw new GraphQLException("B");
+                    throw new GraphQLException("A");
                 }
 
                 _a = true;
@@ -72,6 +106,129 @@ public class MutationTests
             await Task.Delay(100);
             _b = false;
             return Interlocked.Increment(ref _order);
+        }
+    }
+
+    public class Mutation2
+    {
+        private bool _a;
+        private bool _b;
+        internal readonly object _sync = new();
+
+        public bool IsExecutingA => _a;
+
+        public bool IsExecutingB => _a;
+
+        public async Task<SubA> A()
+        {
+            lock (_sync)
+            {
+                if (_b)
+                {
+                    throw new GraphQLException("A");
+                }
+
+                _a = true;
+            }
+
+            await Task.Delay(100);
+            _a = false;
+            return new SubA(this);
+        }
+
+        public async Task<SubB> B()
+        {
+            lock (_sync)
+            {
+                if (_a)
+                {
+                    throw new GraphQLException("B");
+                }
+
+                _b = true;
+            }
+
+            await Task.Delay(100);
+            _b = false;
+            return new SubB(this);
+        }
+    }
+
+    public class SubA
+    {
+        private readonly Mutation2 _mutation;
+
+        public SubA(Mutation2 mutation)
+        {
+            _mutation = mutation;
+        }
+
+        // will be executed in separate task.
+        public async Task<bool> A()
+        {
+            lock (_mutation._sync)
+            {
+                if (_mutation.IsExecutingB)
+                {
+                    throw new GraphQLException("A");
+                }
+            }
+
+            await Task.Delay(100);
+            return true;
+        }
+
+        // will be folded in.
+        public bool B()
+        {
+            lock (_mutation._sync)
+            {
+                if (_mutation.IsExecutingA)
+                {
+                    throw new GraphQLException("B");
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public class SubB
+    {
+        private readonly Mutation2 _mutation;
+
+        public SubB(Mutation2 mutation)
+        {
+            _mutation = mutation;
+        }
+
+        // will be executed in separate task.
+        public async Task<bool> A()
+        {
+            lock (_mutation._sync)
+            {
+                if (_mutation.IsExecutingA)
+                {
+                    throw new GraphQLException("B");
+                }
+            }
+
+            await Task.Delay(100);
+            return true;
+        }
+
+        // will be folded in.
+        public bool B()
+        {
+            lock (_mutation._sync)
+            {
+                if (_mutation.IsExecutingA)
+                {
+                    throw new GraphQLException("B");
+                }
+            }
+
+            return true;
         }
     }
 }
