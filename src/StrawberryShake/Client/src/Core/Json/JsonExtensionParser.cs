16da171e895d3a782d8b176e9f08f68f883325a4
Anton Gogolev <anton.gogolev@gmail.com>
2023-03-16 21:39:22 +0100

    Fix #4956

---
+++ src/StrawberryShake/Client/src/Core/Json/JsonExtensionParser.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace StrawberryShake.Json;
+
+public static class JsonExtensionParser
+{
+    public static IReadOnlyDictionary<string, object?>? ParseExtensions(JsonElement result)
+    {
+        if (result is { ValueKind: JsonValueKind.Object })
+        {
+            var extensions = JsonSerializationHelper.ReadValue(result);
+            return (IReadOnlyDictionary<string, object?>?)extensions;
+        }
+
+        return null;
+    }
+}
