bc11b4edbe46c703603b6923b7f8569ea8ec7792
Michael Staib <michael@chillicream.com>
2023-04-01 00:30:17 +0200

    wip

---
+++ src/HotChocolate/Fusion/src/CommandLine/Commands/SubgraphConfigSetHttpCommand.cs
@@ -0,0 +1,79 @@
+using System.CommandLine;
+using HotChocolate.Fusion.CommandLine.Helpers;
+using HotChocolate.Fusion.CommandLine.Options;
+using HotChocolate.Fusion.Composition;
+using static System.IO.Path;
+using static HotChocolate.Fusion.CommandLine.Defaults;
+
+namespace HotChocolate.Fusion.CommandLine.Commands;
+
+internal sealed class SubgraphConfigSetHttpCommand : Command
+{
+    public SubgraphConfigSetHttpCommand() : base("http")
+    {
+        Description = "Set the graphql-http settings for a subgraph.";
+
+        var url = new Option<Uri>("--url")
+        {
+            Description = "The url of the graphql-http endpoint.",
+            IsRequired = true
+        };
+
+
+        var clientName = new Option<string>("--client-name")
+        {
+            Description = "The name of the graphql-http configuration.",
+            IsRequired = false
+        };
+
+        var configFile = new SubgraphConfigFileOption();
+        var workingDirectory = new WorkingDirectoryOption();
+
+        AddOption(url);
+        AddOption(clientName);
+        AddOption(configFile);
+        AddOption(workingDirectory);
+
+        this.SetHandler(
+            ExecuteAsync,
+            Bind.FromServiceProvider<IConsole>(),
+            url,
+            clientName,
+            configFile,
+            workingDirectory,
+            Bind.FromServiceProvider<CancellationToken>());
+    }
+
+    private static async Task ExecuteAsync(
+        IConsole console,
+        Uri uri,
+        string clientName,
+        FileInfo? configFile,
+        DirectoryInfo workingDirectory,
+        CancellationToken cancellationToken)
+    {
+        configFile ??= new FileInfo(Combine(workingDirectory.FullName, ConfigFile));
+
+        if (!configFile.Exists)
+        {
+            var config = new SubgraphConfigurationDto(
+                SubgraphName,
+                new[]
+                {
+                    new HttpClientConfiguration(uri, clientName)
+                });
+            var configJson = PackageHelper.FormatSubgraphConfig(config);
+            await File.WriteAllTextAsync(configFile.FullName, configJson, cancellationToken);
+        }
+        else
+        {
+            var config = await PackageHelper.LoadSubgraphConfigAsync(configFile.FullName, cancellationToken);
+
+            var clients = config.Clients.ToList();
+            clients.Add(new HttpClientConfiguration(uri, clientName));
+
+            var configJson = PackageHelper.FormatSubgraphConfig(config with { Clients = clients });
+            await File.WriteAllTextAsync(configFile.FullName, configJson, cancellationToken);
+        }
+    }
+}
---
+++ src/HotChocolate/Fusion/src/CommandLine/Extensions.cs
@@ -0,0 +1,7 @@
+namespace HotChocolate.Fusion.CommandLine;
+
+internal static class Extensions
+{
+    public const string SubgraphPackage = ".ccspkg";
+    public const string FusionPackage = ".ccfpkg";
+}
