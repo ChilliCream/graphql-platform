a09cf6dfe236508d1940f224eea15890dddd8927
Michael Staib <michael@chillicream.com>
2023-04-17 11:53:35 +0200

    Renamed Introspection Query OperationName

--- src/HotChocolate/Utilities/src/Utilities.Introspection/ISchemaFeatures.cs
+++ src/HotChocolate/Utilities/src/Utilities.Introspection/ISchemaFeatures.cs
@@ -1,24 +1,23 @@
-namespace HotChocolate.Utilities.Introspection
+namespace HotChocolate.Utilities.Introspection;
+
+/// <summary>
+/// Represents the features that are supported by a GraphQL server.
+/// </summary>
+public interface ISchemaFeatures
 {
     /// <summary>
-    /// Represents the features that are supported by a GraphQL server.
+    /// Gets a value that indicates whether the server supports the
+    /// newer directive locations on when introspecting.
     /// </summary>
-    public interface ISchemaFeatures
-    {
-        /// <summary>
-        /// Gets a value that indicates whether the server supports the
-        /// newer directive locations on when introspecting.
-        /// </summary>
-        bool HasDirectiveLocations { get; }
+    bool HasDirectiveLocations { get; }
 
-        /// <summary>
-        /// Gets a value that indicates whether the server supports the repeatable directives.
-        /// </summary>
-        bool HasRepeatableDirectives { get; }
+    /// <summary>
+    /// Gets a value that indicates whether the server supports the repeatable directives.
+    /// </summary>
+    bool HasRepeatableDirectives { get; }
 
-        /// <summary>
-        /// Gets a value that indicates whether the server supports subscriptions.
-        /// </summary>
-        bool HasSubscriptionSupport { get; }
-    }
+    /// <summary>
+    /// Gets a value that indicates whether the server supports subscriptions.
+    /// </summary>
+    bool HasSubscriptionSupport { get; }
 }
--- src/HotChocolate/Utilities/src/Utilities.Introspection/Models/RootTypeRef.cs
+++ src/HotChocolate/Utilities/src/Utilities.Introspection/Models/RootTypeRef.cs
@@ -1,11 +1,10 @@
 #pragma warning disable CA1812
 #nullable disable
 
-namespace HotChocolate.Utilities.Introspection
+namespace HotChocolate.Utilities.Introspection;
+
+internal class RootTypeRef
 {
-    internal class RootTypeRef
-    {
-        public string Name { get; set; }
-    }
+    public string Name { get; set; }
 }
 #pragma warning restore CA1812
