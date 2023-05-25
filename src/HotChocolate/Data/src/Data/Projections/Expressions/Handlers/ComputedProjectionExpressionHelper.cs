c37ba46d350b49d59b25dadb2f4a93766283a9fb
AntonC9018 <anton.curm@gmail.com>
2023-05-25 16:14:58 +0300

    Computed projection helper

---
+++ src/HotChocolate/Data/src/Data/Projections/Expressions/Handlers/ComputedProjectionExpressionHelper.cs
@@ -0,0 +1,61 @@
+using System.Linq.Expressions;
+using HotChocolate.Data.ExpressionUtils;
+using HotChocolate.Types;
+
+namespace HotChocolate.Data.Projections.Expressions.Handlers;
+
+public static class ComputedProjectionExpressionHelper
+{
+    public const string ComputedProjectionKey = "ComputedProjection";
+
+    public sealed class ComputedProjection
+    {
+        public ComputedProjection(LambdaExpression expression)
+        {
+            Expression = expression;
+        }
+
+        public LambdaExpression Expression { get; }
+        // public List<string> Dependencies { get; }
+    }
+
+    public static ComputedProjection? GetComputedProjection(this IObjectField field)
+    {
+        if (field.ContextData.TryGetValue(ComputedProjectionKey, out var computedProjection))
+        {
+            return (ComputedProjection) computedProjection!;
+        }
+        return null;
+    }
+
+    public static bool HasComputedProjection(this IObjectField field)
+    {
+        return field.ContextData.ContainsKey(ComputedProjectionKey);
+    }
+
+    public static bool CanBeUsedInProjection(this IObjectField field)
+    {
+        return field.Member is not null || field.HasComputedProjection();
+    }
+
+    public static Expression GetProjectionExpression(
+        this IObjectField field,
+        Expression instance)
+    {
+        Expression result;
+        if (field.GetComputedProjection() is { } computedProjection)
+        {
+            result = computedProjection.Expression
+                .ReplaceParameterAndGetBody(instance);
+        }
+        else
+        {
+            result = instance.Append(field.Member);
+        }
+
+        if (result.Type.IsValueType)
+            result = Expression.Convert(result, typeof(object));
+
+        return result;
+    }
+}
