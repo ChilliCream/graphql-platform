ff75b10d82aeced98d424bc7e70fcabaf7e8b3ed
AntonC9018 <anton.curm@gmail.com>
2023-05-26 12:11:36 +0300

    LevelOperations back to a queue (no longer a breaking change too)

--- src/HotChocolate/Data/src/Data/Projections/Expressions/LevelOperations.cs
+++ src/HotChocolate/Data/src/Data/Projections/Expressions/LevelOperations.cs
@@ -5,42 +5,6 @@ using System.Linq.Expressions;
 
 namespace HotChocolate.Data.Projections.Expressions;
 
-public sealed class LevelOperations : IEnumerable<Expression>
+public sealed class LevelOperations : Queue<Expression>
 {
-    private readonly List<Expression> _operations = new();
-
-    // This is to support the queue behavior.
-    private int _startIndex = 0;
-
-    public void Enqueue(Expression expression) => _operations.Add(expression);
-    public Expression Dequeue() => _operations[_startIndex++];
-    public Expression Peek() => _operations[_startIndex];
-    public bool TryPeek(out Expression expression)
-    {
-        if (_startIndex < _operations.Count)
-        {
-            expression = _operations[_startIndex];
-            return true;
-        }
-
-        expression = default!;
-        return false;
-    }
-    public int Count => _operations.Count - _startIndex;
-
-    public Expression this[int index]
-    {
-        get => _operations[_startIndex + index];
-        set => _operations[_startIndex + index] = value;
-    }
-
-    public IEnumerator<Expression> GetEnumerator()
-    {
-        return _operations.Skip(_startIndex).GetEnumerator();
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
 }
