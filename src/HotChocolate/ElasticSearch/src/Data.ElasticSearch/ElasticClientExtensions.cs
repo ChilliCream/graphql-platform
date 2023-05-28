0974d95c785635cb635aa5298d3e5199496a1b2b
Marco Hamann <marcohamann89@hotmail.de>
2023-05-28 11:56:43 +0200

    add basic ElasticSearchParameterExpressionBuilder.cs and add capability to create search request of an elastic client

--- src/HotChocolate/ElasticSearch/src/Data.ElasticSearch/ElasticClientExtensions.cs
+++ src/HotChocolate/ElasticSearch/src/Data.ElasticSearch/ElasticClientExtensions.cs
@@ -7,4 +7,8 @@ public static class ElasticClientExtensions
     public static IExecutable<T> AsExecutable<T>(this IElasticClient client)
         where T : class
         => new NestExecutable<T>(client);
+
+    public static SearchRequest<T> GenerateSearchRequest<T>(this IElasticClient client)
+        where T : class
+        => new NestExecutable<T>(client).CreateQuery();
 }
---
+++ src/HotChocolate/ElasticSearch/src/Data.ElasticSearch/ElasticSearchParameterExpressionBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+using HotChocolate.Internal;
+using Nest;
+
+namespace HotChocolate.Data.ElasticSearch;
+
+public class ElasticSearchParameterExpressionBuilder<T> : IParameterExpressionBuilder
+{
+    /// <inheritdoc />
+    public bool CanHandle(ParameterInfo parameter) =>
+        parameter.ParameterType == typeof(SearchRequest<T>);
+
+    /// <inheritdoc />
+    public Expression Build(ParameterExpressionBuilderContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public ArgumentKind Kind => ArgumentKind.Service;
+
+    /// <inheritdoc />
+    public bool IsPure => false;
+
+    /// <inheritdoc />
+    public bool IsDefaultHandler => false;
+}
