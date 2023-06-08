a543115c2d6d755764811ae995f55e95a493c0a6
AntonC9018 <anton.curm@gmail.com>
2023-06-08 11:05:38 +0300

    Add doc comment for NativeType

--- src/HotChocolate/Core/src/Types/Types/NativeType.cs
+++ src/HotChocolate/Core/src/Types/Types/NativeType.cs
@@ -1,6 +1,9 @@
 namespace HotChocolate.Types;
 
+/// <summary>
+/// Can be used to make the system imply the Graph QL object type
+/// from the underlying regular .NET type.
+/// </summary>
 public class NativeType<T> : FluentWrapperType
 {
-    public NativeType() { }
 }
