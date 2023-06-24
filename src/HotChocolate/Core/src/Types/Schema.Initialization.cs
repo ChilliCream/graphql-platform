ecae0d7fdaa860cafc576f565e1fac499cdf247a
Pascal Senn <senn.pasc@gmail.com>
2023-06-24 14:58:44 +0200

    Fixed memory leak in Schema

--- src/HotChocolate/Core/src/Types/Schema.Initialization.cs
+++ src/HotChocolate/Core/src/Types/Schema.Initialization.cs
@@ -9,7 +9,7 @@ namespace HotChocolate;
 
 public partial class Schema
 {
-    private readonly Action<ISchemaTypeDescriptor> _configure;
+    private Action<ISchemaTypeDescriptor> _configure;
     private bool _sealed;
 
     protected internal Schema()
@@ -24,16 +24,28 @@ public partial class Schema
 
     protected virtual void Configure(ISchemaTypeDescriptor descriptor) { }
 
-    protected sealed override SchemaTypeDefinition CreateDefinition(
-        ITypeDiscoveryContext context)
+    protected sealed override SchemaTypeDefinition CreateDefinition(ITypeDiscoveryContext context)
     {
-        var descriptor = SchemaTypeDescriptor.New(
-            context.DescriptorContext,
-            GetType());
+        var descriptor = SchemaTypeDescriptor.New(context.DescriptorContext, GetType());
 
         _configure(descriptor);
 
         return descriptor.CreateDefinition();
+    }
+
+    protected override void OnAfterInitialize(
+        ITypeDiscoveryContext context,
+        DefinitionBase definition)
+    {
+        base.OnAfterInitialize(context, definition);
+
+        // we clear the configuration delegate to make sure that we do not hold on to any references
+        // if we do not do this all the instances used during initialization will be kept in memory
+        // until the schema is phased out.
+        // We do this in OnAfterInitialized because after this point the schema is marked as
+        // initialized. This means that a subsequent call to Initialize will throw anyway and
+        // therefore we do not need to keep the configuration delegate.
+        _configure = null;
     }
 
     protected override void OnRegisterDependencies(
@@ -46,10 +58,7 @@ public partial class Schema
         {
             foreach (var directive in definition.Directives)
             {
-                context.Dependencies.Add(
-                    new(
-                        directive.Type,
-                        TypeDependencyFulfilled.Completed));
+                context.Dependencies.Add(new(directive.Type, TypeDependencyFulfilled.Completed));
             }
         }
 
@@ -65,15 +74,12 @@ public partial class Schema
     {
         base.OnCompleteType(context, definition);
 
-        Directives = DirectiveCollection.CreateAndComplete(
-            context,
-            this,
-            definition.GetDirectives());
+        Directives = DirectiveCollection
+            .CreateAndComplete(context, this, definition.GetDirectives());
         Services = context.Services;
     }
 
-    internal void CompleteSchema(
-        SchemaTypesDefinition schemaTypesDefinition)
+    internal void CompleteSchema(SchemaTypesDefinition schemaTypesDefinition)
     {
         if (schemaTypesDefinition is null)
         {
