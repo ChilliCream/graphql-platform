4376060abf9f2ecfa59f48fe5c4034c96d39b5af
Ben McCallum <ben.mccallum@live.com.au>
2023-07-01 12:09:58 +1000

    Publicly expose ability to register an error type on a mutation field

--- src/HotChocolate/Core/src/Types.Mutations/Extensions/MutationObjectFieldDescriptorExtensions.cs
+++ src/HotChocolate/Core/src/Types.Mutations/Extensions/MutationObjectFieldDescriptorExtensions.cs
@@ -406,28 +406,68 @@ public static class MutationObjectFieldDescriptorExtensions
             throw new ArgumentNullException(nameof(errorType));
         }
 
-        descriptor.Extend().OnBeforeCreate(ConfigureField);
+        descriptor.Extend().OnBeforeCreate((ctx, d) => d.AddErrorType(ctx, errorType));
 
         return descriptor;
+    }
+}
 
-        void ConfigureField(IDescriptorContext c, ObjectFieldDefinition d)
+/// <summary>
+/// Provides extensions to the <see cref="ObjectFieldDefinition"/> for the mutation convention.
+/// </summary>
+public static class ObjectFieldDefinitionExtensions
+{
+    /// <summary>
+    /// Registers an error type with this field for the mutation convention
+    /// to pick up.
+    /// </summary>
+    /// <param name="fieldDefinition">The object field definition.</param>
+    /// <param name="descriptorContext">The descriptor context.</param>
+    /// <param name="errorType">
+    /// The type of the exception, the class with factory methods or the error with an exception
+    /// as the argument. See the examples in <see cref="Error"/>.
+    /// </param>
+    public static void AddErrorType(
+        this ObjectFieldDefinition fieldDefinition,
+        IDescriptorContext descriptorContext,
+        Type errorType)
+    {
+        if (fieldDefinition is null)
         {
-            var definitions = ErrorFactoryCompiler.Compile(errorType);
+            throw new ArgumentNullException(nameof(fieldDefinition));
+        }
 
-            if (!d.ContextData.TryGetValue(ErrorDefinitions, out var value) ||
-                value is not List<ErrorDefinition> errorFactories)
-            {
-                errorFactories = new List<ErrorDefinition>();
-                d.ContextData[ErrorDefinitions] = errorFactories;
-            }
+        if (descriptorContext is null)
+        {
+            throw new ArgumentNullException(nameof(descriptorContext));
+        }
 
-            errorFactories.AddRange(definitions);
+        if (errorType is null)
+        {
+            throw new ArgumentNullException(nameof(errorType));
+        }
 
-            foreach (var definition in definitions)
-            {
-                var typeRef = c.TypeInspector.GetTypeRef(definition.SchemaType);
-                d.Dependencies.Add(new TypeDependency(typeRef));
-            }
+        if (descriptorContext.ContextData[MutationContextDataKeys.Options] == null)
+        {
+            throw new Exception("Mutation convention isn't turned on");
+            // TODO: Proper exception, or maybe we just let this through?
+        }
+
+        var definitions = ErrorFactoryCompiler.Compile(errorType);
+
+        if (!fieldDefinition.ContextData.TryGetValue(ErrorDefinitions, out var value) ||
+            value is not List<ErrorDefinition> errorFactories)
+        {
+            errorFactories = new List<ErrorDefinition>();
+            fieldDefinition.ContextData[ErrorDefinitions] = errorFactories;
+        }
+
+        errorFactories.AddRange(definitions);
+
+        foreach (var definition in definitions)
+        {
+            var typeRef = descriptorContext.TypeInspector.GetTypeRef(definition.SchemaType);
+            fieldDefinition.Dependencies.Add(new TypeDependency(typeRef));
         }
     }
 }
