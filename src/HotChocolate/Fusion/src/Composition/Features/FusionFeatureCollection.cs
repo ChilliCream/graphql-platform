87b2723f2e0f2eea1d3ecf921e73a7e015578cf5
Michael Staib <michael@chillicream.com>
2023-07-17 16:41:55 +0200

    edits

--- src/HotChocolate/Fusion/src/Composition/Features/FusionFeatureCollection.cs
+++ src/HotChocolate/Fusion/src/Composition/Features/FusionFeatureCollection.cs
@@ -2,10 +2,22 @@ using System.Collections;
 
 namespace HotChocolate.Fusion.Composition.Features;
 
-public sealed class FusionFeatureCollection : IReadOnlyList<IFusionFeature>
+/// <summary>
+/// Represents a collection of fusion composition features.
+/// </summary>
+public sealed class FusionFeatureCollection : IReadOnlyCollection<IFusionFeature>
 {
     private readonly Dictionary<Type, IFusionFeature> _features = new();
 
+    /// <summary>
+    /// Initializes a new instance of <see cref="FusionFeatureCollection"/>.
+    /// </summary>
+    /// <param name="features">
+    /// The features that are supported by composition.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="features"/> is <c>null</c>.
+    /// </exception>
     public FusionFeatureCollection(params IFusionFeature[] features)
     {
         if (features == null)
@@ -19,6 +31,15 @@ public sealed class FusionFeatureCollection : IReadOnlyList<IFusionFeature>
         }
     }
 
+    /// <summary>
+    /// Initializes a new instance of <see cref="FusionFeatureCollection"/>.
+    /// </summary>
+    /// <param name="features">
+    /// The features that are supported by composition.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="features"/> is <c>null</c>.
+    /// </exception>
     public FusionFeatureCollection(IEnumerable<IFusionFeature> features)
     {
         if (features == null)
@@ -32,15 +53,44 @@ public sealed class FusionFeatureCollection : IReadOnlyList<IFusionFeature>
         }
     }
 
-    public IFusionFeature this[int index] => throw new NotImplementedException();
-
+    /// <summary>
+    /// Specifies if the specified feature is supported.
+    /// </summary>
+    /// <param name="feature">
+    /// The feature that shall be checked.
+    /// </param>
+    /// <typeparam name="TFeature">
+    /// The type of the feature that shall be checked.
+    /// </typeparam>
+    /// <returns>
+    /// <c>true</c> if the specified feature is supported; otherwise, <c>false</c>.
+    /// </returns>
     public bool IsSupported<TFeature>(IFusionFeature feature) where TFeature : IFusionFeature
         => _features.TryGetValue(typeof(TFeature), out var registeredFeature) &&
             registeredFeature.Equals(feature);
 
+    /// <summary>
+    /// Specifies if the specified feature is supported.
+    /// </summary>
+    /// <typeparam name="TFeature">
+    /// The type of the feature that shall be checked.
+    /// </typeparam>
+    /// <returns></returns>
     public bool IsSupported<TFeature>() where TFeature : IFusionFeature
         => _features.ContainsKey(typeof(TFeature));
 
+    /// <summary>
+    /// Checks if a feature is supported and returns it's configured instance.
+    /// </summary>
+    /// <param name="feature">
+    /// The configured feature instance.
+    /// </param>
+    /// <typeparam name="TFeature">
+    /// The type of the feature that shall be checked.
+    /// </typeparam>
+    /// <returns>
+    /// <c>true</c> if the specified feature is configured; otherwise, <c>false</c>.
+    /// </returns>
     public bool TryGetFeature<TFeature>(out TFeature feature) where TFeature : IFusionFeature
     {
         if (_features.TryGetValue(typeof(TFeature), out var registeredFeature))
@@ -53,13 +103,23 @@ public sealed class FusionFeatureCollection : IReadOnlyList<IFusionFeature>
         return false;
     }
 
+    /// <summary>
+    /// Gets the number of features that are configured.
+    /// </summary>
     public int Count => _features.Count;
 
+    /// <summary>
+    /// Gets an enumerator for this feature collection.
+    /// </summary>
+    /// <returns></returns>
     public IEnumerator<IFusionFeature> GetEnumerator()
         => _features.Values.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator()
         => GetEnumerator();
     
+    /// <summary>
+    /// Gets an empty feature collection.
+    /// </summary>
     public static FusionFeatureCollection Empty { get; } = new(Array.Empty<IFusionFeature>());
 }
