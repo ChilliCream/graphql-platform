a38ca8a51c3e7a7e6f882c5e68ef0f2ef9b4b3ba
Michael Staib <michael@chillicream.com>
2023-07-17 18:16:23 +0200

    Introduce Fusion Composition Settings File

--- src/HotChocolate/Fusion/test/CommandLine.Tests/ComposeCommandTests.cs
+++ src/HotChocolate/Fusion/test/CommandLine.Tests/ComposeCommandTests.cs
@@ -7,6 +7,7 @@ using HotChocolate.Fusion.CommandLine.Helpers;
 using HotChocolate.Fusion.Composition;
 using HotChocolate.Fusion.Shared;
 using static HotChocolate.Fusion.Shared.DemoProjectSchemaExtensions;
+using Path = HotChocolate.Path;
 
 namespace CommandLine.Tests;
 
@@ -114,5 +115,116 @@ public class ComposeCommandTests : CommandTestBase
         }
 
         snapshot.MatchSnapshot();
+    }
+    
+    [Fact]
+    public async Task Ensure_Default_Settings_Are_Included()
+    {
+        // arrange
+        using var demoProject = await DemoProject.CreateAsync();
+        var accountConfig = demoProject.Accounts.ToConfiguration(AccountsExtensionSdl);
+        var account = CreateFiles(accountConfig);
+        var subgraphPackageFile = CreateTempFile();
+
+        await PackageHelper.CreateSubgraphPackageAsync(
+            subgraphPackageFile,
+            new SubgraphFiles(
+                account.SchemaFile,
+                account.TransportConfigFile,
+                account.ExtensionFiles));
+
+        var packageFile = CreateTempFile(Extensions.FusionPackage);
+
+        // act
+        var app = App.CreateBuilder().Build();
+        await app.InvokeAsync(new[] { "compose", "-p", packageFile, "-s", subgraphPackageFile });
+
+        // assert
+        Assert.True(File.Exists(packageFile));
+
+        await using var package = FusionGraphPackage.Open(packageFile, FileAccess.Read);
+
+        using var settings = await package.GetFusionGraphSettingsAsync(); 
+        settings.RootElement.ToString().MatchSnapshot(extension: ".json");
+    }
+    
+    [Fact]
+    public async Task Ensure_Legacy_Node_Switch_Is_Recognized()
+    {
+        // arrange
+        using var demoProject = await DemoProject.CreateAsync();
+        var accountConfig = demoProject.Accounts.ToConfiguration(AccountsExtensionSdl);
+        var account = CreateFiles(accountConfig);
+        var subgraphPackageFile = CreateTempFile();
+
+        await PackageHelper.CreateSubgraphPackageAsync(
+            subgraphPackageFile,
+            new SubgraphFiles(
+                account.SchemaFile,
+                account.TransportConfigFile,
+                account.ExtensionFiles));
+
+        var packageFile = CreateTempFile(Extensions.FusionPackage);
+
+        // act
+        var app = App.CreateBuilder().Build();
+        await app.InvokeAsync(new[] { "compose", "-p", packageFile, "-s", subgraphPackageFile, "--enable-nodes" });
+
+        // assert
+        Assert.True(File.Exists(packageFile));
+
+        await using var package = FusionGraphPackage.Open(packageFile, FileAccess.Read);
+
+        using var settings = await package.GetFusionGraphSettingsAsync(); 
+        settings.RootElement.ToString().MatchSnapshot(extension: ".json");
+    }
+    
+    [Fact]
+    public async Task Ensure_Settings_Are_Included()
+    {
+        // arrange
+        using var demoProject = await DemoProject.CreateAsync();
+        var accountConfig = demoProject.Accounts.ToConfiguration(AccountsExtensionSdl);
+        var account = CreateFiles(accountConfig);
+        var subgraphPackageFile = CreateTempFile();
+
+        await PackageHelper.CreateSubgraphPackageAsync(
+            subgraphPackageFile,
+            new SubgraphFiles(
+                account.SchemaFile,
+                account.TransportConfigFile,
+                account.ExtensionFiles));
+
+        var packageFile = CreateTempFile(Extensions.FusionPackage);
+        var settingsFile = System.IO.Path.Combine(
+            System.IO.Path.GetDirectoryName(packageFile)!,
+            $"{System.IO.Path.GetFileNameWithoutExtension(packageFile)}-settings.json");
+        
+        await File.WriteAllTextAsync(
+            settingsFile,
+            """
+            {
+              "fusionTypePrefix": null,
+              "fusionTypeSelf": false,
+              "nodeField": { "enabled": true },
+              "tagDirective": {
+                "enabled": true,
+                "makePublic": true,
+                "exclude": ["internal"]
+              }
+            }
+            """);
+
+        // act
+        var app = App.CreateBuilder().Build();
+        await app.InvokeAsync(new[] { "compose", "-p", packageFile, "-s", subgraphPackageFile });
+
+        // assert
+        Assert.True(File.Exists(packageFile));
+
+        await using var package = FusionGraphPackage.Open(packageFile, FileAccess.Read);
+
+        using var settings = await package.GetFusionGraphSettingsAsync(); 
+        settings.RootElement.ToString().MatchSnapshot(extension: ".json");
     }
 }
