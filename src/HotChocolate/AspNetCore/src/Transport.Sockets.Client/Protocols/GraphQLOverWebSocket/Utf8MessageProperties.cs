ea07f05afa10016d9740892a22ba3c12e2d401dd
Michael Staib <michael@chillicream.com>
2023-07-18 16:39:27 +0200

    cleanup

--- src/HotChocolate/AspNetCore/src/Transport.Sockets.Client/Protocols/GraphQLOverWebSocket/Utf8MessageProperties.cs
+++ src/HotChocolate/AspNetCore/src/Transport.Sockets.Client/Protocols/GraphQLOverWebSocket/Utf8MessageProperties.cs
@@ -1,15 +1,13 @@
 using System;
-using System.Text.Unicode;
+using HotChocolate.Transport.Serialization;
 
-namespace HotChocolate.Transport.Serialization;
+namespace HotChocolate.Transport.Sockets.Client.Protocols.GraphQLOverWebSocket;
 
-public static class Utf8MessageProperties
+internal static class Utf8MessageProperties
 {
     // This uses C# compiler's ability to refer to static data directly.
     // For more information see https://vcsjones.dev/2019/02/01/csharp-readonly-span-bytes-static
     public static ReadOnlySpan<byte> IdProp => Utf8GraphQLRequestProperties.IdProp;
-
-    public static ReadOnlySpan<byte> QueryProp => Utf8GraphQLRequestProperties.QueryProp;
 
     public static ReadOnlySpan<byte> TypeProp => "type"u8;
 
@@ -20,8 +18,4 @@ public static class Utf8MessageProperties
     public static ReadOnlySpan<byte> ErrorsProp => Utf8GraphQLResultProperties.ErrorsProp;
 
     public static ReadOnlySpan<byte> ExtensionsProp => Utf8GraphQLRequestProperties.ExtensionsProp;
-
-    public static ReadOnlySpan<byte> VariablesProp => Utf8GraphQLRequestProperties.VariablesProp;
-
-    public static ReadOnlySpan<byte> OperationNameProp => Utf8GraphQLRequestProperties.OperationNameProp;
 }
