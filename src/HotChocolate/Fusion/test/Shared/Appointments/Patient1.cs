7d375fe851c825b5d682d0bc792da31932e9da20
Marco Hamann <marcohamann89@hotmail.de>
2023-07-18 18:45:04 +0200

    Fixed issue with variable forwarding in nested fields. (#6348)
    Co-authored-by: Michael Staib <michael@chillicream.com>

--- src/HotChocolate/Fusion/test/Shared/Appointments/Patient1.cs
+++ src/HotChocolate/Fusion/test/Shared/Appointments/Patient1.cs
@@ -1,3 +1,4 @@
+using HotChocolate.Types;
 using HotChocolate.Types.Relay;
 
 namespace HotChocolate.Fusion.Shared.Appointments;
@@ -5,4 +6,12 @@ namespace HotChocolate.Fusion.Shared.Appointments;
 public class Patient1 : IPatient
 {
     [ID<Patient1>] public int Id { get; set;}
+
+    [UsePaging]
+    public IEnumerable<Appointment> Appointments => new List<Appointment>()
+    {
+        new Appointment(),
+        new Appointment(),
+        new Appointment(),
+    };
 }
