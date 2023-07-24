3aa75eaf1bd564ffb01da737b50cc635a95fb911
Michael Staib <michael@chillicream.com>
2023-07-24 12:19:58 +0200

    edits

--- src/HotChocolate/Fusion/src/Core/Execution/Nodes/QueryPlanNodeKindFormatter.cs
+++ src/HotChocolate/Fusion/src/Core/Execution/Nodes/QueryPlanNodeKindFormatter.cs
@@ -3,19 +3,29 @@ namespace HotChocolate.Fusion.Execution.Nodes;
 
 internal static class QueryPlanNodeKindFormatter
 {
+    private static ReadOnlySpan<byte> Parallel => "Parallel"u8;
+    private static ReadOnlySpan<byte> Sequence => "Sequence"u8;
+    private static ReadOnlySpan<byte> Resolve => "Resolve"u8;
+    private static ReadOnlySpan<byte> ResolveByKeyBatch => "ResolveByKeyBatch"u8;
+    private static ReadOnlySpan<byte> ResolveNode => "ResolveNode"u8;
+    private static ReadOnlySpan<byte> Subscribe => "Subscribe"u8;
+    private static ReadOnlySpan<byte> Introspect => "Introspect"u8;
+    private static ReadOnlySpan<byte> Compose => "Compose"u8;
+    private static ReadOnlySpan<byte> If => "If"u8;
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static string Format(this QueryPlanNodeKind kind)
+    public static ReadOnlySpan<byte> Format(this QueryPlanNodeKind kind)
         => kind switch
         {
-            QueryPlanNodeKind.Parallel => nameof(QueryPlanNodeKind.Parallel),
-            QueryPlanNodeKind.Sequence => nameof(QueryPlanNodeKind.Sequence),
-            QueryPlanNodeKind.Resolve => nameof(QueryPlanNodeKind.Resolve),
-            QueryPlanNodeKind.ResolveByKeyBatch => nameof(QueryPlanNodeKind.ResolveByKeyBatch),
-            QueryPlanNodeKind.ResolveNode => nameof(QueryPlanNodeKind.ResolveNode),
-            QueryPlanNodeKind.Subscribe => nameof(QueryPlanNodeKind.Subscribe),
-            QueryPlanNodeKind.Introspect => nameof(QueryPlanNodeKind.Introspect),
-            QueryPlanNodeKind.Compose => nameof(QueryPlanNodeKind.Compose),
-            QueryPlanNodeKind.If => nameof(QueryPlanNodeKind.If),
+            QueryPlanNodeKind.Parallel => Parallel,
+            QueryPlanNodeKind.Sequence => Sequence,
+            QueryPlanNodeKind.Resolve => Resolve,
+            QueryPlanNodeKind.ResolveByKeyBatch => ResolveByKeyBatch,
+            QueryPlanNodeKind.ResolveNode => ResolveNode,
+            QueryPlanNodeKind.Subscribe => Subscribe,
+            QueryPlanNodeKind.Introspect => Introspect,
+            QueryPlanNodeKind.Compose => Compose,
+            QueryPlanNodeKind.If => If,
             _ => throw new ArgumentOutOfRangeException(nameof(kind)),
         };
 }
