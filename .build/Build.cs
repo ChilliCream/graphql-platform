7b943f7d7dc9c4443d769ae61c22cac81183f412
Michael Staib <michael@chillicream.com>
2023-08-18 00:07:41 +0200

    edits

--- .build/Build.cs
+++ .build/Build.cs
@@ -5,6 +5,11 @@ using Nuke.Common.Execution;
 using Nuke.Common.Tools.DotNet;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 using static Helpers;
+using Newtonsoft.Json;
+using Nuke.Common.ProjectModel;
+using System.Linq;
+using System;
+using System.IO;
 
 [UnsetVisualStudioEnvironmentVariables]
 partial class Build : NukeBuild
@@ -58,4 +63,42 @@ partial class Build : NukeBuild
             DotNetBuildSonarSolution(AllSolutionFile);
             DotNetRestore(c => c.SetProjectFile(AllSolutionFile));
         });
+
+
+    Target GenerateMatrix => _ => _
+        .DependsOn(Clean, Restore)
+        .Executes(() =>
+        {
+            DotNetBuildSonarSolution(AllSolutionFile);
+            Solution all = ProjectModelTasks.ParseSolution(AllSolutionFile);
+
+            var testProjects = all.GetProjects("*.Tests")
+                .Select(p => new TestProject
+                {
+                    Name = Path.GetFileNameWithoutExtension(p.Path),
+                    Path = p.Path
+                })
+                .ToList();
+
+            var matrix = new
+            {
+                test = testProjects.Select(p => new
+                {
+                    name = p.Name,
+                    path = p.Path
+                }).ToArray()
+            };
+
+            File.WriteAllText(
+                RootDirectory / ".github" / "matrix.json",
+                JsonConvert.SerializeObject(matrix));
+        });
+}
+
+
+[Serializable]
+public class TestProject
+{
+    public string Name { get; set; }
+    public string Path { get; set; }
 }
