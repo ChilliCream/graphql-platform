43888e403a31a0a5ab919861eb521659da96a1ae
Michael Staib <michael@chillicream.com>
2023-08-17 08:56:27 +0200

    edits

--- src/HotChocolate/Data/src/Data/Projections/Extensions/ProjectionObjectFieldDescriptorExtensions.cs
+++ src/HotChocolate/Data/src/Data/Projections/Extensions/ProjectionObjectFieldDescriptorExtensions.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Reflection;
-using System.Security.AccessControl;
 using System.Threading;
 using System.Threading.Tasks;
 using HotChocolate.Configuration;
