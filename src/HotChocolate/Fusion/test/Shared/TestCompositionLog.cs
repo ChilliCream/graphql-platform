990e08e838b476e74041953f3fc2eb47def8de62
Michael Staib <michael@chillicream.com>
2023-08-30 09:47:52 +0200

    Introduced subgraph consistency check. (#6493)

--- src/HotChocolate/Fusion/test/Shared/TestCompositionLog.cs
+++ src/HotChocolate/Fusion/test/Shared/TestCompositionLog.cs
@@ -24,3 +24,20 @@ public sealed class TestCompositionLog : ICompositionLog
         _output.WriteLine(entry.Message);
     }
 }
+
+public sealed class ErrorCompositionLog : ICompositionLog
+{
+    private readonly List<LogEntry> _errors = new();
+
+    public bool HasErrors => _errors.Count > 0;
+
+    public List<LogEntry> Errors => _errors;
+
+    public void Write(LogEntry entry)
+    {
+        if(entry.Severity == LogSeverity.Error)
+        {
+            _errors.Add(entry);
+        }
+    }
+}
