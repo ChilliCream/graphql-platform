49ade2117a1babed950d3e4b74abe8181b51ae77
Michael Staib <michael@chillicream.com>
2023-09-06 16:07:35 +0200

    Refactored query planner to allow top queries. (#6508)

---
+++ src/HotChocolate/Fusion/src/Core/Planning/Steps/SelectionPath.cs
@@ -0,0 +1,45 @@
+using System.Text;
+using HotChocolate.Execution.Processing;
+
+namespace HotChocolate.Fusion.Planning;
+
+internal sealed class SelectionPath
+{
+    public SelectionPath(ISelection selection)
+        : this(selection, null)
+    {
+    }
+    
+    private SelectionPath(ISelection selection, SelectionPath? parent)
+    {
+        Selection = selection;
+        Parent = parent;
+    }
+
+    public SelectionPath? Parent { get; }
+    
+    public ISelection Selection { get; }
+
+    public SelectionPath Append(ISelection selection)
+        => new(selection, this);
+
+    public override string ToString()
+    {
+        if (Parent is null)
+        {
+            return $"/{Selection.ResponseName}";
+        }
+        
+        var sb = new StringBuilder();
+        var current = this;
+
+        while (current is not null)
+        {
+            sb.Append('/');
+            sb.Append(Selection.ResponseName);
+            current = current.Parent;
+        }
+
+        return sb.ToString();
+    }
+}
