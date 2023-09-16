35a5c0c94f3310eaad6eadcb2f9dc4ad20ebda51
Michael Staib <michael@chillicream.com>
2023-09-16 16:48:22 -0700

    Introduced Fusion Export Command

---
+++ src/HotChocolate/Fusion/src/CommandLine/Commands/ExportGraphCommand.cs
@@ -0,0 +1,65 @@
+using System.CommandLine;
+using System.Text;
+using HotChocolate.Fusion.CommandLine.Helpers;
+using HotChocolate.Utilities;
+using static System.IO.Path;
+using static HotChocolate.Fusion.CommandLine.Extensions;
+
+namespace HotChocolate.Fusion.CommandLine.Commands;
+
+internal sealed class ExportGraphCommand : Command
+{
+    public ExportGraphCommand() : base("graph")
+    {
+        var fusionPackageFile = new Option<FileInfo?>("--package-file");
+        fusionPackageFile.AddAlias("--package");
+        fusionPackageFile.AddAlias("-p");
+
+        var graphFile = new Option<FileInfo?>("--file");
+        graphFile.AddAlias("-f");
+        
+        AddOption(fusionPackageFile);
+        AddOption(graphFile);
+
+        this.SetHandler(
+            ExecuteAsync,
+            Bind.FromServiceProvider<IConsole>(),
+            fusionPackageFile,
+            graphFile,
+            Bind.FromServiceProvider<CancellationToken>());
+    }
+
+    private static async Task ExecuteAsync(
+        IConsole console,
+        FileInfo? packageFile,
+        FileInfo? graphFile,
+        CancellationToken cancellationToken)
+    {
+        packageFile ??= new FileInfo(Combine(Environment.CurrentDirectory, "gateway" + FusionPackage));
+        
+        if (!packageFile.Exists)
+        {
+            if (Directory.Exists(packageFile.FullName))
+            {
+                packageFile = new FileInfo(Combine(packageFile.FullName, "gateway" + FusionPackage));   
+            }
+            else if (!packageFile.Extension.EqualsOrdinal(FusionPackage) &&
+                !packageFile.Extension.EqualsOrdinal(ZipPackage))
+            {
+                packageFile = new FileInfo(packageFile.FullName + FusionPackage);   
+            }
+
+            if (!packageFile.Exists)
+            {
+                console.WriteLine($"The package file `{packageFile.FullName}` does not exist.");
+                return;
+            }
+        }
+
+        graphFile ??= new FileInfo(Combine(packageFile.DirectoryName!, "fusion.graphql"));
+        
+        await using var package = FusionGraphPackage.Open(packageFile.FullName);
+        var graph = await package.GetFusionGraphAsync(cancellationToken);
+        await File.WriteAllTextAsync(graphFile.FullName, graph.ToString(true), Encoding.UTF8, cancellationToken);
+    }
+}
