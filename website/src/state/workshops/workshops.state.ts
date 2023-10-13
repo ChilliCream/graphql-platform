064e07ebd4bf5308b23e8af66d4a13e14764e05e
martin <martin@chillicream.com>
2023-10-14 01:13:31 +0200

    add online workshop Vienna (#6602)

--- website/src/state/workshops/workshops.state.ts
+++ website/src/state/workshops/workshops.state.ts
@@ -16,18 +16,18 @@ export type WorkshopsState = Workshop[];
 
 export const initialState: WorkshopsState = [
   {
-    id: "online-231010",
-    title: "Fullstack GraphQL",
+    id: "online-231106",
+    title: "Fullstack GraphQL Vienna",
     teaser:
       "Learn to build modern APIs like Facebook and Netflix in our Fullstack GraphQL workshop.",
-    date: "10 - 11 October 2023",
+    date: "6 - 7 November 2023",
     host: "ONLINE",
-    place: "9AM-5PM CDT",
-    url: "https://www.eventbrite.com/e/fullstack-graphql-usa-mountain-time-tickets-710559090367",
+    place: "9AM-5PM CET",
+    url: "https://www.eventbrite.com/e/fullstack-graphql-vienna-tickets-734869182507",
     image: "online",
-    hero: false,
+    hero: true,
     banner: true,
-    active: isActive("2023-10-10"),
+    active: isActive("2023-11-06"),
   },
 ];
 
