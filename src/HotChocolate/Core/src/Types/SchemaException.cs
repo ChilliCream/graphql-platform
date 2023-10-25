f47202ca1ec891591fdfcb88ace9d2f81be7e69a
Michael Staib <michael@chillicream.com>
2023-10-24 19:56:18 -0700

    edits

--- src/HotChocolate/Core/src/Types/SchemaException.cs
+++ src/HotChocolate/Core/src/Types/SchemaException.cs
@@ -2,15 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.Text;
 using HotChocolate.Utilities;
+using static HotChocolate.Properties.TypeResources;
 
 namespace HotChocolate;
 
-[Serializable]
-public class SchemaException
-    : Exception
+public sealed class SchemaException : Exception
 {
     public SchemaException(params ISchemaError[] errors)
         : base(CreateErrorMessage(errors))
@@ -19,33 +17,25 @@ public class SchemaException
         Debug.WriteLine(Message);
     }
 
-    public SchemaException(IEnumerable<ISchemaError> errors)
-        : base(CreateErrorMessage(errors.ToArray()))
+    public SchemaException(IReadOnlyList<ISchemaError> errors)
+        : base(CreateErrorMessage(errors))
     {
         Errors = errors.ToArray();
         Debug.WriteLine(Message);
     }
-
-    protected SchemaException(
-        SerializationInfo info,
-        StreamingContext context)
-        : base(info, context)
-    {
-    }
-
+    
     public IReadOnlyList<ISchemaError> Errors { get; }
-
-    // TODO : resources
+    
     private static string CreateErrorMessage(IReadOnlyList<ISchemaError> errors)
     {
         if (errors is null || errors.Count == 0)
         {
-            return "Unexpected schema exception occurred.";
+            return SchemaException_UnexpectedError;
         }
 
         var message = new StringBuilder();
 
-        message.AppendLine("For more details look at the `Errors` property.");
+        message.AppendLine(SchemaException_ErrorSummaryText);
         message.AppendLine();
 
         for (var i = 0; i < errors.Count; i++)
