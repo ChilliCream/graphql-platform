e32a2f1bc2d48398d99a8a241e888df007ad9dd0
Michael Staib <michael@chillicream.com>
2023-11-01 14:31:17 +0100

    Removed Legacy Components

--- src/HotChocolate/CodeGeneration/src/CodeGeneration/Types/PagingKind.cs
+++
@@ -1,8 +0,0 @@
-namespace HotChocolate.CodeGeneration.Types;
-
-public enum PagingKind
-{
-    Cursor,
-    Offset,
-    None
-}
--- src/HotChocolate/Neo4J/src/Data/Language/Interfaces/IVisitor.cs
+++
@@ -1,8 +0,0 @@
-namespace HotChocolate.Data.Neo4J.Language;
-
-public interface IVisitor
-{
-    void Enter(IVisitable visitable);
-
-    void Leave(IVisitable visitable);
-}
