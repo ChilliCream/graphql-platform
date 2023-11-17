2b9e2c3a958ee3a927e1cc53ae5b30c1db852680
Michael Staib <michael@chillicream.com>
2023-11-17 16:59:05 +0200

    edits

--- src/HotChocolate/ApolloFederation/src/ApolloFederation/ProvidesAttribute.cs
+++ src/HotChocolate/ApolloFederation/src/ApolloFederation/ProvidesAttribute.cs
@@ -5,17 +5,29 @@ using static HotChocolate.ApolloFederation.ThrowHelper;
 namespace HotChocolate.ApolloFederation;
 
 /// <summary>
-/// The @provides directive is used to annotate the expected returned fieldset
-/// from a field on a base type that is guaranteed to be selectable by the gateway.
-///
+/// <code>
+/// directive @provides(fields: _FieldSet!) on FIELD_DEFINITION
+/// </code>
+/// 
+/// The @provides directive is a router optimization hint specifying field set that
+/// can be resolved locally at the given subgraph through this particular query path. This 
+/// allows you to expose only a subset of fields from the underlying entity type to be selectable
+/// from the federated schema without the need to call other subgraphs. Provided fields specified
+/// in the directive field set should correspond to a valid field on the underlying GraphQL 
+/// interface/object type. @provides directive can only be used on fields returning entities.
 /// <example>
-/// type Review @key(fields: "id") {
-///   product: Product @provides(fields: "name")
+/// type Foo @key(fields: "id") {
+///     id: ID!
+///     # implies name field can be resolved locally 
+///     bar: Bar @provides(fields: "name")
+///     # name fields are external 
+///     # so will be fetched from other subgraphs
+///     bars: [Bar]
 /// }
 ///
-/// extend type Product @key(fields: "upc") {
-///   upc: String @external
-///   name: String @external
+/// type Bar @key(fields: "id") {
+///     id: ID!
+///     name: String @external
 /// }
 /// </example>
 /// </summary>
--- src/HotChocolate/ApolloFederation/src/ApolloFederation/RequiresDirectiveType.cs
+++ src/HotChocolate/ApolloFederation/src/ApolloFederation/RequiresDirectiveType.cs
@@ -4,17 +4,22 @@ using HotChocolate.ApolloFederation.Properties;
 namespace HotChocolate.ApolloFederation;
 
 /// <summary>
-/// The @requires directive is used to annotate the required input fieldset
-/// from a base type for a resolver. It is used to develop a query plan
-/// where the required fields may not be needed by the client, but the
-/// service may need additional information from other services.
-///
+/// <code>
+/// directive @requires(fields: _FieldSet!) on FIELD_DEFINITON
+/// </code>
+/// 
+/// The @requires directive is used to specify external (provided by other subgraphs)
+/// entity fields that are needed to resolve target field. It is used to develop a query plan where
+/// the required fields may not be needed by the client, but the service may need additional 
+/// information from other subgraphs. Required fields specified in the directive field set should
+/// correspond to a valid field on the underlying GraphQL interface/object and should be instrumented
+/// with @external directive.
 /// <example>
-/// # extended from the Users service
-/// extend type User @key(fields: "id") {
-///   id: ID! @external
-///   email: String @external
-///   reviews: [Review] @requires(fields: "email")
+/// type Foo @key(fields: "id") {
+///   id: ID!
+///   # this field will be resolved from other subgraph
+///   remote: String @external
+///   local: String @requires(fields: "remote")
 /// }
 /// </example>
 /// </summary>
