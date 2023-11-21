b0e362a8fa638e365e6bdb909849832162d234bd
Sunghwan Bang <sunghwan2789@gmail.com>
2023-11-21 16:11:41 +0900

    add failing test

--- src/StrawberryShake/Client/test/Core.Tests/Serialization/JsonSerializerTests.cs
+++ src/StrawberryShake/Client/test/Core.Tests/Serialization/JsonSerializerTests.cs
@@ -22,6 +22,19 @@ public class JsonSerializerTests
     }
 
     [Fact]
+    public void ParseLarge()
+    {
+        // arrange
+        var json = System.Text.Json.JsonSerializer.SerializeToElement($@"""{"padding",10000}""");
+
+        // act
+        var serialized = _serializer.Parse(json);
+
+        // assert
+        Assert.Equal(json.ToString(), serialized.ToString());
+    }
+
+    [Fact]
     public void Format()
     {
         // arrange
