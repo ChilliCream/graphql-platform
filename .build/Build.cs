79dd046a69b5a0d060d03c91864b93da11f61732
AntonC9018 <anton.curm@gmail.com>
2023-12-04 14:08:44 +0200

    Use `slngen` in Nuke

--- .build/Build.cs
+++ .build/Build.cs
@@ -81,7 +81,7 @@ partial class Build : NukeBuild
                 .Select(p => new TestProject
                 {
                     Name = Path.GetFileNameWithoutExtension(p.Path),
-                    Path = Path.GetRelativePath(RootDirectory, p.Path)
+                    Path = Path.GetRelativePath(RootDirectory, p.Path!),
                 })
                 .OrderBy(p => p.Name)
                 .ToList();
--- .build/Helpers.cs
+++ .build/Helpers.cs
@@ -1,11 +1,6 @@
-using System.Net;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Tools.DotNet;
 
 static class Helpers
 {
@@ -33,81 +28,62 @@ static class Helpers
         Path.Combine("StrawberryShake", "CodeGeneration"),
         Path.Combine("StrawberryShake", "MetaPackages"),
         Path.Combine("StrawberryShake", "Tooling"),
-        "CookieCrumble"
+        "CookieCrumble",
     };
 
-    static IEnumerable<string> GetAllProjects(
+    static readonly string[] IgnoredProjectSegments = new[]
+    {
+        "benchmark",
+        "demo",
+        "sample",
+        "examples",
+    };
+
+    static bool ProjectContainsIgnoredSegment(ReadOnlySpan<char> path)
+    {
+        foreach (var ignoredSegment in IgnoredProjectSegments)
+        {
+            if (path.Contains(ignoredSegment, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static IEnumerable<string> GetAllProjects(
         string sourceDirectory,
         IEnumerable<string> directories,
         Func<string, bool> include = null)
     {
+        directories ??= Directories;
+
         foreach (var directory in directories)
         {
             var fullDirectory = Path.Combine(sourceDirectory, directory);
             foreach (var file in Directory.EnumerateFiles(fullDirectory, "*.csproj", SearchOption.AllDirectories))
             {
-                if (!(include?.Invoke(file) ?? true)
-                    || file.Contains("benchmark", StringComparison.OrdinalIgnoreCase)
-                    || file.Contains("demo", StringComparison.OrdinalIgnoreCase)
-                    || file.Contains("sample", StringComparison.OrdinalIgnoreCase)
-                    || file.Contains("examples", StringComparison.OrdinalIgnoreCase))
+                bool shouldInclude = include?.Invoke(file) ?? true;
+                if (!shouldInclude)
                 {
                     continue;
                 }
+
+                var relativePath = Path.GetRelativePath(sourceDirectory, file);
+                if (ProjectContainsIgnoredSegment(relativePath))
+                {
+                    continue;
+                }
+
                 yield return file;
             }
         }
     }
 
-    public static IReadOnlyCollection<Output> DotNetBuildSonarSolution(
-        string solutionFile,
-        IEnumerable<string> directories = null,
-        Func<string, bool> include = null)
-    {
-        if (File.Exists(solutionFile))
-        {
-            return Array.Empty<Output>();
-        }
-
-        directories ??= Directories;
-
-        var projects = GetAllProjects(Path.GetDirectoryName(solutionFile), directories, include);
-        var workingDirectory = Path.GetDirectoryName(solutionFile);
-        var list = new List<Output>();
-
-        list.AddRange(DotNetTasks.DotNet($"new sln -n {Path.GetFileNameWithoutExtension(solutionFile)}", workingDirectory));
-
-        var projectsArg = string.Join(" ", projects.Select(t => $"\"{t}\""));
-
-        list.AddRange(DotNetTasks.DotNet($"sln \"{solutionFile}\" add {projectsArg}", workingDirectory));
-
-        return list;
-    }
-
-    public static IReadOnlyCollection<Output> DotNetBuildTestSolution(
-        string solutionFile,
-        IEnumerable<Project> projects)
-    {
-        if (File.Exists(solutionFile))
-        {
-            return Array.Empty<Output>();
-        }
-
-        var workingDirectory = Path.GetDirectoryName(solutionFile);
-        var list = new List<Output>();
-
-        list.AddRange(DotNetTasks.DotNet($"new sln -n {Path.GetFileNameWithoutExtension(solutionFile)}", workingDirectory));
-
-        var projectsArg = string.Join(" ", projects.Select(t => $"\"{t}\""));
-
-        list.AddRange(DotNetTasks.DotNet($"sln \"{solutionFile}\" add {projectsArg}", workingDirectory));
-
-        return list;
-    }
-
     public static void TryDelete(string fileName)
     {
-        if(File.Exists(fileName))
+        if (File.Exists(fileName))
         {
             File.Delete(fileName);
         }
