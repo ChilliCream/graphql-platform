5276682bf2d8098584135b316037d91534df9d1f
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2023-12-21 10:32:15 +0100

    Update existing tests

--- src/HotChocolate/Caching/test/Caching.Tests/SchemaTests.cs
+++ src/HotChocolate/Caching/test/Caching.Tests/SchemaTests.cs
@@ -26,20 +26,20 @@ public class SchemaTests
                 .BuildSchemaAsync();
 
         schema.MatchInlineSnapshot(
-            """
+            """"
             schema {
               query: Query
             }
-            
+
             type Book {
               title: String! @cacheControl(maxAge: 5000)
               description: String!
             }
-            
+
             type Query {
               book: Book! @cacheControl(maxAge: 0)
             }
-            
+
             "The scope of a cache hint."
             enum CacheControlScope {
               "The value to cache is not tied to a single user."
@@ -47,13 +47,24 @@ public class SchemaTests
               "The value to cache is specific to a single user."
               PRIVATE
             }
-            
+
             "The `@cacheControl` directive may be provided for individual fields or entire object, interface or union types to provide caching hints to the executor."
             directive @cacheControl("The maximum amount of time this field's cached value is valid, in seconds." maxAge: Int "If `PRIVATE`, the field's value is specific to a single user. The default value is `PUBLIC`, which means the field's value is not tied to a single user." scope: CacheControlScope "If `true`, the field inherits the `maxAge` of its parent field." inheritMaxAge: Boolean) on OBJECT | FIELD_DEFINITION | INTERFACE | UNION
-            
-            "The @tag directive is used to apply arbitrary string\nmetadata to a schema location. Custom tooling can use\nthis metadata during any step of the schema delivery flow,\nincluding composition, static analysis, and documentation.\n\ninterface Book {\n  id: ID! @tag(name: \"your-value\")\n  title: String!\n  author: String!\n}"
+
+            """
+            The @tag directive is used to apply arbitrary string
+            metadata to a schema location. Custom tooling can use
+            this metadata during any step of the schema delivery flow,
+            including composition, static analysis, and documentation.
+
+            interface Book {
+              id: ID! @tag(name: "your-value")
+              title: String!
+              author: String!
+            }
+            """
             directive @tag("The name of the tag." name: String!) repeatable on SCHEMA | SCALAR | OBJECT | FIELD_DEFINITION | ARGUMENT_DEFINITION | INTERFACE | UNION | ENUM | ENUM_VALUE | INPUT_OBJECT | INPUT_FIELD_DEFINITION
-            """);
+            """");
     }
 
     [QueryType]
