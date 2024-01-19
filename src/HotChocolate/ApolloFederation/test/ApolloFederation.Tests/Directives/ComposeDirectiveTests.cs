64ae860998c5986666512d7c46a1f819881ffb88
Michael Staib <michael@chillicream.com>
2024-01-19 18:39:01 +0100

    edits

--- src/HotChocolate/ApolloFederation/test/ApolloFederation.Tests/Directives/ComposeDirectiveTests.cs
+++ src/HotChocolate/ApolloFederation/test/ApolloFederation.Tests/Directives/ComposeDirectiveTests.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Reflection;
 using System.Threading.Tasks;
 using CookieCrumble;
 using HotChocolate.ApolloFederation.Constants;
@@ -7,7 +5,6 @@ using HotChocolate.ApolloFederation.Types;
 using HotChocolate.Execution;
 using HotChocolate.Language;
 using HotChocolate.Types;
-using HotChocolate.Types.Descriptors;
 using Microsoft.Extensions.DependencyInjection;
 using DirectiveLocation = HotChocolate.Types.DirectiveLocation;
 
@@ -44,79 +41,11 @@ public class ComposeDirectiveTests
         [CustomDirective]
         public string Field => "abc";
     }
-    
+
     [Package("https://specs.custom.dev/custom/v1.0")]
     [DirectiveType(DirectiveLocation.FieldDefinition)]
     public sealed class Custom;
-    
-    public sealed class CustomDirectiveAttribute : DirectiveAttribute<Custom>
-    {
-        public CustomDirectiveAttribute() 
-            : base(new Custom())
-        {
-        }
-    }
 
-    public abstract class DirectiveAttribute<T>(T directive) : DescriptorAttribute where T : class
-    {
-        protected override void TryConfigure(
-            IDescriptorContext context, 
-            IDescriptor descriptor, 
-            ICustomAttributeProvider element)
-        {
-            switch (descriptor)
-            {
-                case ArgumentDescriptor desc:
-                    desc.Directive(directive);
-                    break;
-
-                case DirectiveArgumentDescriptor desc:
-                    desc.Directive(directive);
-                    break;
-
-                case EnumTypeDescriptor desc:
-                    desc.Directive(directive);
-                    break;
-
-                case EnumValueDescriptor desc:
-                    desc.Directive(directive);
-                    break;
-
-                case InputFieldDescriptor desc:
-                    desc.Directive(directive);
-                    break;
-
-                case InputObjectTypeDescriptor desc:
-                    desc.Directive(directive);
-                    break;
-
-                case InterfaceFieldDescriptor desc:
-                    desc.Directive(directive);
-                    break;
-
-                case InterfaceTypeDescriptor desc:
-                    desc.Directive(directive);
-                    break;
-
-                case ObjectFieldDescriptor desc:
-                    desc.Directive(directive);
-                    break;
-
-                case ObjectTypeDescriptor desc:
-                    desc.Directive(directive);
-                    break;
-
-                case SchemaTypeDescriptor desc:
-                    desc.Directive(directive);
-                    break;
-
-                case UnionTypeDescriptor desc:
-                    desc.Directive(directive);
-                    break;
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(descriptor));
-            }
-        }
-    } 
+    public sealed class CustomDirectiveAttribute()
+        : DirectiveAttribute<Custom>(new Custom());
 }
