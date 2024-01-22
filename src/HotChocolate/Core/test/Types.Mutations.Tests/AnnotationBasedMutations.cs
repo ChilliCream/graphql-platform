85498d68cff6fc81ef0e32d7a82dd0c78f282591
Michael Staib <michael@chillicream.com>
2024-01-22 11:35:07 +0100

    Fixed List Handling

--- src/HotChocolate/Core/test/Types.Mutations.Tests/AnnotationBasedMutations.cs
+++ src/HotChocolate/Core/test/Types.Mutations.Tests/AnnotationBasedMutations.cs
@@ -1122,7 +1122,6 @@ public class AnnotationBasedMutations
                 .AddGraphQL()
                 .AddQueryType(d => d.Field("abc").Resolve("def"))
                 .AddMutationType<ListReturnMutation>()
-                .AddType<ListReturnMutation.ResultItem>()
                 .AddMutationConventions()
                 .BuildSchemaAsync();
 
