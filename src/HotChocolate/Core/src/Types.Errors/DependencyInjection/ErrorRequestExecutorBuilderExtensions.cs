d082aa41e18ea33860af7a691004772fb90731fe
Michael Staib <michael@chillicream.com>
2024-02-09 16:44:33 +1100

    Refined Query Conventions

--- src/HotChocolate/Core/src/Types.Errors/DependencyInjection/ErrorRequestExecutorBuilderExtensions.cs
+++ src/HotChocolate/Core/src/Types.Errors/DependencyInjection/ErrorRequestExecutorBuilderExtensions.cs
@@ -1,13 +1,22 @@
+using HotChocolate;
 using HotChocolate.Execution.Configuration;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
 /// <summary>
-/// Provides global configuration methods for mutation conventions to the
-/// <see cref="IRequestExecutorBuilder"/>.
+/// Provides global configuration methods for error conventions to the <see cref="IRequestExecutorBuilder"/>.
 /// </summary>
-public static class MutationRequestExecutorBuilderExtensions
+public static class ErrorRequestExecutorBuilderExtensions
 {
+    /// <summary>
+    /// Adds a type discoverer that can handle types implementing <see cref="IFieldResult"/>.
+    /// </summary>
+    /// <param name="builder">
+    /// The GraphQL configuration builder.
+    /// </param>
+    /// <returns>
+    /// Returns the GraphQL configuration builder for configuration chaining.
+    /// </returns>
     public static IRequestExecutorBuilder AddFieldResultTypeDiscovery(
         this IRequestExecutorBuilder builder)
         => builder.AddTypeDiscoveryHandler(c => new FieldResultTypeDiscoveryHandler(c.TypeInspector));
@@ -21,13 +30,13 @@ public static class MutationRequestExecutorBuilderExtensions
     /// <see cref="ErrorObjectFieldDescriptorExtensions.Error"/>
     /// </summary>
     /// <param name="builder">
-    /// The request executor builder
+    /// The GraphQL configuration builder.
     /// </param>
     /// <typeparam name="T">
     /// The type that is used as the common interface
     /// </typeparam>
     /// <returns>
-    /// The schema builder
+    /// Returns the GraphQL configuration builder for configuration chaining.
     /// </returns>
     public static IRequestExecutorBuilder AddErrorInterfaceType<T>(
         this IRequestExecutorBuilder builder)
@@ -42,13 +51,13 @@ public static class MutationRequestExecutorBuilderExtensions
     /// <see cref="ErrorObjectFieldDescriptorExtensions.Error"/>
     /// </summary>
     /// <param name="builder">
-    /// The request executor builder
+    /// The GraphQL configuration builder.
     /// </param>
     /// <param name="type">
     /// The type that is used as the common interface
     /// </param>
     /// <returns>
-    /// The request executor builder
+    /// Returns the GraphQL configuration builder for configuration chaining.
     /// </returns>
     public static IRequestExecutorBuilder AddErrorInterfaceType(
         this IRequestExecutorBuilder builder,
@@ -56,26 +65,26 @@ public static class MutationRequestExecutorBuilderExtensions
         builder.ConfigureSchema(x => x.AddErrorInterfaceType(type));
 
     /// <summary>
-    /// Adds a new error registrar.
+    /// Adds a new error registrar for mutation errors.
     /// </summary>
     /// <param name="builder">
-    /// The request executor builder
+    /// The GraphQL configuration builder.
     /// </param>
     /// <typeparam name="T">
     /// The mutation error registrar.
     /// </typeparam>
     /// <returns>
-    /// The request executor builder
+    /// Returns the GraphQL configuration builder for configuration chaining.
     /// </returns>
     public static IRequestExecutorBuilder AddMutationErrorConfiguration<T>(this IRequestExecutorBuilder builder)
         where T : MutationErrorConfiguration, new()
         => builder.TryAddTypeInterceptor(new MutationErrorTypeInterceptor<T>(new T()));
 
     /// <summary>
-    /// Adds a new error registrar.
+    /// Adds a new error registrar for mutation errors.
     /// </summary>
     /// <param name="builder">
-    /// The request executor builder
+    /// The GraphQL configuration builder.
     /// </param>
     /// <param name="configuration">
     /// The mutation error configuration.
@@ -84,7 +93,7 @@ public static class MutationRequestExecutorBuilderExtensions
     /// The mutation error registrar.
     /// </typeparam>
     /// <returns>
-    /// The request executor builder
+    /// Returns the GraphQL configuration builder for configuration chaining.
     /// </returns>
     public static IRequestExecutorBuilder AddMutationErrorConfiguration<T>(
         this IRequestExecutorBuilder builder,
