596e041a905e6b83ad42dfb48f1fe8eed4ab91e6
Michael Staib <michael@chillicream.com>
2024-02-09 13:31:45 +1100

    edits

--- src/HotChocolate/Core/src/Types.Errors/ErrorMarker.cs
+++ src/HotChocolate/Core/src/Types.Errors/ErrorMarker.cs
@@ -1,5 +1,9 @@
 namespace HotChocolate.Types;
 
+/// <summary>
+/// The error marker is to signal to the execution engine that the result is actually an error object that
+/// is located in the internal execution state.
+/// </summary>
 public sealed class ErrorMarker
 {
     private ErrorMarker()
@@ -7,5 +11,8 @@ public sealed class ErrorMarker
         // Intentionally left blank.
     }
     
+    /// <summary>
+    /// Gets the error marker instance.
+    /// </summary>
     public static ErrorMarker Instance { get; } = new();
 }
