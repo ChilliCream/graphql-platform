dda3cb63f6358759da79944e6db9b0d74ec6cc79
Michael Staib <michael@chillicream.com>
2024-02-17 16:43:21 +1100

    edits

--- src/HotChocolate/Core/src/Abstractions/QueryAttribute.cs
+++ src/HotChocolate/Core/src/Abstractions/QueryAttribute.cs
@@ -2,5 +2,10 @@ using System;
 
 namespace HotChocolate;
 
+/// <summary>
+/// Marks a public/internal static method or property as a query root field.
+/// The Hot Chocolate source generator will collect these and merge them into
+/// the query type. 
+/// </summary>
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
 public sealed class QueryAttribute : Attribute;
