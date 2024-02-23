18658948934e73a219e9b1057e144e38184ab73c
Michael Staib <michael@chillicream.com>
2024-02-23 14:54:01 +0100

    Added more helpers to handle result data objects. (#6941)

--- src/HotChocolate/Core/src/Execution/Internal/MiddlewareContextMarshal.cs
+++ src/HotChocolate/Core/src/Execution/Internal/MiddlewareContextMarshal.cs
@@ -18,7 +18,9 @@ public static class MiddlewareContextMarshal
     /// <param name="context">
     /// The resolver context.
     /// </param>
-    /// <returns></returns>
+    /// <returns>
+    /// Returns the result data of the current resolver context.
+    /// </returns>
     public static ObjectResult? GetParentResultUnsafe(IResolverContext context)
     {
         if (context is null)
@@ -29,5 +31,55 @@ public static class MiddlewareContextMarshal
         return context is MiddlewareContext middlewareContext
             ? middlewareContext.ParentResult
             : null;
+    }
+    
+    /// <summary>
+    /// Gets the parent result data of the current <paramref name="resultData"/>.
+    /// </summary>
+    /// <param name="resultData">
+    /// The result data for which to get the parent.
+    /// </param>
+    /// <typeparam name="T">
+    /// The type of the result data.
+    /// </typeparam>
+    /// <returns>
+    /// Returns the parent result data of the current <paramref name="resultData"/>.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// Throws if <paramref name="resultData"/> is <c>null</c>.
+    /// </exception>
+    public static ResultData? GetParent<T>(T resultData) where T : ResultData
+    {
+        if (resultData == null)
+        {
+            throw new ArgumentNullException(nameof(resultData));
+        }
+        
+        return resultData.Parent;
+    }
+
+    /// <summary>
+    /// Gets the index under which the <paramref name="resultData"/> is stored in the parent result.
+    /// </summary>
+    /// <param name="resultData">
+    /// The result data for which to get the parent index.
+    /// </param>
+    /// <typeparam name="T">
+    /// The type of the result data.
+    /// </typeparam>
+    /// <returns>
+    /// Returns the index under which the <paramref name="resultData"/> is stored in the parent result.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// Throws if <paramref name="resultData"/> is <c>null</c>.
+    /// </exception>
+    public static int GetParentIndex<T>(T resultData) where T : ResultData
+    {
+        if (resultData == null)
+        {
+            throw new ArgumentNullException(nameof(resultData));
+        }
+        
+        return resultData.ParentIndex;
     }
 }
