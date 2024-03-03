288999efaaa95d98e9255ceaaaffb824d936f7d2
Michael Staib <michael@chillicream.com>
2024-03-03 22:45:21 +0100

    Enabled the GraphQL HTTP client to work with the new persisted query routes.

--- src/HotChocolate/AspNetCore/src/Transport.Abstractions/OperationRequest.cs
+++ src/HotChocolate/AspNetCore/src/Transport.Abstractions/OperationRequest.cs
@@ -40,13 +40,6 @@ public readonly struct OperationRequest : IEquatable<OperationRequest>
         ObjectValueNode? variables,
         ObjectValueNode? extensions)
     {
-        if (string.IsNullOrWhiteSpace(query) && id is null && extensions is null)
-        {
-            throw new ArgumentException(
-                OperationRequest_QueryOrPersistedQueryId,
-                nameof(query));
-        }
-
         Query = query;
         Id = id;
         OperationName = operationName;
@@ -82,19 +75,17 @@ public readonly struct OperationRequest : IEquatable<OperationRequest>
         IReadOnlyDictionary<string, object?>? variables = null,
         IReadOnlyDictionary<string, object?>? extensions = null)
     {
-        if (string.IsNullOrWhiteSpace(query) && id is null && extensions is null)
-        {
-            throw new ArgumentException(
-                OperationRequest_QueryOrPersistedQueryId,
-                nameof(query));
-        }
-
         Query = query;
         Id = id;
         OperationName = operationName;
         Variables = variables;
         Extensions = extensions;
     }
+
+    /// <summary>
+    /// Empty Operation Request.
+    /// </summary>
+    public static OperationRequest Empty { get; } = new();
 
     /// <summary>
     /// Gets the ID of a previously persisted query that should be executed.
