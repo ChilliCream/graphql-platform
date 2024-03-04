ed0449f5c1f94bfa12491d5fc66a0e5d616287c8
Michael Staib <michael@chillicream.com>
2024-03-04 11:15:14 +0100

    Added missing this to AddGraphQL extension method (#6964)

--- src/HotChocolate/AspNetCore/src/AspNetCore/Extensions/HotChocolateAspNetCoreHostingBuilderExtensions.cs
+++ src/HotChocolate/AspNetCore/src/AspNetCore/Extensions/HotChocolateAspNetCoreHostingBuilderExtensions.cs
@@ -1,14 +1,14 @@
+#if NET8_0_OR_GREATER
 using HotChocolate.Execution.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using static HotChocolate.AspNetCore.ServerDefaults;
 
-#if NET8_0_OR_GREATER
 namespace Microsoft.Extensions.Hosting;
 
 public static class HotChocolateAspNetCoreHostingBuilderExtensions
 {
     public static IRequestExecutorBuilder AddGraphQL(
-        IHostApplicationBuilder builder,        
+        this IHostApplicationBuilder builder,        
         string? schemaName = default,
         int maxAllowedRequestSize = MaxAllowedRequestSize)
         => builder.Services.AddGraphQLServer(schemaName, maxAllowedRequestSize);
