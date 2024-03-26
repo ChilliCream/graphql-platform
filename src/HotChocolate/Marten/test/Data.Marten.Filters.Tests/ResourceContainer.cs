bed1caf734214e647c90c91c29e9fa1a3df74705
Michael Staib <michael@chillicream.com>
2024-03-26 12:33:40 +0100

    Added Support for Marten 7.4

---
+++ src/HotChocolate/Marten/test/Data.Marten.Filters.Tests/ResourceContainer.cs
@@ -0,0 +1,46 @@
+using Squadron;
+
+namespace HotChocolate.Data;
+
+public sealed class ResourceContainer : IAsyncDisposable
+{
+    private readonly SemaphoreSlim _semaphore = new(1, 1);
+    private int _testClassInstances = 0;
+    
+    public PostgreSqlResource Resource { get; } = new(); 
+    
+    public async ValueTask InitializeAsync()
+    {
+        await _semaphore.WaitAsync();
+
+        try
+        {
+            if (_testClassInstances == 0)
+            {
+                await Resource.InitializeAsync();
+            }
+            _testClassInstances++;
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    } 
+    
+    public async ValueTask DisposeAsync()
+    {
+        await _semaphore.WaitAsync();
+
+        try
+        {
+            if (--_testClassInstances == 0)
+            {
+                await Resource.DisposeAsync();
+            }
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+}
