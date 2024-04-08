8f7b178865ffc09de029105a4fb8f2a581c7d6c0
Michael Staib <michael@chillicream.com>
2024-04-08 08:02:15 +0200

    fixed

--- src/HotChocolate/Data/src/Data/Extensions/DataEnumerableExtensions.cs
+++ src/HotChocolate/Data/src/Data/Extensions/DataEnumerableExtensions.cs
@@ -10,9 +10,7 @@ public static class DataEnumerableExtensions
     /// <typeparam name="T">The type parameter</typeparam>
     /// <returns>The wrapped object</returns>
     public static QueryableExecutable<T> AsExecutable<T>(this IEnumerable<T> source)
-    {
-        return new QueryableExecutable<T>(source.AsQueryable());
-    }
+        => new(source.AsQueryable());
 
     /// <summary>
     /// Wraps the <see cref="IQueryable"/> with <see cref="QueryableExecutable{T}"/> to help the
@@ -22,7 +20,26 @@ public static class DataEnumerableExtensions
     /// <typeparam name="T">The type parameter</typeparam>
     /// <returns>The wrapped object</returns>
     public static QueryableExecutable<T> AsExecutable<T>(this IQueryable<T> source)
-    {
-        return new QueryableExecutable<T>(source);
-    }
+        => new(source);
+
+    /// <summary>
+    /// Unwraps the <see cref="IExecutable"/> to get the <see cref="IQueryable{T}"/>.
+    /// </summary>
+    /// <param name="executable">
+    /// The executable that should be unwrapped to get the <see cref="IQueryable{T}"/>.
+    /// </param>
+    /// <typeparam name="T">
+    /// The type parameter of the <see cref="IQueryable{T}"/> that should be returned.
+    /// </typeparam>
+    /// <returns>
+    /// Returns the <see cref="IQueryable{T}"/> that is wrapped by the executable.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// Throws an <see cref="InvalidOperationException"/> if the source of the executable is not an
+    /// <see cref="IQueryable{T}"/>.
+    /// </exception>
+    public static IQueryable<T> AsQueryable<T>(this IExecutable<T> executable)
+        => executable.Source as IQueryable<T> ??
+            throw new InvalidOperationException(
+                "The source of the executable is not an IQueryable<T>.");
 }
