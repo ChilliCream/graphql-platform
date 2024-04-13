b1b2ce9fb25fc83fb8f0f9a82160c8226d78e63a
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2024-04-13 21:48:40 +0200

    Add naive fix

--- src/HotChocolate/Caching/src/Caching/Types/CacheControlScope.cs
+++ src/HotChocolate/Caching/src/Caching/Types/CacheControlScope.cs
@@ -6,16 +6,16 @@ namespace HotChocolate.Caching;
 public enum CacheControlScope
 {
     /// <summary>
-    /// Private query results contain data
-    /// that is scoped to a user and must therefore
-    /// be only cached for and accessible to that user.
-    /// </summary>
-    Private,
-
-    /// <summary>
     /// Public query results contain data
     /// that is not scoped to a particular user and can
     /// therefore be stored in a shared cache.
     /// </summary>
     Public,
+
+    /// <summary>
+    /// Private query results contain data
+    /// that is scoped to a user and must therefore
+    /// be only cached for and accessible to that user.
+    /// </summary>
+    Private,
 }
