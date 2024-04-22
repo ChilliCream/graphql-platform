ea1afc9ccd1af1ce5c24abbc5dda416e9718bcdf
Michael Staib <michael@chillicream.com>
2024-04-22 10:27:02 +0200

    Make defaults  internal

--- src/HotChocolate/Caching/src/Caching/CacheControlDefaults.cs
+++ src/HotChocolate/Caching/src/Caching/CacheControlDefaults.cs
@@ -1,6 +1,6 @@
 namespace HotChocolate.Caching;
 
-public static class CacheControlDefaults
+internal static class CacheControlDefaults
 {
     public const int MaxAge = 0;
     public const CacheControlScope Scope = CacheControlScope.Public;
