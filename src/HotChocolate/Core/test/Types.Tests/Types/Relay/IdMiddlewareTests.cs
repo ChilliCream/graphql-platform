a0ab1a87e1162ad3b7f5deaa6020dd5020cbb898
PascalSenn <senn.pasc@gmail.com>
2024-05-02 11:00:39 +0200

    Reproduce id behaviour change

--- src/HotChocolate/Core/test/Types.Tests/Types/Relay/IdMiddlewareTests.cs
+++ src/HotChocolate/Core/test/Types.Tests/Types/Relay/IdMiddlewareTests.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using HotChocolate.Execution;
+using Microsoft.Extensions.DependencyInjection;
 using Snapshooter.Xunit;
 
 namespace HotChocolate.Types.Relay;
@@ -24,11 +26,42 @@ public class IdMiddlewareTests
         result.ToJson().MatchSnapshot();
     }
 
+    [Fact]
+    public async Task Mutation_ParameterWithoutExplicitType_Should_NotBeValidated()
+    {
+        // arrange
+        var executor = await new ServiceCollection()
+            .AddGraphQLServer()
+            .AddQueryType<SomeQuery>()
+            .AddMutationType<Mutation>()
+            .AddGlobalObjectIdentification(false)
+            .AddMutationConventions()
+            .BuildRequestExecutorAsync();
+
+        // act
+        var result = await executor.ExecuteAsync("""
+            mutation {
+                do(input: { id: "RXhhbXBsZTp0ZXN0" }) {
+                    string
+                }
+            }
+        """);
+
+        // assert
+        result.ToJson().MatchSnapshot();
+    }
+
     public class SomeQuery
     {
         [ID]
         public string GetId() => "Hello";
 
         public string GetString() => "Hello";
+    }
+
+    public class Mutation
+    {
+        [UseMutationConvention]
+        public string Do([ID] string id) => id;
     }
 }
