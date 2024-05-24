ad98734fdcdd3ca92d31b6cfd324ae859951686d
Jordan Phillips <jordypee27@gmail.com>
2024-05-24 17:01:24 +1000

    Accept structs in ObjectTypeAttribute<T> (#7116)

--- src/HotChocolate/Core/test/Types.Analyzers.Tests/BookType.cs
+++ src/HotChocolate/Core/test/Types.Analyzers.Tests/BookType.cs
@@ -13,6 +13,8 @@ public class Book
     public string? Title { get; set; }
 
     public Author? Author => null;
+
+    public Publisher? Publisher => null;
 }
 
 public class Author
@@ -20,8 +22,16 @@ public class Author
     public string Name { get; set; } = default!;
 }
 
+public readonly record struct Publisher(string Name);
+
 [ObjectType<Author>]
 public static partial class AuthorNode
 {
     public static string Address([Parent] Author author) => "something";
+}
+
+[ObjectType<Publisher>]
+public static partial class PublisherNode
+{
+    public static string Company([Parent] Publisher author) => "something";
 }
