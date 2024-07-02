92f16e2b323b138bc52f19de591226116471ea70
Michael Staib <michael@chillicream.com>
2024-07-02 14:25:27 +0100

    Made Cost Metrics Class Public

--- src/HotChocolate/CostAnalysis/src/CostAnalysis/Extensions/CostAnalyzerRequestContextExtensions.cs
+++ src/HotChocolate/CostAnalysis/src/CostAnalysis/Extensions/CostAnalyzerRequestContextExtensions.cs
@@ -2,9 +2,12 @@ using HotChocolate.CostAnalysis;
 
 namespace HotChocolate.Execution;
 
-internal static class CostAnalyzerRequestContextExtensions
+/// <summary>
+/// Cost Analyzer extensions for the <see cref="IRequestContext"/>.
+/// </summary>
+public static class CostAnalyzerRequestContextExtensions
 {
-    public static IRequestContext AddCostMetrics(
+    internal static IRequestContext AddCostMetrics(
         this IRequestContext context,
         CostMetrics costMetrics)
     {
@@ -22,7 +25,36 @@ internal static class CostAnalyzerRequestContextExtensions
         return context;
     }
 
-    public static CostAnalyzerMode GetCostAnalyzerMode(
+    /// <summary>
+    /// Gets the cost metrics from the context.
+    /// </summary>
+    /// <param name="context">
+    /// The request context.
+    /// </param>
+    /// <returns>
+    /// Returns the cost metrics.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="context"/> is <c>null</c>.
+    /// </exception>
+    public static CostMetrics GetCostMetrics(
+        this IRequestContext context)
+    {
+        if (context is null)
+        {
+            throw new ArgumentNullException(nameof(context));
+        }
+
+        if (context.ContextData.TryGetValue(WellKnownContextData.CostMetrics, out var value) &&
+            value is CostMetrics costMetrics)
+        {
+            return costMetrics;
+        }
+
+        return new CostMetrics();
+    }
+
+    internal static CostAnalyzerMode GetCostAnalyzerMode(
         this IRequestContext context,
         CostOptions options)
     {
