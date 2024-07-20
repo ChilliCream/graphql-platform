151b0b14deba031025d1bfe327c1d43deaf994ac
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2024-07-20 17:02:15 +0200

    Update DefaultNodeIdSerializer

--- src/HotChocolate/Core/test/Types.Tests/Types/Relay/DefaultNodeIdSerializerTests.cs
+++ src/HotChocolate/Core/test/Types.Tests/Types/Relay/DefaultNodeIdSerializerTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using CookieCrumble;
@@ -223,6 +224,18 @@ public class DefaultNodeIdSerializerTests
     }
 
     [Fact]
+    public void Parse_Legacy_StronglyTypedId()
+    {
+        var stronglyTypedId = new StronglyTypedId(123, 456);
+        var serializer = CreateSerializer(new StronglyTypedIdNodeIdValueSerializer());
+        var id = Convert.ToBase64String("Product\nd123-456"u8);
+
+        var parsed = serializer.Parse(id, typeof(StronglyTypedId));
+
+        Assert.Equal(stronglyTypedId, parsed.InternalId);
+    }
+
+    [Fact]
     public void Ensure_Lookup_Works_With_HashCollision()
     {
         // arrange
@@ -351,4 +364,43 @@ public class DefaultNodeIdSerializerTests
     }
 
     private readonly record struct CompositeId(string A, int B, Guid C, bool D);
+
+    public class StronglyTypedIdNodeIdValueSerializer : INodeIdValueSerializer
+    {
+        public bool IsSupported(Type type) => type == typeof(StronglyTypedId);
+
+        public NodeIdFormatterResult Format(Span<byte> buffer, object value, out int written)
+        {
+            if (value is StronglyTypedId stronglyTypedId)
+            {
+                var formattedValue = stronglyTypedId.ToString();
+                written = Encoding.UTF8.GetBytes(formattedValue, buffer);
+                return NodeIdFormatterResult.Success;
+            }
+
+            written = 0;
+            return NodeIdFormatterResult.InvalidValue;
+        }
+
+        public bool TryParse(ReadOnlySpan<byte> buffer, [NotNullWhen(true)] out object? value)
+        {
+            var formattedValue = Encoding.UTF8.GetString(buffer);
+            value = StronglyTypedId.Parse(formattedValue);
+            return true;
+        }
+    }
+
+    public record StronglyTypedId(int Part1, int Part2)
+    {
+        public override string ToString()
+        {
+            return $"{Part1}-{Part2}";
+        }
+
+        public static StronglyTypedId Parse(string value)
+        {
+            var parts = value.Split('-');
+            return new StronglyTypedId(int.Parse(parts[0]), int.Parse(parts[1]));
+        }
+    }
 }
