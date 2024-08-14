41b59f293eaeb7819b8c99e9ea03ff0c8f49b79d
Michael Staib <michael@chillicream.com>
2024-08-14 23:48:46 +0200

    refinements

--- src/HotChocolate/Core/src/Types.CursorPagination/CursorPaginationData.cs
+++ src/HotChocolate/Core/src/Types.CursorPagination/CursorPaginationData.cs
@@ -2,24 +2,29 @@ using System.Collections.Immutable;
 
 namespace HotChocolate.Types.Pagination;
 
+/// <summary>
+/// The raw pagination data that is returned from the query executor.
+/// </summary>
+/// <param name="edges">
+/// The edges that belong to this connection.
+/// </param>
+/// <param name="totalCount">
+/// The total count of items of this connection.
+/// </param>
+/// <typeparam name="TEntity">
+/// The entity type of the data.
+/// </typeparam>
 public readonly struct CursorPaginationData<TEntity>(
     ImmutableArray<Edge<TEntity>> edges,
     int? totalCount)
 {
+    /// <summary>
+    /// Gets the edges that belong to this connection.
+    /// </summary>
     public ImmutableArray<Edge<TEntity>> Edges { get; } = edges;
 
+    /// <summary>
+    /// Gets the total count of items of this connection.
+    /// </summary>
     public int? TotalCount { get; } = totalCount;
-}
-
-public interface ICursorPaginationQueryExecutor<in TQuery, TEntity> where TQuery : notnull
-{
-    ValueTask<int> CountAsync(
-        TQuery originalQuery,
-        CancellationToken cancellationToken);
-
-    ValueTask<CursorPaginationData<TEntity>> QueryAsync(
-        TQuery slicedQuery,
-        int offset,
-        bool includeTotalCount,
-        CancellationToken cancellationToken);
 }
