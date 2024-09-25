86a9c8144142412bbdbe2bd59d2b4696e0f34ed3
Glen <glen.84@gmail.com>
2024-09-25 17:35:27 +0200

    Added a generic version of the ID descriptor method (#7503)

--- src/HotChocolate/Core/test/Types.Tests/Types/Relay/IdDescriptorTests.cs
+++ src/HotChocolate/Core/test/Types.Tests/Types/Relay/IdDescriptorTests.cs
@@ -12,7 +12,7 @@ public class IdDescriptorTests
         // arrange
         var intId = Convert.ToBase64String("Query:1"u8);
         var stringId = Convert.ToBase64String("Query:abc"u8);
-        var guidId = Convert.ToBase64String(Combine("Query:"u8, Guid.Empty.ToByteArray()));
+        var guidId = Convert.ToBase64String(Combine("Another:"u8, Guid.Empty.ToByteArray()));
 
         // act
         var result =
@@ -47,6 +47,7 @@ public class IdDescriptorTests
     {
         // arrange
         var someId = Convert.ToBase64String("Some:1"u8);
+        var anotherId = Convert.ToBase64String("Another:1"u8);
 
         // act
         var result =
@@ -58,12 +59,19 @@ public class IdDescriptorTests
                 .ExecuteRequestAsync(
                     OperationRequestBuilder.New()
                         .SetDocument(
-                            @"query foo ($someId: ID!) {
-                                foo(input: { someId: $someId }) {
+                            """
+                            query foo ($someId: ID!, $anotherId: ID!) {
+                                foo(input: { someId: $someId, anotherId: $anotherId }) {
                                     someId
+                                    anotherId
                                 }
-                            }")
-                        .SetVariableValues(new Dictionary<string, object> { { "someId", someId }, })
+                            }
+                            """)
+                        .SetVariableValues(new Dictionary<string, object>
+                        {
+                            { "someId", someId },
+                            { "anotherId", anotherId }
+                        })
                         .Build());
 
         // assert
@@ -71,6 +79,7 @@ public class IdDescriptorTests
         {
             result = result.ToJson(),
             someId,
+            anotherId
         }.MatchSnapshot();
     }
 
@@ -107,7 +116,7 @@ public class IdDescriptorTests
 
             descriptor
                 .Field(t => t.GuidId(default))
-                .Argument("id", a => a.ID());
+                .Argument("id", a => a.ID<Another>());
 
             descriptor
                 .Field(t => t.Foo(default))
@@ -123,6 +132,10 @@ public class IdDescriptorTests
             descriptor
                 .Field(t => t.SomeId)
                 .ID("Some");
+
+            descriptor
+                .Field(t => t.AnotherId)
+                .ID<Another>();
         }
     }
 
@@ -135,6 +148,10 @@ public class IdDescriptorTests
             descriptor
                 .Field(t => t.SomeId)
                 .ID("Bar");
+
+            descriptor
+                .Field(t => t.AnotherId)
+                .ID<Another>();
         }
     }
 
@@ -145,6 +162,10 @@ public class IdDescriptorTests
             descriptor
                 .Field(t => t.SomeId)
                 .ID();
+
+            descriptor
+                .Field(t => t.AnotherId)
+                .ID();
         }
     }
 
@@ -153,21 +174,27 @@ public class IdDescriptorTests
         public string IntId(int id) => id.ToString();
         public string StringId(string id) => id;
         public string GuidId(Guid id) => id.ToString();
-        public IFooPayload Foo(FooInput input) => new FooPayload { SomeId = input.SomeId, };
+        public IFooPayload Foo(FooInput input)
+            => new FooPayload { SomeId = input.SomeId, AnotherId = input.AnotherId };
     }
 
     public class FooInput
     {
         public string SomeId { get; set; }
+        public string AnotherId { get; set; }
     }
 
     public class FooPayload : IFooPayload
     {
         public string SomeId { get; set; }
+        public string AnotherId { get; set; }
     }
 
     public interface IFooPayload
     {
         string SomeId { get; set; }
+        string AnotherId { get; set; }
     }
+
+    private class Another;
 }
