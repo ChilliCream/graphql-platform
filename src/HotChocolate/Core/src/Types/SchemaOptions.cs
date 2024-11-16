fad06a2f1050ca59bd4fca1982bca2fcd3dc5743
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2024-11-16 15:18:21 +0100

    Add back inheritdoc

--- src/HotChocolate/Core/src/Types/SchemaOptions.cs
+++ src/HotChocolate/Core/src/Types/SchemaOptions.cs
@@ -15,24 +15,34 @@ public class SchemaOptions : IReadOnlySchemaOptions
     private BindingBehavior _defaultBindingBehavior = BindingBehavior.Implicit;
     private FieldBindingFlags _defaultFieldBindingFlags = FieldBindingFlags.Instance;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.QueryTypeName"/>
     public string? QueryTypeName { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.MutationTypeName"/>
     public string? MutationTypeName { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.SubscriptionTypeName"/>
     public string? SubscriptionTypeName { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.StrictValidation"/>
     public bool StrictValidation { get; set; } = true;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.UseXmlDocumentation"/>
     public bool UseXmlDocumentation { get; set; } = true;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.ResolveXmlDocumentationFileName"/>
     public Func<Assembly, string>? ResolveXmlDocumentationFileName { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.SortFieldsByName"/>
     public bool SortFieldsByName { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.RemoveUnreachableTypes"/>
     public bool RemoveUnreachableTypes { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.RemoveUnusedTypeSystemDirectives"/>
     public bool RemoveUnusedTypeSystemDirectives { get; set; } = true;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.DefaultBindingBehavior"/>
     public BindingBehavior DefaultBindingBehavior
     {
         get => _defaultBindingBehavior;
@@ -47,6 +57,7 @@ public class SchemaOptions : IReadOnlySchemaOptions
         }
     }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.DefaultFieldBindingFlags"/>
     public FieldBindingFlags DefaultFieldBindingFlags
     {
         get => _defaultFieldBindingFlags;
@@ -61,47 +72,66 @@ public class SchemaOptions : IReadOnlySchemaOptions
         }
     }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.FieldMiddleware"/>
     public FieldMiddlewareApplication FieldMiddleware { get; set; } =
         FieldMiddlewareApplication.UserDefinedFields;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.EnableDirectiveIntrospection"/>
     public bool EnableDirectiveIntrospection { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.DefaultDirectiveVisibility"/>
     public DirectiveVisibility DefaultDirectiveVisibility { get; set; } =
         DirectiveVisibility.Public;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.DefaultResolverStrategy"/>
     public ExecutionStrategy DefaultResolverStrategy { get; set; } =
         ExecutionStrategy.Parallel;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.ValidatePipelineOrder"/>
     public bool ValidatePipelineOrder { get; set; } = true;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.StrictRuntimeTypeValidation"/>
     public bool StrictRuntimeTypeValidation { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.DefaultIsOfTypeCheck"/>
     public IsOfTypeFallback? DefaultIsOfTypeCheck { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.EnableOneOf"/>
     public bool EnableOneOf { get; set; } = true;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.EnsureAllNodesCanBeResolved"/>
     public bool EnsureAllNodesCanBeResolved { get; set; } = true;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.EnableFlagEnums"/>
     public bool EnableFlagEnums { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.EnableDefer"/>
     public bool EnableDefer { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.EnableStream"/>
     public bool EnableStream { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.EnableSemanticNonNull"/>
     public bool EnableSemanticNonNull { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.MaxAllowedNodeBatchSize"/>
     public int MaxAllowedNodeBatchSize { get; set; } = 50;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.StripLeadingIFromInterface"/>
     public bool StripLeadingIFromInterface { get; set; }
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.EnableTag"/>
     public bool EnableTag { get; set; } = true;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.DefaultQueryDependencyInjectionScope"/>
     public DependencyInjectionScope DefaultQueryDependencyInjectionScope { get; set; } =
         DependencyInjectionScope.Resolver;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.DefaultMutationDependencyInjectionScope"/>
     public DependencyInjectionScope DefaultMutationDependencyInjectionScope { get; set; } =
         DependencyInjectionScope.Request;
 
+    /// <inheritdoc cref="IReadOnlySchemaOptions.PublishRootFieldPagesToPromiseCache"/>
     public bool PublishRootFieldPagesToPromiseCache { get; set; } = true;
 
     /// <summary>
