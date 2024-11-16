14c9b739f67677d5c32294c126c91f6caf74327e
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2024-11-17 00:02:01 +0100

    Implement merging

---
+++ src/HotChocolate/Skimmed/src/Skimmed/BuiltIns/SemanticNonNullDirectiveDefinition.cs
@@ -0,0 +1,19 @@
+using HotChocolate.Language;
+using DirectiveLocation = HotChocolate.Types.DirectiveLocation;
+
+namespace HotChocolate.Skimmed;
+
+public sealed class SemanticNonNullDirectiveDefinition : DirectiveDefinition
+{
+    internal SemanticNonNullDirectiveDefinition(ScalarTypeDefinition intType)
+        : base(BuiltIns.SemanticNonNull.Name)
+    {
+        IsSpecDirective = true;
+
+        var levelsArgument = new InputFieldDefinition(BuiltIns.SemanticNonNull.Levels, new ListTypeDefinition(intType));
+        levelsArgument.DefaultValue = new ListValueNode(new IntValueNode(0));
+        Arguments.Add(levelsArgument);
+
+        Locations = DirectiveLocation.FieldDefinition;
+    }
+}
