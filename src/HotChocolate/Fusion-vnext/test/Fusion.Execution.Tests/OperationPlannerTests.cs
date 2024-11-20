7b66e9d9bcba10ef44f161be80b039b901e3f123
Michael Staib <michael@chillicream.com>
2024-11-20 23:48:39 +0100

    refactoring

--- src/HotChocolate/Fusion-vnext/test/Fusion.Execution.Tests/OperationPlannerTests.cs
+++ src/HotChocolate/Fusion-vnext/test/Fusion.Execution.Tests/OperationPlannerTests.cs
@@ -95,14 +95,3 @@ public class OperationPlannerTests
                 """);
     }
 }
-
-public static class FileResource
-{
-    public static string Open(string name)
-    {
-        var path = Path.Combine("__resources__", name);
-        return File.Exists(path)
-            ? File.ReadAllText(path)
-            : throw new InvalidOperationException($"The test resource {path} is missing!");
-    }
-}
