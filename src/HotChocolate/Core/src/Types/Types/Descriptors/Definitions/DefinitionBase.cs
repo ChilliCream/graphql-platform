d7a074a240a578d36a9a5cd1ff21580679d8ac1a
Michael Staib <michael@chillicream.com>
2024-11-20 14:51:30 +0100

    undo

--- src/HotChocolate/Core/src/Types/Types/Descriptors/Definitions/DefinitionBase.cs
+++ src/HotChocolate/Core/src/Types/Types/Descriptors/Definitions/DefinitionBase.cs
@@ -199,7 +199,7 @@ public class DefinitionBase : IDefinition
             }
         }
 
-        if (target.Description is null && Description is not null)
+        if (Description is not null)
         {
             target.Description = Description;
         }
