1b5b9681269bbb3874e200f7565411249797dd8d
Michael Staib <michael@chillicream.com>
2024-11-27 13:26:03 +0100

    Added more test cases

--- src/HotChocolate/Fusion-vnext/test/Fusion.Execution.Tests/OperationPlannerTests.cs
+++ src/HotChocolate/Fusion-vnext/test/Fusion.Execution.Tests/OperationPlannerTests.cs
@@ -94,4 +94,67 @@ public class OperationPlannerTests
                 }
                 """);
     }
+
+    [Test]
+    public async Task Plan_Simple_Operation_3_Source_Schema()
+    {
+        var compositeSchemaDoc = Utf8GraphQLParser.Parse(FileResource.Open("fusion1.graphql"));
+        var compositeSchema = CompositeSchemaBuilder.Create(compositeSchemaDoc);
+
+        var doc = Utf8GraphQLParser.Parse(
+            """
+            {
+                productById(id: 1) {
+                    ... ProductCard
+                }
+            }
+
+            fragment ProductCard on Product {
+                name
+                reviews(first: 10) {
+                    nodes {
+                        ... ReviewCard
+                    }
+                }
+            }
+
+            fragment ReviewCard on Review {
+                body
+                stars
+                author {
+                    ... AuthorCard
+                }
+            }
+
+            fragment AuthorCard on UserProfile {
+                displayName
+            }
+            """);
+
+        var rewriter = new InlineFragmentOperationRewriter(compositeSchema);
+        var rewritten = rewriter.RewriteDocument(doc, null);
+
+        // act
+        var planner = new OperationPlanner(compositeSchema);
+        var plan = planner.CreatePlan(rewritten, null);
+
+        // assert
+        await Assert
+            .That(plan.ToSyntaxNode().ToString(indented: true))
+            .IsEqualTo(
+                """
+                {
+                  productById {
+                    id
+                    name
+                  }
+                }
+
+                {
+                  productById {
+                    estimatedDelivery
+                  }
+                }
+                """);
+    }
 }
