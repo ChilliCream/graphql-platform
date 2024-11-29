59379f3c11255e4a515dd0f8a6b00129f4acc43e
Michael Staib <michael@chillicream.com>
2024-11-29 17:55:47 +0100

    Fixed .NET compatibility of ported features. (#7782)

--- src/HotChocolate/Core/src/Types/Types/Directives/SemanticNonNullDirective.cs
+++ src/HotChocolate/Core/src/Types/Types/Directives/SemanticNonNullDirective.cs
@@ -5,7 +5,11 @@ namespace HotChocolate.Types;
 [DirectiveType(WellKnownDirectives.SemanticNonNull, DirectiveLocation.FieldDefinition, IsRepeatable = false)]
 public sealed class SemanticNonNullDirective(IReadOnlyList<int> levels)
 {
+#if NETSTANDARD2_0
+    [GraphQLType(typeof(ListType<NonNullType<IntType>>))]
+#else
     [GraphQLType<ListType<NonNullType<IntType>>>]
+#endif
     [DefaultValueSyntax("[0]")]
     public IReadOnlyList<int>? Levels { get; } = levels;
 }
