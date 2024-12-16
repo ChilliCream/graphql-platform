b8ef25b82d49e3be3b3b9fb1b5bf4327ba1c7616
Michael Staib <michael@chillicream.com>
2024-12-16 13:28:39 +0100

    Added XML docs

--- src/HotChocolate/CostAnalysis/src/CostAnalysis/Options/RequestCostOptions.cs
+++ src/HotChocolate/CostAnalysis/src/CostAnalysis/Options/RequestCostOptions.cs
@@ -1,5 +1,20 @@
 namespace HotChocolate.CostAnalysis;
 
+/// <summary>
+/// Request options for cost analysis.
+/// </summary>
+/// <param name="MaxFieldCost">
+/// The maximum allowed field cost.
+/// </param>
+/// <param name="MaxTypeCost">
+/// The maximum allowed type cost.
+/// </param>
+/// <param name="EnforceCostLimits">
+/// Defines if the analyzer shall enforce cost limits.
+/// </param>
+/// <param name="FilterVariableMultiplier">
+/// The filter variable multiplier.
+/// </param>
 public record RequestCostOptions(
     double MaxFieldCost,
     double MaxTypeCost,
