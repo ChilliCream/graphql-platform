4561bb5b06f1cc088fb4be303f429c3aca022f3c
Daniel Reynolds <djreynolds.1@outlook.com>
2024-12-19 13:41:09 +0000

    Make amendments following feedback.

--- src/HotChocolate/Fusion-vnext/src/Fusion.Composition/PreMergeValidation/Rules/ExternalArgumentDefaultMismatchRule.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Composition/PreMergeValidation/Rules/ExternalArgumentDefaultMismatchRule.cs
@@ -1,5 +1,6 @@
 using System.Collections.Immutable;
 using HotChocolate.Fusion.Events;
+using HotChocolate.Language;
 using static HotChocolate.Fusion.Logging.LogEntryHelper;
 
 namespace HotChocolate.Fusion.PreMergeValidation.Rules;
@@ -34,23 +35,14 @@ internal sealed class ExternalArgumentDefaultMismatchRule : IEventHandler<Output
         foreach (var argumentName in argumentNames)
         {
             var arguments = fieldGroup
-                .Select(i => i.Field.Arguments[argumentName])
+                .SelectMany(i => i.Field.Arguments.Where(a => a.Name == argumentName))
                 .ToImmutableArray();
 
             var defaultValue = arguments[0].DefaultValue;
 
             foreach (var argument in arguments)
             {
-                var currentDefaultValue = argument.DefaultValue;
-                var match = (currentDefaultValue, defaultValue) switch
-                {
-                    (null, null) => true,
-                    (not null, null) => false,
-                    (null, not null) => false,
-                    _ => currentDefaultValue.Value!.Equals(defaultValue.Value)
-                };
-
-                if (!match)
+                if (!SyntaxComparer.BySyntax.Equals(argument.DefaultValue, defaultValue))
                 {
                     context.Log.Write(
                         ExternalArgumentDefaultMismatch(argumentName, fieldName, typeName));
