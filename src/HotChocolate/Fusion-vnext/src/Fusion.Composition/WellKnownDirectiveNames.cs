02e8e31936f18d0b43bfff384f1a5817b98f25db
Glen <glen.84@gmail.com>
2024-12-24 12:04:07 +0200

    [Fusion] Added pre-merge validation rule "ExternalUnusedRule" (#7864)

--- src/HotChocolate/Fusion-vnext/src/Fusion.Composition/WellKnownDirectiveNames.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Composition/WellKnownDirectiveNames.cs
@@ -4,4 +4,5 @@ internal static class WellKnownDirectiveNames
 {
     public const string External = "external";
     public const string Inaccessible = "inaccessible";
+    public const string Provides = "provides";
 }
