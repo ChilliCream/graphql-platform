9fac072ba535154db303724c3866931c9686ea70
Glen <glen.84@gmail.com>
2024-12-26 17:13:38 +0200

    [Fusion] Added pre-merge validation rule "KeyFieldsHasArgumentsRule"

--- src/HotChocolate/Fusion-vnext/src/Fusion.Composition/Properties/CompositionResources.Designer.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Composition/Properties/CompositionResources.Designer.cs
@@ -150,6 +150,15 @@ namespace HotChocolate.Fusion.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to An @key directive on type &apos;{0}&apos; in schema &apos;{1}&apos; references field &apos;{2}&apos;, which must not have arguments..
+        /// </summary>
+        internal static string LogEntryHelper_KeyFieldsHasArguments {
+            get {
+                return ResourceManager.GetString("LogEntryHelper_KeyFieldsHasArguments", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to An @key directive on type &apos;{0}&apos; in schema &apos;{1}&apos; references field &apos;{2}&apos;, which must not be a list, interface, or union type..
         /// </summary>
         internal static string LogEntryHelper_KeyFieldsSelectInvalidType {
