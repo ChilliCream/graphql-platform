9cbde223492a1a33db0dcd7df7b62e40dad4de9c
Daniel Reynolds <djreynolds.1@outlook.com>
2025-01-01 16:14:28 +0000

    Removed bold formatting in comment.

--- src/HotChocolate/Fusion-vnext/src/Fusion.Composition/PreMergeValidation/Rules/LookupShouldHaveNullableReturnTypeRule.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Composition/PreMergeValidation/Rules/LookupShouldHaveNullableReturnTypeRule.cs
@@ -7,7 +7,7 @@ namespace HotChocolate.Fusion.PreMergeValidation.Rules;
 /// <summary>
 /// Fields annotated with the <c>@lookup</c> directive are intended to retrieve a single entity
 /// based on provided arguments. To properly handle cases where the requested entity does not exist,
-/// such fields should have a <b>nullable return type</b>. This allows the field to return null when
+/// such fields should have a nullable return type. This allows the field to return null when
 /// an entity matching the provided criteria is not found, following the standard GraphQL practices
 /// for representing missing data.
 /// </summary>
