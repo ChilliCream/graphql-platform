1af5c8fc6e65929a134238386e24ba446056c872
Michael Staib <michael@chillicream.com>
2025-01-02 14:02:25 +0100

    Added tests

--- src/HotChocolate/Fusion-vnext/test/Fusion.Execution.Tests/Types/CompositeInterfaceTypeTests.cs
+++ src/HotChocolate/Fusion-vnext/test/Fusion.Execution.Tests/Types/CompositeInterfaceTypeTests.cs
@@ -34,14 +34,50 @@ public class CompositeInterfaceTypeTests : FusionTestBase
 
         var product = schema.GetType<CompositeObjectType>("Product");
         Assert.Equal("Product", product.Name);
-        Assert.Equal("ACCOUNTS", Assert.Single(node.Sources).SchemaName);
-        Assert.Equal("node", Assert.Single(node.Implements).Name);
+        Assert.Equal("ACCOUNTS", Assert.Single(product.Sources).SchemaName);
+        Assert.Equal("Node", Assert.Single(product.Implements).Name);
     }
 
     [Fact]
     public void Interface_With_Lookup()
     {
+        var schema = CreateCompositeSchema(
+            """
+            interface Node
+                @fusion__type(schema: ACCOUNTS)
+                @fusion__lookup(
+                    schema: ACCOUNTS
+                    key: "{ id }"
+                    field: "node(id: ID!): Node"
+                    map: ["id"]
+                ) {
+                id: ID!
+                    @fusion__field(schema: ACCOUNTS)
+            }
 
+            type Product implements Node
+                @fusion__type(schema: ACCOUNTS)
+                @fusion__lookup(
+                    schema: ACCOUNTS
+                    key: "{ id }"
+                    field: "node(id: ID!): Node"
+                    map: ["id"]
+                ) {
+                id: ID!
+                    @fusion__field(schema: ACCOUNTS)
+                name: String
+                    @fusion__field(schema: ACCOUNTS)
+            }
+
+            type Query
+                @fusion__type(schema: ACCOUNTS) {
+                node(id: ID!): Node
+            }
+            """);
+
+        var node = schema.GetType<CompositeInterfaceType>("Node");
+        var id = Assert.Single(Assert.Single(node.Sources).Lookups).Fields.Single().ToString();
+        Assert.Equal("id", id);
     }
 
 }
