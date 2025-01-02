787142abd47c8eec54fcf7bd76279221c35a2fff
Michael Staib <michael@chillicream.com>
2025-01-02 13:04:00 +0100

    wip

--- src/HotChocolate/Fusion-vnext/src/Fusion.Execution/Planning/Nodes/UnresolvableSelection.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Execution/Planning/Nodes/UnresolvableSelection.cs
@@ -1,4 +1,5 @@
 using System.Collections.Immutable;
+using HotChocolate.Fusion.Types;
 using HotChocolate.Language;
 
 namespace HotChocolate.Fusion.Planning.Nodes;
@@ -7,6 +8,19 @@ public sealed record UnresolvableSelection(
     ISelectionNode Selection,
     ImmutableStack<SelectionPlanNode> Path);
 
+
+// we need also to track:
+// - ITypeNode type
+// - FieldPath RequiredField
 public sealed record DataRequirement(
     SelectionSetNode SelectionSet,
+    ImmutableArray<FieldReference> RequiredFields,
     ImmutableStack<SelectionPlanNode> Path);
+
+// name is not good ...
+public sealed record FieldReference(
+    // what is required
+    FieldPath Path,
+
+    // whats the type of the data that is required
+    ITypeNode Type);
