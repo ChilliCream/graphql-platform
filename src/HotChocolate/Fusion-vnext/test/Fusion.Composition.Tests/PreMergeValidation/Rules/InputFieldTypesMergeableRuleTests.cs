83ff50f074ae5dd9df8c9d1df56c945105b88ba9
Daniel Reynolds <djreynolds.1@outlook.com>
2025-01-04 15:25:23 +0000

    [Fusion] Added pre-merge validation rule InputFieldsTypesMergeableRule

---
+++ src/HotChocolate/Fusion-vnext/test/Fusion.Composition.Tests/PreMergeValidation/Rules/InputFieldTypesMergeableRuleTests.cs
@@ -0,0 +1,163 @@
+using HotChocolate.Fusion.Logging;
+using HotChocolate.Fusion.PreMergeValidation;
+using HotChocolate.Fusion.PreMergeValidation.Rules;
+
+namespace HotChocolate.Composition.PreMergeValidation.Rules;
+
+public sealed class InputFieldTypesMergeableRuleTests : CompositionTestBase
+{
+    private readonly PreMergeValidator _preMergeValidator =
+        new([new InputFieldTypesMergeableRule()]);
+
+    [Theory]
+    [MemberData(nameof(ValidExamplesData))]
+    public void Examples_Valid(string[] sdl)
+    {
+        // arrange
+        var context = CreateCompositionContext(sdl);
+
+        // act
+        var result = _preMergeValidator.Validate(context);
+
+        // assert
+        Assert.True(result.IsSuccess);
+        Assert.True(context.Log.IsEmpty);
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidExamplesData))]
+    public void Examples_Invalid(string[] sdl, string[] errorMessages)
+    {
+        // arrange
+        var context = CreateCompositionContext(sdl);
+
+        // act
+        var result = _preMergeValidator.Validate(context);
+
+        // assert
+        Assert.True(result.IsFailure);
+        Assert.Equal(errorMessages, context.Log.Select(e => e.Message).ToArray());
+        Assert.True(context.Log.All(e => e.Code == "INPUT_FIELD_TYPES_NOT_MERGEABLE"));
+        Assert.True(context.Log.All(e => e.Severity == LogSeverity.Error));
+    }
+
+    public static TheoryData<string[]> ValidExamplesData()
+    {
+        return new TheoryData<string[]>
+        {
+            // In the following example, the field "name" in "AuthorInput" has compatible types
+            // across source schemas, making them mergeable.
+            {
+                [
+                    """
+                    # Schema A
+                    input AuthorInput {
+                        name: String!
+                    }
+                    """,
+                    """
+                    # Schema B
+                    input AuthorInput {
+                        name: String!
+                    }
+                    """
+                ]
+            },
+            // The following example shows that fields are mergeable if they have different
+            // nullability but the named type is the same and the list structure is the same.
+            {
+                [
+                    """
+                    # Schema A
+                    input AuthorInput {
+                        tags: [String!]
+                    }
+                    """,
+                    """
+                    # Schema B
+                    input AuthorInput {
+                        tags: [String]!
+                    }
+                    """,
+                    """
+                    # Schema C
+                    input AuthorInput {
+                        tags: [String]
+                    }
+                    """
+                ]
+            },
+            // Multiple input fields.
+            {
+                [
+                    """
+                    # Schema A
+                    input AuthorInput {
+                        name: String!
+                        tags: [String!]
+                        birthdate: DateTime
+                    }
+                    """,
+                    """
+                    # Schema B
+                    input AuthorInput {
+                        name: String!
+                        tags: [String]!
+                        birthdate: DateTime
+                    }
+                    """
+                ]
+            },
+        };
+    }
+
+    public static TheoryData<string[], string[]> InvalidExamplesData()
+    {
+        return new TheoryData<string[], string[]>
+        {
+            // In this example, the field "birthdate" on "AuthorInput" is not mergeable as the
+            // field has different named types ("String" and "DateTime") across source schemas.
+            {
+                [
+                    """
+                    # Schema A
+                    input AuthorInput {
+                        birthdate: String!
+                    }
+                    """,
+                    """
+                    # Schema B
+                    input AuthorInput {
+                        birthdate: DateTime!
+                    }
+                    """
+                ],
+                [
+                    "Input field 'AuthorInput.birthdate' has a different type shape in schema " +
+                    "'A' than it does in schema 'B'."
+                ]
+            },
+            // List versus non-list
+            {
+                [
+                    """
+                    # Schema A
+                    input AuthorInput {
+                        birthdate: String!
+                    }
+                    """,
+                    """
+                    # Schema B
+                    input AuthorInput {
+                        birthdate: [String!]
+                    }
+                    """
+                ],
+                [
+                    "Input field 'AuthorInput.birthdate' has a different type shape in schema " +
+                    "'A' than it does in schema 'B'."
+                ]
+            }
+        };
+    }
+}
