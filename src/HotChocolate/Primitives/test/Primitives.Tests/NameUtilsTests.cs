a4c76bb042c71396385cfbe08e446e8206197e54
Michael Staib <michael@chillicream.com>
2025-01-22 12:32:34 +0100

    Fixed tests

--- src/HotChocolate/Primitives/test/Primitives.Tests/NameUtilsTests.cs
+++ src/HotChocolate/Primitives/test/Primitives.Tests/NameUtilsTests.cs
@@ -15,7 +15,8 @@ public class NameUtilsTests
         var message = Assert.Throws<ArgumentException>(() => name.EnsureGraphQLName()).Message;
         Assert.Equal(
             $"`{name}` is not a valid GraphQL name.{Environment.NewLine}"
-            + $"https://spec.graphql.org/October2021/#sec-Names (Parameter 'name')",
+            + $"https://spec.graphql.org/October2021/#sec-Names{Environment.NewLine}"
+            + $" (Parameter 'name')",
             message);
     }
 
