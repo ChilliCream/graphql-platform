76d05676b3c0fc39c202189ec5995a1ddd34af8d
Michael Staib <michael@chillicream.com>
2025-01-29 13:00:21 +0000

    wip

--- src/HotChocolate/Pagination/test/Pagination.EntityFramework.Tests/PostgresCacheCollectionFixture.cs
+++
@@ -1,9 +0,0 @@
-using Squadron;
-
-namespace HotChocolate.Data;
-
-[CollectionDefinition(DefinitionName)]
-public class PostgresCacheCollectionFixture : ICollectionFixture<PostgreSqlResource>
-{
-    internal const string DefinitionName = "PostgresSqlResource";
-}
