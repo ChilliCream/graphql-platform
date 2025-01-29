cd7f06bb0aef960e13a1b111bc5a1fc332814da5
Michael Staib <michael@chillicream.com>
2025-01-29 17:16:47 +0000

    Fixed source generator issues.

--- src/HotChocolate/Core/test/Types.Analyzers.Tests/OperationTests.cs
+++ src/HotChocolate/Core/test/Types.Analyzers.Tests/OperationTests.cs
@@ -64,4 +64,31 @@ public class OperationTests
             }
             """).MatchMarkdownAsync();
     }
+
+    [Fact]
+    public async Task Root_Projection_Single_Entity()
+    {
+        await TestHelper.GetGeneratedSourceSnapshot(
+            """
+            using HotChocolate;
+            using HotChocolate.Types;
+            using GreenDonut.Data;
+
+            namespace TestNamespace;
+
+            [QueryType]
+            public static partial class Query
+            {
+                public static Foo GetTest(QueryContext<Foo> context)
+                {
+                    return new Foo { Bar = ""abc"" };
+                }
+            }
+
+            public class Foo
+            {
+                public string Bar { get; set; }
+            }
+            """).MatchMarkdownAsync();
+    }
 }
