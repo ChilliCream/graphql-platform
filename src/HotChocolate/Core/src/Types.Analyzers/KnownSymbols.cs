528862b94ac177979cc048d04752cc64e314aad0
Michael Staib <michael@chillicream.com>
2025-03-21 15:31:12 +0100

    Relaxed type symbol check for connections. (#8165)

--- src/HotChocolate/Core/src/Types.Analyzers/KnownSymbols.cs
+++ src/HotChocolate/Core/src/Types.Analyzers/KnownSymbols.cs
@@ -83,28 +83,23 @@ public static class KnownSymbols
         return false;
     }
 
-    public static INamedTypeSymbol GetConnectionBaseSymbol(this GeneratorSyntaxContext context)
+    public static INamedTypeSymbol? GetConnectionBaseSymbol(this GeneratorSyntaxContext context)
         => context.SemanticModel.Compilation.GetConnectionBaseSymbol();
 
-    public static INamedTypeSymbol GetConnectionBaseSymbol(this Compilation compilation)
-        => compilation.GetTypeByMetadataName("HotChocolate.Types.Pagination.ConnectionBase`3")
-            ?? throw new InvalidOperationException("Could not resolve connection base type.");
+    public static INamedTypeSymbol? GetConnectionBaseSymbol(this Compilation compilation)
+        => compilation.GetTypeByMetadataName("HotChocolate.Types.Pagination.ConnectionBase`3");
 
-    public static INamedTypeSymbol GetEdgeInterfaceSymbol(this Compilation compilation)
-        => compilation.GetTypeByMetadataName("HotChocolate.Types.Pagination.IEdge`1")
-            ?? throw new InvalidOperationException("Could not resolve edge interface type.");
+    public static INamedTypeSymbol? GetEdgeInterfaceSymbol(this Compilation compilation)
+        => compilation.GetTypeByMetadataName("HotChocolate.Types.Pagination.IEdge`1");
 
-    public static INamedTypeSymbol GetTaskSymbol(this Compilation compilation)
-        => compilation.GetTypeByMetadataName("System.Threading.Tasks.Task`1")
-            ?? throw new InvalidOperationException("Could not resolve connection base type.");
+    public static INamedTypeSymbol? GetTaskSymbol(this Compilation compilation)
+        => compilation.GetTypeByMetadataName("System.Threading.Tasks.Task`1");
 
-    public static INamedTypeSymbol GetValueTaskSymbol(this Compilation compilation)
-        => compilation.GetTypeByMetadataName("System.Threading.Tasks.ValueTask`1")
-            ?? throw new InvalidOperationException("Could not resolve connection base type.");
+    public static INamedTypeSymbol? GetValueTaskSymbol(this Compilation compilation)
+        => compilation.GetTypeByMetadataName("System.Threading.Tasks.ValueTask`1");
 
-    public static INamedTypeSymbol GetConnectionFlagsSymbol(this Compilation compilation)
-        => compilation.GetTypeByMetadataName("HotChocolate.Types.Pagination.ConnectionFlags")
-            ?? throw new InvalidOperationException("Could not resolve connection flags type.");
+    public static INamedTypeSymbol? GetConnectionFlagsSymbol(this Compilation compilation)
+        => compilation.GetTypeByMetadataName("HotChocolate.Types.Pagination.ConnectionFlags");
 
     public static bool IsConnectionType(this GeneratorSyntaxContext context, ITypeSymbol possibleConnectionType)
         => context.SemanticModel.Compilation.IsConnectionType(possibleConnectionType);
@@ -174,8 +169,14 @@ public static class KnownSymbols
         return SymbolEqualityComparer.Default.Equals(namedType, compilation.GetConnectionFlagsSymbol());
     }
 
-    private static bool IsDerivedFromGenericBase(INamedTypeSymbol typeSymbol, INamedTypeSymbol baseTypeSymbol)
+    private static bool IsDerivedFromGenericBase(INamedTypeSymbol? typeSymbol, INamedTypeSymbol? baseTypeSymbol)
     {
+        // if we are generating only for GreenDonut some base types might not exist.
+        if (baseTypeSymbol is null)
+        {
+            return false;
+        }
+
         var current = typeSymbol;
 
         while (current is not null)
