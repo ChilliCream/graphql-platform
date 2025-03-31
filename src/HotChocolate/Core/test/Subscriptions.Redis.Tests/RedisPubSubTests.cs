4a3d9b50d11344f497f99b92a77ac848863f8bf4
Danny Heard <danny.heard@airelogic.com>
2025-03-31 13:50:26 +0100

    Test using real channel arg

--- src/HotChocolate/Core/test/Subscriptions.Redis.Tests/RedisPubSubTests.cs
+++ src/HotChocolate/Core/test/Subscriptions.Redis.Tests/RedisPubSubTests.cs
@@ -8,30 +8,22 @@ namespace HotChocolate.Subscriptions.Redis;
 
 public class RedisPubSubTests : IClassFixture<RedisResource>
 {
+    private readonly RedisResource _redisResource;
     private readonly RedisPubSub _redisPubSub;
 
-    private readonly Mock<IConnectionMultiplexer> _connectionMock = new();
     private readonly Mock<ISubscriber> _subscriberMock = new();
 
     public RedisPubSubTests(RedisResource redisResource, ITestOutputHelper outputHelper)
     {
-        var connectionMultiplexer = redisResource.GetConnection();
+        _redisResource = redisResource;
 
-        _connectionMock
+        var connectionMock = new Mock<IConnectionMultiplexer>();
+        connectionMock
             .Setup(x => x.GetSubscriber(It.IsAny<object?>()))
             .Returns(_subscriberMock.Object);
 
-        // Have to use the real implementation since ChannelMessageQueue is internal
-        _subscriberMock
-            .Setup(x => x.SubscribeAsync(It.IsAny<RedisChannel>(), CommandFlags.None))
-            .Returns((RedisChannel channel, CommandFlags cf) => connectionMultiplexer.GetSubscriber().SubscribeAsync(channel, cf));
-
-        _subscriberMock
-            .Setup(x => x.UnsubscribeAsync(It.IsAny<RedisChannel>(), null, CommandFlags.None))
-            .Returns((RedisChannel channel,  Action<RedisChannel,RedisValue>? handler, CommandFlags cf) => connectionMultiplexer.GetSubscriber().UnsubscribeAsync(channel, handler, cf));
-
         _redisPubSub = new RedisPubSub(
-            _connectionMock.Object,
+            connectionMock.Object,
             new DefaultJsonMessageSerializer(),
             new SubscriptionOptions(),
             new SubscriptionTestDiagnostics(outputHelper));
@@ -40,12 +32,25 @@ public class RedisPubSubTests : IClassFixture<RedisResource>
     [Fact]
     public async Task Unsubscribe_Should_UnsubscribeAsync()
     {
+        // Have to use the real implementation since ChannelMessageQueue is internal
+        var connectionMultiplexer = _redisResource.GetConnection();
+
+        RedisChannel subscribedChannel = "test_topic";
+        _subscriberMock
+            .Setup(x => x.SubscribeAsync(It.IsAny<RedisChannel>(), CommandFlags.None))
+            .Returns((RedisChannel channel, CommandFlags flags) => connectionMultiplexer.GetSubscriber().SubscribeAsync(channel, flags))
+            .Callback<RedisChannel, CommandFlags>((channel, _) => subscribedChannel = channel);
+
+        _subscriberMock
+            .Setup(x => x.UnsubscribeAsync(It.IsAny<RedisChannel>(), null, CommandFlags.None))
+            .Returns((RedisChannel channel,  Action<RedisChannel,RedisValue>? handler, CommandFlags cf) => connectionMultiplexer.GetSubscriber().UnsubscribeAsync(channel, handler, cf));
+
         var subscription = await _redisPubSub.SubscribeAsync<TestMessage>("test_topic");
 
         await subscription.DisposeAsync();
 
         _subscriberMock.Verify(
-            x => x.UnsubscribeAsync(It.IsAny<RedisChannel>(), null, CommandFlags.None),
+            x => x.UnsubscribeAsync(subscribedChannel, null, CommandFlags.None),
             Times.Once);
     }
 
