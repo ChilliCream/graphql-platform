4e95453a6c44844e331749da1514a474fec1fef5
Danny Heard <danny.heard@airelogic.com>
2025-04-02 15:05:30 +0100

    Add channel removed integration test

--- src/HotChocolate/Core/test/Subscriptions.Redis.Tests/RedisIntegrationTests.cs
+++ src/HotChocolate/Core/test/Subscriptions.Redis.Tests/RedisIntegrationTests.cs
@@ -1,6 +1,8 @@
+using HotChocolate.Execution;
 using HotChocolate.Execution.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Squadron;
+using StackExchange.Redis;
 using Xunit.Abstractions;
 
 namespace HotChocolate.Subscriptions.Redis;
@@ -46,6 +48,45 @@ public class RedisIntegrationTests : SubscriptionIntegrationTestBase, IClassFixt
     [Fact]
     public override Task Subscribe_And_Complete_Topic_With_ValueTypeMessage()
         => base.Subscribe_And_Complete_Topic_With_ValueTypeMessage();
+
+    [Fact]
+    public async Task Unsubscribe_Should_RemoveChannel()
+    {
+        using var cts = new CancellationTokenSource(Timeout);
+        await using var services = CreateServer<Subscription>();
+
+        var result = await services.ExecuteRequestAsync(
+            "subscription { onMessage }",
+            cancellationToken: cts.Token);
+
+        var activeChannelsAfterSubscribe = await GetActiveChannelsAsync();
+
+        await result.DisposeAsync();
+
+        var channelRemovedEvent = new ManualResetEventSlim(false);
+
+        _ = Task.Run(async () =>
+        {
+            while (true)
+            {
+                var activeChannels = await GetActiveChannelsAsync();
+                if (activeChannels.Length < activeChannelsAfterSubscribe.Length)
+                {
+                    channelRemovedEvent.Set();
+                    break;
+                }
+
+                await Task.Delay(100, cts.Token);
+            }
+        }, cts.Token);
+
+        channelRemovedEvent.Wait(cts.Token);
+    }
+
+    private async Task<RedisResult[]> GetActiveChannelsAsync()
+    {
+        return (RedisResult[])(await _redisResource.GetConnection().GetDatabase().ExecuteAsync("PUBSUB", "CHANNELS"))!;
+    }
 
     protected override void ConfigurePubSub(IRequestExecutorBuilder graphqlBuilder)
         => graphqlBuilder.AddRedisSubscriptions(_ => _redisResource.GetConnection());
