5d5b47d58b22b0258fe32277c2c410129c53e54d
Danny Heard <danny.heard@airelogic.com>
2025-04-02 14:20:04 +0100

    Generic test

---
+++ src/HotChocolate/Core/test/Subscriptions.Tests/DefaultTopicTests.cs
@@ -0,0 +1,62 @@
+﻿using HotChocolate.Subscriptions.Diagnostics;
+using HotChocolate.Tests;
+using Moq;
+using Xunit.Abstractions;
+
+namespace HotChocolate.Subscriptions;
+
+public class DefaultTopicTests(ITestOutputHelper outputHelper)
+{
+    [Fact]
+    public async Task Unsubscribe_ForAsyncDisposableSession_DisposesAsync()
+    {
+        var sessionMock = new Mock<IAsyncAndSyncDisposable>();
+        var pubSub = new NoOpPubSub(sessionMock.Object, new SubscriptionTestDiagnostics(outputHelper));
+
+        var sourceStream = await pubSub.SubscribeAsync<string>("topic");
+        await sourceStream.DisposeAsync();
+
+        sessionMock.Verify(x => x.DisposeAsync(), Times.Once);
+        sessionMock.Verify(x => x.Dispose(), Times.Never);
+    }
+
+    private sealed class NoOpPubSub(IAsyncAndSyncDisposable session, ISubscriptionDiagnosticEvents diagnosticEvents)
+        : DefaultPubSub(new SubscriptionOptions(), diagnosticEvents)
+    {
+        protected override ValueTask OnSendAsync<TMessage>(string formattedTopic, TMessage message, CancellationToken cancellationToken = default)
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        protected override ValueTask OnCompleteAsync(string formattedTopic)
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        protected override DefaultTopic<TMessage> OnCreateTopic<TMessage>(string formattedTopic, int? bufferCapacity, TopicBufferFullMode? bufferFullMode)
+        {
+            return new AsyncDisposableTopic<TMessage>(
+                formattedTopic,
+                bufferCapacity ?? 1,
+                bufferFullMode ?? TopicBufferFullMode.DropOldest,
+                DiagnosticEvents,
+                session);
+        }
+
+        private sealed class AsyncDisposableTopic<TMessage>(
+            string name,
+            int capacity,
+            TopicBufferFullMode fullMode,
+            ISubscriptionDiagnosticEvents diagnosticEvents,
+            IAsyncAndSyncDisposable session)
+            : DefaultTopic<TMessage>(name, capacity, fullMode, diagnosticEvents)
+        {
+            protected override ValueTask<IDisposable> OnConnectAsync(CancellationToken cancellationToken)
+            {
+                return ValueTask.FromResult<IDisposable>(session);
+            }
+        }
+    }
+
+    public interface IAsyncAndSyncDisposable : IAsyncDisposable, IDisposable;
+}
