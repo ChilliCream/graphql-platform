8fac68207c32b2e33890e1fd6fd1d3b8163c7fd8
Michael Staib <michael@chillicream.com>
2025-05-07 11:35:50 +0200

    Enhanced FusionInputFieldDefinition and CompositeInputFieldCompletionContext by introducing IFeatureCollection and updating the handling of DeclaringMember. Improved SchemaCoordinate logic for better type resolution and consistency across field definitions. Adjusted ToSyntaxNode methods to utilize the new SchemaDebugFormatter for streamlined output.

--- src/HotChocolate/Fusion-vnext/src/Fusion.Execution.Types/Completion/CompositeInputFieldCompletionContext.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Execution.Types/Completion/CompositeInputFieldCompletionContext.cs
@@ -1,13 +1,20 @@
+using HotChocolate.Features;
 using HotChocolate.Fusion.Types.Collections;
 using HotChocolate.Types;
 
 namespace HotChocolate.Fusion.Types.Completion;
 
 internal ref struct CompositeInputFieldCompletionContext(
+    ITypeSystemMember declaringMember,
     FusionDirectiveCollection directives,
-    IType type)
+    IType type,
+    IFeatureCollection features)
 {
+    public ITypeSystemMember DeclaringMember { get; } = declaringMember;
+
     public FusionDirectiveCollection Directives { get; } = directives;
 
     public IType Type { get; } = type;
+
+    public IFeatureCollection Features { get; } = features;
 }
