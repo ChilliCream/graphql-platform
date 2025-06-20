2ab9f8f56e8e0e4e55811299e22a4ad7c6ccbe30
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2025-06-20 14:52:01 +0200

    Add reproduction

--- src/HotChocolate/Fusion/test/Core.Tests/DemoIntegrationTests.cs
+++ src/HotChocolate/Fusion/test/Core.Tests/DemoIntegrationTests.cs
@@ -16,6 +16,124 @@ public class DemoIntegrationTests(ITestOutputHelper output)
     private readonly Func<ICompositionLog> _logFactory = () => new TestCompositionLog(output);
 
     [Fact]
+    public async Task Viewer_Bug_1()
+    {
+        // arrange
+        var subgraphA = await TestSubgraph.CreateAsync(
+            """
+            type Query {
+              recentTestimonial: Testimonial
+              viewer: Viewer
+            }
+            type Testimonial {
+              id: ID!
+            }
+            type Viewer {
+              name: String
+            }
+            """);
+        var subgraphB = await TestSubgraph.CreateAsync(
+            """
+            type Query {
+              viewer: Viewer
+            }
+            type Viewer {
+              exclusiveSubgraphB: String
+            }
+            """);
+
+        using var subgraphs = new TestSubgraphCollection(output, [subgraphA, subgraphB]);
+        var executor = await subgraphs.GetExecutorAsync();
+
+        var request = Parse(
+            """
+            query {
+              recentTestimonial {
+                __typename
+              }
+              viewer {
+                exclusiveSubgraphB
+              }
+            }
+            """);
+
+        // act
+        await using var result = await executor.ExecuteAsync(
+            OperationRequestBuilder
+                .New()
+                .SetDocument(request)
+                .Build());
+
+        // assert
+        var snapshot = new Snapshot();
+        CollectSnapshotData(snapshot, request, result);
+        await snapshot.MatchMarkdownAsync();
+    }
+
+    [Fact]
+    public async Task Viewer_Bug_2()
+    {
+        // arrange
+        var subgraphA = await TestSubgraph.CreateAsync(
+            """
+            type Query {
+              recentTestimonial: Testimonial
+              viewer: Viewer
+            }
+            type Testimonial {
+              id: ID!
+            }
+            type Viewer {
+              subType: SubType
+            }
+            type SubType {
+              subgraphA: String
+            }
+            """);
+        var subgraphB = await TestSubgraph.CreateAsync(
+            """
+            type Query {
+              viewer: Viewer
+            }
+            type Viewer {
+              subType: SubType
+            }
+            type SubType {
+              subgraphB: String
+            }
+            """);
+
+        using var subgraphs = new TestSubgraphCollection(output, [subgraphA, subgraphB]);
+        var executor = await subgraphs.GetExecutorAsync();
+
+        var request = Parse(
+            """
+            query {
+              recentTestimonial {
+                __typename
+              }
+              viewer {
+                subType {
+                  subgraphB
+                }
+              }
+            }
+            """);
+
+        // act
+        await using var result = await executor.ExecuteAsync(
+            OperationRequestBuilder
+                .New()
+                .SetDocument(request)
+                .Build());
+
+        // assert
+        var snapshot = new Snapshot();
+        CollectSnapshotData(snapshot, request, result);
+        await snapshot.MatchMarkdownAsync();
+    }
+
+    [Fact]
     public async Task Same_Selection_On_Two_Object_Types_That_Require_Data_From_Another_Subgraph()
     {
         // arrange
@@ -2341,7 +2459,7 @@ public class DemoIntegrationTests(ITestOutputHelper output)
         await snapshot.MatchMarkdownAsync();
     }
 
-    [Fact(Skip = "Fix this test in the new planner")]
+    [Fact]
     public async Task Field_Below_Shared_Field_Only_Available_On_One_Subgraph_Type_Of_Shared_Field_Not_Node()
     {
         // arrange
