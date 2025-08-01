d84a4442d55bfc3eaae1116461b6a369a41ad79b
NOlbert <45466413+N-Olbert@users.noreply.github.com>
2025-08-01 23:23:59 +0200

    Fix test with english strings on non-english systems

--- src/HotChocolate/Core/test/Types.Analyzers.Tests/RequestMiddlewareTests.cs
+++ src/HotChocolate/Core/test/Types.Analyzers.Tests/RequestMiddlewareTests.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace HotChocolate.Types;
 
 public class RequestMiddlewareTests
@@ -5,46 +7,56 @@ public class RequestMiddlewareTests
     [Fact]
     public async Task GenerateSource_RequestMiddleware_MatchesSnapshot()
     {
-        await TestHelper.GetGeneratedSourceSnapshot(
-            """
-            #nullable enable
-            using System.Threading.Tasks;
-            using HotChocolate;
-            using HotChocolate.Execution;
-            using Microsoft.AspNetCore.Builder;
-            using Microsoft.Extensions.DependencyInjection;
+        var currentUiCulture = CultureInfo.CurrentUICulture;
+        try
+        {
+            // Snapshot contains localized strings -> adjust culture
+            CultureInfo.CurrentUICulture = new CultureInfo("en-US");
+            await TestHelper.GetGeneratedSourceSnapshot(
+                """
+                #nullable enable
+                using System.Threading.Tasks;
+                using HotChocolate;
+                using HotChocolate.Execution;
+                using Microsoft.AspNetCore.Builder;
+                using Microsoft.Extensions.DependencyInjection;
 
-            public class Program
-            {
-                public static void Main(string[] args)
+                public class Program
                 {
-                    var builder = WebApplication.CreateBuilder(args);
-                    builder.Services
-                        .AddGraphQLServer()
-                        .UseRequest<SomeRequestMiddleware>();
+                    public static void Main(string[] args)
+                    {
+                        var builder = WebApplication.CreateBuilder(args);
+                        builder.Services
+                            .AddGraphQLServer()
+                            .UseRequest<SomeRequestMiddleware>();
+                    }
                 }
-            }
 
-            public class SomeRequestMiddleware(
-                RequestDelegate next,
-                #pragma warning disable CS9113
-                [SchemaService] Service1 service1,
-                [SchemaService] Service2? service2)
-                #pragma warning restore CS9113
-            {
-                public async ValueTask InvokeAsync(
-                    IRequestContext context,
+                public class SomeRequestMiddleware(
+                    RequestDelegate next,
                     #pragma warning disable CS9113
-                    Service1 service1,
-                    Service2? service2)
+                    [SchemaService] Service1 service1,
+                    [SchemaService] Service2? service2)
                     #pragma warning restore CS9113
                 {
-                    await next(context);
+                    public async ValueTask InvokeAsync(
+                        IRequestContext context,
+                        #pragma warning disable CS9113
+                        Service1 service1,
+                        Service2? service2)
+                        #pragma warning restore CS9113
+                    {
+                        await next(context);
+                    }
                 }
-            }
 
-            public class Service1;
-            public class Service2;
-            """).MatchMarkdownAsync();
+                public class Service1;
+                public class Service2;
+                """).MatchMarkdownAsync();
+        }
+        finally
+        {
+            CultureInfo.CurrentUICulture = currentUiCulture;
+        }
     }
 }
