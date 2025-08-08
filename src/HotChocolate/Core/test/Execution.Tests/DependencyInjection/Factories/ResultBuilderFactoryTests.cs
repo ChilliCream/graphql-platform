a90d54f1786ff5d4315dbb1f53e95d12022ae16c
Jwshyns <joshua.haynes97@gmail.com>
2025-08-08 19:46:38 +1000

    feat: Made test name more intrinsically named

--- src/HotChocolate/Core/test/Execution.Tests/DependencyInjection/Factories/ResultBuilderFactoryTests.cs
+++ src/HotChocolate/Core/test/Execution.Tests/DependencyInjection/Factories/ResultBuilderFactoryTests.cs
@@ -5,10 +5,9 @@ namespace HotChocolate.Execution.DependencyInjection.Factories;
 public class ResultBuilderFactoryTests
 {
     [Fact]
-    public void Create_IsTypeResultBuilder()
+    public void Create_Returns_ResultWithOfTypeResultBuilder()
     {
         var factory = new ResultBuilderFactory();
-
         var resultPool = new ResultPool(
             new ObjectResultPool(16, 16, 16),
             new ListResultPool(16, 16, 16)
