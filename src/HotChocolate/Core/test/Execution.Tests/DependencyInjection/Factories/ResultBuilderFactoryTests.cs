d24cc797bc8f2ac8577710ca3614580ba163ec36
Jwshyns <joshua.haynes97@gmail.com>
2025-08-08 19:36:19 +1000

    feat: Added basic unit test for ResultBuilderFactory

---
+++ src/HotChocolate/Core/test/Execution.Tests/DependencyInjection/Factories/ResultBuilderFactoryTests.cs
@@ -0,0 +1,21 @@
+﻿using HotChocolate.Execution.Processing;
+
+namespace HotChocolate.Execution.DependencyInjection.Factories;
+
+public class ResultBuilderFactoryTests
+{
+    [Fact]
+    public void Create_IsTypeResultBuilder()
+    {
+        var factory = new ResultBuilderFactory();
+
+        var resultPool = new ResultPool(
+            new ObjectResultPool(16, 16, 16),
+            new ListResultPool(16, 16, 16)
+        );
+
+        var result = factory.Create(resultPool);
+
+        Assert.IsType<ResultBuilder>(result);
+    }
+}
