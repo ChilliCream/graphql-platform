94823ab3c1a517baac4ad0a9544abfda9cf87204
Glen <glen.84@gmail.com>
2025-08-08 11:24:09 +0200

    [Fusion] Added missing descriptions to Fusion types (#8506)

--- src/HotChocolate/Fusion-vnext/src/Fusion.Composition/Definitions/FusionSchemaMutableEnumTypeDefinition.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Composition/Definitions/FusionSchemaMutableEnumTypeDefinition.cs
@@ -1,15 +1,22 @@
 using System.Collections.Immutable;
 using HotChocolate.Types.Mutable;
+using static HotChocolate.Fusion.Properties.CompositionResources;
 using static HotChocolate.Fusion.StringUtilities;
 using static HotChocolate.Fusion.WellKnownTypeNames;
 
 namespace HotChocolate.Fusion.Definitions;
 
+/// <summary>
+/// The <c>fusion__Schema</c> enum is a generated type used within an execution schema document to
+/// refer to a source schema in a type-safe manner.
+/// </summary>
 internal sealed class FusionSchemaMutableEnumTypeDefinition : MutableEnumTypeDefinition
 {
     public FusionSchemaMutableEnumTypeDefinition(ImmutableArray<string> schemaNames)
         : base(FusionSchema)
     {
+        Description = FusionSchemaMutableEnumTypeDefinition_Description;
+
         foreach (var schemaName in schemaNames)
         {
             Values.Add(new MutableEnumValue(ToConstantCase(schemaName)));
--- src/HotChocolate/Fusion-vnext/src/Fusion.Composition/SourceSchemaMerger.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Composition/SourceSchemaMerger.cs
@@ -1155,19 +1155,19 @@ internal sealed class SourceSchemaMerger
             // Scalar type definitions.
             {
                 TypeNames.FusionFieldDefinition,
-                MutableScalarTypeDefinition.Create(TypeNames.FusionFieldDefinition)
+                new FusionFieldDefinitionMutableScalarTypeDefinition()
             },
             {
                 TypeNames.FusionFieldSelectionMap,
-                MutableScalarTypeDefinition.Create(TypeNames.FusionFieldSelectionMap)
+                new FusionFieldSelectionMapMutableScalarTypeDefinition()
             },
             {
                 TypeNames.FusionFieldSelectionPath,
-                MutableScalarTypeDefinition.Create(TypeNames.FusionFieldSelectionPath)
+                new FusionFieldSelectionPathMutableScalarTypeDefinition()
             },
             {
                 TypeNames.FusionFieldSelectionSet,
-                MutableScalarTypeDefinition.Create(TypeNames.FusionFieldSelectionSet)
+                new FusionFieldSelectionSetMutableScalarTypeDefinition()
             },
             // Enum type definitions.
             {
