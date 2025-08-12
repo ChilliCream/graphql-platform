a1540f70b47a7aab47e0db2c9de12339517100f2
Michael Staib <michael@chillicream.com>
2025-08-12 10:33:19 +0200

    added summary docs to some types

--- src/HotChocolate/Core/src/Fetching/BatchDispatchEventType.cs
+++ src/HotChocolate/Core/src/Fetching/BatchDispatchEventType.cs
@@ -1,8 +1,22 @@
 namespace HotChocolate.Fetching;
 
+/// <summary>
+/// Represents the type of event that can occur within the <see cref="IBatchDispatcher"/>.
+/// </summary>
 public enum BatchDispatchEventType
 {
+    /// <summary>
+    /// A batch was enqueued to the <see cref="IBatchDispatcher"/>.
+    /// </summary>
     Enqueued,
+
+    /// <summary>
+    /// A batch was evaluated by the <see cref="IBatchDispatcher"/>.
+    /// </summary>
     Evaluated,
+
+    /// <summary>
+    /// A batch was dispatched by the <see cref="IBatchDispatcher"/>.
+    /// </summary>
     Dispatched
 }
