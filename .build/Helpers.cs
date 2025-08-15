5a9654bf6d5543fe2d97661ceb1332dccd50d2e0
Michael Staib <michael@chillicream.com>
2025-08-15 13:51:18 +0200

    Added HotChocolate MCP to release build

--- .build/Helpers.cs
+++ .build/Helpers.cs
@@ -24,6 +24,7 @@ static class Helpers
         Path.Combine("HotChocolate", "Data"),
         Path.Combine("HotChocolate", "Marten"),
         Path.Combine("HotChocolate", "MongoDb"),
+        Path.Combine("HotChocolate", "ModelContextProtocol"),
         // Path.Combine("HotChocolate", "OpenApi"),
         Path.Combine("HotChocolate", "Primitives"),
         Path.Combine("HotChocolate", "Raven"),
