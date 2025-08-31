56622e6a4fbe3baa325b6ff6bf82b6d6bc9c704e
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2025-08-31 16:55:43 +0200

    Setup @chillicream/nitro package

---
+++ src/Nitro/CommandLine/src/js/cli.js
@@ -0,0 +1,57 @@
+"use strict";
+
+import { join } from "path";
+import { spawn } from "child_process";
+import { family, GLIBC, MUSL } from "detect-libc";
+
+async function resolveBinary() {
+  const { platform, arch } = process;
+
+  const binaries = {
+    darwin: {
+      x64: "osx-x64/nitro",
+      arm64: "osx-arm64/nitro",
+    },
+    win32: {
+      x64: "win-x64/nitro.exe",
+      ia32: "win-x86/nitro.exe",
+      arm64: "win-arm64/nitro.exe",
+    },
+    linux: {
+      x64: async () => {
+        const libc = await family();
+
+        if (libc === GLIBC) {
+          return "linux-x64/nitro";
+        } else if (libc === MUSL) {
+          return "linux-musl-x64/nitro";
+        }
+
+        return null;
+      },
+      arm64: "linux-arm64/nitro",
+    },
+  };
+
+  const binary = binaries[platform]?.[arch];
+  if (!binary) {
+    return null;
+  }
+
+  const binaryPath = typeof binary === "function" ? await binary() : binary;
+
+  return binaryPath ? join(import.meta.dirname, binaryPath) : null;
+}
+
+const bin = await resolveBinary();
+const input = process.argv.slice(2);
+
+console.log({ bin, input });
+
+if (bin !== null) {
+  spawn(bin, input, { stdio: "inherit" }).on("exit", process.exit);
+} else {
+  throw new Error(
+    `Platform "${process.platform} (${process.arch})" not supported.`
+  );
+}
