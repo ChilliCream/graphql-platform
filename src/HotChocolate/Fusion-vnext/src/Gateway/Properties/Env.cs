7012ba12131612108aaf89d87eaa3f1544f60c6e
Michael Staib <michael@chillicream.com>
2025-09-01 16:52:02 +0200

    Cleanup

--- src/HotChocolate/Fusion-vnext/src/Gateway/Properties/Env.cs
+++
@@ -1,8 +0,0 @@
-namespace Demo.Gateway.Properties;
-
-public static class Env
-{
-    public const string GatewayApi = "gateway-api";
-
-    public static string Version => typeof(Env).Assembly.GetName().Version?.ToString() ?? "0.0.0";
-}
