5b0cbe2188d319aea0c5ea1f94dd509f4576aef6
Michael Staib <michael@chillicream.com>
2025-09-28 21:35:35 +0200

    Fix schema services dispose handling.

--- src/HotChocolate/Core/src/Execution/RequestExecutorResolver.cs
+++ src/HotChocolate/Core/src/Execution/RequestExecutorResolver.cs
@@ -489,7 +489,7 @@ internal sealed partial class RequestExecutorResolver
 
         public void Dispose()
         {
-            if (_disposed)
+            if (!_disposed)
             {
                 if (Services is IDisposable d)
                 {
