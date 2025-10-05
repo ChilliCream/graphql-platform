ecb1ec0a790a65887dc56b23810dea7b83d3cb23
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2025-10-05 12:49:31 +0200

    Ignore inapplicable type refinements on abstract types

--- src/HotChocolate/Fusion-vnext/test/Fusion.AspNetCore.Tests/InterfaceTests.cs
+++ src/HotChocolate/Fusion-vnext/test/Fusion.AspNetCore.Tests/InterfaceTests.cs
@@ -480,38 +480,23 @@ public class InterfaceTests : FusionTestBase
     }
 
     [Fact]
-    public async Task Interface_Field_Concrete_Types_Exclusive_To_Other_Schema()
+    public async Task Interface_Field_With_Only_Type_Refinements_On_Same_Schema()
     {
         // arrange
         var server1 = CreateSourceSchema(
             "A",
             """
             type Query {
-              item: ProductCatalogItem
+              someField: SomeInterface
             }
 
-            type ProductCatalogItem {
-              # IMPORTANT: interface return type is non-null and only exists here
-              price: Price!
+            interface SomeInterface {
+              value: String
             }
 
-            # Shared interface name across subgraphs
-            interface Price {
-              currency: String
-              value: Float
-            }
-
-            # Concrete types ONLY EXIST in this subgraph
-            type ProductCatalogItemFullPrice implements Price {
-              currency: String
-              value: Float
-              extra: String
-            }
-
-            type ProductCatalogItemSalePrice implements Price {
-              currency: String
-              value: Float
-              discountPercentage: Float
+            type ConcreteTypeA implements SomeInterface {
+              value: String
+              specificToA: String
             }
             """);
 
@@ -519,36 +504,16 @@ public class InterfaceTests : FusionTestBase
             "B",
             """
             type Query {
-              product(id: ID!): Product @lookup
+              anotherField: SomeInterface
             }
 
-            type Product {
-              id: ID!
-              variation: Variation
+            interface SomeInterface {
+              value: String
             }
 
-            type Variation {
-              # Same interface name is composed across subgraphs,
-              # but no ProductCatalogItem* concrete types here
-              price: Price
-            }
-
-            # Same interface name (composed), but different implementations here
-            interface Price {
-              currency: String
-              value: Float
-            }
-
-            type FullPrice implements Price {
-              currency: String
-              value: Float
-              tags: [String!]!
-            }
-
-            type SalePrice implements Price {
-              currency: String
-              value: Float
-              discountPercentage: Float
+            type ConcreteTypeB implements SomeInterface {
+              value: String
+              specificToB: String
             }
             """);
 
@@ -564,17 +529,10 @@ public class InterfaceTests : FusionTestBase
         var request = new Transport.OperationRequest(
             """
             {
-              item {
-                price {
-                  __typename
-                  value
-                  currency
-                  ... on ProductCatalogItemFullPrice {
-                    extra
-                  }
-                  ... on ProductCatalogItemSalePrice {
-                    discountPercentage
-                  }
+              someField {
+                value
+                ... on ConcreteTypeA {
+                  specificToA
                 }
               }
             }
@@ -588,20 +546,141 @@ public class InterfaceTests : FusionTestBase
         await MatchSnapshotAsync(gateway, request, result);
     }
 
-    // TODO: This currently breaks
-    // {
-    //   item {
-    //     price {
-    //       __typename
-    //       value
-    //       currency
-    //       # This type only exists on Subgraph B and there's no way to transition to it from Subgraph A
-    //       ... on SalePrice {
-    //         discountPercentage
-    //       }
-    //     }
-    //   }
-    // }
+    [Fact]
+    public async Task Interface_Field_With_Type_Refinements_Exclusive_To_Other_Schema()
+    {
+        // arrange
+        var server1 = CreateSourceSchema(
+            "A",
+            """
+            type Query {
+              someField: SomeInterface
+            }
+
+            interface SomeInterface {
+              value: String
+            }
+
+            type ConcreteTypeA implements SomeInterface {
+              value: String
+              specificToA: String
+            }
+            """);
+
+        var server2 = CreateSourceSchema(
+            "B",
+            """
+            type Query {
+              anotherField: SomeInterface
+            }
+
+            interface SomeInterface {
+              value: String
+            }
+
+            type ConcreteTypeB implements SomeInterface {
+              value: String
+              specificToB: String
+            }
+            """);
+
+        using var gateway = await CreateCompositeSchemaAsync(
+        [
+            ("A", server1),
+            ("B", server2)
+        ]);
+
+        // act
+        using var client = GraphQLHttpClient.Create(gateway.CreateClient());
+
+        var request = new Transport.OperationRequest(
+            """
+            {
+              someField {
+                value
+                ... on ConcreteTypeA {
+                  specificToA
+                }
+                ... on ConcreteTypeB {
+                  specificToB
+                }
+              }
+            }
+            """);
+
+        using var result = await client.PostAsync(
+            request,
+            new Uri("http://localhost:5000/graphql"));
+
+        // assert
+        await MatchSnapshotAsync(gateway, request, result);
+    }
+
+    [Fact]
+    public async Task Interface_Field_With_Only_Type_Refinements_Exclusive_To_Other_Schema()
+    {
+        // arrange
+        var server1 = CreateSourceSchema(
+            "A",
+            """
+            type Query {
+              someField: SomeInterface
+            }
+
+            interface SomeInterface {
+              value: String
+            }
+
+            type ConcreteTypeA implements SomeInterface {
+              value: String
+              specificToA: String
+            }
+            """);
+
+        var server2 = CreateSourceSchema(
+            "B",
+            """
+            type Query {
+              anotherField: SomeInterface
+            }
+
+            interface SomeInterface {
+              value: String
+            }
+
+            type ConcreteTypeB implements SomeInterface {
+              value: String
+              specificToB: String
+            }
+            """);
+
+        using var gateway = await CreateCompositeSchemaAsync(
+        [
+            ("A", server1),
+            ("B", server2)
+        ]);
+
+        // act
+        using var client = GraphQLHttpClient.Create(gateway.CreateClient());
+
+        var request = new Transport.OperationRequest(
+            """
+            {
+              someField {
+                ... on ConcreteTypeB {
+                  specificToB
+                }
+              }
+            }
+            """);
+
+        using var result = await client.PostAsync(
+            request,
+            new Uri("http://localhost:5000/graphql"));
+
+        // assert
+        await MatchSnapshotAsync(gateway, request, result);
+    }
 
     #endregion
 
