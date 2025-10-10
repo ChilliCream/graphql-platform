e1323b7a7a24d6b35eafcc06552dfa18174eea15
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2025-10-10 21:24:19 +0200

    Update src/HotChocolate/Core/src/Types/SchemaOptions.cs

--- src/HotChocolate/Core/src/Types/SchemaOptions.cs
+++ src/HotChocolate/Core/src/Types/SchemaOptions.cs
@@ -170,7 +170,7 @@ public class SchemaOptions : IReadOnlySchemaOptions
             if (value < 16)
             {
                 throw new ArgumentException(
-                    "The size of operation document cache must be greater than 16.");
+                    "The size of operation document cache must be at least 16");
             }
 
             field = value;
