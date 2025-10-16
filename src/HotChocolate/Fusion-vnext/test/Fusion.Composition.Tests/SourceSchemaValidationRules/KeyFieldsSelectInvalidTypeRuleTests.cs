8267263a8c6ce7f5fde365165f532c1a0dc533bf
Glen <glen.84@gmail.com>
2025-10-16 10:01:58 +0200

    [Fusion] Restructured Composition tests (#8815)

--- src/HotChocolate/Fusion-vnext/test/Fusion.Composition.Tests/SourceSchemaValidationRules/KeyFieldsSelectInvalidTypeRuleTests.cs
+++ src/HotChocolate/Fusion-vnext/test/Fusion.Composition.Tests/SourceSchemaValidationRules/KeyFieldsSelectInvalidTypeRuleTests.cs
@@ -1,231 +1,207 @@
-using System.Collections.Immutable;
-using HotChocolate.Fusion.Logging;
-using static HotChocolate.Fusion.CompositionTestHelper;
-
 namespace HotChocolate.Fusion.SourceSchemaValidationRules;
 
-public sealed class KeyFieldsSelectInvalidTypeRuleTests
+public sealed class KeyFieldsSelectInvalidTypeRuleTests : RuleTestBase
 {
-    private static readonly object s_rule = new KeyFieldsSelectInvalidTypeRule();
-    private static readonly ImmutableArray<object> s_rules = [s_rule];
-    private readonly CompositionLog _log = new();
+    protected override object Rule { get; } = new KeyFieldsSelectInvalidTypeRule();
 
-    [Theory]
-    [MemberData(nameof(ValidExamplesData))]
-    public void Examples_Valid(string[] sdl)
+    // In this example, the "Product" type has a valid @key directive referencing the scalar field
+    // "sku".
+    [Fact]
+    public void Validate_KeyFieldsSelectValidType_Succeeds()
     {
-        // arrange
-        var schemas = CreateSchemaDefinitions(sdl);
-        var validator = new SourceSchemaValidator(schemas, s_rules, _log);
-
-        // act
-        var result = validator.Validate();
-
-        // assert
-        Assert.True(result.IsSuccess);
-        Assert.True(_log.IsEmpty);
-    }
-
-    [Theory]
-    [MemberData(nameof(InvalidExamplesData))]
-    public void Examples_Invalid(string[] sdl, string[] errorMessages)
-    {
-        // arrange
-        var schemas = CreateSchemaDefinitions(sdl);
-        var validator = new SourceSchemaValidator(schemas, s_rules, _log);
-
-        // act
-        var result = validator.Validate();
-
-        // assert
-        Assert.True(result.IsFailure);
-        Assert.Equal(errorMessages, _log.Select(e => e.Message).ToArray());
-        Assert.True(_log.All(e => e.Code == "KEY_FIELDS_SELECT_INVALID_TYPE"));
-        Assert.True(_log.All(e => e.Severity == LogSeverity.Error));
-    }
-
-    public static TheoryData<string[]> ValidExamplesData()
-    {
-        return new TheoryData<string[]>
-        {
-            // In this example, the "Product" type has a valid @key directive referencing the scalar
-            // field "sku".
-            {
-                [
-                    """
-                    type Product @key(fields: "sku") {
-                        sku: String!
-                        name: String
-                    }
-                    """
-                ]
+        AssertValid(
+        [
+            """
+            type Product @key(fields: "sku") {
+                sku: String!
+                name: String
             }
-        };
+            """
+        ]);
     }
 
-    public static TheoryData<string[], string[]> InvalidExamplesData()
+    // In the following example, the "Product" type has an invalid @key directive referencing a
+    // field ("featuredItem") whose type is an interface, violating the rule.
+    [Fact]
+    public void Validate_KeyFieldsSelectInvalidTypeInterface_Fails()
     {
-        return new TheoryData<string[], string[]>
-        {
-            // In the following example, the "Product" type has an invalid @key directive
-            // referencing a field ("featuredItem") whose type is an interface, violating the rule.
-            {
-                [
-                    """
-                    type Product @key(fields: "featuredItem { id }") {
-                        featuredItem: Node!
-                        sku: String!
-                    }
+        AssertInvalid(
+            [
+                """
+                type Product @key(fields: "featuredItem { id }") {
+                    featuredItem: Node!
+                    sku: String!
+                }
 
-                    interface Node {
-                        id: ID!
-                    }
-                    """
-                ],
-                [
-                    "A @key directive on type 'Product' in schema 'A' references field "
-                    + "'Product.featuredItem', which must not be a list, interface, or union type."
-                ]
-            },
-            // In this example, the @key directive references a field ("tags") of type "List", which
-            // is also not allowed.
-            {
-                [
-                    """
-                    type Product @key(fields: "tags") {
-                        tags: [String!]!
-                        sku: String!
-                    }
-                    """
-                ],
-                [
-                    "A @key directive on type 'Product' in schema 'A' references field "
-                    + "'Product.tags', which must not be a list, interface, or union type."
-                ]
-            },
-            // In this example, the @key directive references a field ("relatedItems") of type
-            // "Union", which violates the rule.
-            {
-                [
-                    """
-                    type Product @key(fields: "relatedItems") {
-                        relatedItems: Related!
-                        sku: String!
-                    }
+                interface Node {
+                    id: ID!
+                }
+                """
+            ],
+            [
+                "A @key directive on type 'Product' in schema 'A' references field "
+                + "'Product.featuredItem', which must not be a list, interface, or union type."
+            ]);
+    }
 
-                    union Related = Product | Service
+    // In this example, the @key directive references a field ("tags") of type "List", which is also
+    // not allowed.
+    [Fact]
+    public void Validate_KeyFieldsSelectInvalidTypeList_Fails()
+    {
+        AssertInvalid(
+            [
+                """
+                type Product @key(fields: "tags") {
+                    tags: [String!]!
+                    sku: String!
+                }
+                """
+            ],
+            [
+                "A @key directive on type 'Product' in schema 'A' references field 'Product.tags', "
+                + "which must not be a list, interface, or union type."
+            ]);
+    }
 
-                    type Service {
-                        id: ID!
-                    }
-                    """
-                ],
-                [
-                    "A @key directive on type 'Product' in schema 'A' references field "
-                    + "'Product.relatedItems', which must not be a list, interface, or union type."
-                ]
-            },
-            // Nested interface.
-            {
-                [
-                    """
-                    type Product @key(fields: "info { featuredItem { id } }") {
-                        info: ProductInfo!
-                    }
+    // In this example, the @key directive references a field ("relatedItems") of type "Union",
+    // which violates the rule.
+    [Fact]
+    public void Validate_KeyFieldsSelectInvalidTypeUnion_Fails()
+    {
+        AssertInvalid(
+            [
+                """
+                type Product @key(fields: "relatedItems") {
+                    relatedItems: Related!
+                    sku: String!
+                }
 
-                    type ProductInfo {
-                        featuredItem: Node!
-                    }
+                union Related = Product | Service
 
-                    interface Node {
-                        id: ID!
-                    }
-                    """
-                ],
-                [
-                    "A @key directive on type 'Product' in schema 'A' references field "
-                    + "'ProductInfo.featuredItem', which must not be a list, interface, or union "
-                    + "type."
-                ]
-            },
-            // Nested list.
-            {
-                [
-                    """
-                    type Product @key(fields: "info { tags }") {
-                        info: ProductInfo!
-                    }
+                type Service {
+                    id: ID!
+                }
+                """
+            ],
+            [
+                "A @key directive on type 'Product' in schema 'A' references field "
+                + "'Product.relatedItems', which must not be a list, interface, or union type."
+            ]);
+    }
 
-                    type ProductInfo {
-                        tags: [String!]!
-                    }
-                    """
-                ],
-                [
-                    "A @key directive on type 'Product' in schema 'A' references field "
-                    + "'ProductInfo.tags', which must not be a list, interface, or union type."
-                ]
-            },
-            // Nested union.
-            {
-                [
-                    """
-                    type Product @key(fields: "info { relatedItems }") {
-                        info: ProductInfo!
-                    }
+    // Nested interface.
+    [Fact]
+    public void Validate_KeyFieldsSelectInvalidTypeNestedInterface_Fails()
+    {
+        AssertInvalid(
+            [
+                """
+                type Product @key(fields: "info { featuredItem { id } }") {
+                    info: ProductInfo!
+                }
 
-                    type ProductInfo {
-                        relatedItems: Related!
-                    }
+                type ProductInfo {
+                    featuredItem: Node!
+                }
 
-                    union Related = Product | Service
+                interface Node {
+                    id: ID!
+                }
+                """
+            ],
+            [
+                "A @key directive on type 'Product' in schema 'A' references field "
+                + "'ProductInfo.featuredItem', which must not be a list, interface, or union type."
+            ]);
+    }
 
-                    type Service {
-                        id: ID!
-                    }
-                    """
-                ],
-                [
-                    "A @key directive on type 'Product' in schema 'A' references field "
-                    + "'ProductInfo.relatedItems', which must not be a list, interface, or union "
-                    + "type."
-                ]
-            },
-            // Multiple keys.
-            {
-                [
-                    """
-                    type Product
-                        @key(fields: "featuredItem { id }")
-                        @key(fields: "tags")
-                        @key(fields: "relatedItems") {
-                        featuredItem: Node!
-                        tags: [String!]!
-                        relatedItems: Related!
-                    }
+    // Nested list.
+    [Fact]
+    public void Validate_KeyFieldsSelectInvalidTypeNestedList_Fails()
+    {
+        AssertInvalid(
+            [
+                """
+                type Product @key(fields: "info { tags }") {
+                    info: ProductInfo!
+                }
 
-                    interface Node {
-                        id: ID!
-                    }
+                type ProductInfo {
+                    tags: [String!]!
+                }
+                """
+            ],
+            [
+                "A @key directive on type 'Product' in schema 'A' references field "
+                + "'ProductInfo.tags', which must not be a list, interface, or union type."
+            ]);
+    }
 
-                    union Related = Product | Service
+    // Nested union.
+    [Fact]
+    public void Validate_KeyFieldsSelectInvalidTypeNestedUnion_Fails()
+    {
+        AssertInvalid(
+            [
+                """
+                type Product @key(fields: "info { relatedItems }") {
+                    info: ProductInfo!
+                }
 
-                    type Service {
-                        id: ID!
-                    }
-                    """
-                ],
-                [
-                    "A @key directive on type 'Product' in schema 'A' references field "
-                    + "'Product.featuredItem', which must not be a list, interface, or union type.",
+                type ProductInfo {
+                    relatedItems: Related!
+                }
 
-                    "A @key directive on type 'Product' in schema 'A' references field "
-                    + "'Product.tags', which must not be a list, interface, or union type.",
+                union Related = Product | Service
 
-                    "A @key directive on type 'Product' in schema 'A' references field "
-                    + "'Product.relatedItems', which must not be a list, interface, or union type."
-                ]
-            }
-        };
+                type Service {
+                    id: ID!
+                }
+                """
+            ],
+            [
+                "A @key directive on type 'Product' in schema 'A' references field "
+                + "'ProductInfo.relatedItems', which must not be a list, interface, or union type."
+            ]);
+    }
+
+    // Multiple keys.
+    [Fact]
+    public void Validate_KeyFieldsSelectInvalidTypeMultipleKeys_Fails()
+    {
+        AssertInvalid(
+            [
+                """
+                type Product
+                    @key(fields: "featuredItem { id }")
+                    @key(fields: "tags")
+                    @key(fields: "relatedItems") {
+                    featuredItem: Node!
+                    tags: [String!]!
+                    relatedItems: Related!
+                }
+
+                interface Node {
+                    id: ID!
+                }
+
+                union Related = Product | Service
+
+                type Service {
+                    id: ID!
+                }
+                """
+            ],
+            [
+                "A @key directive on type 'Product' in schema 'A' references field "
+                + "'Product.featuredItem', which must not be a list, interface, or union type.",
+
+                "A @key directive on type 'Product' in schema 'A' references field 'Product.tags', "
+                + "which must not be a list, interface, or union type.",
+
+                "A @key directive on type 'Product' in schema 'A' references field "
+                + "'Product.relatedItems', which must not be a list, interface, or union type."
+            ]);
     }
 }
