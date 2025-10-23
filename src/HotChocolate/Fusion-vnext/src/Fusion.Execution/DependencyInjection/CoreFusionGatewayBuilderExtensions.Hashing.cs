3f3ad80d2b4656693c436d77f1145646a344bc3a
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2025-10-23 19:17:51 +0200

    Support document hash configuration in Fusion

---
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Execution/DependencyInjection/CoreFusionGatewayBuilderExtensions.Hashing.cs
@@ -0,0 +1,41 @@
+using HotChocolate.Fusion.Configuration;
+using HotChocolate.Language;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static partial class CoreFusionGatewayBuilderExtensions
+{
+    public static IFusionGatewayBuilder AddMD5DocumentHashProvider(
+        this IFusionGatewayBuilder builder,
+        HashFormat format = HashFormat.Base64)
+    {
+        return builder.ConfigureSchemaServices((_, services) =>
+        {
+            services.RemoveAll<IDocumentHashProvider>();
+            services.AddSingleton<IDocumentHashProvider>(new MD5DocumentHashProvider(format));
+        });
+    }
+
+    public static IFusionGatewayBuilder AddSha1DocumentHashProvider(
+        this IFusionGatewayBuilder builder,
+        HashFormat format = HashFormat.Base64)
+    {
+        return builder.ConfigureSchemaServices((_, services) =>
+        {
+            services.RemoveAll<IDocumentHashProvider>();
+            services.AddSingleton<IDocumentHashProvider>(new Sha1DocumentHashProvider(format));
+        });
+    }
+
+    public static IFusionGatewayBuilder AddSha256DocumentHashProvider(
+       this IFusionGatewayBuilder builder,
+        HashFormat format = HashFormat.Base64)
+    {
+        return builder.ConfigureSchemaServices((_, services) =>
+        {
+            services.RemoveAll<IDocumentHashProvider>();
+            services.AddSingleton<IDocumentHashProvider>(new Sha256DocumentHashProvider(format));
+        });
+    }
+}
