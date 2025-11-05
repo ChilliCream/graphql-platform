4c3a3f3c65745d9255ee11470669df17cc7cc08e
Michael Staib <michael@chillicream.com>
2025-11-05 21:32:01 +0100

    Make Utilities.Introspection AOT compatible

---
+++ src/HotChocolate/Utilities/src/Utilities.Introspection/IntrospectionJsonSerializerContext.cs
@@ -0,0 +1,19 @@
+using System.Text.Json.Serialization;
+
+namespace HotChocolate.Utilities.Introspection;
+
+[JsonSourceGenerationOptions(
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    DefaultIgnoreCondition = JsonIgnoreCondition.Never)]
+[JsonSerializable(typeof(IntrospectionData))]
+[JsonSerializable(typeof(IntrospectionError[]))]
+[JsonSerializable(typeof(Schema))]
+[JsonSerializable(typeof(FullType))]
+[JsonSerializable(typeof(Field))]
+[JsonSerializable(typeof(InputField))]
+[JsonSerializable(typeof(Directive))]
+[JsonSerializable(typeof(TypeRef))]
+[JsonSerializable(typeof(RootTypeRef))]
+[JsonSerializable(typeof(EnumValue))]
+[JsonSerializable(typeof(TypeKind))]
+internal sealed partial class IntrospectionJsonSerializerContext : JsonSerializerContext;
