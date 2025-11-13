e801a477ae70f73b3e96f4572ffc4cbaaa06ddac
Glen <glen.84@gmail.com>
2025-11-13 14:23:01 +0200

    Implemented test `ListTools_AfterSchemaUpdate_ReturnsUpdatedTools`

--- src/HotChocolate/ModelContextProtocol/test/HotChocolate.ModelContextProtocol.Tests/FusionIntegrationTests.cs
+++ src/HotChocolate/ModelContextProtocol/test/HotChocolate.ModelContextProtocol.Tests/FusionIntegrationTests.cs
@@ -1,17 +1,26 @@
+using System.Buffers;
+using System.Text.Json;
+using CookieCrumble;
+using HotChocolate.Buffers;
 using HotChocolate.Execution;
 using HotChocolate.Fusion;
+using HotChocolate.Fusion.Configuration;
 using HotChocolate.Fusion.Logging;
 using HotChocolate.Fusion.Options;
+using HotChocolate.Language;
 using HotChocolate.ModelContextProtocol.Diagnostics;
 using HotChocolate.ModelContextProtocol.Directives;
 using HotChocolate.ModelContextProtocol.Extensions;
 using HotChocolate.ModelContextProtocol.Storage;
 using HotChocolate.Types;
+using HotChocolate.Types.Mutable;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
 using ModelContextProtocol.Server;
 
 namespace HotChocolate.ModelContextProtocol;
@@ -19,9 +28,94 @@ namespace HotChocolate.ModelContextProtocol;
 public sealed class FusionIntegrationTests : IntegrationTestBase
 {
     [Fact]
-    public void ListTools_AfterSchemaUpdate_ReturnsUpdatedTools()
+    public async Task ListTools_AfterSchemaUpdate_ReturnsUpdatedTools()
     {
-        Assert.Fail("FIXME");
+        // arrange
+        var storage = new TestOperationToolStorage();
+        await storage.AddOrUpdateToolAsync(
+            Utf8GraphQLParser.Parse(
+                await File.ReadAllTextAsync("__resources__/GetBooksWithTitle1.graphql")));
+        var subgraph = CreateSubgraph([]);
+        var schemaDocument = await subgraph.Services.GetSchemaAsync();
+        var schemaComposer =
+            new SchemaComposer(
+                [new SourceSchemaText(schemaDocument.Name, schemaDocument.ToString())],
+                new SchemaComposerOptions(),
+                new CompositionLog());
+        var result = schemaComposer.Compose();
+        var schema = result.Value;
+        var initialConfig =
+            new FusionConfiguration(
+                schema.ToSyntaxNode(),
+                new JsonDocumentOwner(JsonDocument.Parse("{ }"), new EmptyMemoryOwner()));
+        var configProvider = new TestFusionConfigurationProvider(initialConfig);
+        var builder = new WebHostBuilder()
+            .ConfigureServices(
+                services => services
+                    .AddRouting()
+                    .AddGraphQLGatewayServer()
+                    .AddConfigurationProvider(_ => configProvider)
+                    .AddMcp()
+                    .AddMcpToolStorage(storage))
+            .Configure(
+                app => app
+                    .UseRouting()
+                    .UseEndpoints(endpoints => endpoints.MapGraphQLMcp()));
+        var server = new TestServer(builder);
+        var mcpClient1 = await CreateMcpClientAsync(server.CreateClient());
+        var mcpClient2 = await CreateMcpClientAsync(server.CreateClient());
+        var listChangedResetEvent1 = new ManualResetEventSlim(false);
+        var listChangedResetEvent2 = new ManualResetEventSlim(false);
+        mcpClient1.RegisterNotificationHandler(
+            NotificationMethods.ToolListChangedNotification,
+            async (_, _) =>
+            {
+                listChangedResetEvent1.Set();
+                await ValueTask.CompletedTask;
+            });
+        mcpClient2.RegisterNotificationHandler(
+            NotificationMethods.ToolListChangedNotification,
+            async (_, _) =>
+            {
+                listChangedResetEvent2.Set();
+                await ValueTask.CompletedTask;
+            });
+
+        // act
+        var tools = await mcpClient1.ListToolsAsync();
+        ((MutableObjectTypeDefinition)schema.Types["Book"]).Fields["title"].Description = "Description";
+        var newConfig =
+            new FusionConfiguration(
+                schema.ToSyntaxNode(),
+                new JsonDocumentOwner(JsonDocument.Parse("{ }"), new EmptyMemoryOwner()));
+        configProvider.UpdateConfiguration(newConfig);
+        IList<McpClientTool>? updatedTools = null;
+
+        if (listChangedResetEvent1.Wait(TimeSpan.FromSeconds(5)))
+        {
+            var mcpClient3 = await CreateMcpClientAsync(server.CreateClient());
+            updatedTools = await mcpClient3.ListToolsAsync();
+        }
+
+        var secondClientNotified = listChangedResetEvent2.Wait(TimeSpan.FromSeconds(5));
+
+        // assert
+        Assert.NotNull(updatedTools);
+        JsonSerializer.Serialize(
+                tools.Concat(updatedTools).Select(
+                    t =>
+                        new
+                        {
+                            t.Name,
+                            t.Title,
+                            t.Description,
+                            t.JsonSchema,
+                            t.ReturnJsonSchema
+                        }),
+                JsonSerializerOptions)
+            .ReplaceLineEndings("\n")
+            .MatchSnapshot(extension: ".json");
+        Assert.True(secondClientNotified);
     }
 
     protected override async Task<TestServer> CreateTestServerAsync(
@@ -126,5 +220,51 @@ public sealed class FusionIntegrationTests : IntegrationTestBase
                     .UseEndpoints(endpoints => endpoints.MapGraphQL()));
 
         return new TestServer(builder);
+    }
+
+    private sealed class TestFusionConfigurationProvider(FusionConfiguration initialConfig)
+        : IFusionConfigurationProvider
+    {
+        private readonly List<IObserver<FusionConfiguration>> _observers = [];
+
+        public IDisposable Subscribe(IObserver<FusionConfiguration> observer)
+        {
+            if (Configuration is not null)
+            {
+                observer.OnNext(Configuration);
+            }
+
+            _observers.Add(observer);
+
+            return new Observer();
+        }
+
+        public ValueTask DisposeAsync() => ValueTask.CompletedTask;
+
+        public FusionConfiguration? Configuration { get; private set; } = initialConfig;
+
+        public void UpdateConfiguration(FusionConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            foreach (var observer in _observers)
+            {
+                observer.OnNext(Configuration);
+            }
+        }
+
+        private sealed class Observer : IDisposable
+        {
+            public void Dispose()
+            {
+            }
+        }
+    }
+
+    private sealed class EmptyMemoryOwner : IMemoryOwner<byte>
+    {
+        public Memory<byte> Memory => default;
+
+        public void Dispose() { }
     }
 }
