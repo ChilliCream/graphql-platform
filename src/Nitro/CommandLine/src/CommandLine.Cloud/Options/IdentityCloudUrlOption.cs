f9316c4827c3f9b823355ab3ace95c46468eeed4
PascalSenn <senn.pasc@gmail.com>
2025-11-18 22:20:50 +0100

    adds new api key command (#8908)

--- src/Nitro/CommandLine/src/CommandLine.Cloud/Options/IdentityCloudUrlOption.cs
+++ src/Nitro/CommandLine/src/CommandLine.Cloud/Options/IdentityCloudUrlOption.cs
@@ -7,6 +7,8 @@ internal sealed class IdentityCloudUrlOption : Option<string>
         Description = "The url of the api.";
         IsRequired = false;
         IsHidden = false;
-        this.DefaultFromEnvironmentValue("CLOUD_URL", defaultValue: OidcConfiguration.IdentityUrl["https://".Length..]);
+        this.DefaultFromEnvironmentValue(
+            "CLOUD_URL",
+            defaultValue: OidcConfiguration.IdentityUrl["https://".Length..]);
     }
 }
