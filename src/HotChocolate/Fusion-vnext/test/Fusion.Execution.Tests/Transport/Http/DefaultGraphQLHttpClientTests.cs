8a2a08e26762a3c48c0b0fd5ce18145809789740
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2026-01-21 08:29:00 +0100

    wip

--- src/HotChocolate/Fusion-vnext/test/Fusion.Execution.Tests/Transport/Http/DefaultGraphQLHttpClientTests.cs
+++ src/HotChocolate/Fusion-vnext/test/Fusion.Execution.Tests/Transport/Http/DefaultGraphQLHttpClientTests.cs
@@ -1,3 +1,5 @@
+using HotChocolate.Transport;
+
 namespace HotChocolate.Fusion.Transport.Http;
 
 public class DefaultGraphQLHttpClientTests
@@ -23,5 +25,69 @@ public class DefaultGraphQLHttpClientTests
         Assert.Equal(500000, itemCount);
 
         await app.StopAsync();
+    }
+
+    [Fact]
+    public async Task Read_Apollo_Request_Batching_Array_Response()
+    {
+        // arrange
+        var handler = new MockHttpMessageHandler(
+            """
+            [
+              {
+                "data": {
+                  "number": 0
+                }
+              },
+              {
+                "data": {
+                  "number": 1
+                }
+              }
+            ]
+            """,
+            "application/json");
+        using var client = new DefaultGraphQLHttpClient(new HttpClient(handler));
+
+        var operationRequest = new HotChocolate.Transport.OperationBatchRequest(
+            [
+                new OperationRequest("{ number }"),
+                new OperationRequest("{ number }")
+            ]);
+        var request = new GraphQLHttpRequest(operationRequest, new Uri("http://localhost:5000/graphql"));
+
+        // act
+        using var result = await client.SendAsync(request);
+        var stream = result.ReadAsResultStreamAsync();
+
+        // assert
+        var count = 0;
+
+        await foreach (var document in stream)
+        {
+            var number = document.Root.GetProperty("data").GetProperty("number").GetInt32();
+
+            Assert.Equal(count, number);
+
+            document.Dispose();
+
+            count++;
+        }
+
+        Assert.Equal(2, count);
+    }
+
+    private class MockHttpMessageHandler(string responseContent, string contentType) : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = new StringContent(responseContent, System.Text.Encoding.UTF8, contentType)
+            };
+            return Task.FromResult(response);
+        }
     }
 }
