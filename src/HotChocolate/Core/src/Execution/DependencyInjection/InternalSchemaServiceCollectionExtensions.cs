50ec4d18c79fbfcf11888af10dbc3660382b17c1
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2026-01-27 11:07:35 +0100

    Move GetRootServiceProvider to abstractions (#9067)

--- src/HotChocolate/Core/src/Execution/DependencyInjection/InternalSchemaServiceCollectionExtensions.cs
+++ src/HotChocolate/Core/src/Execution/DependencyInjection/InternalSchemaServiceCollectionExtensions.cs
@@ -45,30 +45,4 @@ public static class InternalSchemaServiceCollectionExtensions
 
         return services;
     }
-
-    /// <summary>
-    /// Gets the root service provider from the schema services. This allows
-    /// schema services to access application level services.
-    /// </summary>
-    /// <param name="schema">
-    /// The schema.
-    /// </param>
-    /// <returns>
-    /// The root service provider.
-    /// </returns>
-    public static IServiceProvider GetRootServiceProvider(this Schema schema)
-        => schema.Services.GetRequiredService<IRootServiceProviderAccessor>().ServiceProvider;
-
-    /// <summary>
-    /// Gets the root service provider from the schema services. This allows
-    /// schema services to access application level services.
-    /// </summary>
-    /// <param name="services">
-    /// The schema services.
-    /// </param>
-    /// <returns>
-    /// The root service provider.
-    /// </returns>
-    public static IServiceProvider GetRootServiceProvider(this IServiceProvider services)
-        => services.GetRequiredService<IRootServiceProviderAccessor>().ServiceProvider;
 }
