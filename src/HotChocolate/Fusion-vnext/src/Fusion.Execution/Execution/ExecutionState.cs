867a871c278f6a27d0d4daf60666da89cced80e9
Michael Staib <michael@chillicream.com>
2026-02-25 20:26:53 +0000

    Revert "Streamline scheduling of ready execution nodes"
    This reverts commit 3e3f2d46b4e93e3cbc308782cf949e4817909f1e.

--- src/HotChocolate/Fusion-vnext/src/Fusion.Execution/Execution/ExecutionState.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Execution/Execution/ExecutionState.cs
@@ -288,25 +288,50 @@ internal sealed class ExecutionState(bool collectTelemetry, CancellationTokenSou
 
     public bool EnqueueNextNodes(OperationPlanContext context, CancellationToken cancellationToken)
     {
+        _stack.Clear();
+
         if (_ready.Count == 0)
         {
             return false;
         }
 
-        var hasReadyNodes = false;
+        var isSorted = true;
+        var previousId = int.MinValue;
 
         foreach (var node in _ready)
         {
             if ((uint)node.Id < (uint)_remainingDependencies.Length
                 && _remainingDependencies[node.Id] == 0)
             {
-                hasReadyNodes = true;
-                StartNode(context, node, cancellationToken);
+                _stack.Push(node);
+
+                if (node.Id < previousId)
+                {
+                    isSorted = false;
+                }
+
+                previousId = node.Id;
             }
         }
 
         _ready.Clear();
-        return hasReadyNodes;
+
+        if (_stack.Count == 0)
+        {
+            return false;
+        }
+
+        if (!isSorted && _stack.Count > 1)
+        {
+            _stack.Sort(static (a, b) => a.Id.CompareTo(b.Id));
+        }
+
+        foreach (var node in _stack)
+        {
+            StartNode(context, node, cancellationToken);
+        }
+
+        return true;
     }
 
     private void EnsureDependencyCapacity(int minCapacity)
