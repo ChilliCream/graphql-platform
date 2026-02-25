f97b20e3506fefc9a12b2cebdc9a5749239efa26
Michael Staib <michael@chillicream.com>
2026-02-25 22:44:10 +0100

    Fix projection to preserve EF constructor-injected services (#9199)

--- src/HotChocolate/Data/test/Data.EntityFramework.Tests/IntegrationTests.cs
+++ src/HotChocolate/Data/test/Data.EntityFramework.Tests/IntegrationTests.cs
@@ -1,7 +1,9 @@
 using HotChocolate.Execution;
+using HotChocolate.Execution.Processing;
 using HotChocolate.Types;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
 
 namespace HotChocolate.Data;
 
@@ -458,5 +460,203 @@ public class IntegrationTests : IClassFixture<AuthorFixture>
 
         // assert
         result.MatchSnapshot();
+    }
+
+    [Fact]
+    public async Task UseProjection_Should_Preserve_Entity_Constructor_DbContext_Injection()
+    {
+        var databaseName = $"db-{Guid.NewGuid():N}";
+
+        await using (var seedContext = new ConstructorInjectionDbContext(
+            new DbContextOptionsBuilder<ConstructorInjectionDbContext>()
+                .UseInMemoryDatabase(databaseName)
+                .Options))
+        {
+            await seedContext.Database.EnsureCreatedAsync();
+
+            var blog1 = new ConstructorInjectionBlog { Name = "Blog1" };
+            var blog2 = new ConstructorInjectionBlog { Name = "Blog2" };
+
+            await seedContext.Blogs.AddRangeAsync(blog1, blog2);
+            await seedContext.SaveChangesAsync();
+
+            await seedContext.Posts.AddRangeAsync(
+                new ConstructorInjectionPost { BlogId = blog1.Id },
+                new ConstructorInjectionPost { BlogId = blog1.Id },
+                new ConstructorInjectionPost { BlogId = blog2.Id });
+            await seedContext.SaveChangesAsync();
+        }
+
+        var executor = await new ServiceCollection()
+            .AddDbContext<ConstructorInjectionDbContext>(
+                b => b.UseInMemoryDatabase(databaseName))
+            .AddGraphQL()
+            .AddProjections()
+            .AddQueryType<ConstructorInjectionQuery>()
+            .BuildRequestExecutorAsync();
+
+        var result = await executor.ExecuteAsync(
+            """
+            {
+                blogs {
+                    name
+                    postCount
+                }
+                blogsNoProjection {
+                    name
+                    postCount
+                }
+            }
+            """);
+
+        var operationResult = result.ExpectOperationResult();
+        Assert.True(operationResult.Errors is null || operationResult.Errors.Count == 0);
+        Assert.True(operationResult.Data.HasValue);
+
+        using var document = JsonDocument.Parse(result.ToJson());
+        var data = document.RootElement.GetProperty("data");
+        var projectedCounts = ReadCounts(data.GetProperty("blogs"));
+        var unprojectedCounts = ReadCounts(data.GetProperty("blogsNoProjection"));
+
+        Assert.Equal(unprojectedCounts, projectedCounts);
+        Assert.Equal(3, projectedCounts["Blog1"]);
+        Assert.Equal(3, projectedCounts["Blog2"]);
+
+        static Dictionary<string, int> ReadCounts(JsonElement value)
+        {
+            var result = new Dictionary<string, int>();
+
+            foreach (var item in value.EnumerateArray())
+            {
+                result.Add(item.GetProperty("name").GetString()!, item.GetProperty("postCount").GetInt32());
+            }
+
+            return result;
+        }
+    }
+
+    [Fact]
+    public async Task AsSelector_Should_Preserve_Entity_Constructor_DbContext_Injection()
+    {
+        var databaseName = $"db-{Guid.NewGuid():N}";
+
+        await using (var seedContext = new ConstructorInjectionDbContext(
+            new DbContextOptionsBuilder<ConstructorInjectionDbContext>()
+                .UseInMemoryDatabase(databaseName)
+                .Options))
+        {
+            await seedContext.Database.EnsureCreatedAsync();
+
+            var blog1 = new ConstructorInjectionBlog { Name = "Blog1" };
+            var blog2 = new ConstructorInjectionBlog { Name = "Blog2" };
+
+            await seedContext.Blogs.AddRangeAsync(blog1, blog2);
+            await seedContext.SaveChangesAsync();
+
+            await seedContext.Posts.AddRangeAsync(
+                new ConstructorInjectionPost { BlogId = blog1.Id },
+                new ConstructorInjectionPost { BlogId = blog1.Id },
+                new ConstructorInjectionPost { BlogId = blog2.Id });
+            await seedContext.SaveChangesAsync();
+        }
+
+        var executor = await new ServiceCollection()
+            .AddDbContext<ConstructorInjectionDbContext>(
+                b => b.UseInMemoryDatabase(databaseName))
+            .AddGraphQL()
+            .AddProjections()
+            .AddQueryType<ConstructorInjectionQuery>()
+            .BuildRequestExecutorAsync();
+
+        var result = await executor.ExecuteAsync(
+            """
+            {
+                blogsAsSelector {
+                    name
+                    postCount
+                }
+                blogsNoProjection {
+                    name
+                    postCount
+                }
+            }
+            """);
+
+        var operationResult = result.ExpectOperationResult();
+        Assert.True(operationResult.Errors is null || operationResult.Errors.Count == 0);
+        Assert.True(operationResult.Data.HasValue);
+
+        using var document = JsonDocument.Parse(result.ToJson());
+        var data = document.RootElement.GetProperty("data");
+        var projectedCounts = ReadCounts(data.GetProperty("blogsAsSelector"));
+        var unprojectedCounts = ReadCounts(data.GetProperty("blogsNoProjection"));
+
+        Assert.Equal(unprojectedCounts, projectedCounts);
+        Assert.Equal(3, projectedCounts["Blog1"]);
+        Assert.Equal(3, projectedCounts["Blog2"]);
+
+        static Dictionary<string, int> ReadCounts(JsonElement value)
+        {
+            var result = new Dictionary<string, int>();
+
+            foreach (var item in value.EnumerateArray())
+            {
+                result.Add(item.GetProperty("name").GetString()!, item.GetProperty("postCount").GetInt32());
+            }
+
+            return result;
+        }
+    }
+
+    public class ConstructorInjectionQuery
+    {
+        [UseProjection]
+        public IQueryable<ConstructorInjectionBlog> GetBlogs(ConstructorInjectionDbContext context)
+            => context.Blogs;
+
+        public IQueryable<ConstructorInjectionBlog> GetBlogsAsSelector(
+            ConstructorInjectionDbContext context,
+            ISelection selection)
+            => context.Blogs.Select(selection.AsSelector<ConstructorInjectionBlog>());
+
+        public IQueryable<ConstructorInjectionBlog> GetBlogsNoProjection(
+            ConstructorInjectionDbContext context)
+            => context.Blogs;
+    }
+
+    public class ConstructorInjectionDbContext(
+        DbContextOptions<ConstructorInjectionDbContext> options)
+        : DbContext(options)
+    {
+        public DbSet<ConstructorInjectionBlog> Blogs => Set<ConstructorInjectionBlog>();
+
+        public DbSet<ConstructorInjectionPost> Posts => Set<ConstructorInjectionPost>();
+    }
+
+    public class ConstructorInjectionBlog
+    {
+        public ConstructorInjectionBlog()
+        {
+        }
+
+        private ConstructorInjectionBlog(ConstructorInjectionDbContext context)
+        {
+            Context = context;
+        }
+
+        private ConstructorInjectionDbContext? Context { get; }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } = default!;
+
+        public int PostCount => Context?.Posts.Count() ?? 0;
+    }
+
+    public class ConstructorInjectionPost
+    {
+        public int Id { get; set; }
+
+        public int BlogId { get; set; }
     }
 }
