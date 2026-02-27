49bda118c8facf6b7a6d63511e14d2ebd6bf7110
Michael Staib <michael@chillicream.com>
2026-02-27 22:59:37 +0000

    Fusion: reuse path segment buffer for start result lookup

--- src/HotChocolate/Fusion-vnext/src/Fusion.Execution/Execution/Results/FetchResultStore.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Execution/Execution/Results/FetchResultStore.cs
@@ -33,6 +33,7 @@ internal sealed class FetchResultStore : IDisposable
     private readonly List<CompositeResultElement> _collectTargetCurrent = [];
     private readonly List<CompositeResultElement> _collectTargetNext = [];
     private readonly List<CompositeResultElement> _collectTargetCombined = [];
+    private Path[] _startPathSegments = [];
     private CompositeResultDocument _result;
     private ValueCompletion _valueCompletion;
     private List<IError>? _errors;
@@ -1321,59 +1322,56 @@ AddErrors_Next:
         }
 
         var segmentCount = path.Length;
-        var rented = ArrayPool<Path>.Shared.Rent(segmentCount);
-
-        try
+        var segments = _startPathSegments;
+        if (segments.Length < segmentCount)
         {
-            var segments = rented.AsSpan(0, segmentCount);
-            var currentPath = path;
+            segments = new Path[segmentCount];
+            _startPathSegments = segments;
+        }
 
-            for (var i = segmentCount - 1; i >= 0; i--)
+        var currentPath = path;
+
+        for (var i = segmentCount - 1; i >= 0; i--)
+        {
+            segments[i] = currentPath;
+            currentPath = currentPath.Parent;
+        }
+
+        var element = _result.Data;
+
+        for (var i = 0; i < segmentCount; i++)
+        {
+            var segment = segments[i];
+            var elementKind = element.ValueKind;
+
+            if (elementKind is JsonValueKind.Null)
             {
-                segments[i] = currentPath;
-                currentPath = currentPath.Parent;
+                return element;
             }
 
-            var element = _result.Data;
-
-            for (var i = 0; i < segments.Length; i++)
+            if (elementKind is JsonValueKind.Object && segment is NamePathSegment nameSegment)
             {
-                var segment = segments[i];
-                var elementKind = element.ValueKind;
-
-                if (elementKind is JsonValueKind.Null)
-                {
-                    return element;
-                }
-
-                if (elementKind is JsonValueKind.Object && segment is NamePathSegment nameSegment)
-                {
-                    element = element.TryGetProperty(nameSegment.Name, out var field) ? field : default;
-                    continue;
-                }
-
-                if (elementKind is JsonValueKind.Array && segment is IndexerPathSegment indexSegment)
-                {
-                    if (element.GetArrayLength() <= indexSegment.Index)
-                    {
-                        throw new InvalidOperationException(
-                            $"The path segment '{indexSegment}' does not exist in the data.");
-                    }
-
-                    element = element[indexSegment.Index];
-                    continue;
-                }
-
-                throw new InvalidOperationException(
-                    $"The path segment '{segment.Parent}' does not exist in the data.");
+                element = element.TryGetProperty(nameSegment.Name, out var field) ? field : default;
+                continue;
             }
 
-            return element;
+            if (elementKind is JsonValueKind.Array && segment is IndexerPathSegment indexSegment)
+            {
+                if (element.GetArrayLength() <= indexSegment.Index)
+                {
+                    throw new InvalidOperationException(
+                        $"The path segment '{indexSegment}' does not exist in the data.");
+                }
+
+                element = element[indexSegment.Index];
+                continue;
+            }
+
+            throw new InvalidOperationException(
+                $"The path segment '{segment.Parent}' does not exist in the data.");
         }
-        finally
-        {
-            ArrayPool<Path>.Shared.Return(rented);
-        }
+
+        return element;
     }
 
     public void Dispose()
