30b75e22545c3540e825374609094962fb5712ac
Michael Staib <michael@chillicream.com>
2026-02-27 14:52:02 +0000

    Fusion: fast-path small utf8 selection lookups

--- src/HotChocolate/Fusion-vnext/src/Fusion.Execution/Execution/Nodes/SelectionSet.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Execution/Execution/Nodes/SelectionSet.cs
@@ -1,5 +1,6 @@
 using System.Collections.Frozen;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 using HotChocolate.Execution;
 using HotChocolate.Types;
 
@@ -92,8 +93,79 @@ public sealed class SelectionSet : ISelectionSet
     /// <returns>
     /// Returns true if the selection was successfully resolved.
     /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public bool TryGetSelection(ReadOnlySpan<byte> utf8ResponseName, [NotNullWhen(true)] out Selection? selection)
-        => _utf8ResponseNameLookup.TryGetSelection(utf8ResponseName, out selection);
+    {
+        // Most execution selection sets are tiny (1-3 fields), so direct
+        // span comparisons avoid hash computation/probing in the hot path.
+        var selections = _selections;
+
+        switch (selections.Length)
+        {
+            case 1:
+                var candidate = selections[0];
+
+                if (utf8ResponseName.SequenceEqual(candidate.Utf8ResponseName))
+                {
+                    selection = candidate;
+                    return true;
+                }
+
+                selection = default;
+                return false;
+
+            case 2:
+                var candidate0 = selections[0];
+
+                if (utf8ResponseName.SequenceEqual(candidate0.Utf8ResponseName))
+                {
+                    selection = candidate0;
+                    return true;
+                }
+
+                var candidate1 = selections[1];
+
+                if (utf8ResponseName.SequenceEqual(candidate1.Utf8ResponseName))
+                {
+                    selection = candidate1;
+                    return true;
+                }
+
+                selection = default;
+                return false;
+
+            case 3:
+                var firstCandidate = selections[0];
+
+                if (utf8ResponseName.SequenceEqual(firstCandidate.Utf8ResponseName))
+                {
+                    selection = firstCandidate;
+                    return true;
+                }
+
+                var secondCandidate = selections[1];
+
+                if (utf8ResponseName.SequenceEqual(secondCandidate.Utf8ResponseName))
+                {
+                    selection = secondCandidate;
+                    return true;
+                }
+
+                var thirdCandidate = selections[2];
+
+                if (utf8ResponseName.SequenceEqual(thirdCandidate.Utf8ResponseName))
+                {
+                    selection = thirdCandidate;
+                    return true;
+                }
+
+                selection = default;
+                return false;
+
+            default:
+                return _utf8ResponseNameLookup.TryGetSelection(utf8ResponseName, out selection);
+        }
+    }
 
     internal void Seal(Operation operation)
     {
