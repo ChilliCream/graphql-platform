1d3119c20eab5b3d35454e895e8bb0742628d859
Michael Staib <michael@chillicream.com>
2026-02-27 11:11:17 +0000

    [Fusion][Perf] Defer seen dictionary allocation in single requirement fast path

--- src/HotChocolate/Fusion-vnext/src/Fusion.Execution/Execution/Results/FetchResultStore.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Execution/Execution/Results/FetchResultStore.cs
@@ -697,33 +697,50 @@ AddErrors_Next:
             if (value.ValueKind is JsonValueKind.String)
             {
                 var stringValue = value.AssertString();
-
-                if (seenStrings is not null
-                    && seenStrings.TryGetValue(stringValue, out var existingIndex))
-                {
-                    additionalPaths ??= new List<Path>?[elements.Count];
-                    (additionalPaths[existingIndex] ??= []).Add(result.Path);
-                    continue;
-                }
-
                 mappedValue = ResultDataMapper.GetStringValueNode(stringValue);
-                seenStrings ??= new Dictionary<string, int>(StringComparer.Ordinal);
-                seenStrings[stringValue] = nextIndex;
+
+                if (nextIndex > 0)
+                {
+                    if (seenStrings is null)
+                    {
+                        seenStrings = new Dictionary<string, int>(StringComparer.Ordinal);
+
+                        if (variableValueSets[0].Values.Fields[0].Value is StringValueNode firstStringValue)
+                        {
+                            seenStrings[firstStringValue.Value] = 0;
+                        }
+                    }
+
+                    if (seenStrings.TryGetValue(stringValue, out var existingIndex))
+                    {
+                        additionalPaths ??= new List<Path>?[elements.Count];
+                        (additionalPaths[existingIndex] ??= []).Add(result.Path);
+                        continue;
+                    }
+
+                    seenStrings[stringValue] = nextIndex;
+                }
             }
             else
             {
                 mappedValue = ResultDataMapper.MapLeafValue(value, ref buffer);
 
-                if (seen is not null
-                    && seen.TryGetValue(mappedValue, out var existingIndex))
+                if (nextIndex > 0)
                 {
-                    additionalPaths ??= new List<Path>?[elements.Count];
-                    (additionalPaths[existingIndex] ??= []).Add(result.Path);
-                    continue;
-                }
+                    seen ??= new Dictionary<IValueNode, int>(SingleValueNodeComparer.Instance)
+                    {
+                        [variableValueSets[0].Values.Fields[0].Value] = 0
+                    };
 
-                seen ??= new Dictionary<IValueNode, int>(SingleValueNodeComparer.Instance);
-                seen[mappedValue] = nextIndex;
+                    if (seen.TryGetValue(mappedValue, out var existingIndex))
+                    {
+                        additionalPaths ??= new List<Path>?[elements.Count];
+                        (additionalPaths[existingIndex] ??= []).Add(result.Path);
+                        continue;
+                    }
+
+                    seen[mappedValue] = nextIndex;
+                }
             }
 
             variableValueSets[nextIndex++] = new VariableValues(
