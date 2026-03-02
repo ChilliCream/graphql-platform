0467419cbeeda7120b4b32e500e419bc17ad5c85
Michael Staib <michael@chillicream.com>
2026-03-02 13:19:12 +0000

    Revert "Fusion: fast-path single dependent completion"
    This reverts commit 345d2303eaf713785361147e53e1d4c26cfeb47c.

--- src/HotChocolate/Fusion-vnext/src/Fusion.Execution/Execution/ExecutionState.cs
+++ src/HotChocolate/Fusion-vnext/src/Fusion.Execution/Execution/ExecutionState.cs
@@ -162,25 +162,11 @@ internal sealed class ExecutionState(bool collectTelemetry, CancellationTokenSou
 
         if (result.Status is ExecutionStatus.Success or ExecutionStatus.PartialSuccess)
         {
-            var dependentsToExecute = result.DependentsToExecute;
-
-            if (dependentsToExecute.Length == 1)
-            {
-                var dependentToExecute = dependentsToExecute[0];
-
-                foreach (var dependent in node.Dependents)
-                {
-                    if (!ReferenceEquals(dependent, dependentToExecute))
-                    {
-                        SkipNode(context, dependent);
-                    }
-                }
-            }
-            else if (dependentsToExecute.Length > 1)
+            if (result.DependentsToExecute.Length > 0)
             {
                 foreach (var dependent in node.Dependents)
                 {
-                    if (!dependentsToExecute.Contains(dependent))
+                    if (!result.DependentsToExecute.Contains(dependent))
                     {
                         SkipNode(context, dependent);
                     }
