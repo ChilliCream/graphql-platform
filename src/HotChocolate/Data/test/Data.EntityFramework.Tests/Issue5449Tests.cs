927d61e2a5752633a226ae2ea1a51c6724cdca63
Glen <glen.84@gmail.com>
2026-03-03 10:39:36 +0200

    Restore private constructor

--- src/HotChocolate/Data/test/Data.EntityFramework.Tests/Issue5449Tests.cs
+++ src/HotChocolate/Data/test/Data.EntityFramework.Tests/Issue5449Tests.cs
@@ -91,6 +91,14 @@ public class Issue5449Tests
             Name = name;
         }
 
+#pragma warning disable IDE0051 // Remove unused private members
+        private Issue5449Blog(Issue5449Context context)
+#pragma warning restore IDE0051 // Remove unused private members
+        {
+            _context = context;
+            Name = string.Empty;
+        }
+
         public int Id { get; set; }
 
         public string Name { get; set; }
