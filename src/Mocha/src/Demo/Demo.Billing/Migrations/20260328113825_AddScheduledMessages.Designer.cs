7d4a46a76af4963119ad11c505b009d5a5e1918b
Pascal Senn <pascal@chillicream.com>
2026-03-28 12:52:11 +0100

    cleanup

--- src/Mocha/src/Demo/Demo.Billing/Migrations/20260328113825_AddScheduledMessages.Designer.cs
+++ src/Mocha/src/Demo/Demo.Billing/Migrations/20260328113825_AddScheduledMessages.Designer.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using Demo.Billing.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HotChocolate.Demo.Billing.Migrations
 {
     [DbContext(typeof(BillingDbContext))]
-    partial class BillingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260328113825_AddScheduledMessages")]
+    partial class AddScheduledMessages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
