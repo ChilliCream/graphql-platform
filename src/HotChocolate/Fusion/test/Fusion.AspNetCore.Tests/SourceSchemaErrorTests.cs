8d0997546e5af860ff58f5bb6fe0c6b67aed033c
Glen <glen.84@gmail.com>
2026-04-13 15:10:34 +0200

    Remove test theory

--- src/HotChocolate/Fusion/test/Fusion.AspNetCore.Tests/SourceSchemaErrorTests.cs
+++ src/HotChocolate/Fusion/test/Fusion.AspNetCore.Tests/SourceSchemaErrorTests.cs
@@ -590,48 +590,6 @@ public class SourceSchemaErrorTests : FusionTestBase
     [Theory]
     [InlineData(ErrorHandlingMode.Propagate)]
     [InlineData(ErrorHandlingMode.Null)]
-    public async Task No_Data_And_Error_With_Path_For_Lookup_Field_NonNull(ErrorHandlingMode onError)
-    {
-        // arrange
-        using var server1 = CreateSourceSchema(
-            "A",
-            b => b.AddQueryType<SourceSchema1.Query>());
-
-        using var server2 = CreateSourceSchema(
-            "B",
-            b => b.AddQueryType<SourceSchema4.Query>());
-
-        using var gateway = await CreateCompositeSchemaAsync(
-        [
-            ("A", server1),
-            ("B", server2)
-        ]);
-
-        // act
-        using var client = GraphQLHttpClient.Create(gateway.CreateClient());
-
-        var request = new OperationRequest(
-            """
-            {
-              topProduct {
-                price
-                name
-              }
-            }
-            """,
-            onError: onError);
-
-        using var result = await client.PostAsync(
-            request,
-            new Uri("http://localhost:5000/graphql"));
-
-        // assert
-        await MatchSnapshotAsync(gateway, request, result, postFix: "OnError_" + onError);
-    }
-
-    [Theory]
-    [InlineData(ErrorHandlingMode.Propagate)]
-    [InlineData(ErrorHandlingMode.Null)]
     public async Task No_Data_And_Error_With_Path_For_Lookup_Leaf_NonNull(ErrorHandlingMode onError)
     {
         // arrange
