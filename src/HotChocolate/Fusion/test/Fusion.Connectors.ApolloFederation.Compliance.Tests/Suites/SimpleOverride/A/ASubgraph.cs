20b52a394d4a7ebe7279c7f6626a149977dd6c50
Michael Staib <michael@chillicream.com>
2026-05-01 17:26:31 +0000

    Wire simple-override Apollo Federation compliance test suite

---
+++ src/HotChocolate/Fusion/test/Fusion.Connectors.ApolloFederation.Compliance.Tests/Suites/SimpleOverride/A/ASubgraph.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotChocolate.Fusion.Suites.SimpleOverride.A;
+
+/// <summary>
+/// Builds the <c>a</c> Apollo Federation subgraph for the
+/// <c>simple-override</c> suite.
+/// </summary>
+public static class ASubgraph
+{
+    public const string Name = "a";
+
+    public static async Task<SubgraphHost> BuildAsync()
+    {
+        var builder = WebApplication.CreateBuilder();
+        builder.WebHost.UseTestServer();
+
+        builder.Services
+            .AddRouting()
+            .AddGraphQLServer()
+            .AddApolloFederation()
+            .AddQueryType<QueryType>()
+            .AddType<PostType>();
+
+        var app = builder.Build();
+        app.MapGraphQL();
+
+        await app.StartAsync();
+
+        return new SubgraphHost(Name, app);
+    }
+}
