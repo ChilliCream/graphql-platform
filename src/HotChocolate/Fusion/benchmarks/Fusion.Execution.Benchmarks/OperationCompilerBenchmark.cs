50733bdc47962d7e694f661ddc888b5173209871
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2026-05-13 15:01:06 +0200

    Add DocumentRewriter benchmarks

---
+++ src/HotChocolate/Fusion/benchmarks/Fusion.Execution.Benchmarks/OperationCompilerBenchmark.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.dotMemory;
+using BenchmarkDotNet.Jobs;
+using HotChocolate.Fusion.Execution.Nodes;
+using HotChocolate.Fusion.Planning;
+using HotChocolate.Fusion.Rewriters;
+using HotChocolate.Language;
+using Microsoft.Extensions.ObjectPool;
+
+namespace Fusion.Execution.Benchmarks;
+
+[DotMemoryDiagnoser]
+[MemoryDiagnoser]
+[ShortRunJob(RuntimeMoniker.Net10_0)]
+[MarkdownExporter]
+public class OperationCompilerBenchmark : FusionBenchmarkBase
+{
+    private const string Id = "123456789101112";
+
+    private OperationCompiler _compiler = null!;
+
+    private OperationDefinitionNode _simpleQueryWithRequirements = null!;
+    private OperationDefinitionNode _complexQuery = null!;
+    private OperationDefinitionNode _conditionalRedundancyQuery = null!;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var schema = CreateFusionSchema();
+
+        var documentRewriter = new DocumentRewriter(schema);
+
+        _simpleQueryWithRequirements = documentRewriter.RewriteDocument(CreateSimpleQueryWithRequirementsDocument()).GetOperation(operationName: null);
+        _complexQuery = documentRewriter.RewriteDocument(CreateComplexDocument()).GetOperation(operationName: null);
+        _conditionalRedundancyQuery = documentRewriter.RewriteDocument(CreateConditionalRedundancyDocument()).GetOperation(operationName: null);
+
+        var pool = new NoOpObjectPool<OrderedDictionary<string, List<FieldSelectionNode>>>();
+        _compiler = new OperationCompiler(schema, pool);
+    }
+
+    [Benchmark]
+    public Operation Compile_Simple_Query_With_Requirements()
+    {
+        return _compiler.Compile(Id, Id, _simpleQueryWithRequirements);
+    }
+
+    [Benchmark]
+    public Operation Compile_Complex_Query()
+    {
+        return _compiler.Compile(Id, Id, _complexQuery);
+    }
+
+    [Benchmark]
+    public Operation Compile_ConditionalRedundancy_Query()
+    {
+        return _compiler.Compile(Id, Id, _conditionalRedundancyQuery);
+    }
+
+    private sealed class NoOpObjectPool<T> : ObjectPool<T> where T : class, new()
+    {
+        public override T Get()
+        {
+            return new T();
+        }
+
+        public override void Return(T obj)
+        {
+        }
+    }
+}
