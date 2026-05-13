1485dc4577ea8961b57e9769d6f74e350447e508
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2026-05-13 16:28:07 +0200

    Properly handle `@defer` in DocumentRewriter (#9699)

---
+++ src/HotChocolate/Fusion/benchmarks/Fusion.Execution.Benchmarks/OperationPlannerBenchmark.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using HotChocolate.Fusion.Execution.Nodes;
+using HotChocolate.Fusion.Planning;
+using HotChocolate.Fusion.Rewriters;
+using HotChocolate.Language;
+using Microsoft.Extensions.ObjectPool;
+
+namespace Fusion.Execution.Benchmarks;
+
+[MemoryDiagnoser]
+[ShortRunJob(RuntimeMoniker.Net10_0)]
+[MarkdownExporter]
+public class OperationPlannerBenchmark : FusionBenchmarkBase
+{
+    private const string Id = "123456789101112";
+
+    private OperationPlanner _planner = null!;
+
+    private OperationDefinitionNode _simpleQueryWithRequirements = null!;
+    private OperationDefinitionNode _complexQuery = null!;
+    private OperationDefinitionNode _conditionalRedundancyQuery = null!;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var schema = CreateFusionSchema();
+
+        var documentRewriter = new DocumentRewriter(schema);
+
+        _simpleQueryWithRequirements = documentRewriter.RewriteDocument(CreateSimpleQueryWithRequirementsDocument()).GetOperation(operationName: null);
+        _complexQuery = documentRewriter.RewriteDocument(CreateComplexDocument()).GetOperation(operationName: null);
+        _conditionalRedundancyQuery = documentRewriter.RewriteDocument(CreateConditionalRedundancyDocument()).GetOperation(operationName: null);
+
+        var pool = new DefaultObjectPool<OrderedDictionary<string, List<FieldSelectionNode>>>(
+            new DefaultPooledObjectPolicy<OrderedDictionary<string, List<FieldSelectionNode>>>());
+        var operationCompiler = new OperationCompiler(schema, pool);
+
+        _planner = new OperationPlanner(schema, operationCompiler);
+    }
+
+    [Benchmark]
+    public int Plan_Simple_Query_With_Requirements()
+    {
+        return _planner.CreatePlan(Id, Id, Id, _simpleQueryWithRequirements).SearchSpace;
+    }
+
+    [Benchmark]
+    public int Plan_Complex_Query()
+    {
+        return _planner.CreatePlan(Id, Id, Id, _complexQuery).SearchSpace;
+    }
+
+    [Benchmark]
+    public int Plan_ConditionalRedundancy_Query()
+    {
+        return _planner.CreatePlan(Id, Id, Id, _conditionalRedundancyQuery).SearchSpace;
+    }
+}
