19a110b948bd597876d13059c6ad2386ac57c7a1
Michael Staib <michael@chillicream.com>
2026-05-16 02:52:49 +0000

    Update test snapshots for enum value declaration order

--- src/Nitro/CommandLine/test/CommandLine.Tests/Commands/Fusion/FusionPublishCommandTests.cs
+++ src/Nitro/CommandLine/test/CommandLine.Tests/Commands/Fusion/FusionPublishCommandTests.cs
@@ -4780,10 +4780,10 @@ public sealed class FusionPublishCommandTests(NitroCommandFixture fixture) : Fus
             }
 
             enum CacheControlScope @fusion__type(schema: REVIEWS) {
-              "The value to cache is specific to a single user."
-              PRIVATE @fusion__enumValue(schema: REVIEWS)
               "The value to cache is not tied to a single user."
               PUBLIC @fusion__enumValue(schema: REVIEWS)
+              "The value to cache is specific to a single user."
+              PRIVATE @fusion__enumValue(schema: REVIEWS)
             }
 
             "The fusion__Schema enum is a generated type used within an execution schema document to refer to a source schema in a type-safe manner."
