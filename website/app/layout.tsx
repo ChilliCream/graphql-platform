6f93e67fbf00c15248bb5f2630df7ac3424f136c
Michael Staib <michael@chillicream.com>
2026-05-18 01:15:47 +0000

    edits

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -64,12 +64,13 @@ export default function RootLayout({
             />
           </>
         )}
-        {process.env.NEXT_PUBLIC_GTM_ID && (
-          <Script
-            id="gtm-script"
-            strategy="beforeInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
+        {process.env.NEXT_PUBLIC_COOKIEBOT_CBID &&
+          process.env.NEXT_PUBLIC_GTM_ID && (
+            <Script
+              id="gtm-script"
+              strategy="beforeInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
                 window.dataLayer = window.dataLayer || [];
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -77,21 +78,22 @@ export default function RootLayout({
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
               `,
-            }}
-          />
-        )}
+              }}
+            />
+          )}
       </head>
       <body>
-        {process.env.NEXT_PUBLIC_GTM_ID && (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
-        )}
+        {process.env.NEXT_PUBLIC_COOKIEBOT_CBID &&
+          process.env.NEXT_PUBLIC_GTM_ID && (
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              />
+            </noscript>
+          )}
         <Providers latestBlogPost={latestBlogPost}>{children}</Providers>
       </body>
     </html>
