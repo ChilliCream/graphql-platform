bbe4c5453130145fae90b89829bc6a439bc319ee
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2026-06-03 10:10:27 +0200

    Surface clearer errors for invalid node IDs (#9824)

---
+++ src/HotChocolate/Core/src/Execution.Abstractions/Execution/Relay/NodeIdInvalidValueException.cs
@@ -0,0 +1,8 @@
+namespace HotChocolate.Execution.Relay;
+
+public sealed class NodeIdInvalidValueException(string typeName, object value)
+    : GraphQLException(ErrorBuilder.New()
+        .SetMessage(
+            $"The value could not be formatted into an ID for the type `{typeName}`.")
+        .SetExtension("originalValue", value.ToString())
+        .Build());
