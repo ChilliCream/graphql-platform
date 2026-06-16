8b101d54406a6a50eb07008c12ca7246bd4f9782
Glen <glen.84@gmail.com>
2026-06-16 14:37:53 +0200

    Add directive and deprecation state to MutableDirectiveDefinition

---
+++ src/HotChocolate/Mutable/test/Types.Mutable.Tests/MutableDirectiveDefinitionTests.cs
@@ -0,0 +1,49 @@
+namespace HotChocolate.Types.Mutable;
+
+public class MutableDirectiveDefinitionTests
+{
+    [Fact]
+    public void SetDeprecationReason_Should_MarkAsDeprecated()
+    {
+        // arrange
+        var directiveDefinition = new MutableDirectiveDefinition("foo");
+
+        // act
+        directiveDefinition.DeprecationReason = "Use bar.";
+
+        // assert
+        Assert.True(directiveDefinition.IsDeprecated);
+        Assert.Equal("Use bar.", directiveDefinition.DeprecationReason);
+    }
+
+    [Fact]
+    public void IsDeprecated_Should_ClearDeprecationReason_When_SetToFalse()
+    {
+        // arrange
+        var directiveDefinition = new MutableDirectiveDefinition("foo")
+        {
+            DeprecationReason = "Use bar."
+        };
+
+        // act
+        directiveDefinition.IsDeprecated = false;
+
+        // assert
+        Assert.False(directiveDefinition.IsDeprecated);
+        Assert.Null(directiveDefinition.DeprecationReason);
+    }
+
+    [Fact]
+    public void Directives_Should_ContainAddedDirective()
+    {
+        // arrange
+        var metaDefinition = new MutableDirectiveDefinition("meta");
+        var directiveDefinition = new MutableDirectiveDefinition("foo");
+
+        // act
+        directiveDefinition.Directives.Add(new Directive(metaDefinition));
+
+        // assert
+        Assert.True(directiveDefinition.Directives.ContainsName("meta"));
+    }
+}
