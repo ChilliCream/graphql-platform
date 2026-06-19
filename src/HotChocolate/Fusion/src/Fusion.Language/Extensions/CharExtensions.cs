10e1bbf48feceb13a28809d2959f8b309e78243d
Michael Staib <michael@chillicream.com>
2026-06-19 23:01:02 +0200

    edits

--- src/HotChocolate/Fusion/src/Fusion.Language/Extensions/CharExtensions.cs
+++ src/HotChocolate/Fusion/src/Fusion.Language/Extensions/CharExtensions.cs
@@ -7,81 +7,33 @@ internal static class CharExtensions
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsLetterOrDigitOrUnderscore(this char c)
-    {
-        return char.IsAsciiLetter(c) || char.IsAsciiDigit(c) || c == Underscore;
-    }
+        => char.IsAsciiLetter(c) || char.IsAsciiDigit(c) || c == Underscore;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsLetterOrUnderscore(this char c)
-    {
-        return char.IsAsciiLetter(c) || c == Underscore;
-    }
+        => char.IsAsciiLetter(c) || c == Underscore;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsPunctuator(this char c)
-    {
-        switch (c)
-        {
-            case Colon:
-            case LeftAngleBracket:
-            case LeftBrace:
-            case LeftParenthesis:
-            case LeftSquareBracket:
-            case Period:
-            case Pipe:
-            case RightAngleBracket:
-            case RightBrace:
-            case RightParenthesis:
-            case RightSquareBracket:
-                return true;
-
-            default:
-                return false;
-        }
-    }
+        => c is Colon or LeftAngleBracket or LeftBrace or LeftParenthesis or LeftSquareBracket or Period or Pipe or RightAngleBracket or RightBrace or RightParenthesis or RightSquareBracket;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsDigit(this char c)
-    {
-        return char.IsAsciiDigit(c);
-    }
+        => char.IsAsciiDigit(c);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsDigitOrMinus(this char c)
-    {
-        return char.IsAsciiDigit(c) || c == Minus;
-    }
+        => char.IsAsciiDigit(c) || c == Minus;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsQuote(this char c)
-    {
-        return c == Quote;
-    }
+        => c == Quote;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsControlCharacter(this char c)
-    {
-        return (c < Space && c != HorizontalTab) || c == Delete;
-    }
+        => (c < Space && c != HorizontalTab) || c == Delete;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsValidEscapeCharacter(this char c)
-    {
-        switch (c)
-        {
-            case Quote:
-            case '/':
-            case Backslash:
-            case 'b':
-            case 'f':
-            case 'n':
-            case 'r':
-            case 't':
-            case 'u':
-                return true;
-
-            default:
-                return false;
-        }
-    }
+        => c is Quote or '/' or Backslash or 'b' or 'f' or 'n' or 'r' or 't' or 'u';
 }
