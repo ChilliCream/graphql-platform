81fff5d51bb807bb27647d7d9568c7194d75b166
Pascal Senn <pascal@chillicream.com>
2026-06-22 01:44:40 +0200

    Add generic IMessageBatch interface for typed message handling

--- src/Mocha/src/Mocha/Abstractions/IMessageBatch.cs
+++ src/Mocha/src/Mocha/Abstractions/IMessageBatch.cs
@@ -22,26 +22,3 @@ public interface IMessageBatch
     /// <returns>The consume context for the message.</returns>
     IConsumeContext GetContext(int index);
 }
-
-/// <summary>
-/// An immutable batch of messages delivered to an <see cref="IBatchEventHandler{TEvent}"/>.
-/// Provides indexed access, count, enumeration, and batch metadata.
-/// </summary>
-/// <typeparam name="TEvent">The type of event in the batch.</typeparam>
-public interface IMessageBatch<out TEvent> : IMessageBatch, IReadOnlyList<TEvent>
-{
-    /// <summary>
-    /// Gets the number of messages in the batch.
-    /// </summary>
-    new int Count { get; }
-
-    /// <summary>
-    /// Gets the typed consume context for a specific message in the batch.
-    /// </summary>
-    /// <param name="index">The zero-based index of the message.</param>
-    /// <returns>The consume context for the message.</returns>
-    new IConsumeContext<TEvent> GetContext(int index);
-
-    /// <inheritdoc />
-    IConsumeContext IMessageBatch.GetContext(int index) => GetContext(index);
-}
