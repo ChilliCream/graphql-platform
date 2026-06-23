7e5690f9958525a90bb61266722304745d6973ea
Glen <glen.84@gmail.com>
2026-06-23 11:57:13 +0200

    Simplify field selection map value-node converter

--- src/HotChocolate/Fusion/src/Fusion.Utilities/Rewriters/FieldSelectionMapValueNodeConverter.cs
+++ src/HotChocolate/Fusion/src/Fusion.Utilities/Rewriters/FieldSelectionMapValueNodeConverter.cs
@@ -5,43 +5,26 @@ using HC = HotChocolate.Language;
 namespace HotChocolate.Fusion.Rewriters;
 
 /// <summary>
-/// Converts <see cref="HotChocolate.Fusion.Language"/> constant value nodes (as used in a field
+/// Converts <see cref="Language"/> constant value nodes (as used in a field
 /// selection map) into the equivalent <see cref="HotChocolate.Language"/> value nodes.
 /// </summary>
 public static class FieldSelectionMapValueNodeConverter
 {
     public static HC.IValueNode Convert(IValueNode node)
     {
-        switch (node)
+        return node switch
         {
-            case StringValueNode value:
-                return new HC.StringValueNode(null, value.Value, value.Block);
-
-            case IntValueNode value:
-                // HotChocolate.Language.IntValueNode has no string constructor; parse the literal.
-                return HC.Utf8GraphQLParser.Syntax.ParseValueLiteral(value.Value, /* constant: */ true);
-
-            case FloatValueNode value:
-                return HC.Utf8GraphQLParser.Syntax.ParseValueLiteral(value.Value, /* constant: */ true);
-
-            case BooleanValueNode value:
-                return new HC.BooleanValueNode(value.Value);
-
-            case EnumValueNode value:
-                return new HC.EnumValueNode(value.Value);
-
-            case NullValueNode:
-                return HC.NullValueNode.Default;
-
-            case ListValueNode value:
-                return new HC.ListValueNode(value.Items.Select(Convert).ToArray());
-
-            case ObjectValueNode value:
-                return new HC.ObjectValueNode(value.Fields.Select(Convert).ToArray());
-
-            default:
-                throw new ArgumentOutOfRangeException(nameof(node));
-        }
+            StringValueNode value => new HC.StringValueNode(null, value.Value, value.Block),
+            // HotChocolate.Language.IntValueNode has no string constructor; parse the literal.
+            IntValueNode value => HC.Utf8GraphQLParser.Syntax.ParseValueLiteral(value.Value, constant: true),
+            FloatValueNode value => HC.Utf8GraphQLParser.Syntax.ParseValueLiteral(value.Value, constant: true),
+            BooleanValueNode value => new HC.BooleanValueNode(value.Value),
+            EnumValueNode value => new HC.EnumValueNode(value.Value),
+            NullValueNode => HC.NullValueNode.Default,
+            ListValueNode value => new HC.ListValueNode(value.Items.Select(Convert).ToArray()),
+            ObjectValueNode value => new HC.ObjectValueNode(value.Fields.Select(Convert).ToArray()),
+            _ => throw new ArgumentOutOfRangeException(nameof(node))
+        };
     }
 
     public static HC.ArgumentNode Convert(ArgumentNode argument)
