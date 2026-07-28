1536c017a33ed6a15231bc5c35932959dd4ab30e
Glen <glen.84@gmail.com>
2026-07-28 17:51:49 +0200

    Download deprecated directive definitions and arguments (#10160)

--- src/HotChocolate/Utilities/test/Utilities.Introspection.Tests/IntrospectionQueryBuilderTests.cs
+++ src/HotChocolate/Utilities/test/Utilities.Introspection.Tests/IntrospectionQueryBuilderTests.cs
@@ -36,6 +36,23 @@ public class IntrospectionQueryBuilderTests
     }
 
     [Fact]
+    public void Create_Query_With_DirectiveDeprecation()
+    {
+        // arrange
+        var options = new IntrospectionOptions();
+        var features = new ServerCapabilities
+        {
+            HasDirectiveDeprecation = true
+        };
+
+        // act
+        var document = IntrospectionQueryBuilder.Build(features, options);
+
+        //assert
+        document.Print().MatchSnapshot();
+    }
+
+    [Fact]
     public void Create_Query_With_DirectiveLocations()
     {
         // arrange
