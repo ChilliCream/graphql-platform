3658a28eaa2888715f29d39acd696bf24ad3b7ea
Pascal Senn <pascal@chillicream.com>
2026-08-18 09:29:14 +0200

    refactor: share company social links

---
+++ website/src/helpers/companyLinks.ts
@@ -0,0 +1,6 @@
+export const COMPANY_SOCIAL_LINKS = {
+  github: "https://github.com/ChilliCream",
+  linkedIn: "https://www.linkedin.com/company/chillicream",
+  youtube: "https://www.youtube.com/c/ChilliCream",
+  x: "https://x.com/Chilli_Cream",
+} as const;
