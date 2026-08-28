14c4328366f63087f3e32e4810b114eded5ce31d
Michael Staib <michael@chillicream.com>
2026-08-28 18:09:41 +0000

    Remove unused DataLoader analyzer import

--- src/HotChocolate/Core/src/Types.Analyzers/DataLoaderMultipleAttributesCodeFixProvider.cs
+++ src/HotChocolate/Core/src/Types.Analyzers/DataLoaderMultipleAttributesCodeFixProvider.cs
@@ -3,7 +3,6 @@ using System.Composition;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace HotChocolate.Types.Analyzers;
