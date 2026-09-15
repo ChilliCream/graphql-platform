fd313eb8efcede96fc82700a885cf6bbbffb3a61
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2026-09-15 08:32:11 +0000

    Fix batch request query id and hash tags serializing as objects

--- src/HotChocolate/Diagnostics/src/Diagnostics.Core/Spans/Http/ExecuteHttpRequestSpan.cs
+++ src/HotChocolate/Diagnostics/src/Diagnostics.Core/Spans/Http/ExecuteHttpRequestSpan.cs
@@ -131,13 +131,13 @@ internal sealed class ExecuteHttpRequestSpan(
             if (request.DocumentId is not null
                 && (options.RequestDetails & RequestDetails.Id) == RequestDetails.Id)
             {
-                Activity.SetTag(GraphQL.Http.Request.BatchRequest.QueryId(i), request.DocumentId.Value);
+                Activity.SetTag(GraphQL.Http.Request.BatchRequest.QueryId(i), request.DocumentId.Value.Value);
             }
 
             if (request.DocumentHash is not null
                 && (options.RequestDetails & RequestDetails.Hash) == RequestDetails.Hash)
             {
-                Activity.SetTag(GraphQL.Http.Request.BatchRequest.QueryHash(i), request.DocumentHash.Value);
+                Activity.SetTag(GraphQL.Http.Request.BatchRequest.QueryHash(i), request.DocumentHash.Value.Value);
             }
 
             if (request.Document is not null
