98c8d5c62dd646c934ecc86dac8ddf801a7bd47a
Michael Staib <michael@chillicream.com>
2026-09-18 14:23:58 +0000

    test(cost-analysis): pass ignoreAdditionalInputFields by name at the two Core test call sites

--- src/HotChocolate/CostAnalysis/test/CostAnalysis.Core.Conformance.Tests/ConformanceTests.cs
+++ src/HotChocolate/CostAnalysis/test/CostAnalysis.Core.Conformance.Tests/ConformanceTests.cs
@@ -73,6 +73,7 @@ public sealed class ConformanceTests
                 coercionSchema,
                 operation.VariableDefinitions,
                 variables,
+                ignoreAdditionalInputFields: false,
                 out var coerced,
                 out var error))
         {
